665537c68b01818768d9414e036a69de95bcacf1
Konrad Bonicki <konradbonicki29@gmail.com>
2023-04-26 20:43:02 +0200

    Add icons sidebar

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,6 +1,12 @@
 import { NavLink } from 'react-router-dom';
 
 import './styles/aside.css';
+import { HouseIcon } from '../Components/Icons/HouseIcon';
+import { ElementIcon } from '../Components/Icons/ElementIcon';
+import { EditIcon } from '../Components/Icons/EditIcon';
+import { CalendarIcon } from '../Components/Icons/CalendarIcon';
+import { SettingIcon } from '../Components/Icons/SettingIcon';
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 
 export function AppAside() {
   return (
@@ -8,13 +14,38 @@ export function AppAside() {
       <nav>
         <ul>
           <li>
-            <NavLink to="dashboard">Dashboard</NavLink>
+            <NavLink className="aside-row" to="dashboard">
+              <HouseIcon className="menu-icon" /> Dashboard
+            </NavLink>
           </li>
           <li>
-            <NavLink to="blocks">Bloki</NavLink>
+            <div>
+              <PersonalCardIcon className="menu-icon" />
+              Moje CV
+            </div>
           </li>
           <li>
-            <NavLink to="exercises">Ćwiczenia</NavLink>
+            <NavLink className="aside-row" to="blocks">
+              <ElementIcon className="menu-icon" /> Bloki
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="exercises">
+              <EditIcon className="menu-icon" /> Ćwiczenia
+            </NavLink>
+          </li>
+          <li>
+            <div>
+              <CalendarIcon className="menu-icon" />
+              Kalendarz
+            </div>
+          </li>
+          <hr></hr>
+          <li>
+            <div>
+              <SettingIcon className="menu-icon" />
+              Ustawienia
+            </div>
           </li>
         </ul>
       </nav>
