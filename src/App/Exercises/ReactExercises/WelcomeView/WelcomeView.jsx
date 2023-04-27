b7128dad4f4d3e6ae289258c2e3ceebf900b692e
mbialas <michabialas@gmail.com>
2023-04-27 19:07:42 +0200

    Welcome-page-update

--- src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
+++ src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
@@ -1,5 +1,16 @@
 import './styles.css';
 
 export const WelcomeView = () => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <h1 className="headerH1">Cześć!</h1>
+      <h2 className="headerButton">Wpisz tekst</h2>
+      <form>
+        <label>
+          <input type="text" name="name" />
+        </label>
+        <button type="submit">KLIKNIJ</button>
+      </form>
+    </div>
+  );
 };
