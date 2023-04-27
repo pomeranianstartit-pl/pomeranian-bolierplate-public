5d4396a4d5ab5f30e2c05200e31410595b209792
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-04-27 17:27:22 +0200

    Create welcome view router data

---
+++ src/App/Exercises/WelcomeView/router-data.jsx
@@ -0,0 +1,10 @@
+import { WelcomeView } from './WelcomeView';
+
+export const welcomeViewRouterMetaData = {
+  path: 'welcome-view',
+  date: '27-04-2023',
+  linkLabel: 'WelcomeView',
+  blockNo: 1,
+  element: <WelcomeView />,
+  tags: ['components'],
+};
