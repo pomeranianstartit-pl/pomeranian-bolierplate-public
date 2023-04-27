9f239f5ea135f81c7ccab5df426066167b89fc4f
Angelika Wicka <wicka.angelika@gmail.com>
2023-04-27 19:20:08 +0200

    Content and styles for WelcomeView

--- src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
+++ src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
@@ -1,5 +1,13 @@
 import './styles.css';
 
 export const WelcomeView = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="welcome-view">
+      <h2 className="header1">Cześć!</h2>
+      <h3 className="header2">Wpisz tekst</h3>
+      <input className="typing-window" placeholder="tekst do wpisania"></input>
+      <br />
+      <input className="button" type="button" value="Kliknij"></input>
+    </div>
+  );
 };
