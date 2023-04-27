be8876a70318c2c824b5aa9821ed79c800bdfd48
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-04-27 19:07:47 +0200

    Create WelcomeView

--- src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
+++ src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
@@ -1,5 +1,22 @@
 import './styles.css';
 
-export const WelcomeView = () => {
-  return <div>Welcome</div>;
-};
+export const WelcomeView = () => (
+  <div className="one-line">
+    <div>
+      <div>
+        <h1 className="welcome-view"> Cześć</h1>
+      </div>
+      <div>
+        <h5> Wpisz tekst</h5>
+      </div>
+      <div>
+        <input classname="input" type="text"></input>
+      </div>
+      <div>
+        <button className="button">Kliknij</button>
+      </div>
+    </div>
+
+    <div>Placeholder</div>
+  </div>
+);
