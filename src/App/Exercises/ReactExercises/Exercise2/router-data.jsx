d40f585146c136f809b2fabdb810f6b14225fe03
mbialas <michabialas@gmail.com>
2023-04-28 19:00:56 +0200

    Exercise2

---
+++ src/App/Exercises/ReactExercises/Exercise2/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise2 } from './Exercise2';
+
+export const SubRouteExercise2 = {
+  path: 'Exercise2',
+  date: '12-12-2023',
+  linkLabel: 'Exercise2',
+  blockNo: 1,
+  element: <Exercise2 />,
+  tags: ['components', 'import', 'export'],
+};
--- src/App/Exercises/ReactExercises/Exercise2/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/Exercise2/view-router-data.jsx
@@ -1,12 +1,7 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-import { WelcomeViewRouterMetaData } from './WelcomeView/router-data';
 import { SubRouteExercise2 } from './Exercise2/router-data';
 
-export const blockRouterMetaData = [
-  SubRouteExampleMetaData,
-  WelcomeViewRouterMetaData,
-  SubRouteExercise2,
-];
+export const blockRouterMetaData = [SubRouteExampleMetaData, SubRouteExercise2];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
