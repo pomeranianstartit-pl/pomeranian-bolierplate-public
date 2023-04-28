a7491488985914610c1a4b80c183e999a2b04bc1
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-04-28 20:11:04 +0200

    Excercise 2

---
+++ src/App/Exercises/ReactExercises/Exercise2/Exercise2.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+import './style.css';
+import { isVisible } from '@testing-library/user-event/dist/utils';
+
+export function Exercise2() {
+  const [isVisible, setVisibility] = useState(false);
+  const buttonLabel = isVisible ? 'Hide' : 'Show';
+  const handleClick = () => {
+    setVisibility(!isVisible);
+  };
+  return (
+    <div className="exercise-2">
+      <p>See or not</p>
+      {/* {isVisible && <p>tajny tekst</p>}*/}
+
+      <p>
+        <button onClick={handleClick}>{buttonLabel}</button>
+      </p>
+
+      {isVisible ? (
+        <h2>iVisible jest na True</h2>
+      ) : (
+        <h2>iVisible jest na True</h2>
+      )}
+
+      {isVisible && <h2>tajny text</h2>}
+    </div>
+  );
+}
