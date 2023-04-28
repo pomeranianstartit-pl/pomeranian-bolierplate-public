895716e7fed856d83d413b0a42f3c5c7d12bc001
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-04-28 19:02:46 +0200

    Excercise 1

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { WelcomeViewRouterMetaData } from './WelcomeView/router-data';
+import { SubRouteExample1 } from './Exercise1/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   WelcomeViewRouterMetaData,
+  SubRouteExample1,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
