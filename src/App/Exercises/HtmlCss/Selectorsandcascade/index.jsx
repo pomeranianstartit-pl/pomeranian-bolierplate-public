6e4d6a14ff60a86efdff7505fdcbf6e23a767df6
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-05 18:18:44 +0200

    Create emty component

---
+++ src/App/Exercises/HtmlCss/Selectorsandcascade/index.jsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <p>Ćwiczenia z HTML & CSS - Kaskadowość, specyficzność i dziedziczenie</p>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+import { blockRouterMetaData as blockRouterMetaData050520231 } from './Selectorsandcascade/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  blockRouterMetaData050520231,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
