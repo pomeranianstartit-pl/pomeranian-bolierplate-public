62276c3abb567e23e2e268264da84af150f175a0
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-05 19:08:23 +0200

    Create styles.css

--- src/App/Exercises/HtmlCss/Selectorsandcascade/index.jsx
+++ src/App/Exercises/HtmlCss/Selectorsandcascade/index.jsx
@@ -2,6 +2,37 @@ import './styles.css';
 
 export function SelectorsAndCascade() {
   return (
-    <p>Ćwiczenia z HTML & CSS - Kaskadowość, specyficzność i dziedziczenie</p>
+    <div className="general">
+      <div id="main">Jestem bardzo unikalnym elementem</div>
+      <div>
+        <h1>Jestem bardzo unikalnym elementem</h1>
+      </div>
+      <div>
+        <p>Jestem bardzo unikalnym elementem</p>
+      </div>
+      <div className="unique">Jestem bardzo unikalnym elementem</div>
+      <ul>
+        <li>
+          <div>Jestem bardzo unikalnym elementem</div>
+        </li>
+        <li>
+          <div>Jestem bardzo unikalnym elementem</div>
+        </li>
+        <li>
+          <div>Jestem bardzo unikalnym elementem</div>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <div>Jestem bardzo unikalnym elementem</div>
+        </li>
+        <li>
+          <div>Jestem bardzo unikalnym elementem</div>
+        </li>
+        <li>
+          <div>Jestem bardzo unikalnym elementem</div>
+        </li>
+      </ul>
+    </div>
   );
 }
