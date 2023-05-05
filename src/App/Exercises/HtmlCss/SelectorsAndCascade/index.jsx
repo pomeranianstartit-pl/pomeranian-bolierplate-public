00b356248328ba80e31b28829f07967a48df08e8
mbialas <michabialas@gmail.com>
2023-05-05 19:30:35 +0200

    add styles to comp

--- src/App/Exercises/HtmlCss/SelectorsAndCascade/index.jsx
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/index.jsx
@@ -1,5 +1,20 @@
 import './styles.css';
 
 export function SelectorsAndCascade() {
-  return <p>Kaskadowość, selektywność i dziedziczenie</p>;
+  return (
+    <div className="background1">
+      <div className="one">Jestem unique 1</div>
+      <ol className="lista">
+        <li>
+          <main id="main">
+            <div className="two">Jestem unique 2</div>
+            <div className="three">Jestem unique 3</div>
+          </main>
+          <div className="three">Jestem unique 3</div>
+        </li>
+      </ol>
+      <div className="four">Jestem unique 4</div>
+      <div className="five">Jestem unique 5</div>
+    </div>
+  );
 }
