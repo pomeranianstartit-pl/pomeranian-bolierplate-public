a39f467e580ce4c433ba867ac7c2ab1d337276d2
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-05 18:18:28 +0200

    Create empty component and add to routes

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/index.jsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+export function SelectorsAndCascade() {
+  return (
+    <p>Ćwiczenia z HTML & CSS - kaskadowość, specyficzność i dziedziczenie</p>
+  );
+}
---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SelectorsAndCascade } from './index';
+
+export const blockRouterMetaData = {
+  path: 'selectors-and-cascade',
+  date: '05-05-2023',
+  linkLabel: 'HTML & CSS - kaskadowośc, specyficznośc i dziedziczenie',
+  blockNo: 10,
+  element: <SelectorsAndCascade />,
+  tags: ['flex', 'grid', 'video'],
+};
