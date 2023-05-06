b3a4b807260658d871e9f4472df65e29dd7fb418
KarolinaDuljas <duljas.karolina@gmail.com>
2023-05-06 16:37:05 +0200

    Dodalismy trzy sciezki

---
+++ src/App/Settings/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const Settings = () => {
+  return (
+    <div className="dashboard">
+      <h1>Ustawienia</h1>
+      <p>
+        Tu są ustawienia
+      </p>
+    </div>
+  );
+};
