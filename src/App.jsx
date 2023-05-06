6d889eae2211deed3bb586b7a1ef638dbbdd99c9
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-06 16:44:31 +0200

    Side bar calendar and CV

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 
 import { NotFound } from './App/Components/NotFound/NotFound';
-
+import {CV } from './App/CV';
+import {Settings} from './App/Settings';
+import {Calendar} from './App/Calendar';
 import { Blocks } from './App/Blocks';
 import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
@@ -15,6 +17,9 @@ export function App() {
         <Route path="" element={<Layout withSidebar />}>
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard/*" element={<Dashboard />} />
+          <Route path ="cv/*" element={<CV />} />
+          <Route path ="settings/*" element={<Settings />} />
+          <Route path ="calendar/*" element={<Calendar />} />
           <Route path="exercises/*" element={<Exercises />} />
           <Route path="blocks/*" element={<Blocks />} />
           <Route path="*" element={<NotFound />} />
---
+++ src/App/CV/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export const CV = () => {
+  return( 
+  <div className="cv">
+<h1>CV</h1>
+<p>
+  tutaj znajduje się moje CV!
+</p>
+
+  </div>
+  );
+};
