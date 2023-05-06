de5bc81b528493adb35820463d8c3ff9d3ff31ff
RobertNowak <nowakrobertpl@gmail.com>
2023-05-06 15:00:56 +0200

    Dodane logo jako ReactComponent

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {Logo} from '../Components/Logo/Logo';
 import './styles/header.css';
 
 export function AppHeader() {
   return (
     <header>
       <div>
+        <Logo/>
         <div className="header-title">Tytuł nagłówka</div>
       </div>
     </header>
