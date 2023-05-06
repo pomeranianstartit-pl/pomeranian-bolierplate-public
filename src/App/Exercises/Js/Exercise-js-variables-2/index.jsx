1031517ca81205969e44fd5baba383c607c5031a
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-06 12:38:22 +0200

    Ukonczone zadania z modulu 6

--- src/App/Exercises/Js/Exercise-js-variables-2/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-2/index.jsx
@@ -2,6 +2,12 @@ import './styles.css';
 
 export function Exercise() {
   const mathResult1 = 2 * 5;
+  const mathResult2 = 3 / 2;
+  // const mathResult3 = Math.pow(3,2);
+  const mathResult3 = 3 ** 3;
+  const mathResult4 = Math.sqrt(3);
+  const mathResult4_A = mathResult4.toFixed(2)
+  const mathResult5 = 3 % 2;
 
   return (
     <div className="playing-with-variables">
@@ -10,21 +16,17 @@ export function Exercise() {
         <pre>
           <code>2 * 5 = {mathResult1}</code>
         </pre>
-        {/* Uzupełnij wykorzystując zmienne*/}
+        <code>3 : 2 = {mathResult2}</code>
         <pre>
-          <code>3 : 2 = ___</code>
-        </pre>
-        <pre>
-          {/* kwadrat liczby */}
-          <code>3&#178; = &#9998;</code>
+          <code>3&#178; = {mathResult3}</code>
         </pre>
         <pre>
           {/* pierwiastek z liczby */}
-          <code>&#8730;3 = &#9998;</code>
+          <code>&#8730;3 = {mathResult4_A}</code>
         </pre>
         <pre>
           {/* modulo */}
-          <code>3%2 = &#9998;</code>
+          <code>3%2 = {mathResult5}</code>
         </pre>
       </section>
     </div>
