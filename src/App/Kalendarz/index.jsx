cb2d4bb5835aac1d40c04f0237f9c5b890f7e176
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-06 16:44:48 +0200

    dodanie_CV_Kalendarza_Ustawień

---
+++ src/App/Kalendarz/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const Kalendarz = () => {
+  return (
+    <div className="Kalendarz">
+      <h1>Kalendarz</h1>
+      <p>Tutaj znajduje się kalendarz</p>
+    </div>
+  );
+};
---
+++ src/App/Settings/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const Settings = () => {
+  return (
+    <div className="settings">
+      <h1>Ustawienia</h1>
+      <p>Tutaj znajdują się ustawienia</p>
+    </div>
+  );
+};
