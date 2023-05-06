be8e1770d250837c2919e6c857061720ab5eec94
RobertNowak <nowakrobertpl@gmail.com>
2023-05-06 16:37:02 +0200

    zrobione 6 zakladek z 3

---
+++ src/App/CV/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const CV = () => {
+  return (
+    <div className="dashboard">
+      <h1>CV</h1>
+      <p>Tutaj znajduje się moje CV!</p>
+    </div>
+  );
+};
---
+++ src/App/Kalendarz/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const Kalendarz = () => {
+  return (
+    <div className="dashboard">
+      <h1>Kalendarz</h1>
+      <p>Tutaj będzie kalendarz!</p>
+    </div>
+  );
+};
