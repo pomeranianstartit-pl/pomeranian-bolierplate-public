a8d7280110d37986c53cf2de30966f9413e66881
ŁukaszMaterny <materny13@gmail.com>
2023-05-06 12:38:25 +0200

    Ukonczone zadania z modulu 6

--- src/App/Exercises/Js/Exercise-js-functions/index.jsx
+++ src/App/Exercises/Js/Exercise-js-functions/index.jsx
@@ -1,9 +1,29 @@
 import './styles.css';
 
 export function Exercise() {
+  const list = [{
+    name: 'Wojtek',
+    surname: 'Abc',
+    age: 48,
+  }, 
+  {
+    name: 'Kasia',
+    surname: 'Cbd',
+    age: 28,
+  }];
+  function displayList(list) {
+    list.map((listElement) => {
+    const test = 'test'
+    return <div>
+    Moje imię to {listElement.name}, nazwisko {listElement.surname}. Mam {listElement.age} lat.
+      </div>
+    })
+  }
   function multiply(num1, num2) {
     return num1 * num2;
   }
+  
+
 
   function getWelcomeMessage(name, amount) {
     return `Witaj ${name}! Jesteś ${amount} uzytkownikiem odwiedzającym tę stronę!`;
@@ -28,6 +48,7 @@ export function Exercise() {
         <h2>Wykorzystywanie funkcji - przykłady</h2>
         <pre>
           <code>9 * 9 = {multiply(9, 9)}</code>
+          <code>4 * 7 = {multiply(4, 7)}</code>
         </pre>
         <pre>
           <code>{getWelcomeMessage('Jon', 129384)}</code>
