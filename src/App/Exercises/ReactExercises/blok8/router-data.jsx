1530ea5d2a02d15e50d6b0c6ff88a6918d7ede28
RobertNowak <nowakrobertpl@gmail.com>
2023-05-07 10:36:58 +0200

    Subrouting zakladek

---
+++ src/App/Exercises/ReactExercises/blok8/router-data.jsx
@@ -0,0 +1,10 @@
+import { Blok8 } from './blok8';
+
+export const blok8MetaData = {
+  path: 'sciezkaURL',
+  date: '12-12-2283',
+  linkLabel: 'Etykieta linku',
+  blockNo: 1,
+  element: <Blok8 />,
+  tags: ['components', 'import', 'export'],
+};
