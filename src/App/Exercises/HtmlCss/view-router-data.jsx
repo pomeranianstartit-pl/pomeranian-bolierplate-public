04fcbfc87a02252228eeecfeca67023ef6be3e5e
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-08 20:16:22 +0200

    Add custom fonts

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,10 +2,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaData080520231 } from './StandardTags/router-data';
+import { blockRouterMetaData as blockRouterMetaData080520232 } from './GoogleFonts/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaData080520231,
+  blockRouterMetaData080520232,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
