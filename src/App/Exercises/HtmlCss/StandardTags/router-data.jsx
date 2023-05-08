aa9d015568b4754e08eba8a5c0d822cbce0a6a4a
mbialas <michabialas@gmail.com>
2023-05-08 19:05:02 +0200

    tagi

---
+++ src/App/Exercises/HtmlCss/StandardTags/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { StandardTags } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Standard-tags',
+  date: '08-05-2023',
+  linkLabel: 'HTML & CSS - Style tagów HTML / CSS',
+  blockNo: 11,
+  element: <StandardTags />,
+  tags: ['selectors', 'class', 'attribute'],
+};
