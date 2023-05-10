d68d1ae8d78a986952a02ddc11dd8d32b4b6f4bd
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-05-10 21:17:16 +0200

    Create toggle section with CSS and JS logic using components

---
+++ src/App/FAQ/Section/SectionContent/SectionContent.jsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export const SectionContent = ({ content }) => {
+  return <div className="box-content">{content}</div>;
+};
--- src/App/FAQ/index.jsx
+++ src/App/FAQ/index.jsx
@@ -1,3 +1,4 @@
+import { Section } from './Section/Section';
 import './styles.css';
 
 export const FAQ = () => {
@@ -5,31 +6,28 @@ export const FAQ = () => {
     <div className="faq">
       <h1>FAQ</h1>
       <p>tutaj znajdź odpowiedzi na najczęściej zadawane pytania</p>
-      <div>
-        <div className="box">
-          <div className="box-title">Jak mogę zapisać się na szkolenie?</div>
-          <div className="box-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+
+      <Section
+        title="Jak mogę zapisać się na szkolenie?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
             rutrum ipsum, leo et in. Mattis porttitor volutpat placerat
             suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas
             aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium
             tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit
-          </div>
-        </div>
-
-        <div className="box">
-          <div className="box-title">Jak mogę zapisać się na szkolenie?</div>
-        </div>
-
-        <div className="box">
-          <div className="box-title">Jak mogę zapisać się na szkolenie?</div>
-        </div>
-
-        <div className="box">
-          <div className="box-title">Jak mogę zapisać się na szkolenie?</div>
-        </div>
-      </div>
+            amet, consectetur adipiscing elit"
+      />
+      <Section
+        title="Drugie pytanie?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+            rutrum ipsum, leo et in. Mattis porttitor volutpat placerat
+            suspendisse "
+      />
+      <Section
+        title="Trzecie pytanie?"
+        content="Viverra pretium
+            tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit"
+      />
     </div>
   );
 };
