14e503b2455256f67ef3a540184de153e21eadcc
Konrad Bonicki <konradbonicki29@gmail.com>
2023-05-10 17:34:08 +0200

    box sizing

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,9 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
-
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as blockRouterMetaData10052023 } from './BoxModel/router-data';
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  blockRouterMetaData10052023,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
