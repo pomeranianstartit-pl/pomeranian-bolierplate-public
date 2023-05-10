223cfded312c01ab0d4b3e43c9e746487c58881d
mbialas <michabialas@gmail.com>
2023-05-10 18:48:16 +0200

    backg

--- src/App/Exercises/HtmlCss/BoxModel/BoxModel.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/BoxModel.jsx
@@ -3,8 +3,21 @@ import './styles.css';
 export const BoxModel = () => {
   return (
     <div>
-      <p className="box1">Change me X</p>
-      <p className="box2">Change me X</p>
+      <p className="box1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+        <p className="paragraph">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        <p className="paragraph2">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        <p className="paragraph3">Lorem</p>
+      </p>
     </div>
   );
 };
