c48c4c6fe6dd071c42025e8d02c9f556a8ed348f
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-11 18:12:47 +0200

    create table

---
+++ src/App/Exercises/HtmlCss/HTMLTables/HTMLTables.jsx
@@ -0,0 +1,46 @@
+import './styles.css';
+
+export function HTMLTables() {
+  return (
+    <div>
+      Table:
+      <table>
+        <tr>
+          <th>Plastic</th>
+          <th>Acetone</th>
+          <th>Flame test</th>
+          <th>Heat</th>
+          <th>Crease color</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>No efect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>No efect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>No efect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>No efect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,11 +3,13 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaData080520231 } from './StandardTags/router-data';
 import { blockRouterMetaData as blockRouterMetaData080520232 } from './GoogleFonts/router-data';
+import { blockRouterMetaData as blockRouterMetaData110520231 } from './HTMLTables/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaData080520231,
   blockRouterMetaData080520232,
+  blockRouterMetaData110520231,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
