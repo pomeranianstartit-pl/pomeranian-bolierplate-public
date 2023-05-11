aa972cc3bfc28f9716815ddf53c8ceb26c10acf7
pershy <github@963.pl>
2023-05-11 22:34:14 +0200

    Create second array.

--- src/App/Exercises/HtmlCss/ArrayRendering/ArrayRendering.jsx
+++ src/App/Exercises/HtmlCss/ArrayRendering/ArrayRendering.jsx
@@ -8,6 +8,45 @@ export const ArrayRendering = () => {
     { id: 4, make: 'Tesla', model: 'Model S', year: 2022, owner: 'Sarah' },
   ];
 
+  const bands = [
+    {
+      band: 'The Clash',
+      yearFormed: 1976,
+      albums: 6,
+      mostFamousSong: 'London Calling',
+    },
+    {
+      band: 'Sex Pistols',
+      yearFormed: 1975,
+      albums: 1,
+      mostFamousSong: 'Anarchyin the UK',
+    },
+    {
+      band: 'Ramones',
+      yearFormed: 1974,
+      albums: 14,
+      mostFamousSong: 'Blitzkrie Bop',
+    },
+    {
+      band: 'The Cure',
+      yearFormed: 1976,
+      albums: 13,
+      mostFamousSong: 'Just Like Heaven',
+    },
+    {
+      band: 'Joy Division',
+      yearFormed: 1976,
+      albums: 2,
+      mostFamousSong: 'Love Will Tear Us Apart',
+    },
+    {
+      band: 'Siouxsie and the Banshees',
+      yearFormed: 1976,
+      albums: 11,
+      mostFamousSong: 'Hong Kong Garden',
+    },
+  ];
+
   return (
     <div>
       Auta:
@@ -20,6 +59,34 @@ export const ArrayRendering = () => {
           );
         })}
       </ul>
+      Bands:
+      <table className="table1">
+        <tr className="first_row">
+          <th>Bank</th>
+          <th>Year Formed</th>
+          <th>No. Of Albums</th>
+          <th>Most Famous Song</th>
+        </tr>
+
+        {bands.map((bands) => {
+          return (
+            <tr>
+              <td>
+                <p key={bands.band}>{bands.band}</p>
+              </td>
+              <td>
+                <p key={bands.yearFormed}>{bands.yearFormed}</p>
+              </td>
+              <td>
+                <p key={bands.albums}>{bands.albums}</p>
+              </td>
+              <td>
+                <p key={bands.mostFamousSong}>{bands.mostFamousSong}</p>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 };
