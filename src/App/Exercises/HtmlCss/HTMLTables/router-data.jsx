c0574e10e21134db73b97fcc4bd36277ffd69d73
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-05-11 19:19:20 +0200

    Create two tables

---
+++ src/App/Exercises/HtmlCss/HTMLTables/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { HTMLTables } from './HTMLTables';
+
+export const blockRouterMetaData = {
+  path: 'html-tables',
+  date: '11-05-2023',
+  linkLabel: 'Pierwsze tabelki',
+  blockNo: 1,
+  element: <HTMLTables />,
+  tags: ['HTML', 'CSS', 'tables'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-
 import { blockRouterMetaData as blockRouterMetaData05052023 } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as blockRouterMetaData08052023 } from './StandardTags/router-data';
 import { blockRouterMetaData as blockRouterMetaData08_052023 } from './GoogleFonts/router-data';
+import { blockRouterMetaData as blockRouterMetaData11052023 } from './HTMLTables/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaData05052023,
   blockRouterMetaData08052023,
   blockRouterMetaData08_052023,
+  blockRouterMetaData11052023,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
