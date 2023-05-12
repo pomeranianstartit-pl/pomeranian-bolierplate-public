0c49f8f8aac4ce8286f8bf33a0f859ab3830d4e7
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-12 22:11:35 +0200

    Selectors-and-cascade

---
+++ src/App/Exercises/HtmlCss/Blok10AAA/index.jsx
@@ -0,0 +1,27 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div className="blogpost">
+      <span className="date">30-11-2022</span>
+      <h1>Why are we so nostalgic for the 1990s?</h1>
+      <p>
+        {' '}
+        Pop culture used to define a generation, but it seems the cultural,
+        music and fashion trends of the 1990s have been recycled, and what
+        Generation X considered its rite of passage into adulthood, is being
+        discovered and claimed by fledgling grown-ups as their own.
+        <br />
+        There's been the resurgence of vinyl as the trend-setters' choice of
+        music consumption rather than the ease of a digital download, and now
+        the hipsters have discovered the nostalgic sound of a whirring cassette
+        from which to enjoy the dulcet tones of everyone from Salt-N-Pepa to
+        Rick Astley.{' '}
+      </p>
+      <p className="author">
+        <b>Radosława Majdan</b>
+      </p>
+      <span>Senior Marketing Specialist</span>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,12 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+import { blockRouterMetaData as blockRouterMetaData12052023 } from './Blok10/router-data';
+import { blockRouterMetaData as blockRouterMetaData12052023AAA } from './Blok10AAA/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  blockRouterMetaData12052023,
+  blockRouterMetaData12052023AAA,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
