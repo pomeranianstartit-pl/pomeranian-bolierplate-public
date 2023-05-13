c07e59a3be682eeb41972252a269a6e842a810b7
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-13 12:06:13 +0200

    animacje

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,6 +2,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as standardTags } from './StandardTags/router-data';
+import { blockRouterMetaData as CssAnimations } from './CssAnimations/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, standardTags];
+export const blockRouterMetaData = [blockRouterMetaData121220231, standardTags, CssAnimations];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
