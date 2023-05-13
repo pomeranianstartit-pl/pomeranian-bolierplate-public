d7962a2b96760b56f6ae9962c3c3de9b4a1b9cff
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-13 15:04:39 +0200

    dodane zdjęcia

---
+++ src/App/Exercises/HtmlCss/Exercise-images/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import jpeg from '../../../Images/Cat03jpg.jpeg';
+import png from '../../../Images/obazekpng.png';
+
+export function ExerciseImages() {
+  return <div>
+<img src={png} alt=''/>
+<img src={jpeg} alt=''/>
+
+
+  </div>
+
+
+}
+
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -5,5 +5,6 @@ import { blockRouterMetaData as blockRouterMetaData12052023 } from './Blok10/rou
 import { blockRouterMetaData as standardTags } from './StandardTags/router-data';
 import { blockRouterMetaData as CSSAnimation } from './CSSAnimation/router-data';
 import { blockRouterMetaData as ExerciseMultimedia } from './Exercise-multimedia/router-data';
-export const blockRouterMetaData = [blockRouterMetaData121220231,blockRouterMetaData12052023, standardTags, CSSAnimation, ExerciseMultimedia];
+import { blockRouterMetaData as ExerciseImages } from './Exercise-images/router-data';
+export const blockRouterMetaData = [blockRouterMetaData121220231,blockRouterMetaData12052023, standardTags, CSSAnimation, ExerciseMultimedia, ExerciseImages];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
