9a437d7ecbffb034163aac81a884c6d0c11dbc59
WojciechWysocki <wojciechtadwysocki@gmail.com>
2023-05-13 13:02:13 +0200

    animacja freestyle

---
+++ src/App/Exercises/HtmlCss/CssAnimationsFreestyle/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { CssAnimationsFreestyle } from './index';
+
+export const blockRouterMetaData = {
+  path: '13-05-2023-CssAnimationsFreestyle',
+  date: '13-05-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie CssAnimationsFreestyle z dnia 13-05-2023',
+  blockNo: 1,
+  element: <CssAnimationsFreestyle />,
+  tags: ['css', 'animations', 'attribute', 'text', 'freestyle'],
+};
