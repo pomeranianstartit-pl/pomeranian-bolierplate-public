7891c4ea735b858567d1aaa335f95ec5af762ee1
KamilWerochowski <kwerochowski@gmail.com>
2023-05-13 15:38:05 +0200

    obrazy

---
+++ src/App/Exercises/HtmlCss/Exercise-images/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ExerciseImages } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Exercise-images',
+  date: '13-05-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie Images',
+  blockNo: 1,
+  element: <ExerciseImages />,
+  tags: ['html', 'css', 'images'],
+};
