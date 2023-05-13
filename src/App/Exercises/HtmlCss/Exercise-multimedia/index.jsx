df70de15aca8c15fc8aa5359396bc8214522cf72
WojciechWysocki <wojciechtadwysocki@gmail.com>
2023-05-13 14:21:52 +0200

    dodawanie kolorów

--- src/App/Exercises/HtmlCss/Exercise-multimedia/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-multimedia/index.jsx
@@ -3,13 +3,18 @@ import './styles.css';
 export function Multimedia() {
   return (
     <div>
-      <h1 className='colorName'>colorName</h1>
-      <h1 className='shortenHex'>shortenHex</h1>
-      <h1 className='normalHex'>normalHex</h1>
-      <h1 className='rgb'>rgb</h1>
-      <h1 className='rgba'>rgba</h1>
-      <h1 className='hsl'>hsl</h1>
+      <div className='colors'>
+        <h1 className='colorName'>colorName</h1>
+        <h1 className='shortenHex'>shortenHex</h1>
+        <h1 className='normalHex'>normalHex</h1>
+        <h1 className='rgb'>rgb</h1>
+        <h1 className='rgba'>rgba</h1>
+        <h1 className='hsl'>hsl</h1>
 
+      </div>
+      <div className='opacity'>
+        <span>Element który jest w divie z opacity</span>
+      </div>
     </div>
   )
 }
