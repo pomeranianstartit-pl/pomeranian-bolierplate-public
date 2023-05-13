c162aabb459d9b4ab404ffc2ac2b1a716856e618
KarolinaDuljas <duljas.karolina@gmail.com>
2023-05-13 17:00:49 +0200

    uczylismy sie o propsach

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,6 +3,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Blok8MetaData } from './Blok8/router-data';
 import { Blok9MetaData } from './Blok9/router-data';
+import { ReactProps } from './ReactPropsExercises/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData, Blok8MetaData, Blok9MetaData];
+export const blockRouterMetaData = [SubRouteExampleMetaData, Blok8MetaData, Blok9MetaData, ReactProps];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
