15c56bccae04555d11d57b34aeb97ac1ca9d3862
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-13 14:22:34 +0200

    opacity and rgba differences

--- src/App/Exercises/HtmlCss/Exercise-multimedia/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-multimedia/index.jsx
@@ -2,11 +2,19 @@ import './styles.css';
 
 export function ExerciseMultimedia() {
   return <div>
+    <div className="colors">
     <h1 className="colorName">colorName</h1>
     <h1 className="shorteHex">shorteHex</h1>
     <h1 className="normalHex">normalHex</h1>
     <h1 className="rgb">rgb</h1>
     <h1 className="rgba">rgba</h1>
     <h1 className="hsl">hsl</h1>
+    </div>
+    
+    <div className="opacity">
+      <span>Element którey jest w divie z opacity!</span>
+    </div>
+
+
     </div>
 }
