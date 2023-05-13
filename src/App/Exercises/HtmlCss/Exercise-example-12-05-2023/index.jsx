e1fe6377a70e2204d7e0fd92d883c23492932f7e
RobertNowak <nowakrobertpl@gmail.com>
2023-05-13 12:17:47 +0200

    animacja

---
+++ src/App/Exercises/HtmlCss/Exercise-example-12-05-2023/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './styles.css';
+
+
+export function Exercise() {
+  return (
+  <div className="selectors"><h3>TYTUŁ BLOK 10</h3>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <a href="#">Puste</a>
+    <a href="https://google.com"> Google.com</a>
+    <a href="#">Puste</a>
+    <a href="#">Puste</a>
+    <a href="#">Puste</a>
+    
+    <div data-position ="2">nick</div>
+    <div data-position ="4">nick</div>
+    <div data-position ="6">nick</div>
+    <div data-position ="10" data-nickname>nick</div>
+    <div className="hoverable" data-position ="500">Wielka Promocja</div>
+    <div className="redborder" data-position ="6">Czerwona obwódka</div>
+    <div className="focused" data-position ="6">Czerwona obwódka</div>
+  </div>
+    
+   );  
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,8 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+import { blockRouterMetaData as blockRouterMetaData12052023 } from './Exercise-example-12-05-2023/router-data';
+import { blockRouterMetaData as blockRouterMetaDataStandardTags} from './StandardTags/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+export const blockRouterMetaData = [blockRouterMetaData121220231, blockRouterMetaData12052023, blockRouterMetaDataStandardTags];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
