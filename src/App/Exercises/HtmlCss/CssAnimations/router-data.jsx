9fcfde0ae6d5967f16cd3c531929867d5b420fd2
JakubKruk <jakub.kruk.1997@gmail.com>
2023-05-13 12:10:28 +0200

    animation

---
+++ src/App/Exercises/HtmlCss/CssAnimations/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {CssAnimations} from './index';
+
+export const blockRouterMetaData = {
+  path: '12-12-2023-3',
+  date: '12-12-2023',
+  linkLabel: 'HTML & CSS Animations - ćwiczenie 1 z dnia 13.05.2023',
+  blockNo: 1,
+  element: <CssAnimations />,
+  tags: ['flex', 'grid', 'video'],
+};
