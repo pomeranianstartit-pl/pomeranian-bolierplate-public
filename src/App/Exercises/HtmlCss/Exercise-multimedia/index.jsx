bcf5d538338154c5b164fb999778ae55b19ab55c
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-13 14:11:13 +0200

    working on colors

---
+++ src/App/Exercises/HtmlCss/Exercise-multimedia/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export function ExerciseMultimedia() {
+  return <div>
+    <h1 className='colorName'>colorName</h1>
+    <h1 className='shortenHex'>shortenHex</h1>
+    <h1 className='normalHex'>normalHex</h1>
+    <h1 className='rgb'>rgb</h1>
+    <h1 className='rgba'>rgba</h1>
+    <h1 className='hs1'>hs1</h1>
+   
+  </div>
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -4,5 +4,6 @@ import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-
 import { blockRouterMetaData as blockRouterMetaData12052023 } from './Blok10/router-data';
 import { blockRouterMetaData as standardTags } from './StandardTags/router-data';
 import { blockRouterMetaData as CSSAnimation } from './CSSAnimation/router-data';
-export const blockRouterMetaData = [blockRouterMetaData121220231,blockRouterMetaData12052023, standardTags, CSSAnimation];
+import { blockRouterMetaData as ExerciseMultimedia } from './Exercise-multimedia/router-data';
+export const blockRouterMetaData = [blockRouterMetaData121220231,blockRouterMetaData12052023, standardTags, CSSAnimation, ExerciseMultimedia];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
