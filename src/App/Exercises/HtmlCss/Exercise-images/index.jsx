88681d0b4b05df585e56c79b1b849f03d688dd37
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-13 15:37:47 +0200

    ćwiczenie z images

--- src/App/Exercises/HtmlCss/Exercise-images/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-images/index.jsx
@@ -1,11 +1,15 @@
 import './styles.css';
-import jpeg from '../../../Images/Cat03jpg.jpeg';
+import jpeg from '../../../Images/Image.jpeg';
 import png from '../../../Images/obazekpng.png';
 
 export function ExerciseImages() {
   return <div>
-<img src={png} alt=''/>
-<img src={jpeg} alt=''/>
+<img src={png} alt='Example'/>
+<img src={jpeg} alt='Example'/>
+
+{/* {/* background url /*} */}
+
+<div className='imageBackground'>test</div>
 
 
   </div>
