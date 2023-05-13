dfbf6ea0d4a1e4ca1d16c5be23f9bd164422b615
JakubKruk <jakub.kruk.1997@gmail.com>
2023-05-13 13:33:20 +0200

    animations02

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,5 +2,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as CssAnimations2 } from './CssAnimations-2/router-data';
+
+export const blockRouterMetaData = [blockRouterMetaData121220231,CssAnimations2];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
