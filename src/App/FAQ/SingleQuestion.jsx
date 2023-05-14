961d03a4207fa38b68827597f82efbec3518eb29
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-14 15:49:27 +0200

    Chevron down

--- src/App/FAQ/SingleQuestion.jsx
+++ src/App/FAQ/SingleQuestion.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ReactComponent as Chevron_down } from '../Images/chevron_down.svg';
 export const SingleQuestion = (props) => {
   const question = props.question;
   const answear = props.answear;
@@ -6,20 +7,22 @@ export const SingleQuestion = (props) => {
   const [IsOpen, setIsOpen] = useState(true);
   const clickHandler = () => {
     setIsOpen(!IsOpen);
+    
   };
 
   return (
     <div>
       <div>
-        <button value={IsOpen} onClick={clickHandler}>
-          {`${IsOpen}`}
+        <button onClick={clickHandler}>
+          {' '}
+          <Chevron_down />{' '}
         </button>
       </div>
 
       <div className="box">
         <h1 className="header">{question}</h1>
 
-       {IsOpen && <a className="input">{answear}</a>}
+        {IsOpen && <a className="input">{answear}</a>}
       </div>
     </div>
   );
