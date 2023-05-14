4fa470342dd884c1cd8a28b5c5767483160f7065
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-14 10:38:14 +0200

    zmiana font size

--- src/App/Exercises/HtmlCss/BoxModel/index.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -19,6 +19,16 @@ export function BoxModel() {
     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved 
     <span className='inline-span'>Test inlinowy</span>
     </p>
+
+    <p className='paragraph relative-box klasa-a'>
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved 
+    <span className='inline-span'>Test inlinowy</span>
+    </p>
+
+    <p className='klasa-b'>
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved 
+    <span className='inline-span'>Test inlinowy</span>
+    </p>
   </div>
   );
 }
