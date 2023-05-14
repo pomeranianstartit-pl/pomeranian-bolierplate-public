58ef66a36021b4facb24cb1a7b9b93807fb7fe1d
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-14 09:54:37 +0200

    box-model-displayng elements and conditional rendering

---
+++ src/App/Exercises/HtmlCss/BoxModel/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { BoxModel } from './index';
+
+export const blockRouterMetaData = {
+  path: '14-05-2023-boxmodel',
+  date: '14-05-2023',
+  linkLabel: 'BoxModel',
+  blockNo: 12,
+  element: <BoxModel />,
+  tags: ['css', 'box-model', 'display','padding','border','overflow'],
+};
