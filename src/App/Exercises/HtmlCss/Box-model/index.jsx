6a863ed2b006c1fe2be1dfa13dd376a83e770fd9
WojciechWysocki <wojciechtadwysocki@gmail.com>
2023-05-14 10:03:54 +0200

    difference between content-box and border-box

--- src/App/Exercises/HtmlCss/Box-model/index.jsx
+++ src/App/Exercises/HtmlCss/Box-model/index.jsx
@@ -15,6 +15,10 @@ const clickHandler = () => {
       
       {isTrue && <span className='inline-span'>Test inlinowy</span>}
       </p>
+
+      <p className='paragraph content-box'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <span className='inline-span'>Test inlinowy</span>
+      </p>
       
     </div>
   )
