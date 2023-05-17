588f718f6b6d3907bba5b1def46b42868107e6cf
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-05-17 23:14:13 +0200

    Array render to table

--- src/App/Exercises/HtmlCss/ArrayRenderToTable/ArrayRenderToTable.jsx
+++ src/App/Exercises/HtmlCss/ArrayRenderToTable/ArrayRenderToTable.jsx
@@ -1,43 +1,64 @@
 import './styles.css';
 
 export function ArrayRenderToTable() {
+  const bands = [
+    {
+      band: 'The Clash',
+      yearFormed: 1976,
+      albums: 6,
+      mostFamousSong: 'London Calling',
+    },
+    {
+      band: 'Sex Pistols',
+      yearFormed: 1975,
+      albums: 1,
+      mostFamousSong: 'Anarchy in the UK',
+    },
+    {
+      band: 'Ramones',
+      yearFormed: 1974,
+      albums: 14,
+      mostFamousSong: 'Blitzkrieg Bop',
+    },
+    {
+      band: 'The Cure',
+      yearFormed: 1976,
+      albums: 13,
+      mostFamousSong: 'Just Like Heaven',
+    },
+    {
+      band: 'Joy Division',
+      yearFormed: 1976,
+      albums: 2,
+      mostFamousSong: 'Love Will Tear Us Apart',
+    },
+    {
+      band: 'Siouxsie and the Banshees',
+      yearFormed: 1976,
+      albums: 11,
+      mostFamousSong: 'Hong Kong Garden',
+    },
+  ];
+
+  const headers = ['Band', 'Year formed', 'Albums', 'Most famous song'];
+
   return (
-    <table className="Tabela2">
-      <tr>
-        <th>Plastic</th>
-        <th>Acetone</th>
-        <th>Flame test</th>
-        <th>Heat</th>
-        <th>Crease color</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>No effect</td>
-        <td>Green color</td>
-        <td>Softens</td>
-        <td>None</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Softened</td>
-        <td>No change</td>
-        <td>No change</td>
-        <td>White</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>No effect</td>
-        <td>Red color</td>
-        <td>Softens</td>
-        <td>None</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>No effect</td>
-        <td>Green color</td>
-        <td>Soften</td>
-        <td>none</td>
-      </tr>
-    </table>
+    <div>
+      <table className="Tabela2">
+        <tr className="tableHeader">
+          {headers.map((header) => (
+            <th>{header}</th>
+          ))}
+        </tr>
+        {bands.map((row) => (
+          <tr>
+            <td>{row.band}</td>
+            <td>{row.yearFormed}</td>
+            <td>{row.albums}</td>
+            <td>{row.mostFamousSong}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 }
