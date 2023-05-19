b5a43c02232c8f75512994b268e3fbf5836a7e2a
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-19 18:03:46 +0200

    Date and time examples

---
+++ src/App/Exercises/Js/DateTime/router-data.jsx
@@ -0,0 +1,10 @@
+import { DateTime } from './DateTime';
+
+export const dateTimeRouterMetaData = {
+  path: 'date-time',
+  date: '19-05-2023',
+  linkLabel: 'DateTime',
+  blockNo: 19,
+  element: <DateTime />,
+  tags: ['timestamp', 'date', 'javascript'],
+};
