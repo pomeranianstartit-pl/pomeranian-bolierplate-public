1119558b31231d0b7e62c7b5411f36a0d74dc8b8
JakubKruk <jakub.kruk.1997@gmail.com>
2023-05-19 20:23:00 +0200

    adding appheader

--- src/App/Components/HeaderMenu/HeaderMenu.jsx
+++ src/App/Components/HeaderMenu/HeaderMenu.jsx
@@ -1,5 +1,21 @@
 import './styles.css';
+import { SettingIcon } from '../Icons/SettingIcon';
+import { MenuArrow } from '../Icons/MenuArrow';
 
-export const EmptyComponent = () => {
-  return <div>Change me</div>;
-};
+export function HeaderMenu() {
+  return (
+    <div className="images-container">
+      {' '}
+      <SettingIcon />{' '}
+      <div className="circular_image">
+        {' '}
+        <img src="http://placekitten.com/500/500" />{' '}
+      </div>{' '}
+      <div className='name-surname-container'>
+        <h4>Jakub Kruk</h4>
+        <h5>kurasnt</h5>
+      </div>
+      <MenuArrow className="menu-arrow" />{' '}
+    </div>
+  );
+}
