bbd728c14bf300288b07296902bb7a0c1bc3ce1d
KarolinaDuljas <duljas.karolina@gmail.com>
2023-05-19 21:23:10 +0200

    praca nad headerem i dashboardem

---
+++ src/App/Components/HeaderMenu/HeaderMenu.jsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+import { SettingIcon } from '../../Components/Icons/SettingIcon';
+import { MenuArrow } from '../Icons/MenuArrow';
+
+export const HeaderMenu = () => {
+  return (
+    <div className="images-container">
+      <SettingIcon />
+      <div className="circular_image">
+        <img src="http://placekitten.com/500/500" alt="kitty" />
+      </div>
+      <div className='person-container'>
+        <h4>Karolina Duljas</h4>
+        <h5>kursant</h5>
+      </div>
+      <MenuArrow className="menu-arrow" />
+    </div>
+  );
+};
