deb20bc36cbf9b35934f6212935507d29e628acc
WojciechWysocki <wojciechtadwysocki@gmail.com>
2023-05-19 22:00:33 +0200

    dodano user header, el w dashboardzie i grafiki w techstacku

---
+++ src/App/Dashboard/MyData/MyData.jsx
@@ -0,0 +1,11 @@
+export const MyData = () => {
+
+  return (
+    <div className="dashboard-about-me">
+    <div className="person"></div>
+      <p>
+        <b>Wojciech Wysocki</b>
+      </p>
+    </div>
+   );
+  };
