8feeea6cb66a04e5526279653595f86cf4bc87dd
WeronikaRadtke <weronikaradtke14@gmail.com>
2023-05-19 19:15:59 +0200

    ruszajacy sie kwadrat

---
+++ src/App/Exercises/HtmlCss/CssAnimations/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { CssAnimations } from './index';
+
+export const blockRouterMetaData = {
+  path: '13-05-2023-CssAnimations',
+  date: '12-12-2023',
+  linkLabel: 'HTML & CSS Animations - ćwiczenie 1 z dnia 13-05-2023',
+  blockNo: 1,
+  element: <CssAnimations />,
+  tags: ['attribute', 'text','css', 'animation'],
+};
