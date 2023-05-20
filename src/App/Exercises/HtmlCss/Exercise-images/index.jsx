410139f1245aa60a3ecc30adab22bfb4e8e4edc4
WeronikaRadtke <weronikaradtke14@gmail.com>
2023-05-20 12:41:13 +0200

    floats

--- src/App/Exercises/HtmlCss/Exercise-images/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-images/index.jsx
@@ -1,13 +1,38 @@
 import './styles.css';
-import jpg from '../../../Images/exercise-images-1.jpg'
-import gif from '../../../Images/exercaise-image2.gif'
+import jpg from '../../../Images/exercise-images-1.jpg';
+import gif from '../../../Images/exercaise-image2.gif';
+import { RoundImage } from '../../../Components/RoundImage/RoundImage'
 
 export function ExerciseImages() {
-  return (
-    <div>
-      <img src={jpg} alt="labadz" />
-      <img src={gif} alt="example" />
-      <div className="imageBackground"> </div>
-    </div>
-  )
+ const images = [
+  {
+    title: 'to jest jpg',
+    src: jpg,
+    width: 150,
+    height: 150,
+    backgroundColor: 'red',
+  },
+  {
+    title: 'to jest gif',
+    src: gif,
+    width: 250,
+    height: 250,
+    backgroundColor: 'black',
+  },
+ ];
+
+ return (
+  <div>
+    {images.map((el) => (
+      <RoundImage
+        title={el.title}
+        src={el.src}
+        width={el.width}
+        height={el.height}
+        color={el.color}
+        margin={el.margin}
+        />
+    ))}
+  </div>
+);
 }
