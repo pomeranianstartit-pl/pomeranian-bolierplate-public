e9b3c1bf2d03826b65aa6bfa5c9aef9aadc8d6a2
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-20 15:36:08 +0200

    right corner

---
+++ src/App/Components/RightCornerMenuPopup/RightCornerMenuPopup.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import chevron from '../../Images/chevron_down.svg';
+
+export const RightCornerMenuPopup = () => {
+  return (
+    <div className="container">
+      <div className="center-content">
+        <button>ZALOGUJ SIĘ</button>
+        <div className="bottom-paragraph">Nie masz konta? Zarejestruj się.</div>
+      </div>
+    </div>
+  );
+};
