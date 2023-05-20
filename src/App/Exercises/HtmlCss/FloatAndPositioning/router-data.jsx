84396c204027c14c186eb6b8f0517e950faecfc8
WojciechWysocki <wojciechtadwysocki@gmail.com>
2023-05-20 22:42:48 +0200

    position relative na blogu

---
+++ src/App/Exercises/HtmlCss/FloatAndPositioning/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { FloatAndPositioning } from './index';
+
+export const blockRouterMetaData = {
+  path: '20-05-2023/floats',
+  date: '20-05-2023',
+  linkLabel: 'HTML & CSS - pozycje i oplywy',
+  blockNo: 16,
+  element: <FloatAndPositioning />,
+  tags: ['position', 'float', 'flex'],
+};
