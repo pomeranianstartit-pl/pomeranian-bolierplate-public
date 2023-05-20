d753fbdad94067c8e384176fc938914591012a3c
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-20 12:41:23 +0200

    floating and positioning

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioning/index.jsx
@@ -0,0 +1,81 @@
+import './styles.css';
+
+export function FloatsAndPositioning() {
+  return (
+    <div>
+      {' '}
+      <div className="div1">
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.{' '}
+      </div>{' '}
+      <div className="div2">
+        {' '}
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh
+          mauris cursus mattis molestiea iaculis at. Dictum varius duis at
+          consectetur lorem donec.{' '}
+        </p>{' '}
+        <div className="div2_div1">
+          {' '}
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.{' '}
+        </div>{' '}
+      </div>{' '}
+      <div className="div3">
+        {' '}
+        <img
+          width="100px"
+          height="100px"
+          src="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg"
+          alt="Image not found"
+        />{' '}
+        <p>
+          {' '}
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.{' '}
+        </p>{' '}
+      </div>{' '}
+      <div className="div4">
+        {' '}
+        <p>
+          {' '}
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.{' '}
+        </p>{' '}
+      </div>{' '}
+      <div className="div5">Zapisz</div>{' '}
+      <div className="div6">
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nibh mauris cursus
+        mattis molestie a iaculis at. Dictum varius duis at consectetur lorem
+        donec. Sit amet dictum sit amet justo. Fringilla phasellus faucibus
+        scelerisque eleifend donec. Elementum pulvinar etiam non quam lacus
+        suspendisse faucibus interdum posuere. Arcu dictum varius duis at
+        consectetur lorem. Massa ultricies mi quis hendrerit. Leo vel orci porta
+        non pulvinar neque laoreet. In egestas erat imperdiet sed euismod nisi
+        porta lorem. Ut porttitor leo a diam sollicitudin tempor id. Tortor at
+        auctor urna nunc id. Gravida quis blandit turpis cursus in. Ornare quam
+        viverra orci sagittis eu. Metus dictum at tempor commodo ullamcorper a.
+        Vestibulum mattis ullamcorper velit sed ullamcorper morbi. Amet massa
+        vitae tortor condimentum lacinia quis. Quis enim lobortis scelerisque
+        fermentum dui faucibus in ornare. Auctor elit sed vulputate mi.{' '}
+      </div>{' '}
+      <div className="div7">
+        {' '}
+        <div className="div7_div div_relative">
+          {' '}
+          <div className="div7_div div_absolute"></div>{' '}
+          <div className="div7_div div_absolute"></div>{' '}
+        </div>{' '}
+      </div>{' '}
+    </div>
+  );
+}
