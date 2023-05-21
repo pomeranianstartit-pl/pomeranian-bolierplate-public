ff05d74445e1eac1db38414b0c0416337339416b
WojciechWysocki <wojciechtadwysocki@gmail.com>
2023-05-21 13:37:53 +0200

    zapoznanie z booleanami_2

---
+++ src/App/Exercises/Js/Exercise-js-numbers/index.jsx
@@ -0,0 +1,21 @@
+import './styles.css';
+
+export function Exercise() {
+  // 255; // two-hundred and fifty-five
+  // 255.0; // same number
+  // 255 === 255.0; // true
+  // 255 === 0xff; // true (hexadecimal notation)
+  // 255 === 0b11111111; // true (binary notation)
+  // 255 === 0.255e3; // true (decimal exponential notation)
+
+  // Math.round()
+  // Math.floor()
+  // Math.random()
+  // Math.min()
+  // Math.max()
+  // Math.ceil()
+  // parseInt()
+  // parseFloat()
+
+  return <>test</>;
+}
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJSBooleans } from './Exercise-js-booleans/router-data';
+import { blockRouterMetaData as blockRouterMetaDataJSNumbers } from './Exercise-js-numbers/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataJSBooleans,
+  blockRouterMetaDataJSNumbers,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
