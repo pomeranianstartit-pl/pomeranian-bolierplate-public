15e8bc7423093f5a4cdae7aa2c9e27e1eb884c0d
WeronikaRadtke <weronikaradtke14@gmail.com>
2023-05-21 13:00:55 +0200

    nubmersd1

--- src/App/Exercises/Js/Exercise-js-numbers/index.jsx
+++ src/App/Exercises/Js/Exercise-js-numbers/index.jsx
@@ -2,5 +2,18 @@ import './styles.css';
 
 export function Numbers() {
   return <div>dsd</div>;
-  // 255; // two-hundred and fifty-five // 255.0; // same number // 255 === 255.0; // true // 255 === 0xff; // true (hexadecimal notation) // 255 === 0b11111111; // true (binary notation) // 255 === 0.255e3; // true (decimal exponential notation) // Math.round() // Math.floor() // Math.random() // Math.min() // Math.max() // Math.ceil()
+  // 255; // two-hundred and fifty-five //
+  255.0; // same number
+  // 255 === 255.0; // true
+  // 255 === 0xff; // true
+  // (hexadecimal notation) // 255 === 0b11111111; // true (binary notation)
+  // 255 === 0.255e3; // true (decimal exponential notation) //
+  // Math.round()
+  // Math.floor()
+  // Math.random()
+  // Math.min()
+  // Math.max()
+  // Math.ceil()
+  // parseInt()
+  // parseFloat()
 }
