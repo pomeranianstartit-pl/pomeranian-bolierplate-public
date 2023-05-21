fb91381a4e9297697d080a5343bb14e21713da5c
JakubKruk <jakub.kruk.1997@gmail.com>
2023-05-21 13:00:55 +0200

    numbers

---
+++ src/App/Exercises/Js/Exercise-js-numbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'numbers',
+  date: '21-05-2023',
+  linkLabel: 'Js - numbers',
+  blockNo: 1,
+  element: <Exercise/>,
+  tags: ['components'],
+};
