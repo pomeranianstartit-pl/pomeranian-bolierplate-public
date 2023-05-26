ad08e3952ab51f122b7b1857ff48e014835e0f7c
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-05-26 21:04:45 +0200

    blok 21

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -5,11 +5,15 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaDataBooleans as blockRouterMetaDataBooleans } from './Exercise-js-booleans/router-data';
 import { blockRouterMetaDataNumbers as blockRouterMetaDataNumbers } from './Exercise-js-numbers/router-data';
+import { blockRouterMetaData as blockRouterMetaDataJsArraysBasics } from '../../Exercises/Js/Exercise-js-arrays-basics/router-data';
+import { blockRouterMetaData as blockRouterMetaDataJsArraysMethods } from '../../Exercises/Js/Exercise-js-arrays-methods/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataBooleans,
   blockRouterMetaDataNumbers,
+  blockRouterMetaDataJsArraysBasics,
+  blockRouterMetaDataJsArraysMethods,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
