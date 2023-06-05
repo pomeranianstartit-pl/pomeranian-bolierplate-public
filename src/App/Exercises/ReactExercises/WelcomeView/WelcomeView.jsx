fd0f772b8289cfc3ff38c0ee254b0d900e4fa2e3
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-06-05 14:07:06 +0200

    Finish html and css styling

--- src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
+++ src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './styles.css';
+import './WelcomeView.css';
 
 export const WelcomeView = () => {
   const [count, setCount] = useState(0);
@@ -18,29 +18,38 @@ export const WelcomeView = () => {
     //<button onClick={() => buttonClicked()}>Kliknieto we mnie {x}</button>
     // <div>wartosc inputa: {x}</div>
     // </div>
-    <div>
-      <div className="block-1">
-        <div className="h1-text">
+
+    /* <button onClick={() => setCount(count + 1)}>
+            <p>nacisnieto {count} razy</p> */
+
+    <div className="welcomeview">
+      <div className="welcomeview-inline-block">
+        <div className="welcomeview-text">
           <h1>Cześć!</h1>
         </div>
 
-        <div className="input-label">
+        <div className="welcomeview-input">
           <label>Wpisz tekst</label>
         </div>
 
-        <div className="input-box">
-          <input type="text" placeholder="tekst do wpisania" />
+        <div className="welcomeview-input-box">
+          <input
+            type="text"
+            name="welcomeview"
+            placeholder="tekst do wpisania"
+          />
         </div>
 
-        <div className="button-box">
-          <button onClick={() => setCount(count + 1)}>
-            <p>nacisnieto {count} razy</p>
+        <div className="welcomeview-button">
+          <button type="button">
+            <p>KLIKNIJ</p>
           </button>
         </div>
       </div>
-
-      <div className="placeholder">
-        <h3>Placeholder</h3>
+      <div className="welcomeview-inline-block">
+        <div className="welcomeview-placeholder-box">
+          <p>Placeholder</p>
+        </div>
       </div>
     </div>
   );
