8c0ee37d9098cb7dd874cf1dec45ce3014a4af63
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-06-06 11:20:43 +0200

    Finished react exercise nr 2

--- src/App/Exercises/ReactExercises/Exercise2/Exercise2.jsx
+++ src/App/Exercises/ReactExercises/Exercise2/Exercise2.jsx
@@ -4,22 +4,18 @@ import React, { useState } from 'react';
 export const Exercise2 = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  // const isVisible = true;
-  const buttonLabel = isVisible ? 'HIDE' : 'SHOW';
+  // warunek zmiany nazwy przycisku
+  const buttonLabel = isVisible ? 'HIDE MESSAGE' : 'SHOW MESSAGE';
 
   const handleClick = () => {
-    setIsVisible(!isVisible);
-    {
-      /*wykrzyknik zmienia value na przeciwny */
-    }
+    setIsVisible(!isVisible); //wykrzyknik zmienia value na przeciwny
   };
 
   return (
     <div>
       <h1>See or not</h1>
       <button onClick={handleClick}>{buttonLabel}</button>
-      {isVisible && <h3>Teraz mnie widać</h3>} {/*dziala jak if */}
-      {/* {isVisible ? <p>tajny tekst</p> : <p>tajny tekst jest ukryty</p>} */}
+      {isVisible && <h3>Hello there!</h3>} {/*&& dziala jak if */}
     </div>
   );
 };
