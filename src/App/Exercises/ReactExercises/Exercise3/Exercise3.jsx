8e6ae1b4ec2fba2b64620b0d83ffe6dbe63737a3
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-06-06 11:37:20 +0200

    Finished react exercise nr 3

--- src/App/Exercises/ReactExercises/Exercise3/Exercise3.jsx
+++ src/App/Exercises/ReactExercises/Exercise3/Exercise3.jsx
@@ -10,13 +10,13 @@ export const Exercise3 = () => {
     setGivenNumber(newValue);
 
     if (newValue > 100) {
-      setText('podana liczba jest wieksza niz 100');
+      setText('Podana liczba jest większa niż 100');
     } else if (newValue > 10) {
-      setText('podana liczba jest wieksza niz 10');
+      setText('Podana liczba jest większa niż 10');
     } else if (newValue === 10) {
-      setText('podana liczba jest rowna 10');
+      setText('Podana liczba jest równa 10');
     } else {
-      setText('podana liczba jest mniejsza niz 10');
+      setText('Podana liczba jest mniejsza niż 10');
     }
   };
 
@@ -24,14 +24,19 @@ export const Exercise3 = () => {
 
   return (
     <div>
-      <h1>Czy A jest większe od 10?</h1>
+      <h1>Czy wpisana liczba jest większa od 10?</h1>
 
-      <input type="number" onChange={handleChange} value={givenNumber} />
+      <input
+        type="number"
+        placeholder="wpisz liczbę"
+        onChange={handleChange}
+        // value={givenNumber} ustawia poczatkowa liczbe
+      />
 
       {ifGreaterThan10 ? (
-        <p>Tak liczba {givenNumber} jest wieksza od 10</p>
+        <p>Tak, liczba: {givenNumber} jest większa od 10</p>
       ) : (
-        <p>Nie, liczba {givenNumber} jest mniejsza od 10</p>
+        <p>Nie, liczba: {givenNumber} jest mniejsza od 10</p>
       )}
       <p>{text}</p>
     </div>
