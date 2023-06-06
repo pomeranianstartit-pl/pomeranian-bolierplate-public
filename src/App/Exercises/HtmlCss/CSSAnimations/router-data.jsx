970613118d44019da722af44e800264171e6c2f0
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-06-06 16:49:38 +0200

    Create CSS animation

---
+++ src/App/Exercises/HtmlCss/CSSAnimations/router-data.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { CSSAnimations } from './CSSAnimations';
+
+export const blockRouterMetaData = {
+  path: 'css-animations',
+
+  date: '08-05-2023',
+
+  linkLabel: 'HTML & CSS - tworzenie animacji',
+
+  blockNo: 11,
+
+  element: <CSSAnimations />,
+
+  tags: ['attribute', 'text', 'css', 'animation'],
+};
