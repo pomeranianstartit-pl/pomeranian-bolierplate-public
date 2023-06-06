4e172350de3f55fbb9554f6e7858aa8256e7e87a
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-06-06 21:10:59 +0200

    First steps with JS

--- src/App/Exercises/Js/Exercise-js-functions/index.jsx
+++ src/App/Exercises/Js/Exercise-js-functions/index.jsx
@@ -1,8 +1,12 @@
 import './styles.css';
 
 export function Exercise() {
+  const variableForTemplateString = 'is awesome!';
+
+  const templateString = 'Template string ${variableForTemplateString}';
+ 
   function multiply(num1, num2) {
-    return num1 * num2;
+    return (num1 * num2) +12;
   }
 
   function getWelcomeMessage(name, amount) {
@@ -19,7 +23,7 @@ export function Exercise() {
                 return num1 * num2;
             }
             
-            const result = multiply(9, 9)  // wynik 81
+            const result = multiply(7, 7)  // wynik 49
             `}
         </code>
       </pre>
@@ -30,9 +34,20 @@ export function Exercise() {
           <code>9 * 9 = {multiply(9, 9)}</code>
         </pre>
         <pre>
-          <code>{getWelcomeMessage('Jon', 129384)}</code>
+          <code>{getWelcomeMessage('Jon', 111111)}</code>
         </pre>
       </section>
+
+      <p>Drugie wywołanie funkcji multiply: {multiply(10, 10)}</p>
+      <h1>{getWelcomeMessage('Jon', 111111)}</h1>
+
+      <br />
+      <p>Trzecie wywołanie funkcji multiply: {multiply(11, 11)}</p>
+      <br />
+
+      <p>Nasz templates string: </p>
+      <p id='unique-id'>{templateString}</p>
+
     </div>
   );
 }
--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -19,7 +19,8 @@ export function Exercise() {
         <p>{string2}</p>
         <p>{string3}</p>
         {/* Uzupełnij zanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>Witaj ____! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
+
+        <p>Witaj! Jesteś {10} uzytkownikiem odwiedzającym tę stronę!</p>
       </section>
     </div>
   );
