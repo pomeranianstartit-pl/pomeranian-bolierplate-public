fc24c79cebca1a3badb864eaaa857d0db0ea441a
ABorowiec <borowiec.aleksandra.94@gmail.com>
2023-06-06 11:12:14 +0200

    Finished react exercise nr 1

--- src/App/Exercises/ReactExercises/Exercise1/Exercise1.jsx
+++ src/App/Exercises/ReactExercises/Exercise1/Exercise1.jsx
@@ -3,40 +3,41 @@ import React, { useState } from 'react';
 
 export const Exercise1 = () => {
   const [clicks, setClicks] = useState(1);
-  const [showText, setShowText] = useState('start');
+  const [showText, setShowText] = useState('Ilość kliknięć: 0');
 
   // const handleClick = () => {
   //   setClicks(clicks + 1);
   // };
-
   //const isFiveClicks = clicks === 5;
 
   const handleClick = () => {
     if (clicks < 5) {
       setClicks(clicks + 1);
-      setShowText('ilosc klikniec:' + clicks);
+      setShowText('Ilość kliknięć: ' + clicks);
     } else {
-      setShowText('osiagnieto 5 klikniec');
+      setShowText('BRAWO! Osiągnięto 5 kliknięć!');
     }
   };
 
   return (
     <div>
-      <h1>Kliknij 5</h1>
+      <h1>Kliknij przycisk 5 razy</h1>
 
       <button onClick={handleClick}>START</button>
       <div>{showText}</div>
 
       <button
         onClick={() => {
-          setClicks(0);
+          setClicks(1);
+          setShowText('Ilość kliknięć: 0');
         }}
       >
-        Reset
+        RESET
       </button>
-
-      {/* {isFiveClicks && <h1>kliknieto 5 razy!</h1>} */}
-      {/* {showText && <h1>kliknieto 5 razy!</h1>} */}
     </div>
+
+    //inny zapis wywolania html przez warunek if
+    //{isFiveClicks && <h1>kliknieto 5 razy!</h1>}
+    //{showText && <h1>kliknieto 5 razy!</h1>}
   );
 };
