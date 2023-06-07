f0073cf937996914b7c9bf9ce162f1ce557d8e0f
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-06-07 20:47:15 +0200

    Menu style

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,20 +1,22 @@
 import { NavLink } from 'react-router-dom';
 
 import './styles/aside.css';
-
+import { HouseIcon } from '../Components/Icons/HouseIcon';
+import { ElementIcon } from '../Components/Icons/ElementIcon';
+import {EditIcon} from '../Components/Icons/EditIcon';
 export function AppAside() {
   return (
     <aside>
       <nav>
         <ul>
-          <li>
-            <NavLink to="dashboard">Dashboard</NavLink>
+          <li className='aside-row'>
+            <NavLink to="dashboard"> <HouseIcon className='icon' />Dashboard</NavLink>
           </li>
-          <li>
-            <NavLink to="blocks">Bloki</NavLink>
+          <li className='aside-row'>
+            <NavLink to="blocks"> <ElementIcon className='icon' />Bloki</NavLink>
           </li>
-          <li>
-            <NavLink to="exercises">Ćwiczenia</NavLink>
+          <li className='aside-row'>
+            <NavLink to="exercises"><EditIcon className='icon'/>Ćwiczenia</NavLink>
           </li>
         </ul>
       </nav>
