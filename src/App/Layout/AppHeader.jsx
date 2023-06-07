90e279943036fbc19a6ef8eb222c191eb238f2d4
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-06-07 18:48:39 +0200

    add new logo

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import './styles/header.css';
+import { Logo } from '../Components/Logo/Logo';
 
 export function AppHeader() {
   return (
     <header>
+      <Logo />     
       <div>
         <div className="header-title">Tytuł nagłówka</div>
       </div>
     </header>
   );
 }
+
+
