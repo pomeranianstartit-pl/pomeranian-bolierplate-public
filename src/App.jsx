105cebde3a90d9642bcdc2283a60726e8c6d81ef
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-06-09 22:38:29 +0200

    menu - calendar, settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 
 import { NotFound } from './App/Components/NotFound/NotFound';
 
-import { Blocks } from './App/Blocks';
 import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
-import { Exercises } from './App/Exercises';
 import { CV } from './App/CV/CV';
+import { Blocks } from './App/Blocks';
+import { Exercises } from './App/Exercises';
+import { Calendar } from './App/Calendar';
+import { Settings } from './App/Settings';
+
 
 export function App() {
   return (
@@ -17,8 +20,10 @@ export function App() {
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard/*" element={<Dashboard />} />
           <Route path="CV/*" element={<CV />} />
-          <Route path="exercises/*" element={<Exercises />} />
           <Route path="blocks/*" element={<Blocks />} />
+          <Route path="exercises/*" element={<Exercises />} />
+          <Route path="calendar/*" element={<Calendar />} />
+          <Route path="settings/*" element={<Settings />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
