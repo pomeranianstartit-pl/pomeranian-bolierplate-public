34d4232e12ef35e210212f2b39b3e56200fa6873
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-06-10 15:47:25 +0200

    memory-game-3

--- src/App/Exercises/Js/MemoryGame/Playground/Playground.jsx
+++ src/App/Exercises/Js/MemoryGame/Playground/Playground.jsx
@@ -9,18 +9,15 @@ function shuffleArray(s) {
   return s;
 }
 
-const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const alpha = Array.from(Array(26)).map((e, i) => i + 65); //listy na bazie liczby
 const alphabet = alpha.map((x) => String.fromCharCode(x));
-
 const getRandomLetters = (amount) => {
-  // dopytać Szymona dlaczego Math.random()
-  const shuffled = [...alphabet].sort(() => 0.5 - Math.random());
+  const shuffled = [...alphabet].sort(() => 0.5 - Math.random()); //nie dziala jak powinno bo to robi shuffleArray
   return shuffled.slice(0, amount);
 };
 
 function generateBoard(size) {
-  const randomLetters = getRandomLetters(size / 2);
-
+  const randomLetters = getRandomLetters(size / 2); //bo polowa liter zawsze odkrywane dwie karty na raz
   const letters = randomLetters.map((letter) => {
     return {
       id: null,
@@ -28,78 +25,95 @@ function generateBoard(size) {
       isPaired: false,
     };
   });
-
   const mergedLetters = [...letters, ...letters];
-
   return shuffleArray(mergedLetters).map((obj, index) => {
     return { ...obj, id: index + 1 };
   });
 }
-export const Playground = ({ boardSize }) => {
+export const Playground = ({
+  boardSize,
+  amount,
+  setAmount,
+  setGameEndedMemo,
+  time,
+  setTime,
+  setMyTime,
+  isGameEndedMemo,
+}) => {
   const [firstClickedFieldId, setFirstClickedFieldId] = useState();
   const [secondClickedFieldId, setSecondClickedFieldId] = useState();
-  const [board, setBoard] = useState(generateBoard(16));
-
+  const [board, setBoard] = useState(generateBoard(boardSize));
   const handleClick = (object) => {
     const isFirstClickedSetAndIsDifferentThanPrev =
-      firstClickedFieldId && firstClickedFieldId != object.id;
-
+      firstClickedFieldId && firstClickedFieldId !== object.id;
     if (isFirstClickedSetAndIsDifferentThanPrev) {
       setSecondClickedFieldId(object.id);
+      resetSecondClickedFieldId();
     } else {
       setFirstClickedFieldId(object.id);
-      resetFirstClickedId();
+      resetFirstClickedFieldId();
     }
   };
 
-  const resetFirstClickedId = () => {
+  const resetFirstClickedFieldId = () => {
     setTimeout(() => {
       setFirstClickedFieldId(undefined);
-    }, 800);
+    }, 1000);
   };
-  const resetSecondClickedId = () => {
+  const resetSecondClickedFieldId = () => {
     setTimeout(() => {
       setSecondClickedFieldId(undefined);
-    }, 800);
+    }, 1000);
   };
   useEffect(() => {
     if (firstClickedFieldId && secondClickedFieldId) {
+      setAmount(amount + 1);
       const firstClickedFieldValue = board.find(
         (item) => item.id === firstClickedFieldId
       ).value;
       const secondClickedFieldValue = board.find(
         (item) => item.id === secondClickedFieldId
       ).value;
-
       if (firstClickedFieldValue === secondClickedFieldValue) {
-        board.map((field) => {
-          const isClickedFieldPaired =
-            field.id === firstClickedFieldId ||
-            field.id === secondClickedFieldId;
-
-          return {
-            ...field,
-            isPaired: field.isPaired ? true : isClickedFieldPaired,
-          };
-        });
+        setBoard(
+          board.map((field) => {
+            // const isClickedFieldPaired =
+            //   field.id === firstClickedFieldId ||
+            //   field.id === secondClickedFieldId;
+            return {
+              ...field,
+              isPaired: field.isPaired
+                ? true
+                : firstClickedFieldId === field.id ||
+                  secondClickedFieldId === field.id,
+            };
+          })
+        );
       }
     }
   }, [firstClickedFieldId, secondClickedFieldId]);
-
-  console.log(board);
-
+  useEffect(() => {
+    if (board.find((item) => item.isPaired === false)) {
+      setGameEndedMemo(false);
+    } else {
+      setGameEndedMemo(true);
+      setMyTime(time);
+    }
+  }, [board]);
+  console.log(isGameEndedMemo);
   return (
-    <div className="board">
+    <div className="board_view_main_field">
       {board.map((element) => {
         const isClicked =
           firstClickedFieldId === element.id ||
           secondClickedFieldId === element.id;
         const canShowValue = isClicked || element.isPaired;
-        const clickedFieldClassName = isClicked ? 'field-clicked' : '';
-        const pairedFieldClassName = element.isPaired ? 'field-paired' : '';
+        const clickedFliedClassName = isClicked ? 'field-clicked' : '';
+        const pairedFieldClassName = element.isPaired ? 'green-field' : '';
+
         return (
           <div
-            className={`board-field ${clickedFieldClassName} ${pairedFieldClassName}`}
+            className={`field ${clickedFliedClassName} ${pairedFieldClassName}`}
             onClick={() => handleClick(element)}
           >
             {canShowValue && element.value}
--- src/App/Exercises/Js/MemoryGame/index.jsx
+++ src/App/Exercises/Js/MemoryGame/index.jsx
@@ -1,28 +1,59 @@
-// import './styles.css';
-// import { Button } from '../MemoryGame/Button/Button';
-// import { Menu } from '../MemoryGame/MenuView';
+import './styles.css';
 // import './MenuView.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GameView } from './GameView/GameView';
 import { MenuView } from './MenuView/MenuView';
 import { Playground } from './Playground/Playground';
 
-export function MemoryGame({}) {
+export function MemoryGame() {
   const [isGameStarted, setGameStarted] = useState(false);
+  const [time, setTime] = useState(0);
+  const [timeInterval, setTimeInterval] = useState();
+  const [boardSize, setBoardSize] = useState();
+  const [amount, setAmount] = useState(0);
+  const [isGameEnded, setGameEnded] = useState(false);
+  const [myTime, setMyTime] = useState(0);
+  useEffect(() => {
+    if (isGameStarted) {
+      const gameInterval = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+      setTimeInterval(gameInterval);
+    } else {
+      clearInterval(timeInterval);
+      setTime(0);
+    }
+  }, [isGameStarted]);
 
   return (
     <>
       <h4>Memory game</h4>
       <p>Gra plegająca na</p>
       {isGameStarted ? (
-        <GameView setGameStarted={setGameStarted} />
+        <GameView
+          setGameStarted={setGameStarted}
+          time={time}
+          amount={amount}
+          setAmount={setAmount}
+        />
       ) : (
-        <MenuView setGameStarted={setGameStarted} />
+        <MenuView
+          setGameStarted={setGameStarted}
+          setBoardSize={setBoardSize}
+          setGameEnded={setGameEnded}
+        />
       )}
-      {isGameStarted && <Playground />}
-      {/* <MenuView /> */}
-      {/* <GameView />
-      <Playground /> */}
+      {isGameStarted && (
+        <Playground
+          boardSize={boardSize}
+          amount={amount}
+          setAmount={setAmount}
+          setGameEnded={setGameEnded}
+          setTime={setTime}
+          isGameEnded={isGameEnded}
+          setMyTime={setMyTime}
+        />
+      )}
     </>
   );
 }
