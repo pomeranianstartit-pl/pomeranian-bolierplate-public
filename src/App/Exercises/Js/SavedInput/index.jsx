5f3cb1c2e061f5b495cd644f88268b5331829094
KarolinaDuljas <duljas.karolina@gmail.com>
2023-06-11 11:02:23 +0200

    dodana początkowa logika do dodawania nicków do listy

--- src/App/Exercises/Js/SavedInput/index.jsx
+++ src/App/Exercises/Js/SavedInput/index.jsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import { useState } from 'react';
 
 const nicks = [
   {
@@ -15,27 +16,50 @@ const nicks = [
 }]
 
 export function SavedInput() {
+  const [idCounter, setIdCounter] = useState(nicks.length)
+
+  
+  
+  function addPerson(e) {
+      const inputNickValue = e.target.value
+      nicks[idCounter] = {nick: inputNickValue, id: (idCounter + 1)}
+      console.log('1', nicks)
+      
+      function sendToLocalStorage() {
+        const stringifyNickList = JSON.stringify(nicks)
+        const idCounterList = []
+        for (let i=0;nicks.length===i;i++) {
+          idCounter[i]=i+1
+        }
+        
+        localStorage.setItem("list-nicki", stringifyNickList)
+        localStorage.setItem("list-nicki-index", idCounter)
+      }
+
+      sendToLocalStorage()
+  }
+
+  
   return (
     <div className='saved-input-container'>
+      <form >
       <div className='left-part'>
         <div className='nick-input-row'>
           <p>NICK</p>
-          <input type="text" className='input-nick'/>
+          <input type="text" className='input-nick' placeholder='Podaj swój nick' autoFocus onChange={addPerson}/>
         </div>
-        <button>DODAJ</button>
+        <button type="submit">DODAJ</button>
       </div>
+      </form>
       <div className='list'>
-        {/* {nicks.map(()=>{
-
-        })} */}
-        <div className='row-on-list'>
-          <p>JANEK</p>
-          <button className='x-button'>X</button>
-        </div>
-        <div className='row-on-list'>
-          <p>ZENEK</p>
-          <button className='x-button' onClick={()=>{}}>X</button>
-        </div>
+        {nicks.map((person)=>{
+          return (
+            <div className='row-on-list'>
+              <div className='name-on-list'>{person.nick}</div>
+              <button className='x-button'>X</button>
+            </div>
+          )
+        })}
       </div>
     
     </div>
