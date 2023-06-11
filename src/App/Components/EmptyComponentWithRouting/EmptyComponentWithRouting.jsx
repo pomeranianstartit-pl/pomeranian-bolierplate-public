aac37b1afa2e9a730dfa4b13127d95f6879ca78d
JakubKruk <jakub.kruk.1997@gmail.com>
2023-06-11 16:31:02 +0200

    opp

--- src/App/Components/EmptyComponentWithRouting/EmptyComponentWithRouting.jsx
+++ src/App/Components/EmptyComponentWithRouting/EmptyComponentWithRouting.jsx
@@ -1,5 +1,5 @@
 import './styles.css';
 
-export const RightCornerMenuPopup = () => {
+export const Exercise = () => {
   return <div>Change me X</div>;
 };
---
+++ src/App/Exercises/Js/Exercise-opp/index.jsx
@@ -0,0 +1,21 @@
+import './styles.css';
+
+const user1 = {
+  name: 'Marcin',
+  sayHello: () => {
+    console.log('Cześć jestem Marcin')
+  }
+  
+}
+
+const user2 = {
+  name: 'Sara',
+  sayHello: () => {
+    console.log('Cześć jestem Sara')
+  }
+  
+}
+
+export const ExerciseOpp = () => {
+  return <div>Change me X</div>;
+};
