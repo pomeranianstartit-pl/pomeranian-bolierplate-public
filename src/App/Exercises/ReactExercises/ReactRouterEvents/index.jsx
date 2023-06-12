6ebb6bfe3ab25438f032a284dfe9496cb17d2345
Sourcer-R <mariam8yunis@gmail.com>
2023-06-12 19:50:13 +0200

    React router and events

---
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.css';
+
+export function ReactRouterEventsJS() {
+  return (
+    <div className='form--container'>
+      <h1> Czesc! </h1>
+       <label for="textInput">Wpisz Tekst</label>
+  <input type="text" id="textInput" name="textInput" placeholder='Tekst do wpisania'/>
+  
+
+  <input type="button" value="Kliknij"/>
+    </div>
+  );
+}
---
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/router-data.jsx
@@ -0,0 +1,10 @@
+import { ReactRouterEventsJS } from ".";
+
+export const ReactRouterEventsMetaData = {
+  path: 'react-router-events',
+  date: '12-12-2023',
+  linkLabel: 'react-router-events',
+  blockNo: 8,
+  element: <ReactRouterEventsJS />,
+  tags: ['react', 'router', 'events'],
+};
