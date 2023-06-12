e470bce344dd3d2be17ad14129d0d908962104b7
Sourcer-R <mariam8yunis@gmail.com>
2023-06-12 21:11:29 +0200

    Counter and styling added

--- src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './style.css';
 
 export function ReactRouterEventsJS() {
+  const [getCount, setCount]=useState (0)
+  function handleClick(){
+    return; //console.log("jestem kliknietem buttonem");
+  }
+
+  function handleOnChangeInput(event){
+    const inputValue = event.target.value; 
+    return; //console.log (`Wprowadzona cos`, inputValue);
+  }
+
+  function handleIncrement () {
+    setCount(getCount +1);
+  }
+
+  function handleDecrement () {
+    setCount(getCount -1);
+  }
+
   return (
+    
     <div className='form--container'>
       <h1> Czesc! </h1>
        <label for="textInput">Wpisz Tekst</label>
-  <input type="text" id="textInput" name="textInput" placeholder='Tekst do wpisania'/>
+  <input type="text" id="textInput" name="textInput" placeholder='Tekst do wpisania'
+  onChange= {handleOnChangeInput}/>
   
 
-  <input type="button" value="Kliknij"/>
+
+
+  <div className='counter --container'> 
+    <p>Licznik: {getCount}</p>
+      <input onClick={handleIncrement} type="button" value={getCount === 0 ? `Kliknieto we mnie ${getCount} razy`
+      : `Super! Klieknieto we mnie ${getCount} razy`}/>
+      <input onClick={handleDecrement} type="button" value="Decrement"/>
+  </div>
     </div>
   );
 }
