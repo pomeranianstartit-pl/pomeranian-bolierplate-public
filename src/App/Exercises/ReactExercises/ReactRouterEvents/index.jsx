e0c976269ae4d86ad19ca4a7cd5256f340716df1
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-06-12 20:59:34 +0200

    zmiana w react cw

--- src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './style.css';
 
 export function ReactRouterEvents() {
-  return (
-    <div name="form--container">
-      <h1>Cześć!</h1>
-      <label name="textInput" for="textInput">
-        Wpisz tekst
-      </label>
-      <input
-        id="textInput"
-        type="text"
-        name="textInput"
-        placeholder="tekst do wpisania"
-      />
+  const [getCount, setCount] = useState(0);
 
-      <input type="button" value="KLIKNIJ" />
-    </div>
+  function handleClickButton(event) {
+    return console.log('Jestem klikniętym buttonem');
+  }
+
+  function handleOnChangeInput(event) {
+    const inputValue = event.target.value;
+    return console.log('Wprowadzona coś:', inputValue);
+  }
+
+  function handleIncrement() {
+    setCount(getCount + 1);
+  }
+  function handleDecrement() {
+    setCount(getCount - 1);
+  }
+
+  return (
+    <>
+      <div className="form--container">
+        <h1>Cześć!</h1>
+        <label for="textInput">Wpisz tekst</label>
+        <input
+          id="textInput"
+          type="text"
+          name="textInput"
+          placeholder="tekst do wpisania"
+          onChange={handleOnChangeInput}
+        />
+
+        <input onClick={handleClickButton} type="button" value="KLIKNIJ" />
+      </div>
+
+      <br />
+      <br />
+
+      <div className="counter--cointainer">
+        <p>Licznik: {getCount}</p>
+        <input onClick={handleIncrement} type="button" value="Increment" />
+        <input onClick={handleDecrement} type="button" value="Decrement" />
+      </div>
+    </>
   );
 }
