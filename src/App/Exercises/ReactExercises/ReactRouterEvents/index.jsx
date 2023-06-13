de1798c034edff8187888c8aab7a8b419d65f523
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-06-13 18:52:13 +0200

    Click5

--- src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export function ReactRouterEvents() {
+  const [getCount, setCount] = useState(0);
+
+  function handleClickButton(event) {
+    return; //console.log('jestem klikniętym buttonem');
+  }
+
+  function handleOnChangeInput(event) {
+    const inputValue = event.target.value;
+    //console.log('event', event);
+    return; //console.log('Wprowadzona wartość w input:', inputValue);
+  }
+
+  function handleIncrement() {
+    setCount(getCount + 1);
+  }
+  function handleDecrement() {
+    setCount(getCount - 1);
+  }
+
   return (
-    <div className="form--container">
-      <h1>Cześć!</h1>
+    <>
+      <div className="form--container">
+        <h1>Cześć!</h1>
 
-      <label for="lname">Wpisz tekst</label>
-      <input
-        type="text"
-        id="textInput"
-        name="textInput"
-        placeholder="tekst do wpisania"
-      />
+        <label for="lname">Wpisz tekst</label>
+        <input
+          type="text"
+          id="textInput"
+          name="textInput"
+          placeholder="tekst do wpisania"
+          onChange={handleOnChangeInput}
+        />
 
-      <input type="button" value="Submit" />
-    </div>
+        <input onClick={handleClickButton} type="button" value="KLIKNIJ" />
+      </div>
+
+      <div className="counter--container">
+        <p>Licznik: {getCount}</p>
+        <input
+          onClick={handleIncrement}
+          type="button"
+          value={
+            getCount === 0
+              ? `Kliknięto we mnie ${getCount} razy`
+              : `Super kliknieto we mnie wiele razy - ${getCount}`
+          }
+        />
+        <input onClick={handleDecrement} type="button" value="Decrement" />
+      </div>
+    </>
   );
 }
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,12 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactRouterEventsMetaData } from './ReactRouterEvents/router-data';
+import { Block09MetaData } from './Block09/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactRouterEventsMetaData,
-  ReactRouterEventsMetaData,
-  ReactRouterEventsMetaData,
-  ReactRouterEventsMetaData,
+  Block09MetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
