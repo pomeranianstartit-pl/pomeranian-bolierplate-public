313853a980d2be24ab4c04d848b35da45ca786ae
novelus1 <krzysztof.truszkowski@outlook.com>
2023-06-13 19:41:50 +0200

    Add show hide function

--- src/App/Exercises/ReactExercises/Block09/Block09.jsx
+++ src/App/Exercises/ReactExercises/Block09/Block09.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import './style.css';
 import { Click5 } from '../Click5/Click5';
+import { SeeOrNot } from '../SeeOrNot/SeeOrNot';
 
 export function Block09() {
   return (
     <div>
       <h1>Blok 9 - 13.06.2023</h1>
       <div>
+        <hr />
         <Click5 />
+        <hr />
+        <SeeOrNot />
       </div>
     </div>
   );
---
+++ src/App/Exercises/ReactExercises/SeeOrNot/SeeOrNot.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+import './styles.css';
+
+
+
+
+export const SeeOrNot = () => {
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const [myButton, setMyButton] = useState('ukryj');
+
+  function clickHandler() {
+
+    let currentIsVisible = isVisible;
+
+    setIsVisible(!currentIsVisible);
+
+    if (currentIsVisible) {
+
+      setMyButton('Pokaż');
+
+    } else {
+
+      setMyButton('Ukryj');
+
+    }
+
+  }
+
+
+
+
+  return (
+
+    <div className="HideMe">
+
+      <h1>See or Not</h1>
+
+
+
+
+      <button onClick={clickHandler} className="btn">{myButton}</button>
+
+      {isVisible && <p>{'Teraz mnie widać'}</p>}
+
+    </div>
+
+  );
+
+};
