d747e9cc994976c5a03fc04b7efa374aeaab2933
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-06-13 20:25:50 +0200

    comparsion

--- src/App/Exercises/ReactExercises/Block09/Block09.jsx
+++ src/App/Exercises/ReactExercises/Block09/Block09.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Click5 } from '../Click5/Click5';
 import { SeeOrNot } from '../SeeOrNot/SeeOrNot';
+import { MoreOrLess } from '../MoreOrLess/MoreOrLess';
 import './style.css';
 export function Block09() {
   return (
@@ -8,5 +9,7 @@ export function Block09() {
       <Click5 />
       <hr />
       <SeeOrNot />
+      <hr />
+      <MoreOrLess />
     </div>)
 }
---
+++ src/App/Exercises/ReactExercises/MoreOrLess/MoreOrLess.jsx
@@ -0,0 +1,21 @@
+import './styles.css';
+import { useState } from 'react';
+export const MoreOrLess = () => {
+
+  const [message, setMessage] = useState('Wprowadź');
+
+  function answerHandler(ev) {
+    let currentAnswer = ev.target.value;
+    //console.log(currentAnswer)
+    if (currentAnswer > 10) {
+      setMessage('TAK');
+    } else {
+      setMessage('NIE');
+    }
+  }
+  return <div>
+    <h1>Czy A jest większe od 10?</h1>
+    <input placeholder="A" type='number' onChange={answerHandler}></input>
+    <h2>{'Odpowiedź: '} {message}</h2>
+  </div>;
+};
