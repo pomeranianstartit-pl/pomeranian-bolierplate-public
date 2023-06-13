6a1889ffa5997f188fb37a0f4a74abca0297e08d
karolinagelinska <karolina@Assus>
2023-06-13 15:59:03 +0200

    reactrouterexercises

---
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+import './style.css';
+
+export function ReactRouterEvents() {
+  const [getCount, setCount] = useState(0);
+
+  function handleClickButton() {
+    return; //console.log('jestem kliknietym buttonem');
+  }
+
+  function handleOnChangeInput(event) {
+    const inputValue = event.target.value;
+    //console.log('event', event);
+    return; //console.log('Wprowadzona wartość w input: ', inputValue);
+  }
+  function handleIncrement() {
+    setCount(getCount + 1);
+  }
+  function handleDecrement() {
+    setCount(getCount - 1);
+  }
+
+  return (
+    <>
+      <div className="form--container">
+        <h1>Cześć !</h1>
+
+        <label for="textImput">Wpisz tekst</label>
+        <input
+          type="text"
+          id="textInput"
+          name="textInput"
+          placeholder="tekst do wpisania"
+          onChange={handleOnChangeInput}
+        />
+      </div>
+      <input onClick={handleClickButton} type="button" value="KLIKNIJ" />
+
+      <div className="counter--container">
+        <p>Licznik: {getCount}</p>
+        <input
+          onClick={handleIncrement}
+          type="button"
+          value={
+            getCount === 0
+              ? `Kliknieto we mnie ${getCount} razy`
+              : `Super kliknieto we mnie wiele razy - ${getCount}`
+          }
+        />
+        <input onClick={handleDecrement} type="button" value="Decrement" />
+      </div>
+    </>
+  );
+}
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
+import { ReactRouterEventsMetaData } from './ReactRouterEvents/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  ReactRouterEventsMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
