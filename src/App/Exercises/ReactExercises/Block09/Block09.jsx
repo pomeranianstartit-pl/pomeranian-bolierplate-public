22208a0d264c6b6732b0ece246adefe08d7e7fb6
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-06-13 19:42:36 +0200

    see or not

---
+++ src/App/Exercises/ReactExercises/Block09/Block09.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Click5 } from '../Click5/Click5';
+import { SeeOrNot } from '../SeeOrNot/SeeOrNot';
+import './style.css';
+export function Block09() {
+  return (
+    <div>
+      <Click5 />
+      <hr />
+      <SeeOrNot />
+    </div>)
+}
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -4,8 +4,11 @@ import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
 import { ReactRouterEventsMetaData } from './ReactRouterEvents/router-data';
 
+import { Block09MetaData } from './Block09/router-data';
+
 export const blockRouterMetaData = [
     SubRouteExampleMetaData,
     ReactRouterEventsMetaData,
+    Block09MetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
