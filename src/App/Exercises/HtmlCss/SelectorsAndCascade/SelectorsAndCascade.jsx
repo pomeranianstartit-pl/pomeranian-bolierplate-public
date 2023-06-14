3cdf589d292b8a0be1b32436c65e746aa2e03c74
novelus1 <krzysztof.truszkowski@outlook.com>
2023-06-14 20:14:28 +0200

    Add main menu element hover

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/SelectorsAndCascade.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './style.css';
+
+export function SelectorsAndCascade() {
+  return (
+    <div>
+      <div className="mydiv-1">
+        Jestem bardzo unikalnym elementem 1<p>Paragraf 1</p>
+      </div>
+      <div className="mydiv-2">Jestem bardzo unikalnym elementem 2</div>
+      <div className="mydiv-3">
+        Jestem bardzo unikalnym elementem 3<span>abcabcabcabcabcabc</span>
+      </div>
+      <div className="mydiv-4">Jestem bardzo unikalnym elementem 4</div>
+      <div className="test">
+        <span className="span-1">adsfd</span>Jestem bardzo unikalnym elementem 5
+      </div>
+      <div>Jestem bardzo unikalnym elementem 6</div>
+    </div>
+  );
+}
---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,10 @@
+import { SelectorsAndCascade } from './SelectorsAndCascade';
+
+export const SelectorsAndCascadeMetaData = {
+  path: 'block10',
+  date: '14-06-2023',
+  linkLabel: 'Blok 10',
+  blockNo: 1,
+  element: <SelectorsAndCascade />,
+  tags: ['components', 'import', 'export'],
+};
