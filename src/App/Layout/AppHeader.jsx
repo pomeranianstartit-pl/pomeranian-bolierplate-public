405232180b88bf23cbace127391db2f62d22344b
karolinagelinska <karolina@Assus>
2023-06-14 20:16:21 +0200

    zapis

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -6,7 +6,7 @@ export function AppHeader() {
   return (
     <header>
       <div>
-        <div className="header-title">...</div>
+        <div className="header-title">Zmień to logo w końcu</div>
       </div>
     </header>
   );
