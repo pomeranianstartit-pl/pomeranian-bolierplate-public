a41944c279921eb746396aee6df5f194c563af85
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-06-14 18:14:29 +0200

    functions

--- src/App/Exercises/ReactExercises/Block09/Block09.jsx
+++ src/App/Exercises/ReactExercises/Block09/Block09.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Click5 } from '../Click5/Click5';
 import { SeeOrNot } from '../SeeOrNot/SeeOrNot';
 import { MoreOrLess } from '../MoreOrLess/MoreOrLess';
+import { AOrB } from '../AOrB/AOrB';
 import './style.css';
 export function Block09() {
   return (
@@ -11,5 +12,7 @@ export function Block09() {
       <SeeOrNot />
       <hr />
       <MoreOrLess />
+      <hr />
+      <AOrB />
     </div>)
 }
