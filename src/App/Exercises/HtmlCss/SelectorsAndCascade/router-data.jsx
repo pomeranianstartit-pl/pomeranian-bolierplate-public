ff80754b03a8b7564ebd16d144fcc32601f5f3d6
karolinagelinska <karolina@Assus>
2023-06-14 20:06:44 +0200

    selectors-and-cascade

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SelectorsAndCascade } from './SelectorsAndCascade';
+
+export const SelectorsAndCascadeMetaData = {
+  path: 'selectors-and-cascade',
+  date: '14-06-2023',
+  linkLabel: 'HTML & CSS: Selektory i kaskadowość',
+  blockNo: 1,
+  element: <SelectorsAndCascade />,
+  tags: ['flex', 'grid', 'video'],
+};
--- src/App/Exercises/ReactExercises/Block9/Findnumber/Findnumber.jsx
+++ src/App/Exercises/ReactExercises/Block9/Findnumber/Findnumber.jsx
@@ -2,9 +2,27 @@ import './styles.css';
 import { useState } from 'react';
 
 export const FindNumber = () => {
+  const [message, setMessage] = useState('');
+
+  function answerHandler(ev) {
+    let currentAnswer = ev.target.value;
+    if (currentAnswer === 7) {
+      setMessage('Gratulacje trafiłeś/aś!');
+    } else if (currentAnswer < 7) {
+      setMessage('Jestem mniejsza');
+    } else {
+      setMessage('Jestem większa');
+    }
+  }
+  function randomNumber() {
+    return Math.random();
+  }
   return (
     <div>
-      <h2>Jaką liczbą jestem?</h2>
+      <h2>Jaką jestem liczbą?</h2>
+      <button className="btn" onClick={randomNumber}>
+        {'START'}
+      </button>
     </div>
   );
 };
