e47ea8835fe0330bd22a40b6600303f51428b6dc
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-06-14 20:38:05 +0200

    feature/blog

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -50,6 +50,12 @@ export function AppAside() {
               Ustawienia
             </NavLink>
           </li>
+          <li>
+            <NavLink className="aside-row" to="blog">
+              <EditIcon className="menu-icon" />
+              Blog
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <p style={{ padding: '1rem 0' }}>Sidebar items, widgets, etc</p>
