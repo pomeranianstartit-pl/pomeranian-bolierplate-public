e9cf3a637baacb434b1241091fd824e228c56436
novelus1 <krzysztof.truszkowski@outlook.com>
2023-06-15 18:43:15 +0200

    Blog part 2

--- src/App/Blog/Blog.jsx
+++ src/App/Blog/Blog.jsx
@@ -20,12 +20,61 @@ export const Blog = () => {
           cassette from which to enjoy the dulcet tones of everyone from
           Salt-N-Pepa to Rick Astley.
         </p>
+        <p>
+          There's been the resurgence of vinyl as the trend-setters' choice of
+          music consumption rather than the ease of a digital download, and now
+          the hipsters have discovered the nostalgic sound of a whirring
+          cassette from which to enjoy the dulcet tones of everyone from
+          Salt-N-Pepa to Rick Astley.
+        </p>
         <br></br>
         <div className="blog-author-signature">
-          <p className="blog-author">Radosława Majdan</p>
-          <p className="blog-author-position">Senior Marketing Specialist/</p>
+          <h5 className="blog-author">Radosława Majdan</h5>
+          <h6 className="blog-author-position">Senior Marketing Specialist</h6>
         </div>
       </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <ul>
+        <li>
+          <a
+            href="https://wiadomosci.wp.pl/to-zacieranie-sladow-chodzi-o-pakiety-wyborcze-sasina-6879848507329312a"
+            target="blank"
+            className="blog-link"
+          >
+            WP.PL
+          </a>
+        </li>
+        <br></br>
+        <li>
+          <a href="https://www.wp.pl" target="blank" className="blog-link">
+            WP.PL
+          </a>
+        </li>
+        <br></br>
+        <li>
+          <a
+            href="https://www.onet.pl/film/onetfilm/tvn-wbil-szpile-tvp-pokazano-alternatywna-rzeczywistosc-telewizji-rzadowej/2ksezfp,681c1dfa"
+            target="blank"
+            className="blog-link"
+          >
+            WP.PL
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
