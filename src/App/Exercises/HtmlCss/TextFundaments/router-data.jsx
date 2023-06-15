0994f0ffbb59cbf767fa8eeb874521c5474ed601
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-06-15 21:02:50 +0200

    stylowanie fontów

---
+++ src/App/Exercises/HtmlCss/TextFundaments/router-data.jsx
@@ -0,0 +1,10 @@
+import { TextFundaments } from './TextFundaments';
+
+export const TextFundamentsMetaData = {
+  path: 'text-fundaments',
+  date: '15-06-2023',
+  linkLabel: 'TextFundaments',
+  blockNo: 1,
+  element: <TextFundaments />,
+  tags: ['components', 'import', 'export'],
+};
