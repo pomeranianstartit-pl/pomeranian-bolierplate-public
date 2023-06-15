27028a32c2ccf3bb073d96019d2047c364349726
novelus1 <krzysztof.truszkowski@outlook.com>
2023-06-15 17:10:28 +0200

    Add first part of blog

--- src/App/Blog/Blog.jsx
+++ src/App/Blog/Blog.jsx
@@ -1,5 +1,30 @@
 import './styles.css';
 
 export const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="blog">
+      <h3>BLOG</h3>
+      <p className="date">30-11-2022</p>
+      <h2 class="blog-post-title">Why are we so nostalgic for the 1990s?</h2>
+      <br />
+      <div className="div1">
+        <p>
+          Pop culture used to define a generation, but it seems the cultural,
+          music and fashion trends of the 1990s have been recycled, and what
+          Generation X considered its rite of passage into adulthood, is being
+          discovered and claimed by fledgling grown-ups as their own.
+        </p>
+        <p>
+          There's been the resurgence of vinyl as the trend-setters' choice of
+          music consumption rather than the ease of a digital download, and now
+          the hipsters have discovered the nostalgic sound of a whirring
+          cassette from which to enjoy the dulcet tones of everyone from
+          Salt-N-Pepa to Rick Astley.
+        </p>
+        <br></br>
+        <h4 className="signature">Radosława Majdan</h4>
+        <h6 className="signature1">Senior Marketing Specialist/</h6>
+      </div>
+    </div>
+  );
 };
