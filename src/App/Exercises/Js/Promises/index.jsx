d9addf61bbaf6bf2fee80cb73336cf2afc32871b
JakubKruk <jakub.kruk.1997@gmail.com>
2023-06-16 18:52:58 +0200

    promises

--- src/App/Exercises/Js/Promises/index.jsx
+++ src/App/Exercises/Js/Promises/index.jsx
@@ -18,7 +18,34 @@ export function JsPromisesExercise(parameterToEffect) {
     // possible options in useEffect array to kick useEffect and cause
   }, [personalData, getState]);
   // służy do optymalizacji działania aplikacji
- 
+ function logPromiseState(promise) {
+  promis
+  .them(()=>{
+    // sucess
+    console.log('Stan: fullfilled');
+    console.log( 'Promise: zakonczony sukcesem');
+  })
+  .catch(()=>{
+    // failed
+    console.log('Stan: rejected');
+    console.log( 'Promise: zakonczony bledem');
+  })
+  .finally(()=>{
+    console.log('Stan: fullfilled/rejected');
+    console.log( 'Promise: zakonczony');
+  })
+
+  const myPromises = new Promise ((resolve, rejected)=>{
+    setTimeout(()=>{
+      const random =Math.random();
+      if (random < 0,5) {
+        resolve ()
+      }
+      else { rejected()}
+    }, 2000)
+  })
+
+ }
   return (
     <div className='playing-with-js-promises'>
   
