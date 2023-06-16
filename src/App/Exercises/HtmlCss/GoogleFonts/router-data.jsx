a913bc2aed79d47996615029b38e4c28dcc51089
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-06-16 18:31:20 +0200

    metadata info changes

--- src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
+++ src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
@@ -4,7 +4,7 @@ export const GoogleFontsMetaData = {
   path: 'google-fonts',
   date: '16-06-2023',
   linkLabel: 'GoogleFonts',
-  blockNo: 1,
+  blockNo: 11,
   element: <GoogleFonts />,
-  tags: ['components', 'import', 'export'],
+  tags: ['fonts', 'function'],
 };
