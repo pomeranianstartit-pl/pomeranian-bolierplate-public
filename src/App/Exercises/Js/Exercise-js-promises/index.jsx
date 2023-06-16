9275a24b3fa3a03767f85ed3a65b7b51acdf0dbb
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-06-16 18:52:28 +0200

    fetch-and-promisses

--- src/App/Exercises/Js/Exercise-js-promises/index.jsx
+++ src/App/Exercises/Js/Exercise-js-promises/index.jsx
@@ -14,5 +14,36 @@ export function JSPromisesExercise({ parameterToEffect }) {
     };
   }, [parameterToEffect, getState]);
 
+  function logPromiseState(promise) {
+    promise
+      .then(() => {
+        //succes
+        console.log('stan fullified');
+        console.log('promis sakonczony sukcesem');
+      })
+      .catch(() => {
+        //failed
+        console.log('stan rejected');
+        console.log('promis sakonczony bledem');
+      })
+      .finally(() => {
+        //nevermind I will do this
+        console.log('stan fullified/rejected');
+        console.log('promis sakonczony');
+      });
+  }
+
+  const myPromise = new Promise((resolve, rejected) => {
+    setTimeout(() => {
+      const random = Math.random();
+
+      if (random < 0.5) {
+        resolve();
+      } else {
+        rejected();
+      }
+    }, 2000);
+  });
+  logPromiseState(myPromise);
   return <div>Promises</div>;
 }
