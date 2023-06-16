f044afe88c8782bbd01631fa610d52112acf9c2e
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-06-16 18:25:47 +0200

    Font

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { GoogleFonts } from './GoogleFonts';
+
+export const GoogleFontsMetaData = {
+  path: 'google-fonts',
+  date: '16-06-2023',
+  linkLabel: 'Google Fonts - ćwiczenie 1 z dnia 16-06-2023',
+  blockNo: 1,
+  element: <GoogleFonts />,
+  tags: ['attribute', 'text', 'css', 'fonts'],
+};
