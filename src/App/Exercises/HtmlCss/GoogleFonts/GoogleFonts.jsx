93fcceb585d92436fa59c269140e153b2fc05c1e
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-06-16 18:27:55 +0200

    Font

--- src/App/Exercises/HtmlCss/GoogleFonts/GoogleFonts.jsx
+++ src/App/Exercises/HtmlCss/GoogleFonts/GoogleFonts.jsx
@@ -13,11 +13,11 @@ export function GoogleFonts() {
   };
   return (
   <div>
-  <p className={`${font} ${theme} wrapper`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat faucibus sem, at ultrices magna ultrices id. Fusce vestibulum massa vitae metus sollicitudin scelerisque. Phasellus porta, lectus sed lobortis facilisis, tortor diam sollicitudin elit, vitae vulputate risus leo eget massa. Ut nec lorem pretium, malesuada tellus ut, ullamcorper augue. Nam dui lectus, mollis ac suscipit non, euismod sit amet ligula. Aenean eros nisi, iaculis in feugiat vel, iaculis ut urna. Aliquam nec imperdiet enim, id gravida odio. In et metus interdum, malesuada quam lobortis, tristique nibh. In hac habitasse platea dictumst. </p>
-  <button onClick={() =>handleFontChange('font-poppins')}>Change font to: Poppins</button>
-  <button onClick={() =>handleFontChange('font-lato')}>Change font to: Lato</button><br/>
-  <button onClick={() =>handleThemeChange('light-theme')}>Change font to: Light</button>
-  <button onClick={() =>handleThemeChange('dark-theme')}>Change font to: Dark</button>
+    <p className={`${font} ${theme} wrapper`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat faucibus sem, at ultrices magna ultrices id. Fusce vestibulum massa vitae metus sollicitudin scelerisque. Phasellus porta, lectus sed lobortis facilisis, tortor diam sollicitudin elit, vitae vulputate risus leo eget massa. Ut nec lorem pretium, malesuada tellus ut, ullamcorper augue. Nam dui lectus, mollis ac suscipit non, euismod sit amet ligula. Aenean eros nisi, iaculis in feugiat vel, iaculis ut urna. Aliquam nec imperdiet enim, id gravida odio. In et metus interdum, malesuada quam lobortis, tristique nibh. In hac habitasse platea dictumst. </p>
+    <button onClick={() =>handleFontChange('font-poppins')}>Change font to: Poppins</button>
+    <button onClick={() =>handleFontChange('font-lato')}>Change font to: Lato</button><br/>
+    <button onClick={() =>handleThemeChange('light-theme')}>Change font to: Light</button>
+    <button onClick={() =>handleThemeChange('dark-theme')}>Change font to: Dark</button>
   </div>
   )
   ;
--- src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -6,7 +6,7 @@ export const SelectorsAndCascadeMetaData = {
   path: 'selectors-and-cascade',
   date: '14-06-2023',
   linkLabel: 'HTML & CSS - ćwiczenie 1 z dnia 14-06-2023',
-  blockNo: 10,
+  blockNo: 11,
   element: <SelectorsAndCascade />,
   tags: ['flex', 'grid', 'video'],
 };
