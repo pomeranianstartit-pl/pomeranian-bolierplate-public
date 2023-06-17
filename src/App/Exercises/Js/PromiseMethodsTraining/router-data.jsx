e6a456854c2a137be5c319a5ce0e1502086f85fe
KarolKuligowski <karol.kuligowski18@gmail.com>
2023-06-17 15:23:03 +0200

    async

---
+++ src/App/Exercises/Js/PromiseMethodsTraining/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'promise-methods-training',
+  date: null,
+  linkLabel: 'Promise methods ćwiczenie',
+  blockNo: 35,
+  element: <Exercise />,
+  tags: ['async', 'await', 'asynchronous', 'fetch', 'javascript'],
+};
