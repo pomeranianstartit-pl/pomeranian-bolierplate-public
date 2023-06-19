d5c12cd8c2d0f3b8fc54c0b9df95cfd2d0a46d50
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-06-19 17:04:08 +0200

    przed zadaniem

--- src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
@@ -35,18 +35,22 @@ export const Exercise = () => {
           </li>
           <li>
             Pharetra, mi cras lacus ante adipiscing quam. Viverra tristique
-            integer amet, nibh. <a href="https://www.t-mobile.pl/c/oferta-specjalna/internet-tmobile">
-            Odio nunc pharetra </a> sem turpis
+            integer amet, nibh.{' '}
+            <a href="https://www.t-mobile.pl/c/oferta-specjalna/internet-tmobile">
+              Odio nunc pharetra{' '}
+            </a>{' '}
             sem turpis sem turpis sem turpis sem turpis sem turpis sem turpis
-            sem turpis sem turpis sem turpis commodo, dapibus phasellus enim at posuere at. quis at nunc, diam enim ac varius et augue.
+            sem turpis sem turpis sem turpis sem turpis commodo, dapibus
+            phasellus enim at posuere at. quis at nunc, diam enim ac varius et
+            augue.
           </li>
         </ul>
         <p>
           Tellus in at facilisi scelerisque morbi quis fames facilisi velit.
           Feugiat et <b>pulvinar eu nisi tempus</b>, cras. Facilisis aliquet vel
           mauris tellus phasellus ultrices dui. Feugiat dignissim massa neque,
-          curabitur tellus. Purus volutpat elit sem at volutpat morbi. Odio tortor
-          a blandit et urna. Viverra condimentum gravida et, malesuada.
+          curabitur tellus. Purus volutpat elit sem at volutpat morbi. Odio
+          tortor a blandit et urna. Viverra condimentum gravida et, malesuada.
         </p>
       </div>
       <div className="content-wrapper second">
@@ -57,21 +61,21 @@ export const Exercise = () => {
             et, vel vitae, faucibus euismod. Venenatis, quis arcu, consectetur
             dictum dui, est facilisi. Quam nulla proin eleifend semper habitant
             mattis mi risus. Elementum pellentesque vestibulum pretium nam elit
-            justo, sed id. Mus quam placerat pellentesque et enim odio ut mi. Ante
-            massa nisl, eget egestas pharetra sagittis dolor. Vehicula pulvinar
-            sagittis ut id at odio eget.
+            justo, sed id. Mus quam placerat pellentesque et enim odio ut mi.
+            Ante massa nisl, eget egestas pharetra sagittis dolor. Vehicula
+            pulvinar sagittis ut id at odio eget.
           </li>
           <li>
             Pharetra, mi cras lacus ante adipiscing quam. Viverra tristique
-            integer amet, nibh. <b>Odio nunc pharetra</b> sem turpis fusce viverra
-            commodo, mi euismod. Id ut erat dapibus cursus phasellus enim at
-            posuere at. Ut elit ut non dictum tellus sit. Hac quis at nunc, diam
-            enim ac varius et augue.
+            integer amet, nibh. <b>Odio nunc pharetra</b> sem turpis fusce
+            viverra commodo, mi euismod. Id ut erat dapibus cursus phasellus
+            enim at posuere at. Ut elit ut non dictum tellus sit. Hac quis at
+            nunc, diam enim ac varius et augue.
           </li>
           <li>
             Tellus in at facilisi scelerisque morbi quis fames facilisi velit.
-            Feugiat et pulvinar eu nisi tempus, cras. Facilisis aliquet vel mauris
-            tellus phasellus ultrices dui. Feugiat dignissim massa neque,
+            Feugiat et pulvinar eu nisi tempus, cras. Facilisis aliquet vel
+            mauris tellus phasellus ultrices dui. Feugiat dignissim massa neque,
             curabitur tellus. Purus volutpat elit sem at volutpat morbi. Odio
             tortor a blandit et urna. Viverra condimentum gravida et, malesuada.
           </li>
