ba2e51ca37f1cc16f3522f31bfb4e28cd625ee8c
karolinagelinska <karolina@Assus>
2023-06-20 21:38:21 +0200

    tables-exercise

---
+++ src/App/Exercises/HtmlCss/Tables/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Tables } from './Tables';
+
+export const TablesMetaData = {
+  path: 'tables',
+  date: '20.06.2023',
+  linkLabel: 'Tabele i tablice',
+  blockNo: 14,
+  element: <Tables />,
+  tags: ['tables', 'arrays', 'maps', 'css'],
+};
