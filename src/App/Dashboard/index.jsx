81a2f0d0e2ae59e32c56797267f1c63279e1ccb4
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-06-21 21:26:05 +0200

    zmiany w dashboard

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,13 +1,67 @@
 import './styles.css';
+import hand from '../Images/emogiHand.svg';
+import { useState } from 'react';
+import { DashboardCard } from '../Components/DashboardCard/DashboardCard';
+import { EditIcon } from '../Components/Icons/EditIcon';
+import BlogIcone from '../Images/blogIcon.svg';
+import CodeIcone from '../Images/codeIcon.svg';
+import { FAQIcon } from '../Components/Icons/FaqIcon.jsx';
 
 export const Dashboard = () => {
+  const [availableCards, setAvailableCards] = useState([
+    {
+      sectionTitle: 'Moje CV',
+      icon: <img src={DashboardCard} alt="business card resume" />,
+      description: 'podgląd cv wraz z doświadczeniem',
+      link: '/cv',
+    },
+    {
+      sectionTitle: ' Ćwiczenia',
+      icon: <img src={EditIcon} alt="business card resume" />,
+      description: 'wszystkie wykonane ćwiczenia',
+      link: '/excercise',
+    },
+    {
+      sectionTitle: 'Blog',
+      icon: <img src={BlogIcone} alt="business card resume" />,
+      description: 'wpisy blogowe o technologii front-end',
+      link: '/blog',
+    },
+    {
+      sectionTitle: 'Tech stack',
+      icon: <img src={CodeIcone} alt="business card resume" />,
+      description: 'stack technologiczny realizowany na kursie',
+      link: '/tech',
+    },
+    {
+      sectionTitle: 'FAQ',
+      icon: <FAQIcon/>,
+      description: 'odpowiedzi na najczęstsze pytania',
+      link: '/faq',
+    },
+  ]);
   return (
     <div className="dashboard">
-      <h1>Dashboard</h1>
+      <h3>
+        <img src={hand} /> Hej, tutaj Dominik!
+      </h3>
       <p>
-        Cześć tutaj: Strona główna kursanta (o sobie, zdjęcie, opis narzędzi
-        wykorzystywanych na kursie oraz umejętności zdobytych na kursie)
+        Poniżej znajdziesz najważniejsze informacje na temat mojej działalności
       </p>
+      <div className="windows">
+        {availableCards.map((card) => {
+          return (
+            <div className="window-main">
+              <DashboardCard
+                sectionTitle={card.sectionTitle}
+                icon={card.icon}
+                description={card.description}
+                link={card.link}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,15 +3,35 @@ import React from 'react';
 import './styles/header.css';
 import { Logo } from '../Components/Logo/Logo';
 import { Link } from 'react-router-dom';
+import strzalka from '../Images/strzalka.svg';
+import cogwheel from '../Images/setting.svg';
 
 export function AppHeader() {
   return (
     <header>
-      <Link to="/">
-        <Logo />
-      </Link>
       <div>
-        <div className="header-title">Tytuł nagłówka</div>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+      <div className="menu">
+        <button
+          className="settings"
+          onClick={() => {
+            console.log('button');
+          }}
+        >
+          <img src={cogwheel} alt="" />
+        </button>
+
+        <div className="profile-picture">
+          <div className="placeholder" />
+        </div>
+        <div className="user">
+          <h3>Dominik</h3>
+          <p>Kursant</p>
+        </div>
+        <img src={strzalka} alt="" />
       </div>
     </header>
   );
