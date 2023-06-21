ac53d85adfbd024e24e4bdc7a65ae8d468ff112a
karolinagelinska <karolina@Assus>
2023-06-21 21:30:20 +0200

    dashboard-layout-changing

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,12 +1,59 @@
+import { useState } from 'react';
+import { DashboardCard } from '../Components/DashboardCard/DashboardCard';
 import './styles.css';
+import personalCard from '../Images/tiles/personalcard.svg';
 
 export const Dashboard = () => {
+  const [availableCards, setAvailableCards] = useState([
+    {
+      sectionTitle: 'Moje CV',
+      icon: <img src={personalCard} alt="buisness card resume" />,
+      description: 'podgląd cv wraz z doświadczeniem',
+      link: '/cv',
+    },
+    {
+      sectionTitle: ' Ćwiczenia',
+      icon: <img src={personalCard} alt="business card resume" />,
+      description: 'wszystkie wykonane ćwiczenia',
+      link: '/excercise',
+    },
+    {
+      sectionTitle: 'Blog',
+      icon: <img src={personalCard} alt="business card resume" />,
+      description: 'wpisy blogowe o technologii front-end',
+      link: '/blog',
+    },
+    {
+      sectionTitle: 'Tech stack',
+      icon: <img src={personalCard} alt="business card resume" />,
+      description: 'stack technologiczny realizowany na kursie',
+      link: '/tech',
+    },
+    {
+      sectionTitle: 'FAQ',
+      icon: <img src={personalCard} alt="business card resume" />,
+      description: 'odpowiedzi na najczęstsze pytania',
+      link: '/faq',
+    },
+  ]);
   return (
     <div className="dashboard">
       <h1>Dashboard</h1>
       <p>
         Strona główna kursanta (o sobie, zdjęcie, opis narzędzi wykorzystywanych
         na kursie oraz umejętności zdobytych na kursie)
+        <div className="dashboard-card-wrapper">
+          {availableCards.map((card) => {
+            return (
+              <DashboardCard
+                sectionTitle={card.sectionTitle}
+                description={card.description}
+                link={card.link}
+                icon={card.icon}
+              />
+            );
+          })}
+        </div>
       </p>
     </div>
   );
--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -4,15 +4,41 @@ import './styles/header.css';
 
 import { Logo } from '../Components/Logo/Logo';
 import { Link } from 'react-router-dom';
+import arrowDown from '../Images/toggle-arrow.svg';
+import cogwheel from '../Images/setting.svg';
 
 export function AppHeader() {
   return (
-    <header>
+    <header className="logo">
       <Link to="/">
         <Logo />
       </Link>
-      <div>
-        <div className="header-title">Tytuł nagłówka</div>
+      <div className="menu">
+        <button
+          className="settings"
+          onClick={() => {
+            console.log('Clicked-cogwheel');
+          }}
+        >
+          <img src={cogwheel} />
+        </button>
+        <div className="user-menu">
+          <div className="profile-picture">
+            <div className="placeholder"></div>
+          </div>
+          <div className="user-details">
+            <p className="name">Imię</p>
+            <p className="position">kursant</p>
+          </div>
+          <button
+            className="toggle-arrow"
+            onClick={() => {
+              console.log('Clicked the arrow');
+            }}
+          >
+            <img src={arrowDown} />
+          </button>
+        </div>
       </div>
     </header>
   );
