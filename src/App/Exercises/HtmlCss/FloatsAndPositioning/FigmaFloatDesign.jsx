05d30ae2930481a58c094d51a9a8007a65c4cff5
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-06-23 21:28:25 +0200

    work with floatsandcookies

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioning/FigmaFloatDesign.jsx
@@ -0,0 +1,35 @@
+import './styles.css';
+import React from "react";
+import pic from './Frame 2755.png';
+
+const FigmaFloatDesign = () => {
+  return (
+    <div className='floatcontainer'>
+      &lt; BLOG &lt; ĆWICZENIE
+      <p className="first">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </p>
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh mauris cursus mattis molestiea iaculis at. Dictum varius duis at consectetur lorem donec. 
+      </p>
+      <p>
+        <img src={pic}></img>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh mauris cursus mattis molestiea iaculis at. Dictum varius duis at consectetur lorem donec. 
+      </p>
+      <button>Zapisz na pózniej</button>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh mauris cursus mattis molestie a iaculis at. Dictum varius duis at consectetur lorem donec. Sit amet dictum sit amet justo. Fringilla phasellus faucibus scelerisque eleifend donec. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Arcu dictum varius duis at consectetur lorem. Massa ultricies mi quis hendrerit. 
+
+      Leo vel orci porta non pulvinar neque laoreet. In egestas erat imperdiet sed euismod nisi porta lorem. Ut porttitor leo a diam sollicitudin tempor id. Tortor at auctor urna nunc id. Gravida quis blandit turpis cursus in. Ornare quam viverra orci sagittis eu. Metus dictum at tempor commodo ullamcorper a. Vestibulum mattis ullamcorper velit sed ullamcorper morbi. Amet massa vitae tortor condimentum lacinia quis. Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Auctor elit sed vulputate mi.
+      </p>
+    </div>
+  );
+};
+export default FigmaFloatDesign;
+
---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioning/router-data.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FigmaFloatDesign from './FigmaFloatDesign';
+
+export const FloatsMetaData = {
+  path: 'float-and-positioning',
+  date: '22.06.2023',
+  linkLabel: 'Pozycje i opływy',
+  blockNo: 16,
+  element: <FigmaFloatDesign />,
+  tags: ['z-index', 'positioning', 'css', 'float'],
+};
