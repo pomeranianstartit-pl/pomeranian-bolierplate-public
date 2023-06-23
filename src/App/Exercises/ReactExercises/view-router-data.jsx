4c3bb8518217c04218fc432d8118dee771d52f96
karolinagelinska <karolina@Assus>
2023-06-23 23:18:27 +0200

    mui-exercises

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,6 +3,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
 import { Block9MetaData } from './Block9/router-data';
+import { MaterialUIMetaData } from './MaterialUIBasicElements/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData, Block9MetaData];
+export const blockRouterMetaData = [SubRouteExampleMetaData, Block9MetaData, MaterialUIMetaData];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
