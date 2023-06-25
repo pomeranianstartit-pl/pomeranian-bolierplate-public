e2d9a9d75c0e33c5e909a197af7e002788ba0e9f
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-06-25 21:02:50 +0200

    cookies i blog

--- src/App/Components/Cookies/Cookies.jsx
+++ src/App/Components/Cookies/Cookies.jsx
@@ -1,34 +1,49 @@
-import React from 'react';
-import './style.css';
-import cookie from '../../Images/cookie.svg';
+import React, { useState } from 'react';
+import './styles.css';
+import cookies from '../../Images/cookie.svg';
 
 const Cookies = () => {
-  const handleAgreement = () => {};
+  const [showCookies, setCookies] = useState(true);
+  const [rejectCookies, setIsHidden] = useState(false);
+  const handleAgreement = () => {
+    setIsHidden(true);
+    setTimeout(() => {
+      setCookies(false);
+    },1000);
+  };
   const handleAgreementCustomization = () => {};
 
   return (
-    <div className="cookies-agreement">
-      <div className="text-wrapper">
-        <img className="cookies-icon" src={cookie} alt="cookies icon" />
-        <p>
-          Pozwól na pliki cookies Nasza strona korzysta z ciasteczek, które
-          umożliwiają poprawne działanie strony i pomagają nam świadczyć usługi
-          na najwyższym poziomie. Możesz zaakceptować wykorzystanie przez nas
-          wszystkich tych plików i przejść do strony lub dostosować użycie do
-          swoich referencji. W tym celu kliknij przycisk po prawej stronie
-          “dopasuj zgody”, aby następnie wybrać te które odpowiadają Twoim
-          indywidualnym preferencjom.
-        </p>
-      </div>
-      <div className="button-wrapper">
-        <button className="agree" onClick={handleAgreement}>
-          w porządku
-        </button>
-        <button className="customize" onClick={handleAgreementCustomization}>
-          Dopasuj zgody
-        </button>
-      </div>
-    </div>
+    <>
+      {showCookies && (
+        <div className={`cookies-agreements ${rejectCookies ? 'hidden' : ''}`}>
+          <div className="text-cookie">
+            <img src={cookies} alt="" />
+            <div>
+              <h3>Pozwól na pliki cookies </h3>
+              Nasza strona korzysta z ciasteczek, które umożliwiają poprawne
+              działanie strony i pomagają nam świadczyć usługi na najwyższym
+              poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich
+              tych plików i przejść do strony lub dostosować użycie do swoich
+              referencji. W tym celu kliknij przycisk po prawej stronie “dopasuj
+              zgody”, aby następnie wybrać te które odpowiadają Twoim
+              indywidualnym preferencjom.
+            </div>
+          </div>
+          <div className="button-wrapper">
+            <button className="agree" onClick={handleAgreement}>
+              W porządku
+            </button>
+            <button
+              className="customize"
+              classonClick={handleAgreementCustomization}
+            >
+              Dopasuj zgody
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
