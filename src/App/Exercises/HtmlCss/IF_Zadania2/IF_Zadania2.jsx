80744244ee2d0c89de1f50d3fbb0ce013cf5c5ea
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-06-26 21:57:10 +0200

    liczby, warunki

---
+++ src/App/Exercises/HtmlCss/IF_Zadania2/IF_Zadania2.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+export function IF_Zadania2() {
+  const [age, setAge] = useState(18);
+  const [hasDriverLicense, setHasDriverLicense] = useState(true);
+  const [hasCar, setHasCar] = useState(true);
+  const [result, setResult] = useState('');
+
+  function canDrive(age, hasDriverLicense, hasCar) {
+    if (age < 18) {
+      return "Jesteś za młody, zeby prowadzić";
+    } else if (!hasDriverLicense) {
+      return "Nie mozesz prowadzic bez prawa jazdy";
+    } else if (!hasCar) {
+      return "Mozesz prowadzić wynajęty samochód";
+    } else {
+      return "Mozesz prowadzic swój samochód!";
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const drivingResult = canDrive(age, hasDriverLicense, hasCar);
+    setResult(drivingResult);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p className='no1'>Twoj Wiek:
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(Number(e.target.value))}
+          /></p>
+        </label>
+        <br />
+        <label>
+          <p className='no1'>Czy posiadasz prawo jazdy?
+          <input
+            type="checkbox"
+            checked={hasDriverLicense}
+            onChange={(e) => setHasDriverLicense(e.target.checked)}
+          /></p>
+        </label>
+        <br />
+        <label>
+          <p className='no1'>Czy posiadasz samochód?
+          <input
+            type="checkbox"
+            checked={hasCar}
+            onChange={(e) => setHasCar(e.target.checked)}
+          /></p>
+        </label>
+        <br />
+        <button type="submit"><p className='no2'>Sprawdź, czy mozesz prowadzić</p></button>
+      </form>
+      <p className='no1'>{result}</p>
+    </div>
+  );
+}
+
+
+
