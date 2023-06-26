0887115802db142a114e195e969c7523d4a1f0a2
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-06-26 20:18:10 +0200

    Working with numbers cd

--- src/App/Exercises/Js/Exercise-numbers-if/index.jsx
+++ src/App/Exercises/Js/Exercise-numbers-if/index.jsx
@@ -1,4 +1,6 @@
 // import './styles.css';
+import { IfStatements } from "./IfStatements";
+
 
 export function Exercise() {
   let a = 5;
@@ -22,9 +24,56 @@ export function Exercise() {
 
 const result1 = a > b;
 const result2 = a < b;
+const result3 = a >= b;
+const result4 = a <= b;
+const result5 = a === b;
 
 
-console.log('result', result2);
+
+console.log('result', result2, result3);
+
+// IF -----------------------------------------------------
+
+let c = 'a123b';
+
+console.log(isNaN(c), 'isNan?')
+
+const parseINT = parseInt('5')
+const parseINT_1 = parseInt(5.5)
+// nie zadziała dla : null, underfined...
+const parseINT_2 = parseInt('a123b')
+
+console.log(parseINT, 'parseINT')
+console.log(parseINT_1, 'parseINT_1')
+console.log(parseINT_2, 'parseINT_2')
+
+console.log(Number.isInteger(a), 'isInteger()')
+// const d = Number(15);
+// d.... jakaś metoda
+
+// zaokrąglanie
+console.log(Math.round(5,4), 'Math.round()')
+console.log(Math.round('5,4'), 'Math.round()')
+
+// Math.ceil -----------------------------------------
+
+console.log(Math.ceil(.95));
+// Expected output: 1
+
+// Math.floor ---------------------------------------
+console.log(Math.flor(4.6), 'Math.floor()')
+
+// MAth.sqrt --------------------------------------
+console.log(Math.sqrt(8), 'Math.sqrt()')
+
+// MAth.pow --------------------------------------
+console.log(Math.pow(8,3), 'Math.pow()')
+
+// MAth.random --------------------------------------
+
+console.log(Math.random())
+
+
 
   return (
     <>
@@ -34,6 +83,7 @@ console.log('result', result2);
     <div>divideResult {divideResult}</div>
     <div>moduloResult {moduloResult}</div>
     <div>powerResult {powerResult}</div>
+    <IfStatements />
 
     </>
   );
