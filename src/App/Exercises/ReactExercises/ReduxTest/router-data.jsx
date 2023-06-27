ab79446b6749e8c69e26da784da038f24710736b
Marhw <marhw89@gmail.com>
2023-06-28 01:11:06 +0200

    redux

---
+++ src/App/Exercises/ReactExercises/ReduxTest/router-data.jsx
@@ -0,0 +1,10 @@
+import { ReduxTest } from './ReduxTest';
+
+export const ReduxTestExample = {
+  path: 'react-redux-test',
+  date: '12-12-2023',
+  linkLabel: 'Redux test',
+  blockNo: 1,
+  element: <ReduxTest />,
+  tags: ['js', 'javascript', 'reducers', 'actions', 'react', 'redux'],
+};
