61ff04a6f616f9402c9a4e9cf3b2370333537d6f
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-06-27 17:01:06 +0200

    ExerciseWithJS

--- src/App/Exercises/Js/Exercise-numbers-if/IfStatements.jsx
+++ src/App/Exercises/Js/Exercise-numbers-if/IfStatements.jsx
@@ -1,17 +1,20 @@
 export function IfStatements () {
     const iAmMen = true;
-    const a = 11
-
-    // if (iAmMen) {
-    //     console.log('You are not a men!')
+    const a = 11;
+ 
+    // if (!iAmMen) {
+    //     console.log('You are not a men!!!')
     // }
-
+ 
+    // if else statement
     if (iAmMen) {
         console.log('Youre men')
     } else {
         console.log('Youre not a men')
     }
-
+ 
+    // if (a < 10 || isMen) {
+    // if (a < 10 && isMen) {
     if (a < 10) {
         console.log('a < 10')
     } else if (a > 10) {
@@ -19,12 +22,11 @@ export function IfStatements () {
     } else {
         console.log('a = 10')
     }
-
-    // Switch statement ----------------------------
-
+ 
+    // switch statement
     const exp = 'Pink'
     switch (exp) {
-        case 'Czerwony':
+        case 'Red':
             console.log('Kolor jest czerwony')
         case 'Blue':
             console.log('Kolor jest niebieski')
@@ -32,36 +34,35 @@ export function IfStatements () {
             console.log('Kolor jest zielony')
             break;
         default:
-            console.log('Nie ma')
+            console.log('Przykro mi nie mamy takiego koloru w bazie')
     }
+ 
+    // conditional (ternary) operator
+    const conditionalIf = a > 10 ? console.log('Większe od 10') : console.log('Mniejsze lub równe 10')
+ 
+    const extraConditionalIf = a > 10 ? console.log('Większe od 10') : console.log('Mniejsze lub równe 10')
 
-    //  conditional if statement -----------------
-
-    const conditionalIf = a > 10 ? console.log('Większe od 10'): console.log('Mniejsze lub równe 10')
-    const extraConditionalIf = a > 10 ? console.log('Większe od 10'): console.log('Mniejsze lub równe 10')
-
-    //  function example() {
-    //     return (condition1 ? value1 : condition2) ? (value2 
-    //         : condiition3) ? value3 
-    //         : value4;
-    // }
-
+ 
+    // function example() {
+    //     return (condition1 ? value1 : condition2) ? (value2
+    //           : condition3) ? value3
+    //           : value4;
+    //   }
+ 
     // const result1 = iAmMen && console.log('Youre men');
     // const result2 = !iAmMen && console.log('Youre a women');
 
-    const result1 = iAmMen && 'Youre men';
-    const result2 = !iAmMen && 'Youre women';
 
+ 
+    const result1 = iAmMen && 'Youre man';
+    const result2 = !iAmMen && 'Youre women';
+   
+ 
     return <div>
         {result1 === 'Youre man' && <div>Jesteś mężczyzną</div>}
         {result2 === 'Youre women' && <div>Jesteś kobietą</div>}
-
+ 
         {iAmMen && <div>Jesteś mężczyzną</div>}
         {!iAmMen && <div>Jesteś kobietą</div>}
     </div>
-
-
-
-
-    return <div></div>
 }
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { routerMetaData as blockRouterMetaDataExercisesNumberIf } from './Exercise-numbers-if/router-data';
+import { routerMetaData as blockRouterMetaDataJsNumbersTraining } from './JsNumbersTraining/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataExercisesNumberIf,
+  blockRouterMetaDataJsNumbersTraining,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
