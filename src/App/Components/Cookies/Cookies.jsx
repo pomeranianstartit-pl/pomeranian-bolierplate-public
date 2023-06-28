22d81247fd066c5e47a13529755b7f062770957e
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-06-28 17:01:46 +0200

    Cookie fight

--- src/App/Components/Cookies/Cookies.jsx
+++ src/App/Components/Cookies/Cookies.jsx
@@ -2,20 +2,31 @@ import React from 'react';
 import './style.css';
 import cookie from '../../Images/cookie.svg';
 
-let cookieState = '';
-
-function setCookieState() {
-  localStorage.setItem('cookieState', 1);
-  console.log('Wywolalo CookieState');
-}
-
-function getCookieState() {
-  return localStorage.getItem('cookieState');
-}
-if ((getCookieState.cookieState = 1)) {
-  console.log(cookieState);
-}
 const Cookies = () => {
+  let isCookie;
+
+  const getCookieState = () => {
+    const cookieState = localStorage.getItem('cookieState');
+
+    cookieState === '1' ? (isCookie = true) : (isCookie = false);
+
+    // if (cookieState === '1') {
+    //   console.log('getCookieState()', cookieState);
+    //   isCookie = true;
+    //   console.log('isCookie', isCookie);
+    // } else {
+    //   isCookie = false;
+    //   console.log('isCookie', isCookie);
+    // }
+  };
+
+  getCookieState();
+
+  const setCookieState = () => {
+    localStorage.setItem('cookieState', 1);
+    console.log('Wywolalo CookieState');
+  };
+
   const handleAgreement = () => {
     console.log('Agreement Clicked');
     setCookieState();
@@ -25,27 +36,34 @@ const Cookies = () => {
   };
 
   return (
-    <div className="cookies-agreement">
-      {/* <div className="text-wrapper">
-        <img className="cookies-icon" src={cookie} alt="cookies icon" />
-        <p>
-          Pozwól na pliki cookies Nasza strona korzysta z ciasteczek, które
-          umożliwiają poprawne działanie strony i pomagają nam świadczyć usługi
-          na najwyższym poziomie. Możesz zaakceptować wykorzystanie przez nas
-          wszystkich tych plików i przejść do strony lub dostosować użycie do
-          swoich referencji. W tym celu kliknij przycisk po prawej stronie
-          “dopasuj zgody”, aby następnie wybrać te które odpowiadają Twoim
-          indywidualnym preferencjom.
-        </p>
-      </div>
-      <div className="button-wrapper">
-        <button className="agree" onClick={handleAgreement}>
-          w porządku
-        </button>
-        <button className="customize" onClick={handleAgreementCustomization}>
-          Dopasuj zgody
-        </button>
-      </div> */}
+    <div>
+      {!isCookie && (
+        <div className="cookies-agreement">
+          <div className="text-wrapper">
+            <img className="cookies-icon" src={cookie} alt="cookies icon" />
+            <p>
+              Pozwól na pliki cookies Nasza strona korzysta z ciasteczek, które
+              umożliwiają poprawne działanie strony i pomagają nam świadczyć
+              usługi na najwyższym poziomie. Możesz zaakceptować wykorzystanie
+              przez nas wszystkich tych plików i przejść do strony lub
+              dostosować użycie do swoich referencji. W tym celu kliknij
+              przycisk po prawej stronie “dopasuj zgody”, aby następnie wybrać
+              te które odpowiadają Twoim indywidualnym preferencjom.
+            </p>
+          </div>
+          <div className="button-wrapper">
+            <button className="agree" onClick={handleAgreement}>
+              w porządku
+            </button>
+            <button
+              className="customize"
+              onClick={handleAgreementCustomization}
+            >
+              Dopasuj zgody
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
