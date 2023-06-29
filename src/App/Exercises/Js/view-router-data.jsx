7bc903eeda4a5eb116992849b21b8429a2ea5d0d
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-06-29 21:05:53 +0200

    arrays exercises

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as emptyValuesComments } from './EmptyValuesComments/router-data';
+import { blockRouterMetaData as JsArrayBasics } from './JsArrayBasics/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   emptyValuesComments,
+  JsArrayBasics,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
