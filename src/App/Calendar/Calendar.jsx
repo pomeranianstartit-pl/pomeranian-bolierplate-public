a124d3a744b0f12fc334c55a9b2eec97d1903d7e
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-07-01 01:06:50 +0200

    calendar

--- src/App/Calendar/Calendar.jsx
+++ src/App/Calendar/Calendar.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import './styles.css';
+import Calendar1 from './CalendarComponent';
+
 
 export const Calendar = () => {
-  return <div>Calendar - todo</div>;
+  return (
+    <div>
+      <h1>My Calendar</h1>
+      <Calendar1 />
+    </div>
+  );
 };
+
