0127ae65491e3f5c39fd978399f8ac07da71fe1d
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-06-30 21:04:06 +0200

    add sum of parametr function

--- src/App/Exercises/Js/ExerciseJsFunction/ExerciseJsFunction.jsx
+++ src/App/Exercises/Js/ExerciseJsFunction/ExerciseJsFunction.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 import './style.css';
 
 export function ExerciseJsFunction() {
-  const originalArr = [1, 3, 2, 100, 99, -1];
+  const originalArr = [1, 3, 2, 100, 99];
   const sortImmute = (originalArr) => {
     const sortedArr = [...originalArr];
-    //sortowanie za pomocą a oraz b ponieważ przy wartości 100 londowała na 2 mijscu jako js traktuje cyfry w tym momencie jako str
     return sortedArr.sort((a, b) => a - b);
   };
+
+  const sumArr = (...arr) => {
+    let sumAnswer = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'number') sumAnswer += arr[i];
+      else return 'coś poszło nie tak';
+  };
+  return sumAnswer;
+};
+
   return (
     <div>
       <p>oryginalna tablica: {originalArr.join(', ')}</p>
@@ -16,6 +25,8 @@ export function ExerciseJsFunction() {
       <p>
         oryginalna tablica po wywołaniu sortowania tablica:{' '}
         {originalArr.join(', ')}
+        <hr />
+        <p>{sumArr(1, 2, 3, 4, 5, )}</p>
       </p>
     </div>
   );
