606559953bc6588075b22b29cf6fedc0ebf0a527
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-06-30 21:18:55 +0200

    EmptyValuesAndComments

--- src/App/Exercises/Js/EmptyValuesAndComments/EmptyValuesAndComments_component.jsx
+++ src/App/Exercises/Js/EmptyValuesAndComments/EmptyValuesAndComments_component.jsx
@@ -1,13 +1,25 @@
-import './styles.css';
+import React, { useState } from 'react';
 
 export const EmptyValuesAndCommentsComponent = () => {
-  
-  const array1 = [{name: "Łukasz"}, {name: "Adam"}];const sumWithInitial = array1.reduce(  (accumulator, currentValue) => [...accumulator, currentValue.name],  []);console.log(sumWithInitial);
+  const array1 = [{ name: "Łukasz" }, { name: "Adam" }, { name: "Amadeusz" }];
+
+  const sumWithInitial = array1.reduce(
+    (accumulator, currentValue) => [...accumulator, currentValue.name],
+    []
+  );
+
+  const [result, setResult] = useState(sumWithInitial);
 
   return (
     <div>
-
+      <h1>Wynik:</h1>
+      <ul>
+        {result.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
 
