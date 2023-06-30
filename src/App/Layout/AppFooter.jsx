c9357df4192337ff7e23b16b631c0c7b66140bf5
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-07-01 00:40:36 +0200

    dashboard

--- src/App/Layout/AppFooter.jsx
+++ src/App/Layout/AppFooter.jsx
@@ -10,7 +10,7 @@ export function AppFooter() {
         Projekt uzyskał dofinansowanie ze środków Unii Europejskiej z
         Europejskiego Funduszu Rozwoju
       </div>
-      <a href={`mailto:${email}`}>{`Email: ${email}`}</a>
+      <a href={`mailto:${email}`}>{`${email}`}</a>
       <div>
         Regionalnego w ramach projektu grantowego „Invest in Pomerania 2020”.
       </div>
