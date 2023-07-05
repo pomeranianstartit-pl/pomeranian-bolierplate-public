8f6d58f19ad8c67bc51fa1ec484824f597b88e56
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-07-05 22:36:46 +0200

    HitTheMoleGame

--- src/App/Exercises/Js/HitTheMoleGame/router-data.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/router-data.jsx
@@ -2,7 +2,7 @@ import { Mole } from './HitTheMoleGame'
 
 export const HitTheMoleGameMetaData = {
   path: 'HitTheMoleGame',
-  date: '04.07.2023',
+  date: '05.07.2023',
   linkLabel: 'Hit The Mole Game',
   blockNo: 6,
   element: <Mole />,
