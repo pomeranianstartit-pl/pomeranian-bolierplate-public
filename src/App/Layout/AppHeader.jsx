43944b82d25746ac73646f6320106ee353f7ec7d
Anna Ochocka <annoch22@gmail.com>
2023-07-05 19:31:01 +0200

    Dodanie Logo

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './styles/header.css';
 
+import { Logo } from '../Components/Logo/Logo';
+
 export function AppHeader() {
   return (
     <header>
       <div>
+        {/* <Logo></Logo> */}
+        <Logo />
         <div className="header-title">Witaj nagłówku</div>
       </div>
     </header>
