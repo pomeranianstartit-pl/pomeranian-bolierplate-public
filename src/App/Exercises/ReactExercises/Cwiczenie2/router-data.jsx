790afc577e08c5f0d54952be7adf8270ef0eda84
Anna Ochocka <annoch22@gmail.com>
2023-07-06 20:44:02 +0200

    Cwiczenie2button

--- src/App/Exercises/ReactExercises/Cwiczenie2/router-data.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie2/router-data.jsx
@@ -1,9 +1,9 @@
 import { Cwiczenie2 } from './Cwiczenie2';
 
 export const Cwiczenie2MetaData = {
-  path: 'example-sub-route',
-  date: '12-12-2023',
-  linkLabel: 'Przykładowy sub-route',
+  path: 'cwiczenie-2',
+  date: '06-07-2023',
+  linkLabel: 'Ćwiczenie 2',
   blockNo: 1,
   element: <Cwiczenie2 />,
   tags: ['components', 'import', 'export'],
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,15 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
+import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
+
 import { Cwiczenie1MetaData } from './Cwiczenie1/router-data';
 
-export const blockRouterMetaData = [Cwiczenie1MetaData];
+import { Cwiczenie2MetaData } from './Cwiczenie2/router-data';
+
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  Cwiczenie1MetaData,
+  Cwiczenie2MetaData,
+];
+
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
