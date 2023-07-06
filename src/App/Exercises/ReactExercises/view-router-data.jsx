218c2e53f3d7d4476a0286caf198d7ae1d4ff37c
Iwona Bigos <iwonkabigos@gmail.com>
2023-07-06 19:01:02 +0200

    dodanie zdarzenia submit

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,9 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+import { Cwiczenie1MetaData } from './Cwiczenie1/router-data';
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  Cwiczenie1MetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
