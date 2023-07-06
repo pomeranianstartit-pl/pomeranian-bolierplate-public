cfdb74286359b0fabd355514c7d4c2030a5ea31d
Magda Derkacz <magdalena.derkacz98@gmail.com>
2023-07-06 16:59:28 +0200

    Zmiany 06.07

---
+++ src/App/Components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import {ReactComponent as LogoSVG } from '../../Images/start-it-logo.svg';
+
+export const Logo = () => {
+    return <LogoSVG />;
+};
+
--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './styles/header.css';
 
+import { Logo } from '../Components/Logo/Logo';
+
 export function AppHeader() {
   return (
     <header>
       <div>
-        <div className="header-title">Tytuł nagłówka</div>
+        <Logo />
+        {/* <Logo></Logo> */}
+        <div className="header-title">Witaj nagłówku!</div>
       </div>
     </header>
   );
