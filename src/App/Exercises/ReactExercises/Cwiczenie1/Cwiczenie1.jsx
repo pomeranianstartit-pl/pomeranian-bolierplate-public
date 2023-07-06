bfb3e5ced4a47a5ecf32834df103ae503bec3d5a
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-06 21:13:34 +0200

    ZAKOŃCZONE 2 ZADANIE

--- src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
@@ -3,25 +3,31 @@ import React from 'react';
 import './style.css';
 
 export function Cwiczenie1() {
-  const [tekst, setTekst] = React.useState('Placeholder');
+  const [tekst, setTekst] = React.useState('Tutaj trafi tekst z inputa');
 
   function handleOnSubmit(event) {
     event.preventDefault();
+
     console.log(event);
 
     const trescInputa = event.target[0].value;
+
+    console.log('Treść inputa: ', trescInputa);
+
     setTekst(trescInputa);
-    /*console.log('Treść inputa: ', trescInputa);*/
   }
 
   return (
     <div className="blok-8__cwiczenie-1">
       <h1>Cześć!</h1>
+
       <div>
         <div>
           <p>Wpisz tekst</p>
+
           <form onSubmit={handleOnSubmit}>
-            <input type="text" />
+            <input type="text" placeholder="tekst do wpisania" />
+
             <button type="submit">KLIKNIJ</button>
           </form>
         </div>
