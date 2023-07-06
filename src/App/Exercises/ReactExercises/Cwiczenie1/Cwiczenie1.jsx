f28e7cca13a7764239cac399664af57e36b0d30c
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-06 19:15:19 +0200

    workingbutton update

--- src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
@@ -3,12 +3,19 @@ import React from 'react';
 import './style.css';
 
 export function Cwiczenie1() {
+  const [tekst, setTekst] = React.useState('TEkst domyślny');
   function handleOnSubmit(event) {
     event.preventDefault();
 
     console.log('Kliknięto przycisk');
 
     console.log(event);
+
+    const trescInputa = event.target[0].value;
+
+    console.log('tresc inputa: ', trescInputa);
+
+    setTekst(trescInputa);
   }
 
   return (
@@ -27,7 +34,7 @@ export function Cwiczenie1() {
         </div>
 
         <div>
-          <p>Placeholder</p>
+          <p>{tekst}</p>
         </div>
       </div>
     </div>
