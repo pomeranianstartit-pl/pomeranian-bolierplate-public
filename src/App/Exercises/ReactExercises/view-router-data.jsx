bcebc3b2cd6b221b7e6de7745d9a95ac63ff5429
Magda Derkacz <magdalena.derkacz98@gmail.com>
2023-07-07 19:07:54 +0200

    Ćwiczenie z liczeniem do 5

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Cwiczenie1MetaData } from './Cwiczenie1/router-data';
 import { Cwiczenie2MetaData } from './Cwiczenie2/router-data';
+import { routerMetaData as IfStatementsMetaData } from './IfStatements/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Cwiczenie1MetaData,
   Cwiczenie2MetaData,
+  IfStatementsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
