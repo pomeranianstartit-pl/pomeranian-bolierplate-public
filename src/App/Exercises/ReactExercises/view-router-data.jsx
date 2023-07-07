1de7534fd7aa0fc08bce2205525d234d37877fcd
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-07 21:44:20 +0200

    added more or less functionality

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,11 +2,18 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Cwiczenie1MetaData } from './Cwiczenie1/router-data';
+
 import { Cwiczenie2MetaData } from './Cwiczenie2/router-data';
+import { routerMetaData as IfStatementsMetaData } from './IfStatements/router-data';
+import { routerMetaData as IfStatementsMoreOrLess } from './IfStatementsMoreOrLess/router-data';
+import { routerMetaData as IfStatementsMoreOrLessGame } from './IfStatementsMoreOrLessGame/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Cwiczenie1MetaData,
   Cwiczenie2MetaData,
+  IfStatementsMetaData,
+  IfStatementsMoreOrLess,
+  IfStatementsMoreOrLessGame,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
