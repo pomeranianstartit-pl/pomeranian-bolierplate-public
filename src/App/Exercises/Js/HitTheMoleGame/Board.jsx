816d1fc5a5d43d6be927779e73bea75843ee33ed
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-07 21:07:18 +0200

    Add new Mole Game features

--- src/App/Exercises/Js/HitTheMoleGame/Board.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/Board.jsx
@@ -4,16 +4,18 @@ import Mole from '../../../Images/Mole.svg';
 export const MoleGameBoard = ({ scoreCount, moleArray, hitTheMole }) => {
   return (
     <div className="moleGame">
-      Obecny wynik to: {scoreCount}
-      {moleArray.map((mole, index) => (
-        <div style={{ width: '100px', height: '100px', gap: '5px' }}>
-          <span>
-            {mole.isVisible ? (
-              <img src={Mole} id="Mole!" onClick={() => hitTheMole(index)} />
-            ) : null}
-          </span>
-        </div>
-      ))}
+      <h4 className="board-score">WYNIK: {scoreCount}</h4>
+      <div className="board">
+        {moleArray.map((mole, index) => (
+          <div className="square">
+            <span>
+              {mole.isVisible ? (
+                <img src={Mole} id="Mole!" onClick={() => hitTheMole(index)} />
+              ) : null}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -5,6 +5,7 @@ import MoleGameBoard from './Board';
 
 export const HitTheMoleGame = () => {
   const defaultGameTime = 2 * 60 * 1000;
+  const moleSpeed = 1000;
   const [moleCount, setMoleCount] = useState(1);
   const [gameTime, setGameTime] = useState(defaultGameTime / 1000);
   const [seconds, setSeconds] = useState(gameTime / 1000);
@@ -20,13 +21,35 @@ export const HitTheMoleGame = () => {
       intervalId = setInterval(() => {
         setSeconds(seconds - 1);
       }, 1000);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [gameTime]);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (!intervalId) {
+      intervalId = setInterval(() => {
+        showRandomMole();
+      }, moleSpeed);
+
       return () => clearInterval(intervalId);
     }
   }, [gameTime]);
 
   function hitTheMole(index) {
-    if (!moleArray[index].isVisible) return;
-    moleArray[index].isWhacked = !moleArray[index].isWhacked;
+    if (moleArray[index].isVisible) {
+      setScoreCount(scoreCount + 1);
+
+      setMoleArray((prevVal) => {
+        const newArray = [...prevVal];
+
+        newArray[index].isVisible = false;
+
+        return newArray;
+      });
+    }
   }
 
   function showRandomMole() {
@@ -62,7 +85,6 @@ export const HitTheMoleGame = () => {
         hitTheMole={hitTheMole}
         scoreCount={scoreCount}
       />
-      <button onClick={() => showRandomMole()}>click</button>
     </>
   );
 };
