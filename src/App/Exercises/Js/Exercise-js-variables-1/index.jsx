558baf7952335cc23d9286a5949eae37cfe24207
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-08 12:15:33 +0200

    exercise-variables-1

--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -7,6 +7,12 @@ export function Exercise() {
 
   const string2 = `Ulubiony bohater z serialu "Gra o tron" to: ${string1}`;
 
+  let name = 'Marta';
+
+  const string4 = `Witaj ${name}!`;
+
+  let visitors = '5';
+
   return (
     <div className="playing-with-variables">
       <section>
@@ -19,7 +25,9 @@ export function Exercise() {
         <p>{string2}</p>
         <p>{string3}</p>
         {/* Uzupełnij zanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>Witaj ____! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
+        <p>
+          {string4} Jesteś {visitors} użytkownikiem odwiedzającym tę stronę!
+        </p>
       </section>
     </div>
   );
