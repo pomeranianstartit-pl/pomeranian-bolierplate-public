3372f8562f986db6e57f22406fac983719ca8517
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-08 12:15:19 +0200

    cwiczenie zmienne 1

--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -5,21 +5,24 @@ export function Exercise() {
 
   const string3 = 'Ulubiony bohater z serialu "Gra o tron" to ' + string1;
 
-  const string2 = `Ulubiony bohater z serialu "Gra o tron" to: ${string1}`;
+  const string2 = `Ulubiony bohater z serialu "Gra o tron" to ${string1}`;
 
+  let name = "Yarek";
+  const string4 = `Witaj ${name}!`;
+
+  const newstring = "nowym";
   return (
     <div className="playing-with-variables">
       <section>
         <h2>Operacje na stringach</h2>
         <p>Ulubiony bohater z serialu "Gra o tron" to {string1}</p>
-        <p>
-          Ulubiony bohater z serialu "Gra o tron" to
+        <p>Ulubiony bohater z serialu "Gra o tron" to
           <span className="variable-string-placeholder">{string1}</span>
         </p>
         <p>{string2}</p>
         <p>{string3}</p>
-        {/* Uzupełnij zanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>Witaj ____! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
+        {/* Uzupełnij zdanie korzystając ze zmiennych oraz operacji na stringach */}
+        <p>{string4} Jesteś <b>{newstring}</b> użytkownikiem odwiedzającym tę stronę!</p>
       </section>
     </div>
   );
