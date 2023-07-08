89cbab1dae72691a807b74c386dc9f069d8d0ec8
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-07-08 12:15:27 +0200

    exercise 1 added

--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -1,11 +1,15 @@
 import './styles.css';
 
 export function Exercise() {
-  let string1 = 'Jon Snow';
+  let string1 = 'Eddie the Eagle';
 
   const string3 = 'Ulubiony bohater z serialu "Gra o tron" to ' + string1;
 
-  const string2 = `Ulubiony bohater z serialu "Gra o tron" to: ${string1}`;
+  const string2 = `Ulubiony bohater z serialu "Gra o tron" to ${string1}`;
+
+
+  let name = 'Stefano';
+  const string4 = `Witaj ${name}`
 
   return (
     <div className="playing-with-variables">
@@ -19,7 +23,7 @@ export function Exercise() {
         <p>{string2}</p>
         <p>{string3}</p>
         {/* Uzupełnij zanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>Witaj ____! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
+        <p>{string4}! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
       </section>
     </div>
   );
