cc2b03367069272a7ac4d9eac9ea2536aedc222d
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-08 14:32:55 +0200

    logo

--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -7,22 +7,26 @@ export function Exercise() {
 
   const string2 = `Ulubiony bohater z serialu "Gra o tron" to ${string1}`;
 
-  let name = "Yarek";
+  let name = 'Yarek';
   const string4 = `Witaj ${name}!`;
 
-  const newstring = "nowym";
+  const newstring = 'nowym';
   return (
     <div className="playing-with-variables">
       <section>
         <h2>Operacje na stringach</h2>
         <p>Ulubiony bohater z serialu "Gra o tron" to {string1}</p>
-        <p>Ulubiony bohater z serialu "Gra o tron" to
+        <p>
+          Ulubiony bohater z serialu "Gra o tron" to
           <span className="variable-string-placeholder">{string1}</span>
         </p>
         <p>{string2}</p>
         <p>{string3}</p>
         {/* Uzupełnij zdanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>{string4} Jesteś <b>{newstring}</b> użytkownikiem odwiedzającym tę stronę!</p>
+        <p>
+          {string4} Jesteś <b>{newstring}</b> użytkownikiem odwiedzającym tę
+          stronę!
+        </p>
       </section>
     </div>
   );
