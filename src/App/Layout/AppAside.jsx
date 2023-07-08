ffb6f4faa1a298e271ed94dc04fbad2c04eef304
mikolaj mrozek <mikolaj.mrozek399@gmail.com>
2023-07-08 15:59:14 +0200

    Dodanie ikon do interfacu

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,5 +1,9 @@
 import { NavLink } from 'react-router-dom';
-
+import { EditIcon } from '../Components/Icons/EditIcon';
+import { HouseIcon } from '../Components/Icons/HouseIcon';
+import { ElementIcon } from '../Components/Icons/ElementIcon';
+import { CalendarIcon } from '../Components/Icons/CalendarIcon';
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 import './styles/aside.css';
 
 export function AppAside() {
@@ -8,13 +12,24 @@ export function AppAside() {
       <nav>
         <ul>
           <li>
+            <HouseIcon></HouseIcon>
             <NavLink to="dashboard">Dashboard</NavLink>
           </li>
           <li>
+            <ElementIcon></ElementIcon>
             <NavLink to="blocks">Bloki</NavLink>
           </li>
           <li>
+            <EditIcon></EditIcon>
             <NavLink to="exercises">Ćwiczenia</NavLink>
+          </li>
+          <li>
+            <CalendarIcon></CalendarIcon>
+            <NavLink to="/">Kalendarz</NavLink>
+          </li>
+          <li>
+            <PersonalCardIcon></PersonalCardIcon>
+            <NavLink to="/">Moje CV</NavLink>
           </li>
         </ul>
       </nav>
