80b7498e24d2f79212dd01e41cd51de222afa794
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-08 15:59:22 +0200

    aside-icons-added

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,5 +1,9 @@
 import { NavLink } from 'react-router-dom';
-
+import { CalendarIcon } from '../../App/Components/Icons/CalendarIcon';
+import { PersonalCardIcon } from '../../App/Components/Icons/PersonalCardIcon';
+import { HouseIcon } from '../../App/Components/Icons/HouseIcon';
+import { EditIcon } from '../../App/Components/Icons/EditIcon';
+import { ElementIcon } from '../../App/Components/Icons/ElementIcon';
 import './styles/aside.css';
 
 export function AppAside() {
@@ -8,13 +12,24 @@ export function AppAside() {
       <nav>
         <ul>
           <li>
+            <HouseIcon />
             <NavLink to="dashboard">Dashboard</NavLink>
           </li>
           <li>
-            <NavLink to="blocks">Bloki</NavLink>
+            <PersonalCardIcon />
+            <NavLink to="cv">Moje CV</NavLink>
           </li>
           <li>
+            <EditIcon />
             <NavLink to="exercises">Ćwiczenia</NavLink>
+          </li>
+          <li>
+            <CalendarIcon />
+            <NavLink to="calendar">Kalendarz</NavLink>
+          </li>
+          <li>
+            <ElementIcon />
+            <NavLink to="blog">Blog</NavLink>
           </li>
         </ul>
       </nav>
