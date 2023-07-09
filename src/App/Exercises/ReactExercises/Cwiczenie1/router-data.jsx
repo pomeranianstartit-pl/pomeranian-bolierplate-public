bb57b57b2564862e4746641419b786b5e6f62aa8
mikolaj mrozek <mikolaj.mrozek399@gmail.com>
2023-07-09 12:27:01 +0200

    dodano ćwiczenie 1 i 2 z bloku 8

---
+++ src/App/Exercises/ReactExercises/Cwiczenie1/router-data.jsx
@@ -0,0 +1,10 @@
+import { Cwiczenie1 } from './Cwiczenie1';
+
+export const Cwiczenie1MetaData = {
+  path: 'cwiczenie-1',
+  date: '12-12-2023',
+  linkLabel: 'cwiczenie-1',
+  blockNo: 1,
+  element: <Cwiczenie1 />,
+  tags: ['components', 'import', 'export'],
+};
