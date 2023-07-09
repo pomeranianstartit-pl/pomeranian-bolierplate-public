9803b293b3075e385b7a46d04c24278804f6d4ca
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-09 14:16:20 +0200

    fixing raz/razy

--- src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
@@ -8,12 +8,38 @@ export function Cwiczenie2() {
   function handleOnClick() {
     setCounter(counter + 1);
   }
+
+  let postfix = 'RAZY';
+  if (counter === 1) {
+    postfix = 'RAZ';
+  }
+
+  // if (counter === 1){
+  //  postfix="RAZ";
+  //}
+  //else if (counter === 0){
+  //  postfix="RAZY"
+  //}
+  //else if (counter > 1){
+  //  postfix="RAZY"
+  //}
+
+  //if (counter === 1) {
+  //  postfix = "raz";
+  //}
+  //else if (counter === 0 || counter > 1) {
+  //  postfix = "razy";
+  //}
+
+  // && - koniunkcja (AND) => i to, i to musi być prawdą
+  // || - alternatywa (OR) => albo to, albo to musi być prawdą
+
   return (
     <div className="blok-8_Cwiczenie2">
       <h1>Cześć!</h1>
 
       <button onClick={handleOnClick} type="submit">
-        KLIKNIĘTO WE MNIE {counter} RAZY
+        KLIKNIĘTO WE MNIE {counter} {postfix}
       </button>
     </div>
   );
