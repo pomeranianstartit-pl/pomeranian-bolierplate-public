9bdbd8ed4098fd08d92f7e10cf9a838fe26b1976
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-09 14:43:17 +0000

    more or less advanced czy a jest wieksze od b

---
+++ src/App/Exercises/ReactExercises/MoreOrLessAdvanced/router-data.jsx
@@ -0,0 +1,10 @@
+import { MoreOrLessAdvanced } from './MoreOrLessAdvanced';
+
+export const MoreOrLessAdvancedMetaData = {
+  path: 'MoreOrLessAdvanced',
+  date: '12-12-2023',
+  linkLabel: 'MoreOrLessAdvanced',
+  blockNo: 1,
+  element: <MoreOrLessAdvanced />,
+  tags: ['components', 'import', 'export'],
+};
