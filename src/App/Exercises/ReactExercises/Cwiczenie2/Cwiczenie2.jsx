2e41076d84a89c11426a8c7d5159be5404344e3e
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-09 12:16:21 +0000

    poprawiony postfix raz razy w zaleznosci od countera

--- src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
@@ -9,10 +9,23 @@ export function Cwiczenie2() {
     setCounter(counter + 1);
   }
 
+  let postfix = 'razy';
+
+  if (counter === 1) {
+    postfix = 'raz';
+  } else if (counter === 0 || counter > 1) {
+    postfix = 'razy';
+  }
+  // && - konciunkcja(and) => i to i to musi byc prawdą
+  // || - alternatywa (OR) => albo to albo to musi być prawdą
+
   return (
     <div className="blok-8__cwiczenie-2">
       <h2>Cześć!</h2>
-      <button onClick={handleOnClick}> Kliknięto we mnie {counter} razy</button>
+      <button onClick={handleOnClick}>
+        {' '}
+        Kliknięto we mnie {counter} {postfix}
+      </button>
     </div>
   );
 }
