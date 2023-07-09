e73b2d739d209aba98584ffd6668443410077a68
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-07-09 14:16:43 +0200

    dodane if do countera

--- src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
@@ -5,10 +5,21 @@ export function Cwiczenie2() {
   function handleOnClick() {
     setCounter(counter + 1);
   }
+  let postfix;
+
+  //komentarz
+
+  if (counter === 1) {
+    postfix = 'raz';
+  } else {
+    postfix = 'razy';
+  }
   return (
     <div className="blok-8__cwiczenie-2">
       <h2>Cześć!</h2>
-      <button onClick={handleOnClick}>Kliknięto we mnie {counter} razy</button>
+      <button onClick={handleOnClick}>
+        Kliknięto we mnie {counter} {postfix}
+      </button>
     </div>
   );
 }
