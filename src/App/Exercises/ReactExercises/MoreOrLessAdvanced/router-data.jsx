4120f77c889b4d66694ad0c04d11efeda3a016a7
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-09 16:43:09 +0200

    cwiczenia 1,2,3 blok9

---
+++ src/App/Exercises/ReactExercises/MoreOrLessAdvanced/router-data.jsx
@@ -0,0 +1,10 @@
+import { MoreOrLessAdvanced } from './MoreOrLessAdvanced';
+
+export const MoreOrLessAdvancedMetaData = {
+  path: 'MoreOrLessAdvanced',
+  date: '09-07-2023',
+  linkLabel: 'MoreOrLessAdvanced',
+  blockNo: 1,
+  element: <MoreOrLessAdvanced />,
+  tags: ['components', 'import', 'export'],
+};
