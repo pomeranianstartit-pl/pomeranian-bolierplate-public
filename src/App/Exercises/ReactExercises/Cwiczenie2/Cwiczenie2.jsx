34df739e095d85e7c29addbad77cb89c11c01c9c
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2023-07-10 20:50:45 +0200

    poprawki w ikonach

---
+++ src/App/Exercises/ReactExercises/Cwiczenie2/Cwiczenie2.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './style.css';
+
+export function Cwiczenie2() {
+  const [tekst, setTekst] = React.useState('Placeholder');
+
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    console.log('Kliknięto przycisk');
+    console.log(event);
+
+    const trescInputa = event.target[0].value;
+
+    console.log('Treść inputa: ', trescInputa);
+
+    setTekst(trescInputa);
+  }
+
+  return (
+    <div className="blok-8__cwiczenie-1">
+      <h1>Cześć!</h1>
+      <div>
+        <div>
+          <p>Wpisz tekst</p>
+
+          <form onSubmit={handleOnSubmit}>
+            <input type="text" placeholder="tekst do wpisania" />
+            <button type="submit">KLIKNIJ</button>
+          </form>
+        </div>
+        <div>
+          <p>{tekst}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
