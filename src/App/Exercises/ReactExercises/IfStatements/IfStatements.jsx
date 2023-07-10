5ab3535085e1d32ab769b59b6c4067ecd71535e4
Gabriela Koronkiewicz <gabrielak2k23@gmail.com>
2023-07-10 19:30:06 +0200

    Zapisz zmiany

---
+++ src/App/Exercises/ReactExercises/IfStatements/IfStatements.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+export function IfStatements() {
+    return (
+        <div>
+            <p>Jestem przykładowym routem</p>
+        </div>
+    );
+}
