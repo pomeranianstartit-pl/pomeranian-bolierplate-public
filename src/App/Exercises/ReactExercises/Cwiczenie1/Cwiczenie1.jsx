b663e65c5f8e6a901128dad9c1ffa0a4858253e0
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2023-07-10 19:41:20 +0200

    dodanie obsługi zdarzenia dodania aktywnego przycisku

--- src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
@@ -3,9 +3,28 @@ import React from 'react';
 import './style.css';
 
 export function Cwiczenie1() {
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    console.log('Kliknięto przycisk');
+    console.log(event);
+  }
+
   return (
-    <div>
-      <p>Ćwiczenie 1</p>
+    <div className="blok-8__cwiczenie-1">
+      <h1>Cześć!</h1>
+      <div>
+        <div>
+          <p>Wpisz tekst</p>
+
+          <form onSubmit={handleOnSubmit}>
+            <input type="text" placeholder="tekst do wpisania" />
+            <button type="submit">KLIKNIJ</button>
+          </form>
+        </div>
+        <div>
+          <p>Placeholder</p>
+        </div>
+      </div>
     </div>
   );
 }
