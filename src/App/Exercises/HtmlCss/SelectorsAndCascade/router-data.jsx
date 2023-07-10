70eaaf97abba54061bba4aacedef6b067edd6f5f
Iwona Bigos <iwonkabigos@gmail.com>
2023-07-10 18:38:08 +0200

    podstawy CSS

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'selectors-and-cadcade',
+  date: '10-07-2023',
+  linkLabel: 'Selektory i kaskadowość',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['selectros', 'class', 'attribute'],
+};
