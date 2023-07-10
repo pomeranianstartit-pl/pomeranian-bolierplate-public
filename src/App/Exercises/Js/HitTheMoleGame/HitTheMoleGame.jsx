19a8403a4db8d01eaa8c3c5fe8d77e837ea55419
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-07-10 21:29:07 +0200

    fix many bugs and add more css to mole game

--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -1,196 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import startTimer from './Timer';
-import Mole from './mole.svg';
-
-export const MoleGameSettings = ({
-  seconds,
-  setSeconds,
-  moleCount,
-  setMoleCount,
-  setIsCountingDown,
-  gameTime,
-  setGameTime,
-  score,
-  hightScore,
-  setScore,
-}) => {
-  const gameTimeOption = [
-    { label: '1 minuta', timeValue: 1 * 60 * 1000 },
-    { label: '2 minuty', timeValue: 2 * 60 * 1000 },
-    { label: '3 minuty', timeValue: 3 * 60 * 1000 },
-  ];
-  const moleCountOption = [
-    { label: '1 kret' },
-    { label: '2 krety' },
-    { label: '3 krety' },
-  ];
-  const handleStartTimer = () => {
-    startTimer(seconds, setSeconds, setIsCountingDown); // Wywołanie funkcji startTimer z Timer.jsx
-  };
-
-  useEffect(() => {
-    console.log(seconds.toString().padStart(3, 0));
-  }, [seconds]);
-
-  return (
-    <>
-      <div className="moleGameOptions">
-        <h4>
-          Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
-          którym się pojawił.
-        </h4>
-        <h2 className="scoreStyle">
-          {hightScore >= 0 && seconds === 0
-            ? `Twój NAJWYŻSZY wynik to ${hightScore}`
-            : ''}
-        </h2>
-        <h2 className="scoreStyle">
-          {score >= 0 && seconds === 0
-            ? `Twój wynik w tym podejściu to ${score} w czasie ${
-                gameTime / 1000 / 60
-              } ${gameTime / 1000 / 60 === 1 ? 'minuty' : 'minut'}`
-            : ''}
-        </h2>
-
-        <div className="gameOptionsButtons">
-          <div className="gameButtonsRows">
-            <div>
-              <h4>CZAS GRY {seconds} Sekund</h4>
-              {gameTimeOption.map(({ label, timeValue }) => (
-                <button
-                  className={gameTime === timeValue ? 'activeButton' : ''}
-                  onClick={() => {
-                    setSeconds(timeValue / 1000);
-                    setGameTime(timeValue);
-                  }}
-                  key={label}
-                >
-                  {label}
-                </button>
-              ))}
-            </div>
-
-            <div>
-              <h4>LICZBA KRETÓW</h4>
-
-              {moleCountOption.map(({ label }) => (
-                <button
-                  className={
-                    moleCount === Number(label[0]) ? 'activeButton' : ''
-                  }
-                  onClick={() => setMoleCount(Number(label[0]))}
-                  key={label}
-                >
-                  {label}
-                </button>
-              ))}
-            </div>
-
-            <div>
-              <h4>PRZYCISKI STERUJĄCE</h4>
-
-              <button
-                onClick={() => {
-                  handleStartTimer();
-                  setScore(0);
-                }}
-              >
-                START
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-export const MoleGameBoard = ({
-  seconds,
-  setIsCountingDown,
-  score,
-  hitTheMole,
-  moleArray,
-}) => {
-  console.log(moleArray);
-
-  return (
-    <>
-      <p>Czas: {seconds}</p>
-      <p>Wynik: {score}</p>
-      <button>STOP</button>
-      <div className="moleGame">
-        {moleArray.map((mole, index) => {
-          return (
-            <div key={index}>
-              <span>
-                {mole.isVisible ? (
-                  <div id="spangbob">
-                    <img
-                      src={Mole}
-                      alt="Mole"
-                      onClick={() => hitTheMole(index)}
-                    />
-                  </div>
-                ) : (
-                  <div id="spangbob">pustka</div>
-                )}
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-};
+import { MoleGameSettings } from './MoleGameSettings';
+import { MoleGameBoard } from './Board';
 
 export function HitTheMoleGame() {
   const defaultGameTime = 2 * 60 * 1000;
-  const [gameTime, setGameTime] = useState(defaultGameTime); // 2000 * 60
+  const [gameTime, setGameTime] = useState(defaultGameTime);
   const [moleCount, setMoleCount] = useState(1);
   const [seconds, setSeconds] = useState(gameTime / 1000);
   const [isCountingDown, setIsCountingDown] = useState(false);
   const [score, setScore] = useState(0);
-  const [hightScore, setHightScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
   const [moleArray, setMoleArray] = useState(
     Array(10).fill({ isVisible: false, isWhacked: false })
   );
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isCountingDown) {
+      intervalId = setInterval(() => {
+        setSeconds((prevSeconds) => (prevSeconds > 0 ? prevSeconds : 0));
+      }, 1000);
+    }
+
+    if (seconds === 0) {
+      clearInterval(intervalId);
+      setIsCountingDown(false);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isCountingDown, seconds]);
+
   const generatMole = () => {
     const rand = Math.floor(Math.random() * 10);
     setMoleArray((prevMoleArray) =>
-      prevMoleArray.map((mole, index) => {
-        const uppdatedMole = { ...mole };
-        uppdatedMole.isVisible = index === rand;
-        return uppdatedMole;
-      })
+      prevMoleArray.map((mole, index) => ({
+        ...mole,
+        isVisible: index === rand,
+      }))
     );
   };
+
   useEffect(() => {
-    let tick;
-    if (!tick) {
-      tick = setInterval(() => {
+    let intervalId;
+
+    if (isCountingDown) {
+      intervalId = setInterval(() => {
         generatMole();
       }, 1000);
     }
-    return () => clearInterval(tick);
-  }, [seconds]);
 
-  function hitTheMole(index) {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isCountingDown]);
+
+  const hitTheMole = (index) => {
     if (moleArray[index].isVisible) {
-      setScore(score + 1);
-      setMoleArray((prevVal) => {
-        const newArray = [...prevVal];
-        newArray[index].isVisible = false;
-        return newArray;
-      });
+      setScore((prevScore) => prevScore + 1);
+      setMoleArray((prevMoleArray) =>
+        prevMoleArray.map((mole, idx) =>
+          idx === index ? { ...mole, isVisible: false } : mole
+        )
+      );
     }
-  }
-  if (score > hightScore) {
-    setHightScore(score);
-  }
+  };
+  useEffect(() => {
+    if (score > highScore) {
+      setHighScore(score);
+    }
+  }, [highScore, score]);
   return (
     <>
-      {seconds === 120 || seconds === 0 || seconds === 180 || seconds === 60 ? (
+      {!isCountingDown ? (
         <MoleGameSettings
           seconds={seconds}
           setSeconds={setSeconds}
@@ -201,20 +85,21 @@ export function HitTheMoleGame() {
           gameTime={gameTime}
           setGameTime={setGameTime}
           score={score}
-          hightScore={hightScore}
+          hightScore={highScore}
           setScore={setScore}
         />
       ) : null}
-      {gameTime !== seconds * 1000 && seconds !== 0 ? (
+      {isCountingDown ? (
         <MoleGameBoard
           score={score}
           hitTheMole={hitTheMole}
           moleArray={moleArray}
           seconds={seconds}
           setIsCountingDown={setIsCountingDown}
+          setSeconds={setSeconds}
+          isCountingDown={isCountingDown}
         />
       ) : null}
-      {seconds === 0}
     </>
   );
 }
--- src/App/Exercises/Js/HitTheMoleGame/MoleGameSettings.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/MoleGameSettings.jsx
@@ -1,77 +1,98 @@
-import React, { useState, useEffect } from 'react';
+import formatTime from './FormatTime';
 import './style.css';
-import startTimer from './Timer';
+export const MoleGameSettings = ({
+  seconds,
+  setSeconds,
+  moleCount,
+  setMoleCount,
+  setIsCountingDown,
+  gameTime,
+  setGameTime,
+  score,
+  hightScore,
+  setScore,
+}) => {
+  const gameTimeOption = [
+    { label: '1 minuta', timeValue: 1 * 60 * 1000 },
+    { label: '2 minuty', timeValue: 2 * 60 * 1000 },
+    { label: '1 GODZINA', timeValue: 60 * 60 * 1000 },
+  ];
+  const moleCountOption = [
+    { label: '1 kret' },
+    { label: '2 krety' },
+    { label: '3 krety' },
+  ];
 
-const MoleGameSettings = (seconds, setSeconds, setIsCountingDown, gameTime, setGameTime, moleCount, setMoleCount) => {
+  return (
+    <>
+      <div className="moleGameOptions">
+        <h4>
+          Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
+          którym się pojawił.
+        </h4>
+        <h2 className="scoreStyle">
+          {hightScore >= 0 && seconds === 0
+            ? `Twój NAJWYŻSZY wynik to ${hightScore}`
+            : ''}
+        </h2>
+        <h2 className="scoreStyle">
+          {score >= 0 && seconds === 0
+            ? `Twój wynik w tym podejściu to ${score} w czasie ${
+                gameTime / 1000 / 60
+              } ${gameTime / 1000 / 60 === 1 ? 'minuty' : 'minut'}`
+            : ''}
+        </h2>
 
-    const gameTimeOption = [
-        { label: '1 minuta', timeValue: 1 * 60 * 1000 },
-        { label: '2 minuty', timeValue: 2 * 60 * 1000 },
-        { label: '3 minuty', timeValue: 3 * 60 * 1000 },
-    ];
-    const moleCountOption = [
-        { label: '1 kret' },
-        { label: '2 krety' },
-        { label: '3 krety' },
-    ];
-
-    const handleStartTimer = () => {
-        startTimer(seconds, setSeconds, setIsCountingDown);
-    };
-
-    useEffect(() => {
-        console.log(seconds.toString().padStart(3, 0));
-    }, [seconds]);
-
-    return (
-        <>
-        <div className="moleGameOptions">
-            <p>
-            Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w którym się pojawił.
-            </p>
-
-            <div className="gameOptionsButtons">
-            <div className="gameButtonsRows">
-                <div>
-                <h4>CZAS GRY {seconds} Sekund</h4>
-                {gameTimeOption.map(({ label, timeValue }) => (
-                    <button
-                    className={gameTime === timeValue ? 'activeButton' : ''}
-                    onClick={() => {
-                        setSeconds(timeValue / 1000);
-                        setGameTime(timeValue);
-                    }}
-                    key={label}
-                    >
-                    {label}
-                    </button>
-                ))}
-                </div>
-
-                <div>
-                <h4>LICZBA KRETÓW</h4>
-
-                {moleCountOption.map(({ label }) => (
-                    <button
-                    className={moleCount === Number(label[0]) ? 'activeButton' : ''}
-                    onClick={() => setMoleCount(Number(label[0]))}
-                    key={label}
-                    >
-                    {label}
-                    </button>
-                ))}
-                </div>
-
-                <div>
-                <h4>PRZYCISKI STERUJĄCE</h4>
-
-                <button onClick={handleStartTimer}>START</button>
-                </div>
+        <div className="gameOptionsButtons">
+          <div className="gameButtonsRows">
+            <div>
+              <h4>CZAS GRY {formatTime(seconds)} Sekund</h4>
+              {gameTimeOption.map(({ label, timeValue }) => (
+                <button
+                  className={gameTime === timeValue ? 'activeButton' : ''}
+                  onClick={() => {
+                    setSeconds(timeValue / 1000);
+                    setGameTime(timeValue);
+                    if (label === '1 GODZINA') alert('OSTROŻNIE');
+                  }}
+                  key={label}
+                >
+                  {label}
+                </button>
+              ))}
             </div>
+
+            <div>
+              <h4>LICZBA KRETÓW</h4>
+
+              {moleCountOption.map(({ label }) => (
+                <button
+                  className={
+                    moleCount === Number(label[0]) ? 'activeButton' : ''
+                  }
+                  onClick={() => setMoleCount(Number(label[0]))}
+                  key={label}
+                >
+                  {label}
+                </button>
+              ))}
             </div>
+
+            <div>
+              <h4>PRZYCISKI STERUJĄCE</h4>
+
+              <button
+                onClick={() => {
+                  setIsCountingDown(true);
+                  setScore(0);
+                }}
+              >
+                START
+              </button>
+            </div>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
-
-export default MoleGameSettings;
