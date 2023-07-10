3e4ae3924ff7e132b5512e366795a658c31a8c6a
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-10 18:37:20 +0200

    Ćwiczenia z CSS, selectory

--- src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/router-data.jsx
+++ src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/router-data.jsx
@@ -5,7 +5,7 @@ import { Exercise } from './index';
 export const blockRouterMetaData = {
   path: '12-12-2023-1',
   date: '12-12-2023',
-  linkLabel: 'HTML & CSS - ćwiczenie 1 z dnia 12-12-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie 1 z dnia 10-07-2023',
   blockNo: 1,
   element: <Exercise />,
   tags: ['flex', 'grid', 'video'],
---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SelectorsAndCascade } from './index';
+
+export const blockSelectorsAndCascade = {
+  path: 'selectors-and-cascade',
+  date: '12-12-2023',
+  linkLabel: 'Selectors and cascade',
+  blockNo: 1,
+  element: <SelectorsAndCascade />,
+  tags: ['selectors', 'class', 'attribute'],
+};
