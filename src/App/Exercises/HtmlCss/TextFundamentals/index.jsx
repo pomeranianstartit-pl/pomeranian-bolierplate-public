ac56e3873b71ca943286ba2120b4a0acbd1d0e01
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-11 18:58:15 +0200

    czcionki - fonts.google i css

---
+++ src/App/Exercises/HtmlCss/TextFundamentals/index.jsx
@@ -0,0 +1,43 @@
+import './styles.css';
+
+export function TextFundamentals() {
+  return (
+    <div>
+      <p className="p-1">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+        distinctio iste dolor quos exercitationem praesentium, facilis
+        architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+        deserunt veniam tenetur saepe exercitationem corporis?
+      </p>
+
+      <p className="p-2">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+        distinctio iste dolor quos exercitationem praesentium, facilis
+        architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+        deserunt veniam tenetur saepe exercitationem corporis?
+      </p>
+
+      <p className="p-3">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+        distinctio iste dolor quos exercitationem praesentium, facilis
+        architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+        deserunt veniam tenetur saepe exercitationem corporis?
+      </p>
+
+      <p className="p-4">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+        distinctio iste dolor quos exercitationem praesentium, facilis
+        architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+        deserunt veniam tenetur saepe exercitationem corporis?
+      </p>
+
+      <ol>
+        <li>Mix flour, baking powder, sugar, and salt.</li>
+        <li>In another bowl, mix eggs, milk, and oil.</li>
+        <li>Stir both mixtures together.</li>
+        <li>Fill muffin tray 3/4 full.</li>
+        <li>Bake for 20 minutes.</li>
+      </ol>
+    </div>
+  );
+}
