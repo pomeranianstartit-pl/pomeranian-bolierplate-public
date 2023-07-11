0519c8abd9a91efa4e83e2b51373f45dc7a531e2
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-11 20:59:19 +0200

    feat: dynami fonts

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { GoogleFonts } from './index';
+
+export const blockGoogleFonts = {
+  path: 'google-fonts',
+  date: '11-07-2023',
+  linkLabel: 'Google Fonts Training',
+  blockNo: 1,
+  element: <GoogleFonts />,
+  tags: ['fonts'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,6 +3,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockSelectorsAndCascade as selectorsAndCascade } from './SelectorsAndCascade/router-data';
 import { blockTextFundamentals } from './TextFundamentals/router-data';
+import { blockGoogleFonts } from './GoogleFonts/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, selectorsAndCascade,blockTextFundamentals];
+export const blockRouterMetaData = [blockRouterMetaData121220231, selectorsAndCascade,blockTextFundamentals,blockGoogleFonts];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
