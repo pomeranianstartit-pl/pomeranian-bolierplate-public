e9913fc1e7a5a6dd2c998ec79a1821ae358feae1
Anna Ochocka <annoch22@gmail.com>
2023-07-11 19:03:57 +0200

    fonty

---
+++ src/App/Exercises/HtmlCss/Text-fundamentals/index.jsx
@@ -0,0 +1,32 @@
+import './styles.css';
+
+export function TextFundamentals() {
+  return (
+    <div>
+      <p className="p-1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        eaque, consequatur eum blanditiis magni facere quam, rerum sapiente nam
+        non ipsam corrupti labore aliquam consectetur fuga, saepe soluta debitis
+        deserunt.
+      </p>
+      <p className="p-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        eaque, consequatur eum blanditiis magni facere quam, rerum sapiente nam
+        non ipsam corrupti labore aliquam consectetur fuga, saepe soluta debitis
+        deserunt.
+      </p>
+      <p className="p-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        eaque, consequatur eum blanditiis magni facere quam, rerum sapiente nam
+        non ipsam corrupti labore aliquam consectetur fuga, saepe soluta debitis
+        deserunt.
+      </p>
+      <p className="p-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        eaque, consequatur eum blanditiis magni facere quam, rerum sapiente nam
+        non ipsam corrupti labore aliquam consectetur fuga, saepe soluta debitis
+        deserunt.
+      </p>
+    </div>
+  );
+}
---
+++ src/App/Exercises/HtmlCss/Text-fundamentals/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { TextFundamentals } from './index';
+
+export const TextFundamentalsMetaData = {
+  path: 'text-fundamentals',
+  date: '12-12-2023',
+  linkLabel: 'Text fundamentals',
+  blockNo: 1,
+  element: <TextFundamentals />,
+  tags: ['attribute', 'text', 'css'],
+};
