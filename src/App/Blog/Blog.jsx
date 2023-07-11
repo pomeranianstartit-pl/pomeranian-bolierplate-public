b71471af3748e481a1e90cc278710ac41b020dd4
Joanna Bedra <joannabedra84@gmail.com>
2023-07-11 17:41:06 +0200

    wersja 1

--- src/App/Blog/Blog.jsx
+++ src/App/Blog/Blog.jsx
@@ -8,16 +8,19 @@ export const Blog = () => {
 
         <div>
           <h2 className="tutuł">Why are we so nostalgic for the 1990s?</h2>
-          <p className="tresc">
+          <p className="tresc1">
             Pop culture used to define a generation, but it seems the cultural,
             music and fashion trends of the 1990s have been recycled, and what
             Generation X considered its rite of passage into adulthood, is being
-            discovered and claimed by fledgling grown-ups as their own. There's
-            been the resurgence of vinyl as the trend-setters' choice of music
-            consumption rather than the ease of a digital download, and now the
-            hipsters have discovered the nostalgic sound of a whirring cassette
-            from which to enjoy the dulcet tones of everyone from Salt-N-Pepa to
-            Rick Astley.
+            discovered and claimed by fledgling grown-ups as their own.
+          </p>
+
+          <p className="tresc2">
+            There's been the resurgence of vinyl as the trend-setters' choice of
+            music consumption rather than the ease of a digital download, and
+            now the hipsters have discovered the nostalgic sound of a whirring
+            cassette from which to enjoy the dulcet tones of everyone from
+            Salt-N-Pepa to Rick Astley.
           </p>
         </div>
 
