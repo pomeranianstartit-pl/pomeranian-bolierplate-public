e65dc0eb4802d849617798063a33b2b2a2bd7ba2
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-11 19:29:16 +0200

    new feat

---
+++ src/App/Exercises/HtmlCss/TextFundamentals/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { TextFundamentals } from './index';
+
+export const blockTextFundamentals = {
+  path: 'text-fundamentals',
+  date: '11-07-2023',
+  linkLabel: 'Text Fundamentals',
+  blockNo: 1,
+  element: <TextFundamentals />,
+  tags: ['fundamentals'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,6 +2,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockSelectorsAndCascade as selectorsAndCascade } from './SelectorsAndCascade/router-data';
+import { blockTextFundamentals } from './TextFundamentals/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, selectorsAndCascade];
+export const blockRouterMetaData = [blockRouterMetaData121220231, selectorsAndCascade,blockTextFundamentals];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
