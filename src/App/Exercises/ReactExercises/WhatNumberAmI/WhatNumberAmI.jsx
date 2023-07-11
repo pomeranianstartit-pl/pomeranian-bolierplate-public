1d229a122605c3f40025d393531445acace0948c
Joanna Bedra <joannabedra84@gmail.com>
2023-07-11 17:40:52 +0200

    wersja 1

--- src/App/Exercises/ReactExercises/WhatNumberAmI/WhatNumberAmI.jsx
+++ src/App/Exercises/ReactExercises/WhatNumberAmI/WhatNumberAmI.jsx
@@ -3,22 +3,54 @@ import React, { useState } from 'react';
 import './style.css';
 
 export function WhatNumberAmI() {
+  const [liczba, setLiczba] = useState(0);
+
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  const generateRandomNumber = () => {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+
+    console.log(randomNumber);
+  };
+
+  const handleOnChange = (event) => {
+    let inputValue = event.target.value;
+
+    setLiczba(inputValue++);
+  };
+
   return (
     <div>
-      <h2>Czy A jest większe od B</h2>
-      <input type="number" value={firstValue} onChange={handleFirstValue} />
-      <input type="number" value={secondValue} onChange={handleSecondValue} />
+      <h1>Jaką jestem liczbą?</h1>
+
+      <p> jestem liczbą: {randomNumber}</p>
+
+      <button className="startButton" onClick={generateRandomNumber}>
+        START
+      </button>
+
+      <br></br>
+
+      <input
+        placeholder="A"
+        type="number"
+        value={liczba}
+        onChange={handleOnChange}
+      ></input>
+
       <p>
-        Odpowiedź:
-        {/* {firstValue > secondValue
-          ? 'Wartość A jest większa niż B'
-          : firstValue === secondValue
-          ? 'Wartość A jest równa B'
-          : 'Wartość A jest mniejsza niż B'} */}
-        {firstValue > secondValue && 'Wartość A jest większa niż B'}
-        {firstValue < secondValue && 'Wartość A jest mniejsza niż B'}
-        {firstValue === secondValue && 'Wartość A jest równa B'}
+        {randomNumber > liczba && 'Jestem większa'}
+
+        {randomNumber < liczba && 'Jestem mniejsza'}
+
+        {liczba === randomNumber && 'Gratulacje!'}
       </p>
+
+      <br></br>
+
+      <button className="resetButton" onClick={generateRandomNumber}>
+        RESET
+      </button>
     </div>
   );
 }
