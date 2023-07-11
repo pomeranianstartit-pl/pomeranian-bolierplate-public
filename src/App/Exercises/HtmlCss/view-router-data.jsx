12abade0de828c75f500bcb07e84000a9085dd58
Iwona <iwonkabigos@gmail.com>
2023-07-11 20:58:57 +0200

    dodanie zadania na zmiane fontow

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as SelectorsAndCascade } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as TextFundamentals } from './TextFundamentals/router-data';
+import { blockRouterMetaData as GoogleFonts } from './GoogleFonts/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   SelectorsAndCascade,
   TextFundamentals,
+  GoogleFonts,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
