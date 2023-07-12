61f43e05edab7cc2e7550f8dc847647cb8d31d09
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2023-07-12 16:39:43 +0200

    added more or less functionality

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -6,10 +6,13 @@ import { Cwiczenie2MetaData } from './Cwiczenie2/router-data';
 
 import { routerMetaData as IfStatementsMetaData } from './IfStatements/router-data';
 
+import { routerMetaData as IfStatementsMoreOrLessMetaData } from './IfStatementsMoreOrLess/router-data';
+
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Cwiczenie1MetaData,
   Cwiczenie2MetaData,
   IfStatementsMetaData,
+  IfStatementsMoreOrLessMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
