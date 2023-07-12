4c67dea8eb797ce07045cb7ee06b4657d2181b0a
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-07-12 07:34:57 +0200

    all changes

--- src/App/Exercises/ReactExercises/MoreOrLessAdvanced/MoreOrLessAdvanced.jsx
+++ src/App/Exercises/ReactExercises/MoreOrLessAdvanced/MoreOrLessAdvanced.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './style.css';
 
-export function Cwiczenie4Advanced() {
+export function MoreOrLessAdvanced() {
   const [inputA, setInputA] = React.useState(0);
 
   const [inputB, setInputB] = React.useState(0);
@@ -36,42 +36,6 @@ export function Cwiczenie4Advanced() {
 
         {inputA === inputB && <p>Odpowiedź: A i B są równe</p>}
       </div>
-    </div>
-  );
-}
-
-export function MoreOrLessAdvanced() {
-  const [a, setA] = React.useState(0);
-
-  const [b, setB] = React.useState(0);
-
-  function handleOnChangeA(event) {
-    setA(event.target.value);
-  }
-
-  function handleOnChangeB(event) {
-    setB(event.target.value);
-  }
-
-  return (
-    <div>
-      <h2>Czy A jest większe od B?</h2>
-
-      <input
-        type="number"
-        placeholder="A"
-        onChange={handleOnChangeA}
-        defaultValue={0}
-      />
-
-      <input
-        type="number"
-        placeholder="B"
-        onChange={handleOnChangeB}
-        defaultValue={0}
-      />
-
-      <p>Odpowiedź: {a > b ? 'TAK' : 'NIE'}</p>
     </div>
   );
 }
