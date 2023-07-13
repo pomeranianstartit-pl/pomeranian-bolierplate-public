331537d08667889cd0edba02b61e50829b8a863d
Anna Ochocka <annoch22@gmail.com>
2023-07-13 19:10:22 +0200

    3 images

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -1,11 +1,13 @@
 import './styles.css';
 import logo from './Logo-pomeranian.png';
 import logoIkea from './logo-ikea.svg';
+import images from './images.jpeg';
 export function ImageFiles() {
   return (
     <div>
       <img src={logo} alt="Logo pomeranian" />
       <img src={logoIkea} alt="ikea-logo" />
+      <img src={images} alt="images" />
     </div>
   );
 }
