dfc9c29fdad7913a304e856b81508ad2c710a40f
Gabriela Koronkiewicz <gabrielak2k23@gmail.com>
2023-07-13 18:05:29 +0200

    kolory css

---
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -0,0 +1,21 @@
+import './styles.css';
+
+export function Exercise() {
+  return <div>
+    <h2
+      className="color-name">Jestem pomalowany przez color name</h2>
+
+    <h2
+      className="color-hex">Jestem pomalowany przez color hex</h2>
+
+    <h2
+      className="rgb">
+      Jestem pomalowny przez rgb
+    </h2>
+
+    <h2
+      className="rgba">
+      Jestem pomalowany przez rgba
+    </h2>
+  </div>;
+}
