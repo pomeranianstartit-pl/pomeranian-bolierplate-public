3c8cd3f03f57dfaf007d3d21ff17fd5fe8ee03ae
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-13 18:05:02 +0200

    colory css

---
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div>
+      <h2 className="color-name"> jESTEM POMALOWANY przez kolor </h2>
+
+      <h2 className="color-hex"> Jestem pomalowany rzez color hex</h2>
+      <h2 className="shorten-hex"> Jestem pomalowany rzez color shorten-hex</h2>
+      <h2 className="rgb"> Jestem pomalowany rzez color rgb</h2>
+      <h2 className="rgba">Jestem pomalowany przez rgba</h2>
+      <h2 className="hsl">Jestem pomalowany przez hsl</h2>
+    </div>
+  );
+}
