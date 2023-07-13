23ce22c7b55f6d7707d201d36e652120c1b657ba
Anna Ochocka <annoch22@gmail.com>
2023-07-13 18:23:14 +0200

    Colors style

--- src/App/Exercises/HtmlCss/Color/index.jsx
+++ src/App/Exercises/HtmlCss/Color/index.jsx
@@ -9,6 +9,18 @@ export function Colors() {
       <h2 className="color-rgb">Jestem pomalowany przez rgb</h2>
       <h2 className="color-rgba">Jestem pomalowany przez rgba</h2>
       <h2 className="color-hsl">Jestem pomalowany przez hsl</h2>
+
+      <div className="opacity">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quaerat
+        tempora deleniti aspernatur odit! Expedita praesentium enim adipisci
+        quod odio distinctio molestiae eligendi maxime? Eum veritatis aspernatur
+        aperiam veniam! Iure?
+      </div>
+      <div className="two-squares">
+        <div className="square-1"></div>
+
+        <div className="square-2"></div>
+      </div>
     </div>
   );
 }
