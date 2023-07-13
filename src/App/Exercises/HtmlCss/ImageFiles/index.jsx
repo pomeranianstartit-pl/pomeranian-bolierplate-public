2a55c419fe797f746528f6d815d977d2efad028f
Anna Ochocka <annoch22@gmail.com>
2023-07-13 20:35:49 +0200

    RoundImage

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -2,12 +2,15 @@ import './styles.css';
 import logo from './Logo-pomeranian.png';
 import logoIkea from './Ikea-logo.svg';
 import images from './images.jpeg';
+import { RoundImage } from '../../../Components/RoundImage/RoundImage';
 export function ImageFiles() {
   return (
     <div>
       <img src={logo} alt="Logo pomeranian" />
       <img src={logoIkea} alt="Ikea logo" />
       <img src={images} alt="images" />
+
+      <RoundImage src={images} />
     </div>
   );
 }
