60389fb178719adf85aaa115bd457400507a1025
Olga Ochman <olgaochman346@gmail.com>
2023-07-13 18:09:27 +0200

    kolory css

---
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -0,0 +1,28 @@
+import './styles.css';
+
+
+
+
+export function Colors() {
+
+  return <div>
+
+    <h2 className="color-name">Jestem pomalowany przez color name</h2>
+
+
+    <h2 className='color-hex'>jestem pomalowany przez color hex</h2>
+
+    <h2 className='shorten-hex'>Jestem pomalowany przez shorten hex</h2>
+
+    <h2 className='rgb'> jestem pomalowany przez rgb</h2>
+
+    <h2 className='rgba'>Jestem pomalowany przez rgba</h2>
+   
+    <h2 className='hsl'>Jestem pomalowany przez hsl</h2>
+
+
+
+
+  </div>;
+
+}
