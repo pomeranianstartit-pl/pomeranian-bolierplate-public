501a83d24afb0d8583894be7b020fe5dcd62b164
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-13 19:36:44 +0200

    background

---
+++ src/App/Exercises/HtmlCss/Backgrounds/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div>
+      <div className="background-url"></div>
+      <div className="background-url-2"></div>
+      <div className="background-url-3"></div>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as ColorsblockRouterMetaData } from './Colors/router-data';
 import { blockRouterMetaData as ImageFilesblockRouterMetaData } from './ImageFiles/router-data';
+import { blockRouterMetaData as BackgroundsblockRouterMetaData } from './Backgrounds/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   ColorsblockRouterMetaData,
   ImageFilesblockRouterMetaData,
+  BackgroundsblockRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
