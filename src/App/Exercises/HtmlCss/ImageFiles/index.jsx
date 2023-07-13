5f10737306addb443ff5a223dca621f71dc2322f
Gabriela Koronkiewicz <gabrielak2k23@gmail.com>
2023-07-13 19:07:03 +0200

    imagessave

---
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -0,0 +1,8 @@
+import './styles.css';
+
+import logoPomeranian from './pomeranian-icon.png';
+import logoIkea from './Ikea-logo.svg'
+
+export function Exercise() {
+  return <p>Obrazki</p>;
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,7 +2,10 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as cssColorsMetaData } from './Colors/router-data';
+import { blockRouterMetaData as ImageFilesMetaData } from './ImageFiles/router-data';
 
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, cssColorsMetaData];
+export const blockRouterMetaData = [blockRouterMetaData121220231,
+    cssColorsMetaData,
+    ImageFilesMetaData];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
