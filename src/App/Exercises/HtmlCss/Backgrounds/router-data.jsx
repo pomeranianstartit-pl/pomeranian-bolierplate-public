46f706c5ab4d3ca40c1f993788d450df83515f45
Iwona <iwonkabigos@gmail.com>
2023-07-13 19:37:16 +0200

    dodadnie cwiczenia z tlami

---
+++ src/App/Exercises/HtmlCss/Backgrounds/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Backgrounds } from './index';
+
+export const blockRouterMetaData = {
+  path: 'background',
+  date: '13-07-2023',
+  linkLabel: 'Tła',
+  blockNo: 1,
+  element: <Backgrounds />,
+  tags: ['html', 'image', 'background'],
+};
