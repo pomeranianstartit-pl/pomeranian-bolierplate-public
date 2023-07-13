ffc3bf44ed0d49d5a45e0edebf6010b1874aaffa
Gabriela Koronkiewicz <gabrielak2k23@gmail.com>
2023-07-13 18:24:24 +0200

    kolory css

--- src/App/Exercises/HtmlCss/Colors/index.jsx
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -17,5 +17,11 @@ export function Exercise() {
       className="rgba">
       Jestem pomalowany przez rgba
     </h2>
-  </div>;
+
+    <div
+      className="opacity">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nulla facere sunt dolores, voluptate qui quam vitae magnam tempora aliquam esse pariatur iste doloremque, necessitatibus ut illo perspiciatis fuga eligendi.</div>
+    <div className="two-squares"></div>
+    <div className="square-1"></div>
+    <div className="square-2"></div>
+  </div>
 }
