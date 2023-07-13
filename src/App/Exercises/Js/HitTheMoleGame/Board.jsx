6c8fec531ca6c524b0cce4173010de60ed09a3d2
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-13 17:59:50 +0200

    Add board functionality

--- src/App/Exercises/Js/HitTheMoleGame/Board.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/Board.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import Mole from '../../../Images/Mole.svg';
+import formatTime from './Time';
 
-export const MoleGameBoard = ({ scoreCount, moleArray, hitTheMole }) => {
-  return (
-    <div className="moleGame">
-      <h4 className="board-score">WYNIK: {scoreCount}</h4>
-      <div className="board">
-        {moleArray.map((mole, index) => (
-          <div className="square">
-            <span>
-              {mole.isVisible ? (
-                <img src={Mole} id="Mole!" onClick={() => hitTheMole(index)} />
-              ) : null}
-            </span>
-          </div>
-        ))}
+export const MoleGameBoard = ({
+  scoreCount,
+  moleArray,
+  hitTheMole,
+  gameStarted,
+  timer,
+}) => {
+  if (gameStarted) {
+    return (
+      <div className="moleGame">
+        <h4 className="board-score">WYNIK: {`${scoreCount}`}</h4>
+        <h4 className="timer">Pozostały czas {formatTime(timer)}</h4>
+        <div className="board">
+          {moleArray.map((mole, index) => (
+            <div className="square">
+              <span>
+                {mole.isVisible ? (
+                  <img
+                    src={Mole}
+                    id="Mole!"
+                    onClick={() => hitTheMole(index)}
+                  />
+                ) : null}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default MoleGameBoard;
