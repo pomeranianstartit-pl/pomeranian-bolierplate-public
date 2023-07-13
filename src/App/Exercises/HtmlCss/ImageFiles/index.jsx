9e66ea8f31c52401ec779836dc1c956435ea6a52
Iwona <iwonkabigos@gmail.com>
2023-07-13 20:57:50 +0200

    dodanie komponentu przycinania zdjecia

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -2,6 +2,7 @@ import './styles.css';
 import ikea_logo from './Ikea_logo.svg';
 import pomeranian from './pomeranian.png';
 import flowers from './flowers.jpg';
+import { RoundImage } from '../../../Components/RoundImage/RoundImage';
 
 export function ImageFiles() {
   return (
@@ -9,6 +10,7 @@ export function ImageFiles() {
       <img src={ikea_logo} alt="ikea logo"></img>
       <img src={pomeranian} alt="pomeranian logo"></img>
       <img className="flowers-image" src={flowers} alt="flowers image"></img>
+      <RoundImage src={flowers} size="300px" />
     </div>
   );
 }
