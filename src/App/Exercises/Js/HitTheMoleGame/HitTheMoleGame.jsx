b1c5dd5c5791a011b25aaf85ec9b818dbc542d3e
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-13 18:01:16 +0200

    Fix restart game

--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -5,55 +5,42 @@ import MoleGameBoard from './Board';
 
 export const HitTheMoleGame = () => {
   const defaultGameTime = 1 * 60 * 1000;
-  const moleSpeed = 1000;
-  const [moleCount, setMoleCount] = useState(0);
-  const [gameTime, setGameTime] = useState(0);
-  const [scoreCount, setScoreCount] = useState(0);
-  const [moleArray, setMoleArray] = useState([]);
+  const [gameTime, setGameTime] = useState(defaultGameTime / 1000);
+  const [timer, setTimer] = useState(gameTime);
+  const defaultArrayState = Array(10).fill({
+    isVisible: false,
+    isWhacked: false,
+  });
   const [gameStarted, setGameStarted] = useState(false);
-  const [gameOver, setGameOver] = useState(false);
-  const [scoreReset, setScoreReset] = useState(false);
-  const [gameEnded, setGameEnded] = useState(false);
-  const [gameSettingsValid, setGameSettingsValid] = useState(false);
+  const [moleArray, setMoleArray] = useState(defaultArrayState);
+  const [moleCount, setMoleCount] = useState(1);
+  const moleSpeed = 1000;
+  const [scoreCount, setScoreCount] = useState(0);
 
   useEffect(() => {
-    setMoleArray(Array(10).fill({ isVisible: false, isWhacked: false }));
+    setMoleArray(defaultArrayState);
   }, [moleCount]);
 
   useEffect(() => {
-    if (gameTime !== 0 && moleCount !== 0) {
-      setGameSettingsValid(true);
-    } else {
-      setGameSettingsValid(false);
-    }
-  }, [gameTime, moleCount]);
-
-  useEffect(() => {
-    if (gameTime !== 0 && moleCount !== 0) {
-      setGameEnded(false);
-    }
-  }, [gameTime, moleCount]);
+    setTimer(gameTime);
+  }, [gameTime]);
 
   useEffect(() => {
     let interval;
-
-    if (gameStarted) {
+    if (!interval) {
       interval = setInterval(() => {
-        setGameTime((prevGameTime) => {
-          if (prevGameTime === 0) {
+        setTimer((prevTimer) => {
+          if (prevTimer === 0) {
             clearInterval(interval);
             setGameStarted(false);
-            setGameOver(true);
-            setScoreReset(true);
-            setGameEnded(true);
           }
-          return prevGameTime - 1;
+          return prevTimer - 1;
         });
       }, 1000);
     }
 
     return () => clearInterval(interval);
-  }, [gameStarted]);
+  }, [timer, gameStarted]);
 
   useEffect(() => {
     let intervalId;
@@ -63,19 +50,17 @@ export const HitTheMoleGame = () => {
     }
 
     return () => clearInterval(intervalId);
-  }, [moleArray, gameStarted]);
+  }, [gameStarted]);
 
   useEffect(() => {
     if (gameStarted && scoreCount === 3) {
+      setTimer(gameTime);
       setGameStarted(false);
-      setGameOver(true);
-      setScoreReset(true);
-      setGameEnded(true);
     }
-  }, [scoreCount, gameStarted]);
+  }, [scoreCount, gameStarted, gameTime]);
 
   const hitTheMole = (index) => {
-    if (moleArray[index].isVisible && gameTime > 0 && gameStarted) {
+    if (moleArray[index].isVisible) {
       setScoreCount((prevScore) => prevScore + 1);
 
       setMoleArray((prevVal) => {
@@ -89,75 +74,62 @@ export const HitTheMoleGame = () => {
   };
 
   const showRandomMole = () => {
-    if (gameStarted) {
-      function getRandom(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      }
-
-      const randomIndices = [];
-
-      while (randomIndices.length < moleCount) {
-        const randomIndex = getRandom(0, moleArray.length - 1);
-        if (!randomIndices.includes(randomIndex)) {
-          randomIndices.push(randomIndex);
-        }
-      }
-
-      setMoleArray((previousMoleArray) =>
-        previousMoleArray.map((mole, index) => {
-          const newMole = { ...mole };
-          newMole.isVisible = randomIndices.includes(index);
-          return newMole;
-        })
-      );
+    function getRandom(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+    const randomIndices = [];
+
+    while (randomIndices.length < moleCount) {
+      const randomIndex = getRandom(0, moleArray.length - 1);
+      if (!randomIndices.includes(randomIndex)) {
+        randomIndices.push(randomIndex);
+      }
+    }
+
+    setMoleArray((previousMoleArray) =>
+      previousMoleArray.map((mole, index) => {
+        const newMole = { ...mole };
+        newMole.isVisible = randomIndices.includes(index);
+        return newMole;
+      })
+    );
   };
 
-  const startStopGame = () => {
-    if (!gameStarted && !gameEnded) {
-      setGameStarted(true);
-      setGameOver(false);
-      if (gameTime === 0) {
-        setScoreCount(0);
-      }
-      setScoreReset(false);
-    } else if (!gameStarted && gameEnded) {
-      restartGame();
-    } else {
-      setGameStarted(false);
-    }
+  const startGame = () => {
+    setGameStarted(true);
+    setScoreCount(0);
+    setTimer(gameTime);
   };
 
   const restartGame = () => {
-    if (gameSettingsValid) {
-      setGameTime(defaultGameTime / 1000);
-      setMoleCount(0);
-      setScoreCount(0);
-      setMoleArray(Array(10).fill({ isVisible: false, isWhacked: false }));
-      setGameStarted(false);
-      setGameOver(false);
-      setScoreReset(false);
-      setGameEnded(true);
-    }
+    setGameStarted(false);
+    setTimer(defaultGameTime / 1000);
+    setMoleCount(1);
+    setScoreCount(0);
+    setMoleArray(defaultArrayState);
   };
+
   return (
     <>
       <MoleGameSettings
         gameTime={gameTime}
-        moleCount={moleCount}
         setGameTime={setGameTime}
+        timer={timer}
+        moleCount={moleCount}
         setMoleCount={setMoleCount}
-        restartGame={restartGame}
         gameStarted={gameStarted}
-        startStopGame={startStopGame}
-        gameSettingsValid={gameSettingsValid}
+        startGame={startGame}
+        restartGame={restartGame}
       />
       <MoleGameBoard
         moleArray={moleArray}
         hitTheMole={hitTheMole}
         scoreCount={scoreCount}
+        gameStarted={gameStarted}
+        timer={timer}
       />
     </>
   );
