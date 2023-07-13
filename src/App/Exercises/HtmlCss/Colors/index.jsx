649973dd8aff0b7d3bcaffc01115135c801e3159
Joanna Bedra <joannabedra84@gmail.com>
2023-07-13 18:10:04 +0200

    Kolory

---
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div>
+      <h2 className="color-name">Jestem pomalowany przez color name</h2>
+
+      <h2 className="color-hex">Jestem pomalowany przez color hex</h2>
+
+      <h2 className="shorten-hex">Jestem pomalowany przez shorten hex</h2>
+
+      <h2 className="rgb">Jestem pomalowany przez rgb</h2>
+
+      <h2 className="rgba">Jestem pomalowany przez rgba</h2>
+
+      <h2 className="hsl">Jestem pomalowany przez hsl</h2>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,13 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
-import { blockRouterMetaData as blockRouterMetaData121220231 } from './SelectorsAndCascade/router-data';
+import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as cssColorsMetaData } from './Colors/router-data';
+
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+
+  cssColorsMetaData,
+];
+
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
