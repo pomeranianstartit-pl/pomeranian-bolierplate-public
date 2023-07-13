d15d117a1c17e53829bfe41a2d18fe9762885035
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-13 18:04:57 +0200

    colors

---
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div>
+      <h2 className="color-name">Jestem pomalowany przez color name</h2>
+      <span></span>
+      <h2 className="color-hex">Jestem pomalowany przez color hex</h2>
+      <span></span>
+      <h2 className="shorten-hex">Jestem pomalowany przez color shorten-hex</h2>
+      <span></span>
+      <h2 className="color-rgb">Jestem pomalowany przez color rgb</h2>
+      <span></span>
+      <h2 className="color-rgba">Jestem pomalowany przez color rgba</h2>
+      <span></span>
+      <h2 className="color-hsl">Jestem pomalowany przez color hsl</h2>
+    </div>
+  );
+}
---
+++ src/App/Exercises/HtmlCss/Colors/router-data.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'css-colors',
+  date: '12-12-2023',
+  linkLabel: 'Kolory CSS',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['css', 'colors'],
+};
