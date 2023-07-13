9b9d7aaa59df0f43177c60450fdd0db4bb748f1d
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-13 18:02:28 +0200

    Fix timer after win

--- src/App/Exercises/Js/HitTheMoleGame/Settings.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/Settings.jsx
@@ -5,8 +5,7 @@ export const MoleGameSettings = ({
   setMoleCount,
   gameStarted,
   restartGame,
-  startStopGame,
-  gameSettingsValid,
+  startGame,
 }) => {
   const gameTimeOption = [
     { label: '1 minuta', timeValue: 1 * 60 },
@@ -20,50 +19,55 @@ export const MoleGameSettings = ({
     { label: '3 krety' },
   ];
 
-  return (
-    <div className="moleGameOptions">
-      <p>
-        Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
-        którym się pojawił.
-      </p>
+  if (gameStarted) {
+    return <button onClick={restartGame}>Restart</button>;
+  }
 
-      <div className="gameOptionsButtons">
-        <div className="gameButtonsRows">
-          <div>
-            <h4>CZAS GRY: {Math.max(gameTime, 0)}</h4>
-            {gameTimeOption.map(({ label, timeValue }) => (
-              <button
-                className={gameTime === timeValue ? 'activeButton' : ''}
-                onClick={() => {
-                  setGameTime(timeValue);
-                }}
-              >
-                {label}
-              </button>
-            ))}
-          </div>
-          <div>
-            <h4>LICZBA KRETÓW</h4>
-            {moleCountOption.map(({ label }) => (
-              <button
-                className={moleCount === Number(label[0]) ? 'activeButton' : ''}
-                onClick={() => setMoleCount(Number(label[0]))}
-              >
-                {label}
-              </button>
-            ))}
-          </div>
-          <div>
-            <h4>PRZYCISKI STERUJĄCE</h4>
-            <button disabled={!gameSettingsValid} onClick={startStopGame}>
-              {gameStarted ? 'Stop' : 'Start'}
-            </button>
-            <button onClick={restartGame}>Restart</button>
+  if (!gameStarted) {
+    return (
+      <div className="moleGameOptions">
+        <p className="instructions">
+          Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
+          którym się pojawił.
+        </p>
+
+        <div className="gameOptionsButtons">
+          <div className="gameButtonsRows">
+            <div>
+              <h4 className="game-time">CZAS GRY</h4>
+              {gameTimeOption.map(({ label, timeValue }) => (
+                <button
+                  className={gameTime === timeValue ? 'activeButton' : ''}
+                  onClick={() => {
+                    setGameTime(timeValue);
+                  }}
+                >
+                  {label}
+                </button>
+              ))}
+            </div>
+            <div>
+              <h4>LICZBA KRETÓW</h4>
+              {moleCountOption.map(({ label }) => (
+                <button
+                  className={
+                    moleCount === Number(label[0]) ? 'activeButton' : ''
+                  }
+                  onClick={() => setMoleCount(Number(label[0]))}
+                >
+                  {label}
+                </button>
+              ))}
+            </div>
+            <div>
+              <h4>PRZYCISKI STERUJĄCE</h4>
+              <button onClick={startGame}>Start</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default MoleGameSettings;
