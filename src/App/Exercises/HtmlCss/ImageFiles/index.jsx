de2906535fde7f87c4266c1d38bc91b9b63b5de0
Olga Ochman <olgaochman346@gmail.com>
2023-07-13 19:04:48 +0200

    images

---
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+import logoPomeranian from './pomeranian-icon.png'
+import logoIkea from './logo-ikea.svg'
+
+export function ImageFiles() {
+  return (
+  
+  
+  <div>
+
+    <img src={logoPomeranian} alt='Pomeranian logo' />
+
+    <img scr={logoIkea} alt="Ikea logo" />
+
+
+  </div>
+  
+  )
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,7 +1,8 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
-import { blockRouterMetaData as Colors } from './Colors/router-data';
+import { blockRouterMetaData as colorsMetaData } from './Colors/router-data';
+import { blockRouterMetaData as imageFilesMetaData } from './ImageFiles/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, Colors];
+export const blockRouterMetaData = [blockRouterMetaData121220231, colorsMetaData, imageFilesMetaData];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
