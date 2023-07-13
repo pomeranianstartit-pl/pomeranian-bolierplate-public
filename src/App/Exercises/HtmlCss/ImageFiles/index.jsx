77f0abbdb5fbedac84c2f0c3f0cce8fe46398904
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-13 20:30:38 +0200

    RoundImgae

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -2,6 +2,7 @@ import './styles.css';
 import background from './14-blue-light-leak.jpg';
 import logo from './Original_Adidas_logo.svg.png';
 import logoSvg from './Unreal_idle.svg';
+import { RoundImage } from '../../../Components/RoundImage/RoundImage';
 
 export function Exercise() {
   return (
@@ -9,6 +10,7 @@ export function Exercise() {
       <img src={background} alt="Background"></img>
       <img src={logo} alt="Logo"></img>
       <img src={logoSvg} alt="Logo"></img>
+      <RoundImage src={background} />
     </div>
   );
 }
