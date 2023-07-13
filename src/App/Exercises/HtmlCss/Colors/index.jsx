b50d7e67645f733dc8d879ef8d4b3affedeb3b17
Iwona <iwonkabigos@gmail.com>
2023-07-13 18:05:13 +0200

    typy zapisu kolorow css

---
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export function Colors() {
+  return (
+    <div>
+      <h2 className="color-name">Jestem pomalowany przez color name</h2>
+      <h2 className="color-hex">Jestem pomalowany przez color hex</h2>
+      <h2 className="shorten-hex">Jestem pomalowany przez shorten hex</h2>
+      <h2 className="rgb">Jestem pomalowany przez shorten rgb</h2>
+      <h2 className="rgba">Jestem pomalowany przez rgba</h2>
+      <h2 className="hsl">Jestem pomalowany przez hsl</h2>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-
 import { blockRouterMetaData as SelectorsAndCascade } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as TextFundamentals } from './TextFundamentals/router-data';
 import { blockRouterMetaData as GoogleFonts } from './GoogleFonts/router-data';
+import { blockRouterMetaData as Colors } from './Colors/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   SelectorsAndCascade,
   TextFundamentals,
   GoogleFonts,
+  Colors,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
