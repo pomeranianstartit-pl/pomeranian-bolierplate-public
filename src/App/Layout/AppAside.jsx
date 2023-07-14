d464450f31e1cd45de87c64b59d0f624b39c9343
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-14 18:25:11 +0000

    cwiczenie blok-10

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -32,6 +32,9 @@ export function AppAside() {
             <CalendarIcon />
             <NavLink to="calendar">Kalendarz</NavLink>
           </li>
+          <li>
+            <NavLink to="blog">Blog</NavLink>
+          </li>
         </ul>
       </nav>
       <p style={{ padding: '1rem 0' }}>Sidebar items, widgets, etc</p>
