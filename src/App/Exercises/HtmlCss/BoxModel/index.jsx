3731f03981018edbf32ccfb4076cde1fc2434f23
Olga Ochman <olgaochman346@gmail.com>
2023-07-14 19:55:22 +0200

    css units

--- src/App/Exercises/HtmlCss/BoxModel/index.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -25,6 +25,14 @@ export function Exercise() {
       {!isVisible && (
         <div className="div_nr1">Przykładowy div - !conditional rendering</div>
       )}
+      
+       {/* CSS UNITS */}
+       <div className="css_units_wrapper">
+        <div className="div_nr1 css_units_1">Przykładowy div 1</div>
+        <div className="div_nr1 css_units_2">Przykładowy div 2</div>
+        <div className="div_nr1 css_units_3">Przykładowy div 3</div>
+        <div className="div_nr1 css_units_4">Przykładowy div 4</div>
+      </div>
     </div>
   );
 }
