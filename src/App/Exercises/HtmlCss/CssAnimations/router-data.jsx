dbbad3736017d339c9fb193441019a0018a48ff3
mikolaj mrozek <mikolaj.mrozek399@gmail.com>
2023-07-15 12:59:05 +0200

    Dodanie CssAnimations

---
+++ src/App/Exercises/HtmlCss/CssAnimations/router-data.jsx
@@ -0,0 +1,10 @@
+import { CssAnimations } from './index';
+
+export const blockRouterMetaData = {
+  path: ' css-Animations',
+  date: '12-12-2023',
+  linkLabel: ' CssAnimations',
+  blockNo: 1,
+  element: <CssAnimations />,
+  tags: ['components'],
+};
