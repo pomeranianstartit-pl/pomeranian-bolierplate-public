17722906c41dd1bcae56d685acaa819fbc6daadb
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-07-15 11:02:04 +0200

    Ćwiczenia z kopiowania styli

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as Selectors } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as TextFundaments } from './TextFundaments/router-data';
+import { blockRouterMetaData as StandardTags } from './StandardTags/router-data';
 
 export const blockRouterMetaData = [
     blockRouterMetaData121220231,
     Selectors,
     TextFundaments,
+    StandardTags,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
