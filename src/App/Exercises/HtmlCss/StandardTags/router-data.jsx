488c095eb210b23f5bf59f26a214e86564988f7d
mikolaj mrozek <mikolaj.mrozek399@gmail.com>
2023-07-15 11:13:08 +0200

    Nowe cwiczenie StandardTags

---
+++ src/App/Exercises/HtmlCss/StandardTags/router-data.jsx
@@ -0,0 +1,10 @@
+import { StandardTags } from './index';
+
+export const blockRouterMetaData = {
+  path: 'StandardTags',
+  date: '15-07-2023',
+  linkLabel: 'StandardTags',
+  blockNo: 1,
+  element: <StandardTags />,
+  tags: ['StandardTags'],
+};
