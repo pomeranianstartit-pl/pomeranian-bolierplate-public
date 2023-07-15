70a502dc2e55a554a9badac56072a820336800bb
mikolaj mrozek <mikolaj.mrozek399@gmail.com>
2023-07-15 15:32:34 +0200

    Dodanie zawartości Video Audio Filtry etc. Css

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -6,7 +6,10 @@ import { blockRouterMetaData as GoogleFonts } from './GoogleFonts/router-data';
 import { blockRouterMetaData as Selectors } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as TextFundaments } from './TextFundaments/router-data';
 import { blockRouterMetaData as Colors } from './Colors/router-data';
-
+import { blockRouterMetaData as ImageFiles } from './ImageFiles/router-data';
+import { blockRouterMetaData as Backgrounds } from './Backgrounds/router-data';
+import { blockRouterMetaData as CssFilter } from './CssFilter/router-data';
+import { blockRouterMetaData as MediaFiles } from './MediaFiles/router-data';
 export const blockRouterMetaData = [
     blockRouterMetaData121220231,
     StandardTags,
@@ -14,5 +17,9 @@ export const blockRouterMetaData = [
     Selectors,
     TextFundaments,
     Colors,
+    ImageFiles,
+    Backgrounds,
+    CssFilter,
+    MediaFiles,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
