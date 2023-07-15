022bf1bbdef63c6a82b6b210f584b7a7dbc4fb55
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-07-15 12:12:17 +0200

    buttony zmiana czcionki

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,7 +2,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-import { standardTagsMetaData as StandardTags } from './StandardTags/router-data';
+import { googleFontsMetaData as GoogleFonts } from './GoogleFonts/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, StandardTags];
+export const blockRouterMetaData = [blockRouterMetaData121220231, GoogleFonts];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
