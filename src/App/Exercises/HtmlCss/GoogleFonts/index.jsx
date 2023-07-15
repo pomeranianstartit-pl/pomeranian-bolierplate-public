0e5d0215f2a8a86c509c4a69ae58aeb7252a9701
Michał Hadrysiak <michal.hadrysiak@indiebi.com>
2023-07-15 12:30:48 +0200

    google fonts

--- src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
+++ src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
@@ -1,19 +1,52 @@
 import './styles.css';
+import { useState } from 'react';
 
 export const GoogleFonts = () => {
+  const state = useState('f-roboto');
+  console.log(state);
+  const [dynamicClass, setDynamicClass] = state;
+
+  function handleOnChangeRobot(event) {
+    setDynamicClass('f-roboto');
+  }
+
   return (
-    <p className="f-roboto">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non
-      lorem mollis, tempor nisl in, ultricies mauris. Nunc malesuada varius
-      rhoncus. Vestibulum elementum tristique suscipit. Morbi porttitor vel
-      ipsum eu venenatis. Ut ligula erat, mattis ut dapibus eget, malesuada ut
-      odio. Fusce venenatis interdum tellus, non consectetur erat ullamcorper
-      et. Donec dictum cursus elementum. Morbi dolor eros, rutrum sed arcu sit
-      amet, cursus tempus sem. Duis velit nibh, tempus vel ex eu, facilisis
-      viverra nisi. Suspendisse et orci tempus ligula iaculis feugiat. Integer
-      at nisi erat. Suspendisse potenti. Pellentesque laoreet sapien erat, nec
-      malesuada nulla auctor euismod. Nulla porta mauris ut nunc ornare, id
-      tristique purus accumsan. Donec at ante ipsum.
-    </p>
+    <>
+      <p className={dynamicClass}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non
+        lorem mollis, tempor nisl in, ultricies mauris. Nunc malesuada varius
+        rhoncus. Vestibulum elementum tristique suscipit. Morbi porttitor vel
+        ipsum eu venenatis. Ut ligula erat, mattis ut dapibus eget, malesuada ut
+        odio. Fusce venenatis interdum tellus, non consectetur erat ullamcorper
+        et. Donec dictum cursus elementum. Morbi dolor eros, rutrum sed arcu sit
+        amet, cursus tempus sem. Duis velit nibh, tempus vel ex eu, facilisis
+        viverra nisi. Suspendisse et orci tempus ligula iaculis feugiat. Integer
+        at nisi erat. Suspendisse potenti. Pellentesque laoreet sapien erat, nec
+        malesuada nulla auctor euismod. Nulla porta mauris ut nunc ornare, id
+        tristique purus accumsan. Donec at ante ipsum.
+      </p>
+      <button onClick={handleOnChangeRobot}>Roboto</button>
+      <button
+        onClick={() => {
+          setDynamicClass('f-pacifico');
+        }}
+      >
+        Pacifico
+      </button>
+      <button
+        onClick={() => {
+          setDynamicClass('f-dancing-script');
+        }}
+      >
+        Dancing Script
+      </button>
+      <button
+        onClick={() => {
+          setDynamicClass('f-bebas-neue');
+        }}
+      >
+        Bebas Neue
+      </button>
+    </>
   );
 };
