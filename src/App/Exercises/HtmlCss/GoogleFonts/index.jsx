5604eb9722a5f480771bd506de97aa44443bdd0b
Michał Hadrysiak <michal.hadrysiak@indiebi.com>
2023-07-15 11:46:50 +0200

    google fonts

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+export const GoogleFonts = () => {
+  return (
+    <p className="f-roboto">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non
+      lorem mollis, tempor nisl in, ultricies mauris. Nunc malesuada varius
+      rhoncus. Vestibulum elementum tristique suscipit. Morbi porttitor vel
+      ipsum eu venenatis. Ut ligula erat, mattis ut dapibus eget, malesuada ut
+      odio. Fusce venenatis interdum tellus, non consectetur erat ullamcorper
+      et. Donec dictum cursus elementum. Morbi dolor eros, rutrum sed arcu sit
+      amet, cursus tempus sem. Duis velit nibh, tempus vel ex eu, facilisis
+      viverra nisi. Suspendisse et orci tempus ligula iaculis feugiat. Integer
+      at nisi erat. Suspendisse potenti. Pellentesque laoreet sapien erat, nec
+      malesuada nulla auctor euismod. Nulla porta mauris ut nunc ornare, id
+      tristique purus accumsan. Donec at ante ipsum.
+    </p>
+  );
+};
