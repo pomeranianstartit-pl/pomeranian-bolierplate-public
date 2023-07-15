3918f0f517fcb03a4f33aff4c1747631033d91e3
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-07-15 11:47:15 +0200

    Ćwiczenie z czcionkami

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
@@ -0,0 +1,44 @@
+import './styles.css';
+
+export const GoogleFonts = () => {
+  return (
+    <>
+      <p class="font-lato">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        quidem nobis odio voluptatum quam molestiae architecto, nulla sequi.
+        Mollitia consectetur perspiciatis vel sed omnis, quam fugit debitis
+        consequatur dolorum similique? Quisquam, accusantium! Officiis eos,
+        laborum veritatis quidem temporibus quis a cum harum obcaecati maiores
+        libero velit voluptates itaque quisquam quasi aperiam enim labore quo,
+        aut neque sunt aspernatur similique recusandae. Tempore consequuntur,
+        inventore earum eius, id distinctio, est nobis aspernatur aperiam quo
+        error blanditiis! Nobis, laboriosam voluptate. Necessitatibus facilis
+        accusantium placeat voluptatibus, voluptate laudantium, assumenda
+        laborum vero animi ipsum ab. Nesciunt qui reiciendis rerum ex vero
+        tempora dolor deleniti consequuntur eveniet, voluptate quidem vel
+        quisquam quibusdam exercitationem dolorem alias! Perferendis voluptatem
+        obcaecati deserunt minima aspernatur ut beatae dolorem sequi officiis.
+        Corporis, porro facilis reiciendis itaque architecto perferendis
+        excepturi labore explicabo debitis assumenda! Dicta illo, voluptas vero
+        nobis, pariatur harum modi perferendis nihil distinctio ex aliquam
+        incidunt amet voluptatibus voluptatum optio? Nisi eius molestias
+        explicabo, sint quaerat deleniti culpa ipsum rem quibusdam ex non natus
+        repellendus sed iusto deserunt debitis magni eos modi, possimus omnis
+        praesentium accusantium inventore. Doloribus, ipsum est! Repellendus
+        neque suscipit, minima nam, dolores voluptate dignissimos iusto
+        consectetur officiis sit natus deleniti! Nulla dolore ab iste veritatis
+        nemo, numquam tempore in dolor voluptates incidunt libero ratione
+        voluptatum adipisci? Ipsa asperiores harum, veritatis temporibus aliquam
+        fugiat autem, fuga ab atque eum aspernatur eius numquam tenetur
+        consectetur tempore quam pariatur adipisci molestias voluptas possimus
+        similique vero sapiente maxime aut? Ut? Expedita reprehenderit modi nam
+        voluptate voluptatem itaque vitae. Amet sint corrupti, error nam in
+        eligendi, sed adipisci ad commodi accusantium cum deleniti, laudantium
+        voluptas saepe excepturi quasi placeat ut numquam. Consequatur
+        praesentium ab ex. Excepturi in saepe, voluptatum qui nesciunt natus
+        perspiciatis hic facere quis quia ab, non minima consequuntur. Placeat
+        natus earum aut voluptates dolores atque accusantium quo eaque?
+      </p>
+    </>
+  );
+};
