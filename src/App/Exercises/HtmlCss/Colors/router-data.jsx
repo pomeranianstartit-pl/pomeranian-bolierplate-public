901aa6fb484dcdbb65b68970f0d84e57506c81ea
Wojciech D W <w.d.wisniewski@gmail.com>
2023-07-15 13:36:44 +0200

    colors

---
+++ src/App/Exercises/HtmlCss/Colors/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Colors } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Kolort CSS',
+  date: '15-07-2023',
+  linkLabel: 'Kolory CSS',
+  blockNo: 1,
+  element: <Colors />,
+  tags: ['css', 'colors'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as Selectors } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as CssAnimations } from './CssAnimations/router-data';
+import { blockRouterMetaData as Colors } from './Colors/router-data';
 
 export const blockRouterMetaData = [
     blockRouterMetaData121220231,
     Selectors,
     CssAnimations,
+    Colors,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
