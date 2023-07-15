565b2d7b21c53054d117b81d34f377b1f7fc81eb
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-15 11:01:23 +0000

    blok11

---
+++ src/App/Exercises/HtmlCss/CssAnimations/router-data.jsx
@@ -0,0 +1,10 @@
+import { CssAnimations } from './index';
+
+export const blockRouterMetaData = {
+  path: 'css-animations',
+  date: '12-12-2023',
+  linkLabel: 'Css Animations',
+  blockNo: 1,
+  element: <CssAnimations />,
+  tags: ['attribute', 'text', 'css', 'animations'],
+};
