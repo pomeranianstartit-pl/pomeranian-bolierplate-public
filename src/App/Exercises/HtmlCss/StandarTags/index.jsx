838354560c410372368e6c16eb0f6733b2d35189
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-15 11:02:07 +0200

    new styles added - making divs simillar

--- src/App/Exercises/HtmlCss/StandarTags/index.jsx
+++ src/App/Exercises/HtmlCss/StandarTags/index.jsx
@@ -3,29 +3,82 @@ import './styles.css';
 export const StandarTags = () => {
   return (
     <div>
-      <h1>title</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
-        doloremque quo numquam impedit vero consectetur minus ea nam incidunt
-        sapiente laborum, distinctio aspernatur eaque, tempore minima ex.
-        Placeat, nesciunt ipsam? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Quidem numquam iure ut, esse totam provident ab, modi
-        eius quod consequatur odio possimus dicta suscipit autem eveniet sint.
-        Minima, repudiandae dignissimos! Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Qui placeat aliquid id. Sunt fuga rerum consectetur
-        non dicta est magnam inventore nobis minus natus magni cum, temporibus
-        deserunt vitae ab! Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Aperiam consectetur enim impedit officiis in rem? Nobis
-        repudiandae ex, vero deserunt ea hic, culpa facere reiciendis minima
-        alias, porro inventore in? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Quaerat, repellat, suscipit corporis nesciunt totam
-        placeat asperiores nostrum obcaecati deserunt aperiam hic? Quaerat sunt
-        laboriosam porro magni beatae unde accusamus eaque. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Ipsa ullam repellendus odit,
-        veritatis, nulla quo non labore suscipit quisquam aperiam esse
-        recusandae, maxime quibusdam autem! Reprehenderit animi numquam ipsum
-        enim.
-      </p>
+      <div class="original-tags">
+        <h1>Tytuł</h1>
+        <small>05.2023</small>
+        <h3>
+          H<sub>2</sub>O is good for you
+        </h3>
+        <p>
+          Staying <b>hydrated</b> is one of the best things you can do for your
+          overall health. Staying <b>hydrated</b> can help support physical
+          performance, prevent headaches and constipation, and more.
+        </p>
+        <h3>
+          25<sup>th</sup> birthday
+        </h3>
+        <p>
+          Make the next birthday you celebrate a special one with a hand-picked
+          happy birthday <q>quote</q> or wish that will surely make that special
+          someone’s day a great day. Whether you’re celebrating your birthday or
+          someone else’s, you can rely on our list of <i>happy birthday</i>{' '}
+          <q>quotes</q> to help put a smile on someone’s face. From
+          inspirational to funny to cute sayings, browse our list of{' '}
+          <q>quotes</q> to find one that resonates with you. <del>Nothing</del>{' '}
+          can stop you!
+        </p>
+        <h3>
+          Why not to use <ins>lists</ins> ?
+        </h3>
+        <p>
+          <h5>
+            A lot of things to do <i>#todo</i>
+          </h5>
+          <ul>
+            <li>Todo 1</li>
+            <li>Todo 2</li>
+            <li>Todo 3</li>
+          </ul>
+        </p>
+      </div>
+      <div class="faked-tags">
+        <div className="primary-title">Tytuł</div>
+        <div>05.2023</div>
+        <div className="primary-title">
+          H<span>2</span>O is good for you
+        </div>
+        <div className="primary-text">
+          Staying <span>hydrated</span> is one of the best things you can do for
+          your overall health. Staying <span>hydrated</span> can help support
+          physical performance, prevent headaches and constipation, and more.
+        </div>
+        <div className="primary-title">
+          25<sup>th</sup> birthday
+        </div>
+        <div>
+          Make the next birthday you celebrate a special one with a hand-picked
+          happy birthday <span>quote</span> or wish that will surely make that
+          special someone’s day a great day. Whether you’re celebrating your
+          birthday or someone else’s, you can rely on our list of{' '}
+          <span>happy birthday</span> <span>quotes</span> to help put a smile on
+          someone’s face. From inspirational to funny to cute sayings, browse
+          our list of <span>quotes</span> to find one that resonates with you.{' '}
+          <span>Nothing</span> can stop you!
+        </div>
+        <div className="primary-title underline">
+          Why not to use <span>lists</span> ?
+        </div>
+        <div>
+          <div className="secondary-title">
+            A lot of things to do <span>#todo</span>
+          </div>
+          <ul className="display-list">
+            <li>Todo 1</li>
+            <li>Todo 2</li>
+            <li>Todo 3</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
