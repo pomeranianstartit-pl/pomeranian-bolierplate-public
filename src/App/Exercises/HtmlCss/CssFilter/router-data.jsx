08d139286682193ad17c7afe45aa9f068f112398
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-15 13:32:27 +0000

    cwiczenia dot kolorow, zdjec, filmow

---
+++ src/App/Exercises/HtmlCss/CssFilter/router-data.jsx
@@ -0,0 +1,10 @@
+import { CssFilter } from './index';
+
+export const blockRouterMetaData = {
+  path: 'css-filter',
+  date: '15-07-2023',
+  linkLabel: 'Filtry w Css',
+  blockNo: 12,
+  element: <CssFilter />,
+  tags: ['css', 'filter'],
+};
---
+++ src/App/Exercises/HtmlCss/MediaFiles/router-data.jsx
@@ -0,0 +1,10 @@
+import { MediaFiles } from './index';
+
+export const blockRouterMetaData = {
+  path: 'media-files',
+  date: '15-07-2023',
+  linkLabel: 'Pliki multimedialne',
+  blockNo: 12,
+  element: <MediaFiles />,
+  tags: ['html', 'video', 'audio', 'iframe'],
+};
