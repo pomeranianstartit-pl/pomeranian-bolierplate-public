c167ce3bcc0a07a399a09802dbe8ae9d6faed6f8
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-07-15 12:57:27 +0200

    CSS animations

---
+++ src/App/Exercises/HtmlCss/CssAnimations/router-data.jsx
@@ -0,0 +1,10 @@
+import { CssAnimations } from './index';
+
+export const blockRouterMetaData = {
+  path: 'css-animations',
+  date: '15-07-2023',
+  linkLabel: 'Animacje CSS',
+  blockNo: 1,
+  element: <CssAnimations />,
+  tags: ['attribute', 'text', 'css', 'animations'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -5,6 +5,7 @@ import { blockRouterMetaData as Selectors } from './SelectorsAndCascade/router-d
 import { blockRouterMetaData as TextFundaments } from './TextFundaments/router-data';
 import { blockRouterMetaData as StandardTags } from './StandardTags/router-data';
 import { blockRouterMetaData as GoogleFonts } from './GoogleFonts/router-data';
+import { blockRouterMetaData as CssAnimations } from './CssAnimations/router-data';
 
 export const blockRouterMetaData = [
     blockRouterMetaData121220231,
@@ -12,5 +13,6 @@ export const blockRouterMetaData = [
     TextFundaments,
     StandardTags,
     GoogleFonts,
+    CssAnimations,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
