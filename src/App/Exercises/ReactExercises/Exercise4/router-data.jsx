bc0c4882a6b12f38d165471d6636550166b19747
Marianna Palka <marianna.palka02@gmail.com>
2023-07-15 21:04:04 +0200

    Dodano dwie wersje ćwiczenia 4

---
+++ src/App/Exercises/ReactExercises/Exercise4/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise4 } from './exercise4';
+
+export const Exercise4MetaData = {
+  path: 'exercise4',
+  date: '12-12-2023',
+  linkLabel: 'Ćwiczenie 4',
+  blockNo: 1,
+  element: <Exercise4 />,
+  tags: ['components', 'import', 'export'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -8,10 +8,16 @@ import { Exercise2MetaData } from './Exercise2/router-data';
 
 import { Exercise3MetaData } from './Exercise3/router-data';
 
+import { Exercise4MetaData } from './Exercise4/router-data';
+
+import { Exercise4AdvancedMetaData } from './Exercise4Advanced/router-data';
+
 export const blockRouterMetaData = [
     SubRouteExampleMetaData,
     Exercise1MetaData,
     Exercise2MetaData,
     Exercise3MetaData,
+    Exercise4MetaData,
+    Exercise4AdvancedMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
