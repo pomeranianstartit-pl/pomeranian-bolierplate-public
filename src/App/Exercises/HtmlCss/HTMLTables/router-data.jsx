dd18fff84e0561a43fe61cae5cfed5ff39be0583
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-16 14:33:35 +0200

    tables added

---
+++ src/App/Exercises/HtmlCss/HTMLTables/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { HTMLTables } from './index';
+
+export const blockRouterMetaData = {
+  path: 'html-tables',
+  date: '16-07-2023',
+  linkLabel: 'Pierwsze tabelki',
+  blockNo: 14,
+  element: <HTMLTables />,
+  tags: ['tables', 'HTML', 'CSS'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -12,6 +12,7 @@ import { blockRouterMetaData as Backgrounds } from './Backgrounds/router-data';
 import { blockRouterMetaData as CssFilter } from './CssFilter/router-data';
 import { blockRouterMetaData as MediaFiles } from './MediaFiles/router-data';
 import { blockRouterMetaData as BoxModel } from './BoxModel/router-data';
+import { blockRouterMetaData as HTMLTables } from './HTMLTables/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   Selectors,
@@ -25,6 +26,7 @@ export const blockRouterMetaData = [
   CssFilter,
   MediaFiles,
   BoxModel,
+  HTMLTables,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
