a25dd3fa97eaae40c0038aaa6a8bc5be814d28bc
Marianna Palka <marianna.palka02@gmail.com>
2023-07-16 13:45:40 +0200

    Dodano seksję 'Selektory i kaskadowość' oraz zakładkę Blog.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Dashboard } from './App/Dashboard';
 import { Exercises } from './App/Exercises';
 import { CV } from './App/CV';
 import { Calendar } from './App/Calendar';
+import { Blog } from './App/Blog';
 
 export function App() {
   return (
@@ -21,6 +22,7 @@ export function App() {
           <Route path="exercises/*" element={<Exercises />} />
           <Route path="blocks/*" element={<Blocks />} />
           <Route path="calendar/*" element={<Calendar />} />
+          <Route path="blog/*" element={<Blog />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,5 +2,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as Selectors } from './SelectorsAndCascade/router-data';
+
+export const blockRouterMetaData = [blockRouterMetaData121220231, Selectors];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
