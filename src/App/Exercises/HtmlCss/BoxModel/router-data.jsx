5874e165feabb8f1c16f93f7849438b4d15ae3aa
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-16 10:48:42 +0200

    added boxModel component, tested nesting styles

---
+++ src/App/Exercises/HtmlCss/BoxModel/router-data.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BoxModel } from './index';
+
+export const blockRouterMetaData = {
+  path: 'box-model',
+  date: '16-07-2023',
+  linkLabel: 'Box Model',
+  blockNo: 13,
+  element: <BoxModel />,
+  tags: [
+    'css',
+    'box-model',
+    'display',
+    'paddding',
+    'margin',
+    'border',
+    'box-sizing',
+    'overflow',
+  ],
+};
