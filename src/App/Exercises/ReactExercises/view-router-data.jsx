6aa33f5c4db5e611f84fc64b3cc837c1764fdb34
Joanna Bedra <joannabedra84@gmail.com>
2023-07-17 20:39:46 +0200

    arrayrendering

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -5,11 +5,13 @@ import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Cwiczenie1MetaData } from './Cwiczenie1/router-data';
 import { TextFundamentalsMetaData } from '../HtmlCss/TextFundamentals/router-data';
 import { GoogleFontsMetaData } from '../HtmlCss/Google-fonts/router-data';
+import { ArrayRenderingMetaData } from '../HtmlCss/ArrayRendering/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Cwiczenie1MetaData,
   TextFundamentalsMetaData,
   GoogleFontsMetaData,
+  ArrayRenderingMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
