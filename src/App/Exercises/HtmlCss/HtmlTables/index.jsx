5d5fd3afaf4c49adcb7cbccbea4f66f6e03b9dec
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-17 18:02:35 +0200

    s

---
+++ src/App/Exercises/HtmlCss/HtmlTables/index.jsx
@@ -0,0 +1,48 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div className='html-tables'>
+      <table>
+        <tr>
+          <th>Plastic</th>
+          <th>Acetone</th>
+          <th>Flame test </th>
+          <th>Heat </th>
+          <th>Crease color </th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>No effect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Softened</td>
+          <td>No change</td>
+          <td>No change</td>
+          <td>White</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>No effect</td>
+          <td>Red color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>No effect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>Nosne</td>
+        </tr>
+
+      </table>
+
+
+    </div>
+  );
+}
