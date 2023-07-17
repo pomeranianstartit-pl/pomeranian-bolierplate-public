45564e7275e39d5b37f8e0d951c9b3330d9c6542
Olga Ochman <olgaochman346@gmail.com>
2023-07-17 17:43:52 +0200

    html tables exercise

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -8,9 +8,10 @@ import { blockRouterMetaData as backgroundsMetaData } from './Backgrounds/router
 import { blockRouterMetaData as cssFilterMetaData } from './CssFilter/router-data';
 import { blockRouterMetaData as googleFonts } from './Google-fonts/router-data';
 import { blockRouterMetaData as BoxModelExercise } from './BoxModel/router-data';
+import { blockRouterMetaData as HtmlTablesMetaData } from './HtmlTables/router-data';
 
 export const blockRouterMetaData = [blockRouterMetaData121220231, googleFonts, colorsMetaData, imageFilesMetaData,
-     backgroundsMetaData, cssFilterMetaData, selectorsAndCascade, BoxModelExercise];
+     backgroundsMetaData, cssFilterMetaData, selectorsAndCascade, BoxModelExercise, HtmlTablesMetaData];
 
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
