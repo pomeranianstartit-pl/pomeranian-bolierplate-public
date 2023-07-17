1748e4bbcfb2811c4ac876a7f36b7ba239e86e86
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-17 14:26:17 +0200

    Omówienie box modelu, css units(px, em, rem), flaga-isVisible

---
+++ src/App/Exercises/HtmlCss/BoxModel/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'box-model',
+  date: '17-07-2023',
+  linkLabel: 'BoxModel',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['boxmodel'],
+};
