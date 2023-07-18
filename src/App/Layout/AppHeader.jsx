d6781c5e591224ba78f030783ff29eb1b751859f
Anna Ochocka <annoch22@gmail.com>
2023-07-18 18:31:59 +0200

    arrowIcon

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,14 +3,20 @@ import React from 'react';
 import './styles/header.css';
 
 import { Logo } from '../Components/Logo/Logo';
+import { ArrowIcon } from '../Components/Icons/ArrowIcon';
 
 export function AppHeader() {
   return (
     <header>
       <div>
         {/* <Logo></Logo> */}
-        <Logo />
-        <div className="header-title">Witaj nagłówku</div>
+
+        <div className="header-title">
+          <Logo />
+        </div>
+        <div className="header-user">
+          <ArrowIcon />
+        </div>
       </div>
     </header>
   );
