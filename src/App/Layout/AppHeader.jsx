45e0ed0a643b82b4d3e05d172af8f5c8bf4e721e
Olga Ochman <olgaochman346@gmail.com>
2023-07-18 19:27:59 +0200

    header styling exercise

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,19 +1,66 @@
 import React from 'react';
-
+import {SettingIcon} from '../Components/Icons/SettingIcon';
 import './styles/header.css';
+import arrowDownSvg from '../Images/toggle-arrow.svg';
 
 import {Logo} from '../Components/Logo/Logo';
 
-export function AppHeader() {
-  return (
-    <header>
-      <div>
 
-       <Logo />
-        <div className="header-title">Witaj nagłówku!</div>
-      </div>
-    
-    
-    </header>
-  );
-}
+
+  export function AppHeader() {
+
+    function handleClickButton(message) {
+  
+      console.log(message);
+  
+    }
+  
+    return (
+  
+      <header>
+  
+        <Logo />
+  
+        <div className="header-info">
+  
+          <button
+  
+            onClick={() => handleClickButton('I am settings button!')}
+  
+            className="header-settings-btn"
+  
+          >
+  
+            <SettingIcon/>
+  
+          </button>
+  
+          <div className="header-image-placeholder"></div>
+  
+          <div className="header-personal-info">
+  
+            <p>Olga</p>
+  
+            <p>kursantka</p>
+  
+          </div>
+  
+          <button
+  
+            onClick={() => handleClickButton('I am dropdown button!')}
+  
+            className="header-dropdown-btn"
+  
+          >
+  
+            <img src={arrowDownSvg} alt="dropdown button" />
+  
+          </button>
+  
+        </div>
+  
+      </header>
+  
+    );
+  
+  }
