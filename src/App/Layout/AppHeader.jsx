ff74c9f598a637143ccea5adf7b2650276a85bad
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-18 18:25:37 +0200

    Created header with flex

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 
 import './styles/header.css';
 
-import {Logo} from '../Components/Logo/Logo';
-
-
+import { Logo } from '../Components/Logo/Logo';
+import { SettingIcon } from '../Components/Icons/SettingIcon';
+import { ArrowDown } from '../Components/Icons/ArrowDown';
 
 export function AppHeader() {
   return (
     <header>
-      <div>
-        <Logo />
-        {/* <div className="header-title">Witaj nagłówku!</div> */}
-      </div>
+      <>
+        <div className="header-left">
+          <Logo />
+        </div>
+        <div className="header-right">
+          {/* <SettingIcon/> */}
+          <SettingIcon />
+          <div className="mid_component">
+            <div className="circle"></div>
+            <div className="name_rank">
+              <p className="name">Krzysztof</p>
+              <p className="rank_1">kursant</p>
+            </div>
+          </div>
+          <ArrowDown />
+        </div>
+      </>
     </header>
   );
 }
