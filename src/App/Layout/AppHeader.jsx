4f4ea7550ad886ce5fadd61b95182e9dd85e478a
Anna Ochocka <annoch22@gmail.com>
2023-07-18 19:29:25 +0200

    Header bar

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -4,18 +4,30 @@ import './styles/header.css';
 
 import { Logo } from '../Components/Logo/Logo';
 import { ArrowIcon } from '../Components/Icons/ArrowIcon';
+import { SettingIcon } from '../Components/Icons/SettingIcon';
 
 export function AppHeader() {
   return (
     <header>
-      <div>
+      <div className="naglowek">
         {/* <Logo></Logo> */}
 
         <div className="header-title">
           <Logo />
         </div>
-        <div className="header-user">
-          <ArrowIcon />
+        <div>
+          <div className="settings-button">
+            <button>
+              <SettingIcon />
+            </button>
+          </div>
+          <p className="name">Anna</p>
+          <p className="kursant">kursant</p>
+          <div className="header-user">
+            <button>
+              <ArrowIcon />
+            </button>
+          </div>
         </div>
       </div>
     </header>
