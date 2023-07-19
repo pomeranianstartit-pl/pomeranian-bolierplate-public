8c5867cc263d5ede8713644c6b0222d18b8a4e0c
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-07-19 15:52:20 +0200

    Array render

--- src/App/Exercises/HtmlCss/ArrayRendering/ArrayRendering.jsx
+++ src/App/Exercises/HtmlCss/ArrayRendering/ArrayRendering.jsx
@@ -2,9 +2,32 @@ import './styles.css';
 
 
 export const ArrayRendering = () => {
-  return (
-    <article>
 
-    </article>
+  const carsArray = ['Saab', 'Volvo', 'BMW', 'Skoda', 'Mercedes']
+
+  function mojaFunkcja(element) {
+
+    return <li>{element}</li>
+  }
+  // zmienna.map(mojaFunkcja)
+
+  // console.log(array.length);
+
+  // const array1 = [1, 2, 3, 4]
+  // const map1 = array1.map(function (e) {
+  //   return e  3
+  // })
+  // console.log(map1);
+  // console.log(zmienna);
+
+  const carsArray2 = carsArray.map(mojaFunkcja)
+  console.log(carsArray2);
+  return (
+    <div>
+      <h2>Renderowanie z tablicy po map</h2>
+      <ul>
+        {carsArray2}
+      </ul>
+    </div>
   );
 };
