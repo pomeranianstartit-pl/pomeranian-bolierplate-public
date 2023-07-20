22e2161ea1c0d62bdaf3b356511bdc698efa1b7e
Gabriela Koronkiewicz <gabrielak2k23@gmail.com>
2023-07-20 17:21:30 +0200

    heder zmiany

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,13 +3,26 @@ import React from 'react';
 import './styles/header.css';
 
 import { Logo } from '../Components/Logo/Logo';
+import { SettingIcon } from '../Components/Icons/SettingIcon'
+import { default as toggleArrowSvg } from '../Images/toggle-arrow.svg'
+import { default as ellipseImageSvg } from '../Images/ellipse.svg'
 
 export function AppHeader() {
   return (
     <header>
-      <div>
-        <Logo />
-        <div className="header-title"></div>
+      <Logo />
+      <div className="header-info">
+        <SettingIcon />
+        <div>
+          <img src={ellipseImageSvg} alt="name photo"></img>
+        </div>
+        <div>
+          <p>Gabriela</p>
+          <p>kursantka</p>
+        </div>
+        <div>
+          <img src={toggleArrowSvg} alt="dropdown button"></img>
+        </div>
       </div>
     </header>
   );
