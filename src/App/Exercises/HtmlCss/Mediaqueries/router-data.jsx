d7b66cd23109cb3acb31aa9b6773f2999847e3b1
Iwona <iwonkabigos@gmail.com>
2023-07-20 21:22:05 +0200

    zadanko do przecwiczenia

---
+++ src/App/Exercises/HtmlCss/Mediaqueries/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Mediaqueries } from './index';
+
+export const blockRouterMetaData = {
+  path: 'media-queries',
+  date: '20-07-2023',
+  linkLabel: 'Mediaqueries',
+  blockNo: 1,
+  element: <Mediaqueries />,
+  tags: ['css', 'media'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -14,6 +14,7 @@ import { blockRouterMetaData as TableTenisMetaData } from './TableTenis/router-d
 import { blockRouterMetaData as ArrayRenderingMetaData } from './ArrayRendering/router-data';
 import { blockRouterMetaData as ArrayRenderToTableMetaData } from './ArrayRenderToTable/router-data';
 import { blockRouterMetaData as FloatsAndPositioningMetaData } from './FloatsAndPositioning/router-data';
+import { blockRouterMetaData as MediaqueriesMetaData } from './Mediaqueries/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
@@ -30,5 +31,6 @@ export const blockRouterMetaData = [
   ArrayRenderingMetaData,
   ArrayRenderToTableMetaData,
   FloatsAndPositioningMetaData,
+  MediaqueriesMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
