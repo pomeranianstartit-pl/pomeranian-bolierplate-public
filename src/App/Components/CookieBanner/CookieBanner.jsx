63db7e8e7b1838b312b0e93c5b0ce2950053273f
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-20 19:51:06 +0200

    Finished cookie banner. It hides after user accept cookies

--- src/App/Components/CookieBanner/CookieBanner.jsx
+++ src/App/Components/CookieBanner/CookieBanner.jsx
@@ -3,48 +3,55 @@ import './styles.css';
 import React, { useState } from 'react';
 
 export const CookieBanner = () => {
-  const [overlayClassName, setOverlayClassName] = useState('overlay');
-  const [showCookieBanner, setShowCookieBanner] = useState(true);
 
-  function setCookieState() {
-    setOverlayClassName('');
-    setShowCookieBanner(false);
-    localStorage.setItem('cookieState', 1);
+  const currentBannerState = localStorage.getItem('cookie-banner');
+  const [showBanner, setShowBanner] = useState(!currentBannerState);
+
+  // function setCookieState() {
+  //   setOverlayClassName('');
+  //   setDisplayCookieBanner(false);
+  //   localStorage.setItem('cookieState', 1);
+  // }
+
+  // function getCookieState() {
+  //   return localStorage.getItem('cookieState');
+  // }
+
+  function hideBanner() {
+    setShowBanner(0);
+    localStorage.setItem('cookie-banner', 1);
   }
 
-  function getCookieState() {
-    return localStorage.getItem('cookieState');
+  if (!showBanner) {
+    return null;
   }
+
   return (
     <>
-      {showCookieBanner && (
-        <>
-          <div className={overlayClassName}></div>
-          <div className="cookieBanner">
-            <div className="section-left">
-              <CookieIcon />
-              <h3 className="cookies-title">Pozwól na pliki cookies</h3>
-              <p className="cookies-text">
-                Nasza strona korzysta z ciasteczek, które umożliwiają poprawne
-                działanie strony i pomagają nam świadczyć usługi na najwyższym
-                poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich
-                tych plików i przejść do strony lub dostosować użycie do swoich
-                preferencji. W tym celu kliknij przycisk po prawej stronie
-                “dopasuj zgody”, aby następnie wybrać te które odpowiadają Twoim
-                indywidualnym preferencjom.
-              </p>
-            </div>
-            <div className="section-right">
-              <button onClick={setCookieState} className="btn-accept">
-                W porządku
-              </button>
-              <button onClick={getCookieState} className="btn-adjust">
-                Dopasuj zgody
-              </button>
-            </div>
-          </div>
-        </>
-      )}
+      <div className="overlay"></div>
+      <div className="cookieBanner">
+        <div className="section-left">
+          <CookieIcon />
+          <h3 className="cookies-title">Pozwól na pliki cookies</h3>
+          <p className="cookies-text">
+            Nasza strona korzysta z ciasteczek, które umożliwiają poprawne
+            działanie strony i pomagają nam świadczyć usługi na najwyższym
+            poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich
+            tych plików i przejść do strony lub dostosować użycie do swoich
+            preferencji. W tym celu kliknij przycisk po prawej stronie “dopasuj
+            zgody”, aby następnie wybrać te które odpowiadają Twoim
+            indywidualnym preferencjom.
+          </p>
+        </div>
+        <div className="section-right">
+          <button onClick={hideBanner} className="btn-accept">
+            W porządku
+          </button>
+          <button onClick={hideBanner} className="btn-adjust">
+            Dopasuj zgody
+          </button>
+        </div>
+      </div>
     </>
   );
 };
