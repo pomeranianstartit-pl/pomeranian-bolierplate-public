abfdb76df0bbc1aa814afc350dbeaa36d6beadd8
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-20 16:38:29 +0200

    Finished exercise in HTML-CSS section, floats and positioning

---
+++ src/App/Exercises/HtmlCss/FloatAndPositioning/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'floats_and_positioning',
+  date: '19-07-2023',
+  linkLabel: 'Floats and positioning',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['css', 'float', 'video'],
+};
