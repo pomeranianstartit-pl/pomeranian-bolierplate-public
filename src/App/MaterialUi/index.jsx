0d6a90e0356bd4b642a53533e4bfcca687b099a4
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-21 17:34:02 +0200

    MaterialUi

---
+++ src/App/MaterialUi/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import Button from '@mui/material/Button';
+
+export const MaterialUi = () => {
+  return (
+    <div className="MaterialUi">
+      <h1> Material Ui</h1>
+
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
