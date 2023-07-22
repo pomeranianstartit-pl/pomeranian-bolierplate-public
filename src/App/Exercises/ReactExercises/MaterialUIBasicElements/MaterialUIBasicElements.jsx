1916ea54f1ff1a89bf80653bbd7d7c1486443496
Michał Hadrysiak <michal.hadrysiak@indiebi.com>
2023-07-22 14:31:00 +0200

    MUI select

--- src/App/Exercises/ReactExercises/MaterialUIBasicElements/MaterialUIBasicElements.jsx
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/MaterialUIBasicElements.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { Button, Card } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Button,
+  Card,
+  InputLabel,
+  Select,
+  FormControl,
+  MenuItem,
+  Box,
+} from '@mui/material';
 import './style.css';
 import { CalendarIcon } from '../../../Components/Icons/CalendarIcon';
 import { ShoppingBagRounded } from '@mui/icons-material';
 
+export default function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
 export function MaterialUIBasicElements() {
+  const [age, setAge] = useState(0);
   return (
     <Card>
-      <Button variant="text" startIcon={<ShoppingBagRounded />}>
+      <Button variant="text" startIcon={<ShoppingBagRounded />} color="error">
         Add to Cart
       </Button>
       <Button variant="contained" startIcon={<CalendarIcon />}>
         Add to Cart
       </Button>
       <Button variant="outlined">Add to Cart</Button>
+      <BasicSelect />
     </Card>
   );
 }
