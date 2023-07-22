6b5121929305d2ff3d1de9b3487cb70463bc2071
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-07-22 14:06:12 +0200

    Dodanie MUI

---
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/router-data.jsx
@@ -0,0 +1,10 @@
+import { MaterialUIBasicElements } from './MaterialUIBasicElements';
+
+export const blockRouterMetaData = {
+  path: 'material-ui-basic-elements',
+  date: '22-07-2023',
+  linkLabel: 'Material design - elementy',
+  blockNo: 1,
+  element: <MaterialUIBasicElements />,
+  tags: ['Button', 'material-ui', 'React'],
+};
