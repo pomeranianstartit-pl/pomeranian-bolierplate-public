fba5fd1848c8807d2c3593d3af6207be533736bb
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-22 09:53:57 +0200

    floats

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositions/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export const FloatsAndPositioning = () => {
+  return (
+    <p className="container">
+      <div className="square">1</div>
+      <div className="square x">2</div>
+      <div className="square y">3</div>
+    </p>
+  );
+};
