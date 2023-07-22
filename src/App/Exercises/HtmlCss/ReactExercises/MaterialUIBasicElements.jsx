1b87e51e9ab7611ae134c85b0e7bc273baf1b344
Wojciech D W <w.d.wisniewski@gmail.com>
2023-07-22 14:31:18 +0200

    ćwiczenia z mui

--- src/App/Exercises/HtmlCss/ReactExercises/MaterialUIBasicElements.jsx
+++ src/App/Exercises/HtmlCss/ReactExercises/MaterialUIBasicElements.jsx
@@ -1,6 +1,32 @@
-import { Button } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+} from '@mui/material';
 import './styles.css';
+import { Fingerprint } from '@mui/icons-material';
 
 export function MaterialUIBasicElements() {
-  return <Button variant="contained">MaterialUIBasicElements</Button>;
+  return (
+    <>
+      <IconButton aria-label="fingerprint" color="secondary">
+        <Fingerprint />
+      </IconButton>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Age"
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  );
 }
