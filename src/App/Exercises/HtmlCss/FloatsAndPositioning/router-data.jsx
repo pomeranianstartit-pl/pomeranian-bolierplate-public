4680d6824f9a1dcf3f7caa1769200883123e7283
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-22 07:53:46 +0000

    cwiczenia z bloku16

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioning/router-data.jsx
@@ -0,0 +1,10 @@
+import { FloatsAndPositioning } from './index';
+
+export const blockRouterMetaData = {
+  path: `float-and-positioning`,
+  date: '22-07-2023',
+  linkLabel: 'Pozycje i opływy',
+  blockNo: 16,
+  element: <FloatsAndPositioning />,
+  tags: ['z-index', 'positioning', 'css', 'float'],
+};
