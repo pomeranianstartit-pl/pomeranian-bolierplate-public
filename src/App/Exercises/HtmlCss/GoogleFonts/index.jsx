6d9b4975f8acd3b2310edf96e829c8dd1e745d24
Marianna Palka <marianna.palka02@gmail.com>
2023-07-22 17:15:34 +0200

    Dodano interaktywną zmianę czcionki w Google Fonts

--- src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
+++ src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
@@ -1,10 +1,39 @@
 import './styles.css';
+import { useState } from 'react';
 
 export const GoogleFonts = () => {
+  const [dynamicClass, setDynamicClass] = useState('fLugarismo');
+
   return (
-    <div className="font">
-      <h1>Google Fonts</h1>
-      <p>
+    <>
+      <h1 className="fontsHeading">Google Fonts</h1>
+      <div className="buttons">
+        <button
+          className="changeButton"
+          onClick={() => {
+            setDynamicClass('fLugarismo');
+          }}
+        >
+          Lugarismo
+        </button>
+        <button
+          className="changeButton"
+          onClick={() => {
+            setDynamicClass('fBebas');
+          }}
+        >
+          Bebas Neue
+        </button>
+        <button
+          className="changeButton"
+          onClick={() => {
+            setDynamicClass('fMynerve');
+          }}
+        >
+          Mynerve
+        </button>
+      </div>
+      <p className={dynamicClass} id="fontChange">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -13,6 +42,6 @@ export const GoogleFonts = () => {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </p>
-    </div>
+    </>
   );
 };
