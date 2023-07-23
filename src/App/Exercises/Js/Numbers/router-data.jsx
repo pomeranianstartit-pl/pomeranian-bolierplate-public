264ba48b39bf00562659d27fd0099d0bc4054bc2
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-23 12:20:25 +0200

    Numbers

---
+++ src/App/Exercises/Js/Numbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './Numbers';
+
+export const blockRouterMetaData = {
+  path: 'numbers',
+  date: '12-12-2023',
+  linkLabel: 'Liczby cwiczenia',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['components'],
+};
