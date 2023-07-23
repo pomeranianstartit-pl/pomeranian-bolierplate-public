12ecd11a2f99b932a2f64c42505d7cb233caccf3
Yaroslav Sulko <sulkoyaroslav90@gmail.com>
2023-07-23 11:16:30 +0200

    Switch

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataBoolean } from './Boolean/router-data';
+import { blockRouterMetaData as blockRouterDataIfSwitch } from './IfSwitch/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataBoolean,
+  blockRouterDataIfSwitch,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
