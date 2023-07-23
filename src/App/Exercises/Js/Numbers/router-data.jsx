de46470f18f56c578e223e3a9a274efa6eed6212
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-07-23 12:24:27 +0200

    finish exercise numbers

---
+++ src/App/Exercises/Js/Numbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { Numbers } from './index';
+
+export const blockRouterMetaData = {
+  path: 'numbers',
+  date: '23-07-2023',
+  linkLabel: 'Numbers',
+  blockNo: 18,
+  element: <Numbers />,
+  tags: ['numbers'],
+};
