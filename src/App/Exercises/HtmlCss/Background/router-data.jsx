8420f13622d816a7489ad61e41af61e33a67f538
Marianna Palka <marianna.palka02@gmail.com>
2023-07-23 10:52:39 +0200

    Dodano sekcję o tłach.

---
+++ src/App/Exercises/HtmlCss/Background/router-data.jsx
@@ -0,0 +1,10 @@
+import { Background } from './index';
+
+export const BackgroundMetaData = {
+  path: 'background',
+  date: '12-12-2023',
+  linkLabel: 'Tła',
+  blockNo: 1,
+  element: <Background />,
+  tags: ['components'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,22 +1,14 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
-
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
-
 import { blockRouterMetaData as Selectors } from './SelectorsAndCascade/router-data';
-
 import { TextFundamentsMetaData } from './TextFundaments/router-data';
-
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
-
 import { blockRouterMetaData as Positioning } from './FloatsAndPositioning/router-data';
-
 import { blockRouterMetaData as PositioningExercise } from './FloatsAndPositioningExercise/router-data';
-
 import { CSSAnimationMetaData } from './CSSAnimations/router-data';
-
 import { ColorsrMetaData } from './Colors/router-data';
-
 import { ImagesFilesMetaData } from './ImagesFiles/router-data';
+import { BackgroundMetaData } from './Background/router-data';
 
 export const blockRouterMetaData = [
     blockRouterMetaData121220231,
@@ -28,5 +20,6 @@ export const blockRouterMetaData = [
     CSSAnimationMetaData,
     ColorsrMetaData,
     ImagesFilesMetaData,
+    BackgroundMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
