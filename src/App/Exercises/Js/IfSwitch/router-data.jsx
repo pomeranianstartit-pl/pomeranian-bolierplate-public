a32f22e158167b83a751e199738b3f86380d3725
Joanna Bedra <joannabedra84@gmail.com>
2023-07-24 19:32:31 +0200

    ifswitch

---
+++ src/App/Exercises/Js/IfSwitch/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'if-switch',
+  date: null,
+  linkLabel: 'if switch ćwiczenia',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['boolean', 'let', 'const'],
+};
