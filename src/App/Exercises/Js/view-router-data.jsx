d78ab6642d3ab040fbd2d569bf8279b5896db4e7
Olga Ochman <olgaochman346@gmail.com>
2023-07-24 19:31:18 +0200

    notes if statements

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as Boolean } from './Boolean/router-data';
+import { blockRouterMetaData as IfSwitch } from './IfSwitch/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   Boolean,
+  IfSwitch,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
