0667c692e8afbee9da53b3f1ab5927af953740d4
Krzysztof Nadolny <krzysztofnadolny1@gmail.com>
2023-07-24 18:25:45 +0200

    Teoria Boolean

---
+++ src/App/Exercises/Js/Boolean/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'boolean-1',
+  date: "24-07-2023",
+  linkLabel: 'Boolean ćwiczenia',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['boolean', 'let', 'const', 'true', 'false'],
+};
