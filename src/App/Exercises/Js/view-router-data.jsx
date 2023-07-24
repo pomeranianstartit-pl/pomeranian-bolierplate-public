b2a0a2a8aa5bd3165d7bde7f0de41b3cbfde7310
Iwona <iwonkabigos@gmail.com>
2023-07-24 19:31:36 +0200

    operacje warunkowe if, if elese, switch

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as BooleanMetaData } from './Boolean/router-data';
+import { blockRouterMetaData as IfSwitchMetaData } from './IfSwitch/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   BooleanMetaData,
+  IfSwitchMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
