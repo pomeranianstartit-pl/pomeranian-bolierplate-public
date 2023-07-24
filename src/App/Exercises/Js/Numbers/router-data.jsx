1eafe95ab5f7ed5cd0e508b99f849fe51bab704f
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-24 20:16:10 +0200

    numbers math

---
+++ src/App/Exercises/Js/Numbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Numbers',
+  date: null,
+  linkLabel: 'Numbers',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['numbers', 'const', 'let', 'operators'],
+};
