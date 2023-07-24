a02f8e2b0bc30d33b3d4bf35456022e2f27c6070
Olga Ochman <olgaochman346@gmail.com>
2023-07-24 18:25:12 +0200

    notes

--- src/App/Exercises/HtmlCss/ArrayRendering/index.jsx
+++ src/App/Exercises/HtmlCss/ArrayRendering/index.jsx
@@ -1,20 +1,15 @@
-
 import './styles.css';
 
-
 export function Exercise() {
-
   const cars = [
+    { id: 1, make: 'Saab', model: '92B', year: 1953, owner: 'Jon' },
 
-    {id: 1, make: "Saab", model: "92B", year: 1953, owner: "Jon"},
+    { id: 2, make: 'Volvo', model: 'C40', year: 2021, owner: 'Jenny' },
 
-    {id: 2, make: "Volvo", model: "C40", year: 2021, owner: "Jenny"},
+    { id: 3, make: 'Audi', model: 'A3', year: 2019, owner: 'Michael' },
 
-    {id: 3, make: "Audi", model: "A3", year: 2019, owner: "Michael"},
-
-    {id: 4, make: "Tesla", model: "Model S", year: 2022, owner: "Sarah"}
-
-];
+    { id: 4, make: 'Tesla', model: 'Model S', year: 2022, owner: 'Sarah' },
+  ];
 
   // Saab => <li>Saab</li>
 
@@ -26,32 +21,34 @@ export function Exercise() {
 
   // Mercedes => <li>Mercedes</li>
 
-  return <div className='array-rendering'>
-                
-        <ul>
-              {cars.map(function(car) {
-          
-                return <li key={car.id}>{car.make} {car.model} produced in {car.year}</li>;
-              })}
-              
-        </ul>
-        <table>
-          <tr>
-                <th>Make</th>
-                <th>Model</th>
-                <th>Year</th>
-                </tr>
-      
-      {cars.map(function(car) {
-return <tr key={car.id}>
-<td>{car.make}</td>
-<td>{car.model}</td>
-<td>{car.year}</td>
+  return (
+    <div className="array-rendering">
+      <ul>
+        {cars.map(function (car) {
+          return (
+            <li key={car.id}>
+              {car.make} {car.model} produced in {car.year}
+            </li>
+          );
+        })}
+      </ul>
+      <table>
+        <tr>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Year</th>
+        </tr>
 
-</tr>
-  })}
-
-</table>
- </div>;
-
+        {cars.map(function (car) {
+          return (
+            <tr key={car.id}>
+              <td>{car.make}</td>
+              <td>{car.model}</td>
+              <td>{car.year}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
