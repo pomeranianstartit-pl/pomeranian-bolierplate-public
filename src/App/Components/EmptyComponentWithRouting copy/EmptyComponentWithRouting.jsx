f4d29f03dfc707ff4620e443a5953be64c6442d5
Iwona <iwonkabigos@gmail.com>
2023-07-24 18:24:55 +0200

    Teoria Boolean, operatory logiczne

---
+++ src/App/Components/EmptyComponentWithRouting copy/EmptyComponentWithRouting.jsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export const EmptyComponentWithRouting = () => {
+  return <div>Change me X</div>;
+};
--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#bdeefa',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#3D9E1E',
+    },
+  },
+});
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
