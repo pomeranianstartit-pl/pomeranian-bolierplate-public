541e01dc822609e755e5debdee2ead87b67e0dd1
Olga Ochman <olgaochman346@gmail.com>
2023-07-25 20:53:24 +0200

    exercises

---
+++ src/App/Exercises/Js/JsStringTraining/router-data.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-string-training-route',
+
+  date: '25-12-2023',
+
+  linkLabel: 'metody stringów',
+
+  blockNo: 19,
+
+  element: <Exercise />,
+
+  tags: ['boolean', 'const', 'true', 'false'],
+};
