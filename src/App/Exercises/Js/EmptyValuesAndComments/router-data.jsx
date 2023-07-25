64e10aa1b1e9b9d8607170f4a82ce656e406111e
Magda Gadomska <m.srutkowska@gmail.com>
2023-07-25 20:06:55 +0200

    EmptyValuesAndComments

---
+++ src/App/Exercises/Js/EmptyValuesAndComments/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'EmptyValuesAndComments',
+  date: null,
+  linkLabel: 'EmptyValuesAndComments',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['js', 'empty values', 'null', 'undefined'],
+};
