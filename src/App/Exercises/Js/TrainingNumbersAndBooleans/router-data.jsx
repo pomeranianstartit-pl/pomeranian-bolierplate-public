ef310c6efaaa13f98a763fc6da0a5031a655afbb
Joanna Bedra <joannabedra84@gmail.com>
2023-07-26 16:42:25 +0200

    Empty Values and Comments

---
+++ src/App/Exercises/Js/TrainingNumbersAndBooleans/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'training_numbers_and_booleans',
+  date: '24-07-2023',
+  linkLabel: 'Ćwiczenia numbers and booleans',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['fnumbers', 'booleans'],
+};
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,15 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsBoolean } from './Boolean/router-data';
+import { blockRouterMetaData as blockRouterMetaDataJsTrainingNumbersAndBooleans } from './TrainingNumbersAndBooleans/router-data';
+import { blockRouterMetaData as blockRouterMetaDataJsEmptyValuesAndComments } from './EmptyValuesAndComments/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataJsBoolean,
+  blockRouterMetaDataJsTrainingNumbersAndBooleans,
+  blockRouterMetaDataJsEmptyValuesAndComments,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
