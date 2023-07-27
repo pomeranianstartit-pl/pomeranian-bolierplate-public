7097fc5e98b0eab90a9cbbe1b273f432b6849c41
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-07-27 18:00:31 +0200

    cookies na dwa sposoby

--- src/App/Components/CookieBanner/CookieBanner.jsx
+++ src/App/Components/CookieBanner/CookieBanner.jsx
@@ -4,38 +4,60 @@ import { useState } from 'react';
 import './styles.css';
 
 export const CookieBanner = () => {
-  function setCookies() {
-    localStorage.setItem('Cookies', 1);
-    // setCookies(getCookies());
-  }
+  // pierwsza metoda bez wstawiania value
 
-  // function getCookies() {
-
-  //   const cookies = localStorage.getItem('Cookies');
+  // const [cookiesState, setCookiesState] = useState(false);
+  // function handleCookies() {
+  //   setCookiesState(!cookiesState);
   // }
-  const [cookiesState, setCookiesState] = useState(false);
+  // if (cookiesState) {
+  //   return <></>;
+  // }
+  // else
+
+  // function getCookieState() {
+  //   return localStorage.getItem('cookieState');
+  // }
+
+  // const [cookieState, setCookieState] = useState();
+
+  // function handleCookies() {
+  //   localStorage.setItem('cookieState', 1);
+  //   setCookieState(getCookieState());
+  // }
+
+  // if (cookieState) {
+  //   return <></>;
+  // }
+
+  function getCookies() {
+    return localStorage.getItem('CookiesState');
+  }
+  const [cookiesState, setCookiesState] = useState();
+
   function handleCookies() {
-    setCookiesState(!cookiesState);
+    localStorage.setItem('CookiesState', 1);
+    setCookiesState(getCookies());
   }
   if (cookiesState) {
     return <></>;
-  } else
-    return (
-      <div className="cookies-banner-container">
-        <div className="cookies-banner">
-          <div>
-            <h5>Pozwól na pliki cookies</h5>
-            <p>
-              Nasza strona korzysta z ciasteczek, które umożliwiają poprawne
-              działanie strony i pomagają nam świadczyć usługi na najwyższym
-              poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich
-              tych plików i przejść do strony lub dostosować
-            </p>
-          </div>
-          <button className="accept-cookies" onClick={handleCookies}>
-            W porządku
-          </button>
+  }
+  return (
+    <div className="cookies-banner-container">
+      <div className="cookies-banner">
+        <div>
+          <h5>Pozwól na pliki cookies</h5>
+          <p>
+            Nasza strona korzysta z ciasteczek, które umożliwiają poprawne
+            działanie strony i pomagają nam świadczyć usługi na najwyższym
+            poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich
+            tych plików i przejść do strony lub dostosować
+          </p>
         </div>
+        <button className="accept-cookies" onClick={handleCookies}>
+          W porządku
+        </button>
       </div>
-    );
+    </div>
+  );
 };
