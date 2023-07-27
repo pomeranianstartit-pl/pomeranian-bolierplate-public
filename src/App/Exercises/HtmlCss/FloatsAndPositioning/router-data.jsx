eb1e7034ce35dd193780a10acaddc4627f564b6f
mikolaj mrozek <mikolaj.mrozek399@gmail.com>
2023-07-27 14:48:39 +0200

    Dodanie Zagadanienia Floats and Positioning i stworzenia cwiczenia

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioning/router-data.jsx
@@ -0,0 +1,10 @@
+import { FloatsAndPositioning } from '../FloatsAndPositioning/FloatsAndPositioning';
+
+export const blockRouterMetaData = {
+  path: 'floats-and-positioning',
+  date: '22-07-2023',
+  linkLabel: 'Pozycje i opływy',
+  blockNo: 1,
+  element: <FloatsAndPositioning />,
+  tags: ['z-index', 'positioning', 'css', 'float'],
+};
---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioningExercises/router-data.jsx
@@ -0,0 +1,18 @@
+import { FloatsAndPositioningExercises } from '../FloatsAndPositioningExercises/FloatsAndPositioningExercises';
+
+export const blockRouterMetaData = {
+  path: 'floats-and-positioning-exercises',
+  date: '22-07-2023',
+  linkLabel: 'Pozycje i opływy Ćwiczenia',
+  blockNo: 1,
+  element: <FloatsAndPositioningExercises />,
+  tags: [
+    'z-index',
+    'positioning',
+    'css',
+    'float',
+    'absolute',
+    'fixed',
+    'relative',
+  ],
+};
