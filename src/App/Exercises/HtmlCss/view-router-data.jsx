3085d280d53765c1215d367b04670dcfe0d0e5b7
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2023-07-27 20:21:38 +0200

    feat: dynamic fonts

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as selectorAndCascade } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as textFundamentals } from './Text-fundamentals/router-data';
+import { blockRouterMetaData as googleFonts } from './Google-fonts/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   selectorAndCascade,
   textFundamentals,
+  googleFonts,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
