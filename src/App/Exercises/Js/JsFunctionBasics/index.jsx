ca8af68b09de247eff4beacbba2bcbc629c1d46f
Michał Hadrysiak <michal.hadrysiak@indiebi.com>
2023-07-29 11:23:39 +0200

    func2

--- src/App/Exercises/Js/JsFunctionBasics/index.jsx
+++ src/App/Exercises/Js/JsFunctionBasics/index.jsx
@@ -1,64 +1,91 @@
 import './styles.css';
 
+function newObj(props) {
+  return { ...props, nested: props.nested ? newObj(props.nested) : undefined };
+}
+
+function f1(number1, number2) {}
+const f2 = () => {};
+
+function multiplyBy(factor = 1, ...rest) {
+  return rest.map((number) => number * factor).join(',');
+}
+
+function tags(...tags) {
+  return tags.join(',');
+}
+
+export function ExampleComponent(obj) {
+  const args = [3, 'react', 8, 'js', JSON.stringify({ foo: 'bar' })];
+  return (
+    <>
+      <div>{multiplyBy(obj.factor, obj.x, 3, 4, 5, 6, 7, 8)}</div>
+      <div>{tags(...args)}</div>
+    </>
+  );
+}
+
 export function Exercise() {
-  return <div>X</div>;
+  const props = {
+    factor: 5,
+    x: 10,
+    nested: { y: 7 },
+  };
+
+  const props2 = newObj(props);
+
+  props.nested.y = 13;
+  console.log(props2.nested.y);
+
+  return (
+    <>
+      <ExampleComponent {...props} />
+    </>
+  );
 }
+// // funkcja anonimowa
 
-// funkcja nazwana
-function f1() {}
+// // funkcja strzałkowa
+// const f3 = () => ({ foo: 'bar' });
 
-const x = f1();
+// f1(function () {});
+// f2(() => {});
 
-// funkcja anonimowa
+// const multiplyBy2 = (number) => {
+//   if (number === undefined) {
+//     return 'brak argumentu';
+//   }
+//   return number * 2;
+// };
 
-// funkcja strzałkowa
-const f2 = () => {
-  // ...
-  // ...
-  // ...
+// const array = [1, 2, 3];
+// array.map(multiplyBy2);
 
-  return () => {};
-};
-const f3 = () => ({ foo: 'bar' });
+// multiplyBy2(1, 2, 3, 4, 5, 6, 7);
+// multiplyBy2();
 
-f1(function () {});
-f2(() => {});
+// function funkcjaZewnętrzna() {
+//   function funkcjaWewnętrzna() {
+//     debugger;
+//     console.log('To jest funkcja wewnętrzna');
+//   }
+//   return funkcjaWewnętrzna;
+// }
 
-const multiplyBy2 = (number) => {
-  if (number === undefined) {
-    return 'brak argumentu';
-  }
-  return number * 2;
-};
+// funkcjaZewnętrzna()();
 
-const array = [1, 2, 3];
-array.map(multiplyBy2);
+// const obj = {
+//   x: {
+//     foo: 'bar',
+//     prop: {
+//       name: 'Michał',
+//     },
+//   },
+// };
 
-multiplyBy2(1, 2, 3, 4, 5, 6, 7);
-multiplyBy2();
-
-function funkcjaZewnętrzna() {
-  function funkcjaWewnętrzna() {
-    debugger;
-    console.log('To jest funkcja wewnętrzna');
-  }
-  return funkcjaWewnętrzna;
-}
-
-funkcjaZewnętrzna()();
-
-const obj = {
-  x: {
-    foo: 'bar',
-    prop: {
-      name: 'Michał',
-    },
-  },
-};
-
-const splaszcz = (obj) => {
-  let temp = {};
-  for (const key in obj) {
-    temp[key] = splaszcz(obj[key]);
-  }
-};
+// const splaszcz = (obj) => {
+//   let temp = {};
+//   for (const key in obj) {
+//     temp[key] = splaszcz(obj[key]);
+//   }
+// };
