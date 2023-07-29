66d61fbf637924eb82d838d28efe3c2503688598
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-07-29 11:09:55 +0200

    PromisesMethodsTraining

---
+++ src/App/Exercises/Js/Exercise-js-promises-methods-training/router-data.jsx
@@ -0,0 +1,10 @@
+import { PromiseMethodsTraining } from './PromiseMethodsTraining';
+
+export const PromiseMethodsTrainingMetaData = {
+  path: 'promise-methods-training',
+  date: '07.25.2023',
+  linkLabel: 'Promise methods ćwiczenie',
+  blockNo: 35,
+  element: <PromiseMethodsTraining />,
+  tags: ['promise', 'methods', 'all', 'allSettled', 'any', 'race'],
+};
