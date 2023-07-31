7f73618c1414a31b44b22849156b49a79d31275e
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-07-31 22:01:56 +0200

    Local Dev&Fetch

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -7,11 +7,13 @@ import { ReactRouterEventsMetaData } from './ReactRouterEvents/router-data';
 import { Block09MetaData } from './Block09/router-data';
 
 import { MuiMetaData } from './Mui/router-data';
+import { LocalDevAndFetchMetaData } from './LocalDevAndFetch/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactRouterEventsMetaData,
   Block09MetaData,
   MuiMetaData,
+  LocalDevAndFetchMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
