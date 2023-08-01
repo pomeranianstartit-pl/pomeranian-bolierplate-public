33b275a5f300c0cb8cb4e66664a3be348e2e960b
Iwona <iwonkabigos@gmail.com>
2023-08-01 12:52:14 +0200

    rozwiazane zadan z funkcji oraz lekcja IntervalsDate

---
+++ src/App/Exercises/Js/IntervalsDate/router-data.jsx
@@ -0,0 +1,10 @@
+import { IntervalsDate } from './index';
+
+export const blockRouterMetaData = {
+  path: 'intervals-date',
+  date: '31-07-2023',
+  linkLabel: 'Intervals Date',
+  blockNo: 1,
+  element: <IntervalsDate />,
+  tags: ['components'],
+};
