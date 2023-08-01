c0c659fcd62fca171e642477555bf4eeeeb5272e
Olga Ochman <olgaochman346@gmail.com>
2023-08-01 21:33:28 +0200

    hit the mole basics

---
+++ src/App/Exercises/Js/HitTheMole/router-data.jsx
@@ -0,0 +1,10 @@
+import { HitTheMole } from './HitTheMole';
+
+export const hitTheMoleMetaData= {
+  path: 'hit-the-mole',
+  date: null,
+  linkLabel: 'Hit The Mole',
+  blockNo: 20,
+  element: <HitTheMole />,
+  tags: ['uderz', 'kreta', 'gra', 'rendering', 'changestate'],
+};
