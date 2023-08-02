be89df40507b07a5d28180f29b7b46cd5b4cca32
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-08-02 15:47:36 +0200

    Swagger-api

---
+++ src/App/Exercises/ReactExercises/Swagger/router-data.jsx
@@ -0,0 +1,10 @@
+import { SwaggerExercise } from './index';
+
+export const SwaggerRoute = {
+  path: 'swagger',
+  date: '31-07-2023',
+  linkLabel: 'local dev z fetchem',
+  blockNo: 36,
+  element: <SwaggerExercise />,
+  tags: ['endpoint', 'fetch', 'rest', 'api', 'javascript'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactRouterEventsMetaData } from './ReactRouterEvents/router-data';
 import { Block09MetaData } from './Block09/router-data';
+import { SwaggerRoute } from './Swagger/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactRouterEventsMetaData,
   Block09MetaData,
+  SwaggerRoute,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
