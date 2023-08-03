0757b7783b6f08c71437fddee42258b3d2e464ca
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-08-03 14:59:43 +0200

    add bin icon

--- src/App/Images/IconsBinIcon/BinIcon.jsx
+++ src/App/Images/IconsBinIcon/BinIcon.jsx
@@ -1,51 +1,47 @@
 import './BinIcon.css';
-export function BinIcon ( {variant} ) {
-    const colors = {
-        normal: '#000',
-        error: '#ff0000',
-        hover: '#06a0f9',
-    };
 
-    // function getColor() {
-    //     if (variant === 'normal') {
-    //         return colors.normal;
-    //     } else if (variant === 'error') {
-    //         return colors.error;
-    //     } else if (variant === 'hover') {
-    //         return colors.hover;
-    //     } else {
-    //         return 'normal';
-    //     }
-    // }
+export function BinIcon({ isError }) {
+  const svgClassName = `bin-icon ${isError ? 'bin-icon--error' : ''}`;
 
-    // --------------
-
-    switch (variant) {
-        case 'normal':
-        return colors.normal;
-        case 'error':
-        return colors.error;
-        case 'hover':
-            return colors.hover;
-        default:
-            return colors.normal;
-    }
-
-    //---------------
-
-    // if (!variant) {
-    //     return colors.normal;
-    // }
-    // return colors[variant]; // === colors.normal
-
-    return (
-        <svg width="27" height="26" viewBox="0 0 27 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M23.6023 6.47831C19.9948 6.12081 16.3656 5.93665 12.7473 5.93665C10.6023 5.93665 8.45727 6.04498 6.31227 6.26165L4.10227 6.47831" stroke="#6B7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M10.0606 5.38413L10.2989 3.96496C10.4722 2.93579 10.6022 2.16663 12.4331 2.16663H15.2714C17.1022 2.16663 17.2431 2.97913 17.4056 3.97579L17.6439 5.38413" stroke="#6B7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M21.2731 9.90173L20.5689 20.8109C20.4497 22.5117 20.3522 23.8334 17.3297 23.8334H10.3747C7.35223 23.8334 7.25473 22.5117 7.13556 20.8109L6.4314 9.90173" stroke="#6B7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M12.0432 17.875H15.6507" stroke="#6B7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M11.144 13.5416H16.5607" stroke="#6B7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-
-    )
+  return (
+    <svg
+      className={svgClassName}
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M22.75 6.47831C19.1425 6.12081 15.5133 5.93665 11.895 5.93665C9.75 5.93665 7.605 6.04498 5.46 6.26165L3.25 6.47831"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.20825 5.38413L9.44659 3.96496C9.61992 2.93579 9.74992 2.16663 11.5808 2.16663H14.4191C16.2499 2.16663 16.3908 2.97913 16.5533 3.97579L16.7916 5.38413"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M20.4208 9.90173L19.7166 20.8109C19.5974 22.5117 19.4999 23.8334 16.4774 23.8334H9.52243C6.49993 23.8334 6.40243 22.5117 6.28327 20.8109L5.5791 9.90173"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.1909 17.875H14.7984"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.2917 13.5416H15.7084"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
 }
