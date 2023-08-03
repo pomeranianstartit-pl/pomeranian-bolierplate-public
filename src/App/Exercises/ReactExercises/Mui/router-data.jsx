9165ca50c239cb12b0ad26dca3879a2991f9704d
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-08-03 14:19:42 +0200

    add mui

---
+++ src/App/Exercises/ReactExercises/Mui/router-data.jsx
@@ -0,0 +1,10 @@
+import { Mui } from './Mui';
+
+export const MuiMetaData = {
+  path: 'Mui',
+  date: '13-06-2023',
+  linkLabel: 'MUI Ćwiczenie',
+  blockNo: 1,
+  element: <Mui />,
+  tags: ['Mui'],
+};
