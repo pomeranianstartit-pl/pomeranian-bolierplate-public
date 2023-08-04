c71e254c294f03f26204d779a78d2f0f6f9e7e43
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2023-08-04 20:53:08 +0200

    Nowy komponent RoundImage

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -2,6 +2,7 @@ import './styles.css';
 import logoPomeranian from './pomeranian-icon.png';
 import logoIkea from './Ikea_logo.svg.png';
 import zdjecie from './zdjecie.jpg';
+import { RoundImage } from '../../../Components/RoundImage/RoundImage';
 
 export function ImageFiles() {
   return (
@@ -10,6 +11,9 @@ export function ImageFiles() {
       <img src={logoIkea} alt="Ikea logo" />
       <img src={zdjecie} alt="Zdjęcie" />
       <img src="#" alt="Brak zdjęcia" />
+
+      <RoundImage src={zdjecie} size="150px" />
+      <RoundImage src={zdjecie} size="300px" />
     </div>
   );
 }
