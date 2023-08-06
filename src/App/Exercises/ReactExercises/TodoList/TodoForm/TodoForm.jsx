38fc5e3ea3351fac5b55451249d20486cdaaa038
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-08-06 20:24:33 +0200

    working todo with addition of refresh if error

--- src/App/Exercises/ReactExercises/TodoList/TodoForm/TodoForm.jsx
+++ src/App/Exercises/ReactExercises/TodoList/TodoForm/TodoForm.jsx
@@ -3,56 +3,118 @@ import './TodoForm.css';
 import axios from 'axios';
 import { BASE_API_URL } from '../TodoList';
 
-export function TodoForm({ setAddingMode }) {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [note, setNote] = useState('');
+export function TodoForm({
+  handleFetchTodoData,
+  data,
+  setFormVisible,
+  setIdForEdit,
+}) {
+  const isEditMode = Boolean(data);
+
+  const [title, setTitle] = useState(isEditMode ? data.title : '');
+  const [author, setAuthor] = useState(isEditMode ? data.author : '');
+  const [note, setNote] = useState(isEditMode ? data.note : '');
+
+  const [prevTitle, setPrevTitle] = useState('');
 
   const [isError, setError] = useState(false);
-  const [isSucces, setIsSucces] = useState(false);
+  const [isSucces, setSucces] = useState(false);
+  const [isInProgress, setInProgress] = useState(false);
 
   //Zadanie: obsłuż errory i znikanie, komunikaty. etc
 
   async function handleCreateTodo() {
-    console.log(title, author, note);
     try {
+      setInProgress(true);
       await axios.post(BASE_API_URL + '/api/todo', {
         title: title,
         note: note,
         author, // === author: author
       });
+
+      setPrevTitle(title);
+
+      setTitle('');
+      setAuthor('');
+      setNote('');
+
+      setSucces(true);
     } catch (error) {
       console.log('error', error);
+      setError(true);
+    } finally {
+      setInProgress(false);
     }
   }
 
-  const isReadyToSend =
-    title.length > 0 && author.length > 0 && note.length > 0;
-  console.log(isReadyToSend);
+  async function handleEditTodo() {
+    console.log(title, author, note);
+    try {
+      setInProgress(true);
+      await axios.put(BASE_API_URL + '/api/todo/' + data.id, {
+        title, // skrócony zapis w funkcji
+        note,
+        author,
+      });
+      setSucces(true);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setInProgress(false);
+    }
+  }
+
+  function resetSuccesMessage() {
+    setSucces(false);
+  }
+
+  //-------------------------prosta walidacj pól formularza-------------------------
+
+  const isTitleToLong = title.length > 15;
+  const isAuthorToLong = author.length > 15;
+  const isNoteToLong = note.length > 50;
+
+  const isFormValid = !isTitleToLong && !isAuthorToLong && !isNoteToLong;
+
+  const isTitleGiven = title.length > 0;
+  const isAuthorGiven = author.length > 0;
+  const isNoteGiven = note.length > 0;
+
+  const isFormFilled = isTitleGiven && isAuthorGiven && isNoteGiven;
+
+  const isReadyToSend = isFormFilled && isFormValid;
 
   return (
     <div className="todo-form">
       <div className="todo-form__field">
         <label>Tytuł</label>
         <input
+          className={isTitleToLong ? 'input-error' : ''}
+          placeholder="Wpisz tytuł (max 15 znaków)"
           type="text"
           value={title}
           onChange={(event) => {
             setTitle(event.target.value);
           }}
+          onFocus={resetSuccesMessage}
+          maxLength={15}
         ></input>
       </div>
-      <div className="todo-form__field">
-        <label>Autor</label>
-        <input
-          type="text"
-          placeholder="Autor"
-          value={author}
-          onChange={(event) => {
-            setAuthor(event.target.value);
-          }}
-        ></input>
-      </div>
+
+      {!isEditMode && (
+        <div className="todo-form__field">
+          <label>Autor</label>
+          <input
+            type="text"
+            placeholder="Autor"
+            value={author}
+            onChange={(event) => {
+              setAuthor(event.target.value);
+            }}
+            onFocus={resetSuccesMessage}
+          ></input>
+        </div>
+      )}
       <div className="todo-form__field">
         <label>Treść</label>
         <textarea
@@ -63,14 +125,34 @@ export function TodoForm({ setAddingMode }) {
           onChange={(event) => {
             setNote(event.target.value);
           }}
+          onFocus={resetSuccesMessage}
         />
       </div>
-      Formularz
+      {isInProgress && (
+        <p className="todo-form_progress-mesage">
+          Todo "{title}" jest dodawane...
+        </p>
+      )}
+      {isError && (
+        <p className="todo-form_error-mesage">Todo "{title}" nie udało się!</p>
+      )}
+      {isSucces && (
+        <p className="todo-form_sucsess-mesage">
+          {' '}
+          {isEditMode ? (
+            <>Todo "{title}" zostało zapisane pomyślnie!</>
+          ) : (
+            <>Todo "{prevTitle}" dodało się!</>
+          )}
+        </p>
+      )}
       <div className="todo-form__buttons">
         <button
           className="app-button"
           onClick={() => {
-            setAddingMode(false);
+            handleFetchTodoData(isEditMode ? data.id : undefined);
+            setFormVisible(false);
+            setIdForEdit(null);
           }}
         >
           COFNIJ
@@ -78,11 +160,15 @@ export function TodoForm({ setAddingMode }) {
         <button
           className="app-button"
           onClick={() => {
-            handleCreateTodo();
+            if (isEditMode) {
+              handleEditTodo();
+            } else {
+              handleCreateTodo();
+            }
           }}
-          disabled={!isReadyToSend}
+          disabled={!isReadyToSend || isInProgress}
         >
-          ZAPISZ
+          {isEditMode ? 'ZAPISZ' : 'DODAJ'}
         </button>
       </div>
     </div>
--- src/App/Exercises/ReactExercises/TodoList/TodoItem/TodoItem.jsx
+++ src/App/Exercises/ReactExercises/TodoList/TodoItem/TodoItem.jsx
@@ -4,54 +4,95 @@ import { BinIcon } from '../../../../Images/Icons/BinIcon/BinIcon';
 import './TodoItem.css';
 import { BASE_API_URL } from '../TodoList';
 import { useState } from 'react';
+import ModeEditOutlinedIcon from '@mui/icons-material/ModeEditOutlined';
 
-export function TodoItem({ todo, handleFetchTodoData }) {
+export function TodoItem({
+  todo,
+  handleFetchTodoData,
+  setIdForEdit,
+  setFormVisible,
+  updateTodoList,
+}) {
   const { id, title, author, createdAt, isDone, doneDate, note } = todo;
   const [isRemoveError, setRemoveError] = useState(false);
-  const itemClasses = `item ${todo.isDone ? 'item--darker' : ''}`;
+  const [isDoneError, setDoneError] = useState(false);
+  const itemClasses = `todo-item ${todo.isDone ? 'item--darker' : ''}`;
 
   function handleRemoveClick() {
-    console.log('UsuńId:', id);
-
     axios
       .delete(BASE_API_URL + '/api/todo/' + id)
       .then((response) => {
-        console.log('usunięty to do id:', response);
         handleFetchTodoData();
       })
       .catch((error) => {
-        console.log('usuwanie nie udało się:', error);
         setRemoveError(true);
       });
   }
+
+  function handleMarkAsDoneClick() {
+    axios
+      .put(BASE_API_URL + '/api/todo/' + id + '/markAsDone')
+      .then((response) => {
+        const updatedTodo = response.data;
+        updateTodoList(updatedTodo);
+        setDoneError(false); //jeśli wykona po ponownym kliknięciu to wyczyści błąd
+      })
+      .catch(() => {
+        setDoneError(true);
+      });
+  }
+
   return (
     <div className={itemClasses} key={id}>
-      <div className="item__wrapper">
+      <div className="todo-item__wrapper">
         <h3>{title}</h3>
-        <div className="item__wrapper__text item__wrapper__text--smaller">
+        <div className="todo-item__wrapper__text todo-item__wrapper__text--smaller">
           {author}
         </div>
-        <div className="item__wrapper__text item__wrapper__text--smaller">
+        <div className="todo-item__wrapper__text todo-item__wrapper__text--smaller">
           {formatDate(createdAt)}
         </div>
-        <p className="item__wrapper__text">{note}</p>
+        <p className="todo-item__wrapper__text">{note}</p>
       </div>
-      <div className="item__actions">
+      <div className="todo-item__actions">
         <button
-          className="item__actions__button 
-        item__actions__icon"
+          className="todo-item__actions__button"
+          onClick={() => {
+            handleMarkAsDoneClick();
+          }}
+        >
+          &#10003; {/*ikona znakiem */}
+        </button>
+        <button
+          className="todo-item__actions__button 
+        todo-item__actions__icon"
+          onClick={() => {
+            setIdForEdit(id);
+            setFormVisible(true);
+          }}
+        >
+          <ModeEditOutlinedIcon /> {/*ikona z MUI */}
+        </button>
+        <button
+          className="todo-item__actions__button 
+        todo-item__actions__icon"
           onClick={() => {
             handleRemoveClick();
           }}
         >
-          <BinIcon isError={isRemoveError} />
+          <BinIcon isError={isRemoveError} /> {/*ikona z FIGMY */}
         </button>
+        {isDoneError && (
+          <div className="todo-item__actions__error">
+            Nie udało się zapamietać ukończenia
+          </div>
+        )}
         {isRemoveError && (
-          <div className="item__actions__error">nie udało się usunąć</div>
+          <div className="todo-item__actions__error">Nie udało się usunąć</div>
         )}
         {isDone && (
           <>
-            <div className="item__actions__check">&#10003;</div>
+            <div className="todo-item__actions__check">&#10003;</div>
             <div>{formatDate(doneDate)}</div>
           </>
         )}
