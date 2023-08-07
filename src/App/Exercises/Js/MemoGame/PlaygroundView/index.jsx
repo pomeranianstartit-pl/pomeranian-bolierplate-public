1fa4adba32daf8e6055bfc6760d4e4e1a1d48eb4
Magda Gadomska <m.srutkowska@gmail.com>
2023-08-07 23:27:05 +0200

    Memo v2

--- src/App/Exercises/Js/MemoGame/PlaygroundView/index.jsx
+++ src/App/Exercises/Js/MemoGame/PlaygroundView/index.jsx
@@ -1,148 +1,217 @@
 import { useEffect, useState } from 'react';
 
-const initArrayWithLetters = [
+const initialCardsArray = [
   {
     id: 1,
     name: 'A',
     hasClicked: false,
+    active: true,
   },
   {
     id: 2,
     name: 'A',
     hasClicked: false,
+    active: true,
   },
   {
     id: 3,
     name: 'B',
     hasClicked: false,
+    active: true,
   },
   {
     id: 4,
     name: 'B',
     hasClicked: false,
+    active: true,
   },
   {
     id: 5,
     name: 'C',
     hasClicked: false,
+    active: true,
   },
   {
     id: 6,
     name: 'C',
     hasClicked: false,
+    active: true,
   },
   {
     id: 7,
     name: 'D',
     hasClicked: false,
+    active: true,
   },
   {
     id: 8,
     name: 'D',
     hasClicked: false,
+    active: true,
   },
   {
     id: 9,
     name: 'E',
     hasClicked: false,
+    active: true,
   },
   {
     id: 10,
     name: 'E',
     hasClicked: false,
+    active: true,
   },
   {
     id: 11,
     name: 'F',
     hasClicked: false,
+    active: true,
   },
   {
     id: 12,
     name: 'F',
     hasClicked: false,
+    active: true,
   },
   {
     id: 13,
     name: 'G',
     hasClicked: false,
+    active: true,
   },
   {
     id: 14,
     name: 'G',
     hasClicked: false,
+    active: true,
   },
   {
     id: 15,
     name: 'H',
     hasClicked: false,
+    active: true,
   },
   {
     id: 16,
     name: 'H',
     hasClicked: false,
+    active: true,
   },
 ];
 
-const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
 export const PlaygroundView = ({ score, setScore }) => {
-  const [arrayWithLetters, setArrayWithLetters] =
-    useState(initArrayWithLetters);
+  const [cardsArray, setCardsArray] = useState(initialCardsArray);
 
-  const [memoFieldId, setMemoFieldId] = useState(-1);
-
-  const handleScore = (isMemoPresent) => {
-    isMemoPresent && setScore(score + 1);
-    !isMemoPresent && score > 0 && setScore(score - 1);
+  const countClickedCards = () => {
+    return cardsArray.reduce((count, element) => {
+      return count + (element.hasClicked ? 1 : 0);
+    }, 0);
   };
 
-  const handleReset = () => {
+  const markCardAsClicked = (cardId) => {
+    const newTable = cardsArray.map((element) => {
+      return {
+        ...element,
+        hasClicked: element.hasClicked || element.id === cardId,
+      };
+    });
+    setCardsArray(newTable);
+  };
+
+  const countCardsByName = (cardName) => {
+    return cardsArray.filter(
+      (element) => element.hasClicked && element.name === cardName
+    ).length;
+  };
+
+  const deactivateCardsByCardName = (cardName) => {
     setTimeout(() => {
-      setArrayWithLetters(
-        arrayWithLetters.map((field) => {
-          return {
-            ...field,
-            hasClicked: false,
-          };
-        })
-      );
+      const newTable = cardsArray.map((element) => {
+        return {
+          ...element,
+          hasClicked: false,
+          active: element.name === cardName ? false : element.active,
+        };
+      });
+      setCardsArray(newTable);
     }, 500);
   };
 
-  const handleClick = (clickedElement, isMemoPresent) => {
-    setArrayWithLetters(
-      arrayWithLetters.map((field) => {
+  const unclickActiveCards = () => {
+    setTimeout(() => {
+      const newTable = cardsArray.map((element) => {
         return {
-          ...field,
-          hasClicked: field.id === clickedElement.id,
+          ...element,
+          hasClicked: false,
+          active: element.active,
         };
-      })
-    );
+      });
+      setCardsArray(newTable);
+    }, 500);
+  };
 
-    setMemoFieldId(clickedElement.id);
+  const handleCardClick = (clickedCard) => {
+    // nie można klikać w nieaktywną lub już klikniętą kartę
+    if (clickedCard.active === false || clickedCard.hasClicked === true) {
+      return;
+    }
 
-    console.log({ arrayWithLetters });
+    // liczymy ile mamy klikniętych kart
+    let numberOfClickedCards = countClickedCards();
 
-    //handleReset();
-    handleScore(isMemoPresent);
+    if (numberOfClickedCards >= 2) {
+      // ignorujemy gdy osiągneliśmy limit klikniętych kart (max 2 karty)
+      // do momentu, aż ponownie kliknięte karty zostaną ukryte na planszy
+      return;
+    } else {
+      // oznaczamy kartę jako klikniętą
+      markCardAsClicked(clickedCard.id);
 
-    // TODO: add score functionality
+      // pomownie zliczamy ile mamy klikniętych kart po oznaczeniu nowego kliknięcia
+      numberOfClickedCards = countClickedCards();
+
+      if (numberOfClickedCards !== 2) {
+        // jeżeli nie ma dwóch klikniętych kart to czekamy na kolejne kliknięcie i wychodzimy z funkcji
+        return;
+      }
+
+      // liczymy ile mamy klikniętych kart z tą samą literą
+      const sameClickedCardsNumber = countCardsByName(clickedCard.name);
+
+      if (sameClickedCardsNumber === 2) {
+        // sukces! dwie odkryte karty mają tą samą literę!
+        // Trzeba je deaktywować, żeby nie dało się w nie klikać
+        deactivateCardsByCardName(clickedCard.name);
+
+        // No i zwiększamy nasz wynik o jeden punkt :-)
+        setScore(score + 1);
+      } else {
+        // Porażka! Dwie karty nie są takie same,
+        // więc trzeba je zaznaczyć, że już nie są kliknięte
+        // ale dotyczy to tylko tych aktywnych kart!
+        unclickActiveCards();
+      }
+    }
   };
 
   return (
     <div className="htm-stage-memo">
-      {arrayWithLetters.map((element) => {
-        const isMemoPresent = element.id === memoFieldId;
-        const isClickedFieldWithMemoClass =
-          isMemoPresent && element.hasClicked ? 'htm-stage-cell--green' : '';
-        const isClickedFieldWithoutMemoClass =
-          !isMemoPresent && element.hasClicked ? 'htm-stage-cell--red' : '';
+      {cardsArray.map((element) => {
+        const isDisabledClassName =
+          element.active === false ? 'htm-stage-cell-memo--red' : '';
+
+        const isClickedClassName =
+          element.active === true && element.hasClicked
+            ? 'htm-stage-cell--green'
+            : '';
+
         return (
           <div
             key={element.id}
-            onClick={() => handleClick(element, isMemoPresent)}
-            className={`htm-stage-cell ${isClickedFieldWithMemoClass} ${isClickedFieldWithoutMemoClass}`}
+            onClick={() => handleCardClick(element)}
+            className={`htm-stage-cell ${isDisabledClassName} ${isClickedClassName}`}
           >
-            {element.name}
+            {element.hasClicked ? element.name : ''}
           </div>
         );
       })}
