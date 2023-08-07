ef2397e8b8562fd332ab7ad7745445bcb270c0fc
Magda Gadomska <m.srutkowska@gmail.com>
2023-08-08 01:03:31 +0200

    memo

--- src/App/Exercises/Js/MemoGame/PlaygroundView/index.jsx
+++ src/App/Exercises/Js/MemoGame/PlaygroundView/index.jsx
@@ -100,12 +100,16 @@ const initialCardsArray = [
 ];
 
 export const PlaygroundView = ({ score, setScore }) => {
-  const [cardsArray, setCardsArray] = useState(initialCardsArray);
+  // tworzymy tablicę kart z tablicy inicjalnej losując kolejność elementów
+  const [cardsArray, setCardsArray] = useState(
+    initialCardsArray.sort(() => Math.random() - 0.5)
 
-  const countClickedCards = () => {
-    return cardsArray.reduce((count, element) => {
-      return count + (element.hasClicked ? 1 : 0);
-    }, 0);
+    // poprzednia wersja tworzenia tablicy kart bez losowej koleności:
+    //  const [cardsArray, setCardsArray] = useState(initialCardsArray)
+  );
+
+  const getClickedCards = () => {
+    return cardsArray.filter((element) => element.hasClicked === true);
   };
 
   const markCardAsClicked = (cardId) => {
@@ -116,12 +120,6 @@ export const PlaygroundView = ({ score, setScore }) => {
       };
     });
     setCardsArray(newTable);
-  };
-
-  const countCardsByName = (cardName) => {
-    return cardsArray.filter(
-      (element) => element.hasClicked && element.name === cardName
-    ).length;
   };
 
   const deactivateCardsByCardName = (cardName) => {
@@ -143,7 +141,6 @@ export const PlaygroundView = ({ score, setScore }) => {
         return {
           ...element,
           hasClicked: false,
-          active: element.active,
         };
       });
       setCardsArray(newTable);
@@ -157,7 +154,7 @@ export const PlaygroundView = ({ score, setScore }) => {
     }
 
     // liczymy ile mamy klikniętych kart
-    let numberOfClickedCards = countClickedCards();
+    let numberOfClickedCards = getClickedCards().length;
 
     if (numberOfClickedCards >= 2) {
       // ignorujemy gdy osiągneliśmy limit klikniętych kart (max 2 karty)
@@ -166,33 +163,39 @@ export const PlaygroundView = ({ score, setScore }) => {
     } else {
       // oznaczamy kartę jako klikniętą
       markCardAsClicked(clickedCard.id);
-
-      // pomownie zliczamy ile mamy klikniętych kart po oznaczeniu nowego kliknięcia
-      numberOfClickedCards = countClickedCards();
-
-      if (numberOfClickedCards !== 2) {
-        // jeżeli nie ma dwóch klikniętych kart to czekamy na kolejne kliknięcie i wychodzimy z funkcji
-        return;
-      }
-
-      // liczymy ile mamy klikniętych kart z tą samą literą
-      const sameClickedCardsNumber = countCardsByName(clickedCard.name);
-
-      if (sameClickedCardsNumber === 2) {
-        // sukces! dwie odkryte karty mają tą samą literę!
-        // Trzeba je deaktywować, żeby nie dało się w nie klikać
-        deactivateCardsByCardName(clickedCard.name);
-
-        // No i zwiększamy nasz wynik o jeden punkt :-)
-        setScore(score + 1);
-      } else {
-        // Porażka! Dwie karty nie są takie same,
-        // więc trzeba je zaznaczyć, że już nie są kliknięte
-        // ale dotyczy to tylko tych aktywnych kart!
-        unclickActiveCards();
-      }
     }
   };
+
+  // ta funkcja uruchomi się po aktualizacji tablicy kart
+  useEffect(() => {
+    // pobieramy kliknięte karty
+    const clickedCards = getClickedCards();
+
+    if (clickedCards.length < 2) {
+      // Jeżeli nie ma dwóch klikniętych kart to czekamy na kolejne kliknięcie i wychodzimy z funkcji.
+      // Jeżeli nie ma żadnej klikniętej karty to wychodzimy z funkcji.
+      // Taka sytuacja może się tak zdarzyć gdy deaktywujemy karty (po dopasowaniu)
+      // lub odznaczamy ich kliknięcie (po porażce)
+      return;
+    }
+
+    const firstClickedLetter = clickedCards[0].name;
+    const secondClickedLetter = clickedCards[1].name;
+    // sprawdzamy czy kliknięte litery są takie same
+    if (firstClickedLetter === secondClickedLetter) {
+      // sukces! dwie odkryte karty mają tą samą literę!
+      // Trzeba je deaktywować, żeby nie dało się w nie klikać
+      deactivateCardsByCardName(firstClickedLetter);
+
+      // No i zwiększamy nasz wynik o jeden punkt :-)
+      setScore(score + 1);
+    } else {
+      // Porażka! Dwie karty nie są takie same,
+      // więc trzeba je zaznaczyć, że już nie są kliknięte
+      // ale dotyczy to tylko tych aktywnych kart!
+      unclickActiveCards();
+    }
+  }, [cardsArray]);
 
   return (
     <div className="htm-stage-memo">
