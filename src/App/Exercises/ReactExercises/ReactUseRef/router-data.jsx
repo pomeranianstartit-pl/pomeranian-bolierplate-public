82eee8f5ab63e845015061acfc4ea901e4951f1b
Klaudia0099 <klaudiaastefaniak@gmail.com>
2023-08-08 20:15:54 +0200

    ReactUseRef

---
+++ src/App/Exercises/ReactExercises/ReactUseRef/router-data.jsx
@@ -0,0 +1,10 @@
+import { ReactUseRefExercise } from './index';
+
+export const ReactUseRefRoute = {
+  path: 'react-use-ref',
+  date: '07-08-2023',
+  linkLabel: 'useRef w reacie',
+  blockNo: 36,
+  element: <ReactUseRefExercise />,
+  tags: ['useref', 'react', 'hook', 'javascript'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -4,17 +4,17 @@ import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactRouterEventsMetaData } from './ReactRouterEvents/router-data';
 import { Block09MetaData } from './Block09/router-data';
 import { SwaggerRoute } from './Swagger/router-data';
-import { todoListMetaData } from './TodoList/router-data';
 import { MaterialUIBasicElementsMetaData } from './MaterialUIBasicElements/router-data';
 import { LocalDevAndFetchMetaData } from './LocalDevAndFetch/router-data';
 import { TodoListMetaData } from './TodoList/router-data';
+import { ReactUseRefRoute } from './ReactUseRef/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactRouterEventsMetaData,
   Block09MetaData,
   SwaggerRoute,
-  todoListMetaData,
+  ReactUseRefRoute,
   LocalDevAndFetchMetaData,
   TodoListMetaData,
   MaterialUIBasicElementsMetaData,
