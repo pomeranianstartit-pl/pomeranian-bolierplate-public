2595039a89f836938f12dd05a10e8a1eb7f67849
Mike <michal.sikorski@pomeranianstartit.pl>
2023-08-20 13:44:45 +0200

    adde basic if statements exercise

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactEventsUseStateMetaData } from './ReactEventsExample/router-data';
+import { ReactIfStatementsMetaData } from './ReactIfStatements/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactEventsUseStateMetaData,
+  ReactIfStatementsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
