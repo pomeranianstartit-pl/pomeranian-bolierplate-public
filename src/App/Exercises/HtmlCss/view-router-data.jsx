81b4fbc6ef57e575b6edc364ba33ccc90fc574e8
Edyta Muras <edyta.muras@pomeranianstartit.pl>
2023-08-20 16:57:08 +0200

    added basic information about cascade

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,5 +2,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as CSSCascadesblockRouterMetaData } from './SelectorsAndCascade/router-data';
+
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+
+  CSSCascadesblockRouterMetaData,
+];
+
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
