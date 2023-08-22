2d73f812c2690b5bfd20b533c732b554c9ee4f12
Magda Gadomska <m.srutkowska@gmail.com>
2023-08-22 16:30:08 +0200

    Frame icon css html toDo

--- src/App/Exercises/ReactExercises/ToDoWithServer/ToDoWithServer.jsx
+++ src/App/Exercises/ReactExercises/ToDoWithServer/ToDoWithServer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import { CheckIcon } from '../../../Components/Icons/CheckIcon';
 import { TrashIcon } from '../../../Components/Icons/TrashIcon';
+import { FrameIcon } from '../../../Components/Icons/FrameIcon';
 
 export function ToDoWithServer() {
   return (
@@ -12,46 +13,64 @@ export function ToDoWithServer() {
         <b>ToDo</b>
       </a>
       <p>Tutaj znajdziesz listę swoich zadań.</p>
-      <div className="first-list-element">
-        <h3>Zamówić catering </h3>
-        <p>
-          Wojtek
-          <br />
-          20.04.2024, 18:03{' '}
-        </p>
-        <p>
-          Dla mnie wege, dla Pauliny ryba + wege. Zrobić zamówienie, zapłacić.{' '}
-        </p>
-        <CheckIcon />
-        <TrashIcon />
-      </div>
-      <div className="second-list-element">
-        <h3>Kupić spray na kleszcze</h3>
-        <p>
-          Wojtek
-          <br />
-          10.04.2024, 11:30
-        </p>
-        <p>
-          Pamiętać, żeby sprawdzić skład i termin ważności preparatu. Zadzwonić
-          do weta, żeby się upewnić, czy mają na stanie.
-        </p>
-        <CheckIcon />
-        <TrashIcon />
-      </div>
-      <div className="third-list-element">
-        <h3>Szczepienie kotka</h3>
-        <p>
-          Wojtek
-          <br />
-          15.04.2024, 15:30
-        </p>
-        <p>
-          Sprawdzić w książeczce zdrowia, kiedy Nala była ostatni raz szczepiona
-          i umówić się z naszym wetery
-        </p>
-        <CheckIcon />
-        <TrashIcon />
+      <div className="lists-elements">
+        <div className="list-element">
+          <div className="content">
+            <h3>Zamówić catering </h3>
+            <p>
+              Wojtek
+              <br />
+              20.04.2024, 18:03{' '}
+            </p>
+            <p>
+              Dla mnie wege, dla Pauliny ryba + wege. Zrobić zamówienie,
+              zapłacić.{' '}
+            </p>
+          </div>
+          <div className="Icon">
+            <CheckIcon />
+            <TrashIcon />
+            <FrameIcon />
+          </div>
+        </div>
+        <div className="list-element">
+          <div className="content">
+            <h3>Kupić spray na kleszcze</h3>
+            <p>
+              Wojtek
+              <br />
+              10.04.2024, 11:30
+            </p>
+            <p>
+              Pamiętać, żeby sprawdzić skład i termin ważności preparatu.
+              Zadzwonić do weta, żeby się upewnić, czy mają na stanie.
+            </p>
+          </div>
+          <div className="Icon">
+            <CheckIcon />
+            <TrashIcon />
+            <FrameIcon />
+          </div>
+        </div>
+        <div className="list-element">
+          <div className="content">
+            <h3>Szczepienie kotka</h3>
+            <p>
+              Wojtek
+              <br />
+              15.04.2024, 15:30
+            </p>
+            <p>
+              Sprawdzić w książeczce zdrowia, kiedy Nala była ostatni raz
+              szczepiona i umówić się z naszym wetery
+            </p>
+          </div>
+          <div className="Icon">
+            <CheckIcon />
+            <TrashIcon />
+            <FrameIcon />
+          </div>
+        </div>
       </div>
     </div>
   );
