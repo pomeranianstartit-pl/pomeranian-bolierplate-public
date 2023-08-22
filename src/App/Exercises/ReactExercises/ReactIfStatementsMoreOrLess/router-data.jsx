fd33d770bf515073510bde9f95eebc04e4eda6c0
Ola <owaniczek.it@gmail.com>
2023-08-22 21:53:01 +0200

    added exercises with ifs

---
+++ src/App/Exercises/ReactExercises/ReactIfStatementsMoreOrLess/router-data.jsx
@@ -0,0 +1,11 @@
+import { ReactIfStatementsMoreOrLess } from './ReactIfStatementsMoreOrLess';
+
+export const ReactIfStatementsMoreOrLessMetaData = {
+  // eksportujemy meta dane
+  path: 'react-if-statements-more-or-less', // klucz path - na jakim adresie url cwiczenie bedzie sie znajdowac
+  date: '20-08-2023',
+  linkLabel: 'React - instrukcje warunkowe - ćwiczenie more or less', // opis widoczny na liscie cwiczen
+  blockNo: 9, // przy jakim bloku bedziemy  pracowac
+  element: <ReactIfStatementsMoreOrLess />, // przekazany komponent importowany który bedzie renderowany
+  tags: ['react', 'ifs', 'useState', 'events'], //
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -4,11 +4,13 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactEventsUseStateMetaData } from './ReactEventsUseState/router-data';
 import { ReactIfStatementsMetaData } from './ReactIfStatements/router-data';
+import { ReactIfStatementsMoreOrLessMetaData } from './ReactIfStatementsMoreOrLess/router-data';
 
 // dodajemy nasz kolejny element META DATA do eksportu
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactEventsUseStateMetaData,
   ReactIfStatementsMetaData,
+  ReactIfStatementsMoreOrLessMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
