ac2bb545f018fec54686be00acfcf13e7e8e95ac
Michał Hadrysiak <michal.hadrysiak@indiebi.com>
2023-08-26 11:52:32 +0200

    style text

--- src/App/Exercises/HtmlCss/TextFundaments/index.jsx
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -1,5 +1,34 @@
 import './styles.css';
 
 export function Exercise() {
-  return <p>Text</p>;
+  return (
+    <>
+      <p className="test">
+        Lorem ipsum dolor, sit <span>amet</span>amet consectetur adipisicing
+        elit. Sed possimus quasi facere modi temporibus nulla sequi esse
+        distinctio amet optio perspiciatis natus, similique quis at? Minima
+        laborum nostrum cum suscipit.Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Sed possimus quasi facere modi temporibus nulla sequi
+        esse distinctio amet optio perspiciatis natus, similique quis at? Minima
+        laborum nostrum cum suscipit.Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Sed possimus quasi facere modi temporibus nulla sequi
+        esse distinctio amet optio perspiciatis natus, similique quis at? Minima
+        laborum nostrum cum suscipit.Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Sed possimus quasi facere modi temporibus nulla sequi
+        esse distinctio amet optio perspiciatis natus, similique quis at? Minima
+        laborum nostrum cum suscipit.
+      </p>
+      <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>
+          <ol>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ol>
+        </li>
+      </ol>
+    </>
+  );
 }
