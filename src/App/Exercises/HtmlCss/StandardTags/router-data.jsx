96818a1cdae712164dafaf80d948f37f6ee6e507
Ola <owaniczek.it@gmail.com>
2023-08-26 13:30:41 +0200

    added fonts exercise

---
+++ src/App/Exercises/HtmlCss/StandardTags/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'standard-tags',
+  date: '12-12-2023',
+  linkLabel: 'Styl tagów standardowych',
+  blockNo: 11,
+  element: <Exercise />,
+  tags: ['attribute', 'text', 'css'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,14 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as CSSCascadesBlockRouterMetaData } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as TextFundamentsMetaData } from './TextFundaments/router-data';
+import { blockRouterMetaData as StandardTagsMetaData } from './StandardTags/router-data';
+import { blockRouterMetaData as GoogleFontsMetaData } from './GoogleFonts/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   CSSCascadesBlockRouterMetaData,
   TextFundamentsMetaData,
+  StandardTagsMetaData,
+  GoogleFontsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
