2d63feb1b99d42b7c4ea6cb2eccc0e074a7c98b0
Krzysiek C <krzysztof.cybula@pomeranianstartit.pl>
2023-08-26 11:53:03 +0200

    elementary property in css

---
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <p className="test">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+      repudiandae tempore, veritatis minus minima, eligendi harum unde
+      repellendus optio temporibus accusamus ut qui ab iusto dolorum quidem
+      quisquam nihil eos!
+    </p>
+  );
+}
---
+++ src/App/Exercises/HtmlCss/TextFundaments/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'text-fundaments-css',
+  date: '26-08-2023',
+  linkLabel: 'praca z textem',
+  blockNo: 11,
+  element: <Exercise />,
+  tags: ['attribute', 'text', 'css'],
+};
