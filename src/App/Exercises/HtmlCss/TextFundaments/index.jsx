51fdc39a0e25d38ad517b08eb84a0a849013d0d3
Ola <owaniczek.it@gmail.com>
2023-08-26 11:52:48 +0200

    style css w teksie lorem

---
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -0,0 +1,22 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <>
+      <p className="test">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.{' '}
+        <span id="wyraz">Earum </span>vel nihil a pariatur praesentium ex dicta
+        deleniti, dolore sequi iste odio aliquid eaque id tempore omnis
+        reprehenderit quidem illo provident. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Earum vel nihil a pariatur praesentium ex
+        dicta deleniti, dolore sequi iste odio aliquid eaque id tempore omnis
+        reprehenderit quidem illo provident.
+      </p>
+      <ol id="lista">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ol>
+    </>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as CSSCascadesBlockRouterMetaData } from './SelectorsAndCascade/router-data';
+import { blockRouterMetaData as TextFundamentsMetaData } from './TextFundaments/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   CSSCascadesBlockRouterMetaData,
+  TextFundamentsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
