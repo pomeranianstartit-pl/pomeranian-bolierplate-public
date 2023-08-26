1ff471fd8df3781565198e5dc412708ff7b19684
Krzysiek C <krzysztof.cybula@pomeranianstartit.pl>
2023-08-26 12:45:15 +0200

    StandardTagsExercise

---
+++ src/App/Exercises/HtmlCss/StandardTags/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'standard-tags',
+  date: '26-08-2023',
+  linkLabel: 'Style tagów standardowych',
+  blockNo: 11,
+  element: <Exercise />,
+  tags: ['attribute', 'text', 'css'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,10 +2,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataTextFundaments } from './TextFundaments/router-data';
+import { blockRouterMetaData as blockRouterMetaDataStandardTags } from './StandardTags/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaDataTextFundaments,
+  blockRouterMetaDataStandardTags,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
