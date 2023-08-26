50b9d654c5ba590fee9c16f32da981d41319ad30
Mike <michal.sikorski@pomeranianstartit.pl>
2023-08-26 13:30:33 +0200

    fontsGoogle

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div className="czcionka">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
+        tempora quos labore minus esse voluptatibus voluptas laudantium?
+        Provident quo, vel quisquam ullam commodi reprehenderit harum, fugit
+        cupiditate iusto maxime consequatur!
+      </p>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,10 +2,14 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataTextFundaments } from './TextFundaments/router-data';
+import { blockRouterMetaData as blockRouterMetaDataStandardTags } from './StandardTags/router-data';
+import { blockRouterMetaData as blockRouterMetaDataGoogleFonts } from './GoogleFonts/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaDataTextFundaments,
+  blockRouterMetaDataStandardTags,
+  blockRouterMetaDataGoogleFonts,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
