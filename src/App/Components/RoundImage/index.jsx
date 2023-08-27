285dfad5d5585bebff25678f77971d94a0514f4e
Ola <owaniczek.it@gmail.com>
2023-08-27 10:48:48 +0200

    added new component with props - url and size

--- src/App/Components/RoundImage/index.jsx
+++ src/App/Components/RoundImage/index.jsx
@@ -1,12 +1,17 @@
 import './styles.css';
 
-// export const RoundImage = ({url}) =>
-export const RoundImage = (props) => {
-  const { url } = props;
+// export const RoundImage = (props) =>
+export const RoundImage = ({ url, size = '150px' }) => {
+  // const { url } = props;
   return (
     <div
       className="round-image"
-      style={{ backgroundImage: `url('${props.url}')` }}
+      style={{
+        backgroundImage: `url('${url}')`, //`url('${props.url}')`,
+        width: size,
+        height: size,
+      }}
+      // pierwsze wąsy - kawalek JS, drugie wąsy - przekazujemy obiekt
     ></div>
   );
 };
--- src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
@@ -4,8 +4,11 @@ import './styles.css';
 export function Exercise() {
   return (
     <>
-      <RoundImage url="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png" />
-      <RoundImage url="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png" />
+      <RoundImage
+        url="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png"
+        // size="100px"
+      />
+      <RoundImage url="https://media.licdn.com/dms/image/C4D03AQFNuBGti63RUA/profile-displayphoto-shrink_800_800/0/1639596139513?e=1698883200&v=beta&t=xh2fQ6EOvxLKN2b4jaiGcGfGL7PJ9BcYft1_t30e4YI" />
     </>
   );
 }
