0d09d51ff1daa3ee0f2dbd39af3b5d444c4f5301
Mike <michal.sikorski@pomeranianstartit.pl>
2023-08-27 11:34:43 +0200

    BoxModel230827

---
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -0,0 +1,61 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <>
+      <p className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+        accusantium ad amet, neque quam omnis, perferendis fuga similique, sit
+        natus id doloremque modi laborum facere aperiam vero totam excepturi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum optio
+        harum repellendus officia voluptates aliquam soluta vero, voluptas hic
+        quibusdam iusto cumque aperiam quisquam neque eius tempora. Delectus,
+        sunt voluptate? Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Ut aperiam iste veniam reprehenderit beatae dolorum asperiores
+        dicta cupiditate magnam? Praesentium unde similique sunt voluptas
+        pariatur harum nostrum impedit ipsam ea!
+      </p>
+      <p className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+        accusantium ad amet, neque quam omnis, perferendis fuga similique, sit
+        natus id doloremque modi laborum facere aperiam vero totam excepturi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum optio
+        harum repellendus officia voluptates aliquam soluta vero, voluptas hic
+        quibusdam iusto cumque aperiam quisquam neque eius tempora. Delectus,
+        sunt voluptate? Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Ut aperiam iste veniam reprehenderit beatae dolorum asperiores
+        dicta cupiditate magnam? Praesentium unde similique sunt voluptas
+        pariatur harum nostrum impedit ipsam ea!
+      </p>
+      <p className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+        accusantium ad amet, neque quam omnis, perferendis fuga similique, sit
+        natus id doloremque modi laborum facere aperiam vero totam excepturi.
+        Lorem ipsum dolor
+        <p className="box">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          accusantium ad amet, neque quam omnis, perferendis fuga similique, sit
+          natus id doloremque modi laborum facere aperiam vero totam excepturi.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum optio
+          harum repellendus officia voluptates aliquam soluta vero, voluptas hic
+          quibusdam iusto cumque aperiam<span>Hej</span> quisquam neque eius
+          tempora. Delectus, sunt voluptate? Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. Ut aperiam iste veniam reprehenderit
+          beatae dolorum asperiores dicta cupiditate magnam? Praesentium unde
+          similique sunt voluptas pariatur harum nostrum impedit ipsam ea!
+        </p>
+        sit amet consectetur adipisicing elit. Rerum optio harum repellendus
+        officia voluptates aliquam soluta vero, voluptas hic quibusdam iusto
+        cumque aperiam quisquam neque<span>Hej</span> eius tempora. Delectus,
+        sunt voluptate? Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Ut aperiam iste veniam reprehenderit beatae dolorum asperiores
+        dicta cupiditate magnam? Praesentium unde similique sunt voluptas
+        pariatur harum nostrum impedit ipsam ea!
+      </p>
+      <span>Hej</span>
+      <span>Hej</span>
+      <span>Hej</span>
+      <span>Hej</span>
+    </>
+  );
+}
