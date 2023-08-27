f2495c6756f07470a1b680a402aff63986b6209e
Mike <michal.sikorski@pomeranianstartit.pl>
2023-08-27 10:48:54 +0200

    RoundImagePropsy

--- src/App/Components/RoundImage/index.jsx
+++ src/App/Components/RoundImage/index.jsx
@@ -1,10 +1,14 @@
 import './styles.css';
 
-export const RoundImage = (props) => {
+export const RoundImage = ({ url, size }) => {
   return (
     <div
       className="round-image"
-      style={{ backgroundImage: `url('${props.url}')` }}
+      style={{
+        backgroundImage: `url('${url}')`,
+        width: size,
+        height: size,
+      }}
     ></div>
   );
 };
