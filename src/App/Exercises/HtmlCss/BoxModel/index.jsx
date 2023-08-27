ac97d0b18fb82a1b17621f8ecbe12752f62ba75e
EMuras <edyta.muras@pomeranianstartit.pl>
2023-08-27 12:53:37 +0200

    Jednostki CSS

--- src/App/Exercises/HtmlCss/BoxModel/index.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -3,9 +3,38 @@ import './styles.css';
 export const Exercise = () => {
   return (
     <div>
-      <p className="lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in, nostrum ducimus animi hic mollitia quod officia! Obcaecati expedita ea a, voluptatem accusantium maiores. Neque cupiditate dignissimos debitis nostrum cumque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in, nostrum ducimus animi hic mollitia quod officia! Obcaecati expedita ea a, voluptatem accusantium maiores. Neque cupiditate dignissimos debitis nostrum cumque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in, nostrum ducimus animi hic mollitia quod officia! Obcaecati expedita ea a, voluptatem accusantium maiores. Neque cupiditate dignissimos debitis nostrum cumque!
+      <p className="lorem">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in,
+        nostrum ducimus animi hic mollitia quod officia! Obcaecati expedita ea
+        a, voluptatem accusantium maiores. Neque cupiditate dignissimos debitis
+        nostrum cumque!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Libero in, nostrum ducimus animi hic mollitia quod officia! Obcaecati{' '}
+        <span className="span">
+          debitis nostrum cumque!Lorem ipsum dolor sit amet consectetur
+          adipisicing elit.
+        </span>{' '}
+        expedita ea a, voluptatem accusantium maiores. Neque cupiditate
+        dignissimos debitis nostrum cumque!Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Libero in, nostrum ducimus animi hic
+        mollitia quod officia! Obcaecati expedita ea a, voluptatem accusantium
+        maiores. Neque cupiditate dignissimos debitis nostrum cumque!
       </p>
-      
+      <p className="lorem">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in,
+        nostrum ducimus animi hic mollitia quod officia! Obcaecati expedita ea
+        a, voluptatem accusantium maiores. Neque cupiditate dignissimos debitis
+        nostrum cumque!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Libero in, nostrum ducimus animi hic mollitia quod officia! Obcaecati
+        expedita ea a, voluptatem accusantium maiores. Neque cupiditate
+        dignissimos{' '}
+        <span className="span">
+          debitis nostrum cumque!Lorem ipsum dolor sit amet consectetur
+          adipisicing elit.
+        </span>{' '}
+        Libero in, nostrum ducimus animi hic mollitia quod officia! Obcaecati
+        expedita ea a, voluptatem accusantium maiores. Neque cupiditate
+        dignissimos debitis nostrum cumque!
+      </p>
     </div>
   );
 };
--- src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/router-data.jsx
+++ src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/router-data.jsx
@@ -5,7 +5,7 @@ import { Exercise } from './index';
 export const blockRouterMetaData = {
   path: '12-12-2023-1',
   date: '12-12-2023',
-  linkLabel: '',
+  linkLabel: 'Ćwiczenia',
   blockNo: 1,
   element: <Exercise />,
   tags: ['attribute', 'text', 'css'],
