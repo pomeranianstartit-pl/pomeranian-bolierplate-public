ed802eae060c2e8d29b28d02e27ce6429e8b07af
Krzysiek C <krzysztof.cybula@pomeranianstartit.pl>
2023-08-27 10:16:42 +0200

    RoundImageExercise

--- src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
@@ -1,5 +1,13 @@
+import { RoundImage } from '../../../Components/RoundImage';
+
 import './styles.css';
 
 export function Exercise() {
-  return <p>Text</p>;
+  return (
+    <>
+      <RoundImage url="https://media.licdn.com/dms/image/D4D03AQGmpz3QYUd46g/profile-displayphoto-shrink_200_200/0/1688659151125?e=1698883200&v=beta&t=r-JchaH7Ukw8WNJ9rZqPMnYl2R07tHdxCyELAADNeSA" />
+
+      <RoundImage url="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png" />
+    </>
+  );
 }
