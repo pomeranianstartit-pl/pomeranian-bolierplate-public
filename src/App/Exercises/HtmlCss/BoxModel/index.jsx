5f728801527dfd03a6d6e79b5224b4af829c688e
Michał Hadrysiak <michal.hadrysiak@indiebi.com>
2023-08-27 11:55:49 +0200

    Test123

---
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -0,0 +1,59 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <>
+      <p className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+        modi, deserunt asperiores quam error! Recusandae id quisquam reiciendis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+        modi, deserunt asperiores quam error! Recusandae id quisquam reiciendis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+        modi, deserunt asperiores quam error! Recusandae id quisquam reiciendis.
+      </p>
+      <p className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+        modi, deserunt asperiores quam error! Recusandae id quisquam reiciendis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+        modi, deserunt <span>Hej</span> asperiores quam error! Recusandae id
+        quisquam reiciendis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Nisi suscipit repellendus quis <span>Hej</span> nostrum id
+        molestias veniam itaque non rem! Nihil rem modi, deserunt asperiores{' '}
+        <span>Hej</span> quam error! Recusandae id quisquam reiciendis.
+      </p>
+      <p className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+        modi, deserunt asperiores quam error! Recusandae id quisquam reiciendis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+        repellendus quis{' '}
+        <p className="box">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit
+          repellendus quis nostrum id molestias veniam itaque non rem! Nihil rem
+          modi, deserunt asperiores quam error! Recusandae id quisquam
+          reiciendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nisi suscipit repellendus quis nostrum id molestias veniam itaque non
+          rem! Nihil rem modi, deserunt asperiores quam error! Recusandae id
+          quisquam reiciendis. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Nisi suscipit repellendus quis nostrum id molestias
+          veniam itaque non rem! Nihil rem modi, deserunt asperiores quam error!
+          Recusandae id quisquam reiciendis.
+        </p>
+        nostrum id molestias veniam itaque non rem! Nihil rem modi, deserunt
+        asperiores quam error! Recusandae id quisquam reiciendis. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Nisi suscipit repellendus
+        quis nostrum id molestias veniam itaque non rem! Nihil rem modi,
+        deserunt asperiores quam error! Recusandae id quisquam reiciendis.
+      </p>
+      <span>Hej</span>
+      <span>Hej</span>
+      <span>Hej</span>
+      <span>Hej</span>
+    </>
+  );
+}
