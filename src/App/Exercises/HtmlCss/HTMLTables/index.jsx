bb753e725712ee65c5d24f0674a228e67e51d3d4
Ola <owaniczek.it@gmail.com>
2023-08-27 13:43:41 +0200

    added table from exercise

--- src/App/Exercises/HtmlCss/HTMLTables/index.jsx
+++ src/App/Exercises/HtmlCss/HTMLTables/index.jsx
@@ -2,48 +2,92 @@ import './styles.css';
 
 export function Exercise() {
   return (
-    <table>
-      <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
-      </tr>
+    <>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
 
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-      </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
 
-      <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
-      </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
 
-      <tr>
-        <td>Island Trading</td>
-        <td>Helen Bennett</td>
-        <td>UK</td>
-      </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
 
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
 
-      <tr>
-        <td>Laughing Bacchus Winecellars</td>
-        <td>Yoshi Tannamuri</td>
-        <td>Canada</td>
-      </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
 
-      <tr>
-        <td>Magazzini Alimentari Riuniti</td>
-        <td>Giovanni Rovelli</td>
-        <td>Italy</td>
-      </tr>
-    </table>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+      <br></br>
+      <table>
+        <tr>
+          <th>Plastic</th>
+          <th>Acetone</th>
+          <th>Flame test</th>
+          <th>Heat</th>
+          <th>Crease color</th>
+        </tr>
+
+        <tr>
+          <td>1</td>
+          <td>No effect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+
+        <tr>
+          <td>2</td>
+          <td>Softened</td>
+          <td>No chenge</td>
+          <td>No chenge</td>
+          <td>White</td>
+        </tr>
+
+        <tr>
+          <td>3</td>
+          <td>No effect</td>
+          <td>Red color</td>
+          <td>Softens</td>
+          <td>None</td>
+        </tr>
+
+        <tr>
+          <td>4</td>
+          <td>No effect</td>
+          <td>Green color</td>
+          <td>Softens</td>
+          <td>none</td>
+        </tr>
+      </table>
+    </>
   );
 }
