46b59495afb2d3b0918f0f23d8f12e6e22298fe1
EMuras <edyta.muras@pomeranianstartit.pl>
2023-08-27 10:01:36 +0200

    added the roundimage

--- src/App/Exercises/HtmlCss/CssFilter/index.jsx
+++ src/App/Exercises/HtmlCss/CssFilter/index.jsx
@@ -1,12 +1,12 @@
 import './styles.css';
+import { RoundImage } from '../../../Exercises/HtmlCss/RoundImage';
 
 export const Exercise = () => {
-  return (
-    <>
-      <div className="background1"></div>
-      <div className="filtr4"></div>
-      <div className="filtr1"></div>
-      <div className="filtr2"></div>
-    </>
-  );
+  return <RoundImage />;
+  <>
+    <div className="background1"></div>
+    <div className="filtr4"></div>
+    <div className="filtr1"></div>
+    <div className="filtr2"></div>
+  </>;
 };
