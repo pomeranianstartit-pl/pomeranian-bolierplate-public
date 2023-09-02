5c081760d2da9872560e8bc8c99eea81adffc017
Mike <michal.sikorski@pomeranianstartit.pl>
2023-09-02 16:14:38 +0200

    media queries Cwiczenie z oknem Cookies

---
+++ src/App/Components/CookieBanner/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+import { CalendarIcon } from '../Icons/CalendarIcon';
+
+import './styles.css';
+
+export const CookieBanner = () => {
+  const [hideCookieBanner, setHideCookieBanner] = useState(
+    !!localStorage.getItem('cookie')
+  );
+
+  const handleAccept = () => {
+    localStorage.setItem('cookie', true);
+    setHideCookieBanner(true);
+  };
+
+  return (
+    <>
+      {!hideCookieBanner && (
+        <section className="cookie__wrapper">
+          <div className="cookie__content">
+            <CalendarIcon className="content__icon" />
+            <h4>Pozwól na pliki cookies</h4>
+            <p>
+              Nasza strona korzysta z ciasteczek, które umożliwiają poprawne
+              działanie strony i pomagają nam świadczyć usługi na najwyższym
+              poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich
+              tych plików i przejść do strony lub dostosować użycie do swoich
+              referencji. W tym celu kliknij przycisk po prawej stronie “dopasuj
+              zgody”, aby następnie wybrać te które odpowiadają Twoim
+              indywidualnym preferencjom.
+            </p>
+          </div>
+          <div className="cookie__permissions clearfix">
+            <button className="permissions__accept" onClick={handleAccept}>
+              W porządku
+            </button>
+            <button className="permissions__reject">Dopasuj zgody</button>
+          </div>
+        </section>
+      )}
+    </>
+  );
+};
