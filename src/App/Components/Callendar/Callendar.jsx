a785d0f9a168ee3514598154f0562d9de4af918b
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-09-04 11:21:03 +0200

    Working selection of exercise by date

--- src/App/Components/Callendar/Callendar.jsx
+++ src/App/Components/Callendar/Callendar.jsx
@@ -3,21 +3,46 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import TextField from '@mui/material/TextField';
 import { format } from 'date-fns';
-import { blockRouterData } from '../../Exercises/HtmlCss/view-router-data';
+import { blockRouterData as htmlCssRouterData } from '../../Exercises/HtmlCss/view-router-data';
+import { blockRouterData as jsRouterData } from '../../Exercises/Js/view-router-data';
+import { blockRouterData as reactRouterData } from '../../Exercises/ReactExercises/view-router-data';
 import './styles.css';
 import { StaticDatePicker } from '@mui/x-date-pickers';
 import { Link } from 'react-router-dom';
 
 export const Callendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const prefixHtmlCss = '../exercises/html-css/'; //prefixes to uptade paths
+  const prefixJs = '../exercises/Js/';
+  const prefixReact = '../exercises/React/';
+  //updating paths
+  const updatedHtmlCssRouterData = htmlCssRouterData.map((exercise) => ({
+    ...exercise,
+    path: prefixHtmlCss + exercise.path,
+  }));
+
+  const updatedJsRouterData = jsRouterData.map((exercise) => ({
+    ...exercise,
+    path: prefixJs + exercise.path,
+  }));
+
+  const updatedReactRouterData = reactRouterData.map((exercise) => ({
+    ...exercise,
+    path: prefixReact + exercise.path,
+  }));
+  //merging all arrays to one
+  const blockRouterData = [
+    ...updatedHtmlCssRouterData,
+    ...updatedJsRouterData,
+    ...updatedReactRouterData,
+  ];
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
-  // Define a function to check if there's an exercise on the selected date
   const hasExerciseOnDate = (date) => {
-    const formattedDate = format(date, 'dd-MM-yyyy'); // Format the date to match your exercise date format
+    const formattedDate = format(date, 'dd-MM-yyyy');
     return blockRouterData.some((exercise) => {
       console.log(
         'Exercise:',
@@ -50,9 +75,7 @@ export const Callendar = () => {
         <ul>
           {filteredExercises.map((exercise) => (
             <li key={exercise.path}>
-              <Link to={`../exercises/html-css/${exercise.path}`}>
-                {exercise.linkLabel}
-              </Link>
+              <Link to={exercise.path}>{exercise.linkLabel}</Link>
             </li>
           ))}
         </ul>
