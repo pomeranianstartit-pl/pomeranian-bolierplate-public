b931b7188333bd9176993c13bbdbfe0cfa6fd1d1
Ola <owaniczek.it@gmail.com>
2023-09-06 22:44:25 +0200

    edited header and aside

--- src/App/Components/Menu/index.jsx
+++ src/App/Components/Menu/index.jsx
@@ -6,7 +6,11 @@ import { ArrowIcon } from '../Arrow';
 export const Menu = () => {
   return (
     <div className="header-menu">
-      <SettingIcon />
+      <div className="setting-icon">
+        <button>
+          <SettingIcon />
+        </button>
+      </div>
       <RoundImage url="https://media.licdn.com/dms/image/C4D03AQFNuBGti63RUA/profile-displayphoto-shrink_800_800/0/1639596139513?e=1698883200&v=beta&t=xh2fQ6EOvxLKN2b4jaiGcGfGL7PJ9BcYft1_t30e4YI" />
       <p className="osoba">
         <span>
@@ -14,7 +18,11 @@ export const Menu = () => {
         </span>
         <span>kursant</span>
       </p>
-      <ArrowIcon />
+      <div className="arrow">
+        <button>
+          <ArrowIcon />
+        </button>
+      </div>
     </div>
   );
 };
