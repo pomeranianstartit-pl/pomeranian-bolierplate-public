ba44a968d80c3cc0280826e103e609d3620e9082
Piowija <piotrwijas123@gmail.com>
2023-09-15 00:06:33 +0200

    -onclick2

--- src/App/Exercises/ReactExercises/ReactOnClick/ReactOnClick.jsx
+++ src/App/Exercises/ReactExercises/ReactOnClick/ReactOnClick.jsx
@@ -2,18 +2,22 @@ import { useState } from 'react';
 import './styles.css';
 
 export const ReactOnClick = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  const text = isClicked ? 'Kliknięte' : 'Nie kliknięte';
+  //const [isClicked, setIsClicked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  //const text = isClicked ? 'Kliknięte' : 'Nie kliknięte';
 
   function handleClick() {
-    setIsClicked(!isClicked);
+    //  setIsClicked(!isClicked);
+    setCount(count + 1);
+    //setIsClicked(!isClicked);
   }
 
   return (
     <div>
       <h1>Cześć!</h1>
 
-      <button onClick={handleClick}>{text}</button>
+      <button onClick={handleClick}>{count}</button>
     </div>
   );
 };
