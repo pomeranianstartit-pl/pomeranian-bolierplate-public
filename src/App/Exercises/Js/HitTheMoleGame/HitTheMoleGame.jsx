0e807090758d506860609f22e4e3f53158a0ee8e
EMuras <edyta.muras@pomeranianstartit.pl>
2023-09-16 11:36:18 +0200

    added changes to the game

--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -1,36 +1,106 @@
 import { useEffect, useState } from 'react';
 
 import { Button } from '../../../Components/Button';
+
 import { MainHeader } from '../../../Components/MainHeader';
 
 import './styles.css';
+
 import { Tile } from './Tile/Tile';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 const MINUTE = 1; //1 minuta
 
+const MOLES_SPEED = [1000, 500, 350];
+
 export const HitTheMoleGame = () => {
   // notStarted, started, finished
+
   //getter i setter
+
   const [status, setStatus] = useState('notStarted');
+
   const [score, setScore] = useState(0);
+
   const [duration, setDuration] = useState(MINUTE);
+
   const [getTimeLeft, setTimeLeft] = useState(0);
+
   const [resultTime, setResultTime] = useState(0);
+
   const [molesNo, setMolesNo] = useState(1);
+
+  const [moles, setMoles] = useState([]);
+
+  const [molesTimeouts, setMolesTimeouts] = useState({});
+
   const [tiles, setTiles] = useState([]);
 
   const [getMinutes, setMinutes] = useState(MINUTE);
+
   const [getSeconds, setSeconds] = useState(0);
+
   const [getIsActiveTimer, setIsActiveTimer] = useState(false); // Flaga aktywności licznika
 
+  const createMole = (newMoles, id) => {
+    const maxPosition = molesNo * 5 + 5;
+
+    let newPosition = getRandomInt(maxPosition);
+
+    while (newMoles.find((mole) => mole.position === newPosition)) {
+      newPosition = getRandomInt(maxPosition);
+    }
+
+    return {
+      id,
+
+      position: newPosition,
+
+      timeout: setTimeout(() => moveMole(newPosition), MOLES_SPEED[0]),
+    };
+  };
+
+  const shuffleMoles = () => {
+    const newMolesPositions = [];
+
+    for (let i = 0; i < molesNo; i++) {
+      newMolesPositions.push(createMole(newMolesPositions));
+    }
+
+    setMoles(newMolesPositions);
+  };
+
+  const moveMole = (position) => {
+    setMolesTimeouts((prevTimeouts) => {
+      clearTimeout(prevTimeouts[position]);
+
+      return { ...prevTimeouts, [position]: null };
+    });
+
+    const newMolesPositions = moles.filter(
+      (molePosition) => molePosition !== position
+    );
+
+    newMolesPositions.push(createMole(newMolesPositions));
+
+    setMoles(newMolesPositions);
+  };
+
   const getDecrementTime = () => {
-    if (getMinutes === 0 && getSeconds === 0) {
+    setTimeLeft(`${getMinutes}:${getSeconds} s`);
+
+    if (getMinutes <= 0 && getSeconds <= 0) {
       setStatus('finished');
+
       setIsActiveTimer(false);
     }
 
     if (getSeconds === 0) {
       setMinutes((prevMinutes) => prevMinutes - 1);
+
       setSeconds(59);
     } else {
       setSeconds((prevSeconds) => prevSeconds - 1);
@@ -38,6 +108,7 @@ export const HitTheMoleGame = () => {
   };
 
   //react lifecycle hook
+
   useEffect(() => {
     let timerInterval;
 
@@ -50,24 +121,66 @@ export const HitTheMoleGame = () => {
     };
   }, [getIsActiveTimer, getSeconds, getMinutes]);
 
+  // useEffect(() => {
+
+  //   for (const position in molesPositions) {
+
+  //     molesTimeouts();
+
+  //     setTimeout(() => moveMole(position), MOLES_SPEED[0]);
+
+  //   }
+
+  // }, [molesPositions]);
+
   const handleStart = () => {
     setStatus('started');
+
     setResultTime(duration);
+
+    setSeconds(0);
+
+    setScore(0);
+
+    setMinutes(duration);
+
     setIsActiveTimer(true);
+
     setTiles(getInitialTiles());
+
+    shuffleMoles();
   };
 
   // uchwyt dla eventu JS i React
+
   const handleStop = () => {
     setStatus('finished');
+
     setIsActiveTimer(false);
-    setTimeLeft(`${duration}:${getSeconds} s`);
+
+    setTimeLeft(`${getMinutes}:${getSeconds} s`);
+
     setResultTime(
       `${getGameMinutesByGameMode()}m : ${
         getSeconds === 0 ? '0' : 60 - getSeconds
       }s`
     );
+
     setSeconds(0);
+  };
+
+  const isMoleOnPosition = (position) => {
+    return moles.find((mole) => mole.position === position);
+  };
+
+  const handleTileClick = (position) => () => {
+    if (isMoleOnPosition(position)) {
+      setScore(score + 1);
+
+      moveMole(position);
+    } else {
+      setScore(score - 1);
+    }
   };
 
   const getGameMinutesByGameMode = () => {
@@ -88,11 +201,12 @@ export const HitTheMoleGame = () => {
 
   const getInitialTiles = () => {
     const emptyTilesMatrix = Array(molesNo * 5 + 5);
+
     const tilesMatrix = emptyTilesMatrix.fill(0);
+
     // wypelnij ta tablice obiektami ktore beda mialy nastepujaca strukture [{index:0}]
-    const arrayOfTilesObjects = tilesMatrix.map(
-      (_, index) => ({ index: 0 }, { index: 1 })
-    );
+
+    const arrayOfTilesObjects = tilesMatrix.map((_, index) => ({ index }));
 
     return arrayOfTilesObjects;
   };
@@ -100,39 +214,47 @@ export const HitTheMoleGame = () => {
   return (
     <div className="hit-the-mole-game">
       <MainHeader>Kret</MainHeader>
+
       <p className="mole-description">
         Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
         którym się pojawił.
       </p>
+
       {status === 'finished' && (
         <div className="mole-result">
           Gratulację! Twój wynik to {score} złapane krety w czasie {resultTime}{' '}
           !
         </div>
       )}
+
       {status !== 'started' && (
         <>
           <div className="mole-settings-container">
             <span className="mole-label">czas gry</span>
+
             <Button
               id="1"
               variant={duration !== MINUTE ? 'primary' : 'secondary'}
               onClick={() => {
                 setDuration(MINUTE);
+
                 setMinutes(MINUTE);
               }}
             >
               1 minuta
             </Button>
+
             <Button
               variant={duration !== 2 * MINUTE ? 'primary' : 'secondary'}
               onClick={() => {
                 setDuration(2 * MINUTE);
+
                 setMinutes(2 * MINUTE);
               }}
             >
               2 minuty
             </Button>
+
             <Button
               variant={duration !== 3 * MINUTE ? 'primary' : 'secondary'}
               onClick={() => setDuration(3 * MINUTE)}
@@ -140,20 +262,24 @@ export const HitTheMoleGame = () => {
               3 minuty
             </Button>
           </div>
+
           <div className="mole-settings-container">
             <span className="mole-label">liczba kretów</span>
+
             <Button
               variant={molesNo !== 1 ? 'primary' : 'secondary'}
               onClick={() => setMolesNo(1)}
             >
               1 kret
             </Button>
+
             <Button
               variant={molesNo !== 2 ? 'primary' : 'secondary'}
               onClick={() => setMolesNo(2)}
             >
               2 krety
             </Button>
+
             <Button
               variant={molesNo !== 3 ? 'primary' : 'secondary'}
               onClick={() => setMolesNo(3)}
@@ -161,8 +287,10 @@ export const HitTheMoleGame = () => {
               3 krety
             </Button>
           </div>
+
           <div className="mole-settings-container">
             <span className="mole-label">przyciski sterujące</span>
+
             <Button onClick={handleStart}>Start</Button>
           </div>
         </>
@@ -170,27 +298,35 @@ export const HitTheMoleGame = () => {
 
       {/* conditional rendering of jsx w React  */}
 
-
       {status === 'started' && (
         <>
           <div className="mole-settings-container">
             <span className="mole-label">przyciski sterujące</span>
-            <span className="mole-output">1:35</span>
+
+            <span className="mole-output">{getTimeLeft}</span>
           </div>
+
           <div className="mole-settings-container">
             <span className="mole-label">Wynik</span>
-            <span className="mole-output">16</span>
+
+            <span className="mole-output">{score}</span>
           </div>
+
           <div className="mole-settings-container">
             <span className="mole-label">Przyciski sterujące</span>
-            <Button onHandlerClick={handleStop} variant="tertiary">
+
+            <Button onClick={handleStop} variant="tertiary">
               Stop
             </Button>
           </div>
 
           <div className="mole-tile-board">
             {tiles.map(({ index }) => (
-              <Tile key={index} />
+              <Tile
+                key={index}
+                isMole={isMoleOnPosition(index)}
+                onClick={handleTileClick(index)}
+              />
             ))}
           </div>
         </>
