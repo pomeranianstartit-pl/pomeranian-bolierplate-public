988d62b88afd7a999fed9354f1732e687ba72315
Mike <michal.sikorski@pomeranianstartit.pl>
2023-09-16 13:26:42 +0200

    game mole finish version

--- src/App/Exercises/Js/HitTheMoleGame/Tile.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/Tile.jsx
@@ -1,13 +1,18 @@
-import './styles.css';
+import './style.css';
 import mole from '../../../Images/Mole.svg';
 
 // isMole:true | false
 // tileStatus correct | incorrect | neutral
 
-export const Tile = ({ onClick, isMole, tileStatus = 'neutral' }) => {
+export const Tile = ({ onClick, isMole }) => {
   return (
-    <button className={`mole-tile mole-tile-${tileStatus} `} onClick={onClick}>
-      {isMole && <img src={mole} alt="Kret obrazek" />}
+    <button
+      className={`mole-tile mole-tile-neutral ${
+        isMole ? 'mole-with-mole' : ''
+      }`}
+      onClick={onClick}
+    >
+      {isMole && <img src={mole} alt="Obrazek kreta" />}
     </button>
   );
 };
