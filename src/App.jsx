57af5a5caf7e6a3111bc4c6e7685032ea9a4b0f6
Mateusz Tywczynski <mateusz.tywczynski@gmail.com>
2023-09-18 21:12:23 +0200

    zajecia-18-09-2023

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
 import { Exercises } from './App/Exercises';
 
+import { Calendar } from './App/Calendar';
+import { CV } from './App/CV';
+import { Blog } from './App/Blog';
+
 export function App() {
   return (
     <BrowserRouter>
@@ -15,8 +19,11 @@ export function App() {
         <Route path="" element={<Layout withSidebar />}>
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard/*" element={<Dashboard />} />
+          <Route path="cv/*" element={<CV />} />
           <Route path="exercises/*" element={<Exercises />} />
+          <Route path="calendar/*" element={<Calendar />} />
           <Route path="blocks/*" element={<Blocks />} />
+          <Route path="blog/" element={<Blog />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
