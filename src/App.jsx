666a3bf41d4e2df498a35d2fd35b5b87af50139a
Patryk Idzikowski <patryk.idzikowski@interia.pl>
2023-09-18 21:07:48 +0200

    Zmiana

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 
 import { NotFound } from './App/Components/NotFound/NotFound';
 
+import { Calendar } from './App/Calendar';
+import { CV } from './App/CV';
+import { Blog } from './App/Blog';
 import { Blocks } from './App/Blocks';
 import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
@@ -15,8 +18,11 @@ export function App() {
         <Route path="" element={<Layout withSidebar />}>
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard/*" element={<Dashboard />} />
+          <Route path="cv/*" element={<CV />} />
           <Route path="exercises/*" element={<Exercises />} />
+          <Route path="calendar/*" element={<Calendar />} />
           <Route path="blocks/*" element={<Blocks />} />
+          <Route path="blog/*" element={<Blog />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
