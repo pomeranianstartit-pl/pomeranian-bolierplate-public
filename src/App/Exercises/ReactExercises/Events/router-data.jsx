aec8f7f084bd04f7df070d702d0bbca9c47aa829
Mateusz Tywczynski <mateusz.tywczynski@gmail.com>
2023-09-19 21:02:18 +0200

    react-events-prolog

---
+++ src/App/Exercises/ReactExercises/Events/router-data.jsx
@@ -0,0 +1,10 @@
+import { Events } from './index';
+
+export const EventsMetaData = {
+  path: 'events',
+  date: '19-09-2023',
+  linkLabel: 'Events',
+  blockNo: 1,
+  element: <Events />,
+  tags: ['react', 'useState', 'onChange'],
+};
