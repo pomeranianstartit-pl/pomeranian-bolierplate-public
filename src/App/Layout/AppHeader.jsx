708ddc69796661f7ec966bc7e3c1369cab7006c0
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-09-19 17:16:20 +0200

    feat: change aside

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './styles/header.css';
 
+import { ReactComponent as PomeranianLogo } from '../Images/start-it-logo.svg';
+//import { HeaderMenu } from '../Components/HeaderMenu';
+
 export function AppHeader() {
   return (
     <header>
-      <div>
-        <div className="header-title">Tytuł nagłówka</div>
-      </div>
+      <PomeranianLogo />
     </header>
   );
 }
