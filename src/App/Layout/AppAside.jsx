5b5415d25cbeecc92d33d00c4ca3bd1f93869a88
caffeine108 <kamila.wyroslak@gmail.com>
2023-09-19 18:42:02 +0100

    chaged colors and blog icon added

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -7,6 +7,7 @@ import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 import { EditIcon } from '../Components/Icons/EditIcon';
 import { CalendarIcon } from '../Components/Icons/CalendarIcon';
 import { ElementIcon } from '../Components/Icons/ElementIcon';
+import { BookIcon } from '../Components/Icons/BookIcon';
 
 export function AppAside() {
   const location = useLocation();
@@ -40,6 +41,7 @@ export function AppAside() {
             <NavLink to="blocks">Bloki</NavLink>
           </li>
           <li className="aside-row">
+            <BookIcon />
             <NavLink to="blog">Blog</NavLink>
           </li>
           <hr />
