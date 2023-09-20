c610aa4e88774af3dcabf858d6fb625e2fb78679
Natalia Drwal <nataliadrwal1984@gmail.com>
2023-09-20 20:14:14 +0200

    git exercise 2

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -9,16 +9,42 @@ import './style.css';
 // 5. Ostylować
 
 export function Events() {
-  const [text, setText] = useState('Nie kliknięto we mnie');
-
+  const [text, setText] = useState('Kliknięto we mnie');
+  const [counter, setCounter] = useState(0);
   function handleOnClick() {
     setText('Kliknięto we mnie');
-  }
 
-  return (
-    <div>
-      <h2>Cześć! 👋</h2>
-      <button onClick={handleOnClick}>{text}</button>
-    </div>
-  );
+    if (counter >= 3) {
+      return setCounter((counter) => counter + 2);
+    }
+
+    setCounter((counter) => counter + 1);
+
+    // If..else if..else
+    // if (counter >= 5) {
+    //   setCounter((counter) => counter + 2);
+    // } else if (counter >= 3) {
+    //   setCounter((counter) => counter + 3);
+    // } else {
+    //   setCounter((counter) => counter + 1);
+    // }
+
+    // Ternary operator
+    // counter >= 3
+    //   ? setCounter((counter) => counter + 2)
+    //   : setCounter((counter) => counter + 1);
+
+    // if..else
+    // if (counter >= 3) {
+    //   setCounter((counter) => counter + 2);
+    // } else {
+    //   setCounter((counter) => counter + 1);
+    // }
+
+    // if + if
+    // if (counter < 3) {
+    //   //         początkowa   początkowa + 1
+    //   setCounter((counter) => counter + 1);
+    // }
+  }
 }
