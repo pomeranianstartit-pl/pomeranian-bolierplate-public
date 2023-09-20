b65ed3f68b1ad8050b4f650c076c3c580ef89b77
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-09-20 21:11:39 +0200

    Events ex3 >>> Events/index.jsx and Events/style.css

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -9,16 +9,66 @@ import './style.css';
 export function Events() {
   const [text, setText] = useState('Nie kliknięto we mnie');
   const [counter, setCounter] = useState(0);
+  const [inputText, setInputText] = useState('');
 
   function handleOnClick() {
     setText('Kliknięto we mnie');
+
+    if (counter >= 3) {
+      return setCounter((counter) => counter + 2);
+    }
+
     setCounter((counter) => counter + 1);
+
+    // If..else if..else
+    // if (counter >= 5) {
+    //   setCounter((counter) => counter + 2);
+    // } else if (counter >= 3) {
+    //   setCounter((counter) => counter + 3);
+    // } else {
+    //   setCounter((counter) => counter + 1);
+    // }
+
+    // Ternary operator
+    // counter >= 3
+    //   ? setCounter((counter) => counter + 2)
+    //   : setCounter((counter) => counter + 1);
+
+    // if..else
+    // if (counter >= 3) {
+    //   setCounter((counter) => counter + 2);
+    // } else {
+    //   setCounter((counter) => counter + 1);
+    // }
+
+    // if + if
+    // if (counter < 3) {
+    //   //         początkowa   początkowa + 1
+    //   setCounter((counter) => counter + 1);
+    // }
+  }
+  function handleOnChange(event) {
+    console.log(event.target.value);
+    setInputText(event.target.value);
+  }
+
+  function handleGreeting() {
+    alert(`Cześć 👋 ${inputText} 🥳`);
   }
 
   return (
     <div>
       <h2>Cześć! 👋</h2>
-      <button onClick={handleOnClick}>
+      <input
+        className="events--input"
+        type="text"
+        value={inputText}
+        onChange={(event) => handleOnChange(event)}
+      />
+      <button className="events--btn" onClick={handleGreeting}>
+        Kliknij
+      </button>
+      <button className="events--btn" onClick={handleOnClick}>
         {counter > 0 ? `${text} ${counter} razy` : text}
       </button>
     </div>
