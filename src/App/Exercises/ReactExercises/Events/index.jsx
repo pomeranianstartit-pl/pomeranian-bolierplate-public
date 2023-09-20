a0716f8d429517c037a2fd54448b61345d10210a
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-09-20 21:10:11 +0200

    new button exercise nr 3

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -4,8 +4,8 @@ import './style.css';
 
 export function Events() {
   const [text, setText] = useState ("Nie kliknięto we mnie");
-
   const [counter, setCounter] = useState (0);
+  const [inputText, setInputText] = useState ("");
 
   function handleOnClick() {
     setText("Kliknięto we mnie");
@@ -15,10 +15,28 @@ export function Events() {
     else {
     setCounter((counter) => counter + 1);
   }}
+    function handleOnChange(event){
+      setInputText(event.target.value);
+    }
+
+    function handleGreeting() {
+      alert("Cześć ${inputText}");
+    }
+
+
   return (
     <div>
       <h2>Cześć!</h2>
-      <button onClick={handleOnClick}>{counter > 0 ? `${text} ${counter} razy` : text}
+      <input 
+      className='events-input' 
+      type='text' 
+      onChange={(event) => handleOnChange(event)}
+      />
+      <button className="events-btn" onClick={handleOnClick}>
+        {counter > 0 ? `${text} ${counter} razy` : text}
+      </button>
+      <button className="events-btn" onClick={handleGreeting}>
+        Kliknij
       </button>
     </div>
   );
