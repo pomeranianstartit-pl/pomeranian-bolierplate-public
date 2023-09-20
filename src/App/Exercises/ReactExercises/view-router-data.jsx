d3734d4ff4c0bc55d1bb2ba6f708a80b52a0e6fc
Natalia Drwal <nataliadrwal1984@gmail.com>
2023-09-20 18:27:53 +0200

    feat: event exercise 1

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,6 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+import { EventsMetaData } from './Events/router-data';
+export const blockRouterMetaData = [SubRouteExampleMetaData, EventsMetaData];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
