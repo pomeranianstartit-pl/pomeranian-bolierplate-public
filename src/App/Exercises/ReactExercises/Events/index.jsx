53b647a0723beae6a270d01791748ef306b3f7b9
Mateusz Tywczynski <mateusz.tywczynski@gmail.com>
2023-09-20 21:02:45 +0200

    feat/zadanie-2

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -10,10 +10,18 @@ import './style.css';
 
 export function Events() {
   const [text, setText] = useState('Nie kliknięto we mnie');
-  let text2 = 'Nie kliknięto we mnie 2';
+  const [counter, setCounter] = useState(0);
+
+  //let text2 = 'Nie kliknięto we mnie 2';
 
   function handleOnClick() {
     setText('Kliknięto we mnie');
+
+    if (counter >= 3) {
+      return setCounter((counter) => counter + 2);
+    }
+
+    setCounter((counter) => counter + 1);
   }
 
   // Zwykła funkcja
@@ -35,7 +43,59 @@ export function Events() {
   return (
     <div>
       <h2>Cześć i czołem!</h2>
-      <button onClick={handleOnClick}>{text}</button>
+      <button className="events-button" onClick={handleOnClick}>
+        <span>{text}</span> {counter > 0 && <span>{counter} razy</span>}
+      </button>
     </div>
   );
 }
+
+// 1. Dodać nowy useState do liczenia kliknięć
+//2. Inkrementować nową zmienną counter po każdym kliknięciu
+// 3. wyświertlić
+// ad1. const [counter, setCounter] = useState(0);
+// ad2. function handleOnClick() {
+//  setText('Kliknięto we mnie');
+//  setCounter((counter) => counter + 1);
+//ad3.
+//{counter > 0 ? `${text} ${counter} razy` : text}   TO TO SAMO CO <span>{text}</span> {counter > 0 && <span>{counter} razy</span>}
+//to pierwsze to zapis w JS a drugie w React
+
+//Funkcje warunkowe ---> if(counter >=3) {setCounter((counter) ==> counter + 2);
+//else {setCounter((counter) ==> counter + 1)}}
+
+//function handleOnClick() {
+//setText('Kliknięto we mnie');
+
+//if (counter >= 3) {
+//return setCounter((counter) => counter + 2);
+//}
+
+//setCounter((counter) => counter + 1);
+
+// If..else if..else
+// if (counter >= 5) {
+//   setCounter((counter) => counter + 2);
+// } else if (counter >= 3) {
+//   setCounter((counter) => counter + 3);
+// } else {
+//   setCounter((counter) => counter + 1);
+// }
+
+// Ternary operator
+// counter >= 3
+//   ? setCounter((counter) => counter + 2)
+//   : setCounter((counter) => counter + 1);
+
+// if..else
+// if (counter >= 3) {
+//   setCounter((counter) => counter + 2);
+// } else {
+//   setCounter((counter) => counter + 1);
+// }
+
+// if + if
+// if (counter < 3) {
+//   //         początkowa   początkowa + 1
+//   setCounter((counter) => counter + 1);
+// }
