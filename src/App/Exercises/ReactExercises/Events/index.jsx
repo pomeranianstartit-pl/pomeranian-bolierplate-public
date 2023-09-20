a9c0299a9254d4c661f04012ca0c8083fc8c9bd9
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-09-20 18:27:56 +0200

    Events ex1

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
+// 1. Dodać element <button>Jakiś tekst</button>
+// 2. Import useState z React
+// 3. Zadeklarowanie zmiennej która będzie przetrzymywać stan
+// 4. Zadeklarowanie zmiennej która będzie funkcją zmieniającą stan
+// 5. Ostylować
+
 export function Events() {
+  const [text, setText] = useState('Nie kliknięto we mnie');
+
+  function handleOnClick() {
+    setText('Kliknięto we mnie');
+  }
+
   return (
     <div>
-      <p>Pochodzę z Events</p>
+      <h2>Cześć! 👋</h2>
+      <button onClick={handleOnClick}>{text}</button>
     </div>
   );
 }
