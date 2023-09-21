cef482fb24e0adcc54581483b0e56dc54a11e497
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-09-21 21:22:02 +0200

    exs >> src/App/Blog/

--- src/App/Blog/index.jsx
+++ src/App/Blog/index.jsx
@@ -2,5 +2,76 @@ import './styles.css';
 
 export const Blog = () => {
   //BEM methodology
-  return <div className="container--blog">Blog</div>;
+  // <div className="container--blog">Blog</div>;
+  return (
+    <div>
+      <a href="/home/vladislon/projects/pomeranian-bolierplate-public/src/App/Blog/index.jsx"></a>
+
+      <section>
+        <data>30-11-2022</data>
+        <h1>Why are we so nostalgic for the 1990s?</h1>
+
+        <article>
+          Pop culture used to define a generation, but it seems the cultural,
+          music and fashion trends of the 1990s have been recycled, and what
+          Generation X considered its rite of passage into adulthood, is being
+          discovered and claimed by fledgling grown-ups as their own. There's
+          been the resurgence of vinyl as the trend-setters' choice of music
+          consumption rather than the ease of a digital download, and now the
+          hipsters have discovered the nostalgic sound of a whirring cassette
+          from which to enjoy the dulcet tones of everyone from Salt-N-Pepa to
+          Rick Astley.
+        </article>
+
+        <h4>Radosław Majdan</h4>
+        <p>Senior Marketing Specialist</p>
+      </section>
+
+      <section>
+        <data>30-11-2022</data>
+        <h1>I make mistakes!</h1>
+        <article>
+          “I’m selfish, impatient and a little insecure. I make mistakes, I am
+          out of control and at times hard to handle. But if you can’t handle me
+          at my worst, then you sure as hell don’t deserve me at my best.”
+        </article>
+
+        <h4>Radosław Majdan</h4>
+        <p>Senior Marketing Specialist</p>
+      </section>
+
+      <section>
+        <data>30-11-2022</data>
+        <h1>18 Record-Breaking, Controversial, and Weird Facts</h1>
+
+        <article>
+          The death of Queen Elizabeth II on September 8 at the age of 96
+          represents a monumental shift for the British monarchy and the people
+          of England. Royals have died before, of course, but the Queen ruled
+          for more than 70 years, and represented a certain stability and
+          decorum that held the institution of the Royal Family together as it
+          slowly grew more visibly anachronistic and battered by endless
+          scandals.
+        </article>
+
+        <div>
+          <a href="https://www.wp.pl/">WP.PL 1</a>
+        </div>
+
+        <div>
+          <a href="https://wiadomosci.wp.pl/to-zacieranie-sladow-chodzi-o-pakiety-wyborcze-sasina-6879848507329312a">
+            WP.PL 2
+          </a>
+        </div>
+        <div>
+          <a href="https://www.onet.pl/film/onetfilm/tvn-wbil-szpile-tvp-pokazano-alternatywna-rzeczywistosc-telewizji-rzadowej/2ksezfp,681c1dfa">
+            ONET.PL
+          </a>
+        </div>
+
+        <h4>Radosław Majdan</h4>
+        <p>Senior Marketing Specialist</p>
+      </section>
+    </div>
+  );
 };
