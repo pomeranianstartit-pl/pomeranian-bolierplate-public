fd67df19ac5b37da96dda1a7cdf555d285afc93c
Patryk Idzikowski <patryk.idzikowski@interia.pl>
2023-09-22 21:00:56 +0200

    dodanie animacji

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,12 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+import { blockRouterMetaData as BlockRouterMetaDataText } from './Text/router-data';
+import { blockRouterMetaData as BlockRouterMetaDataAnimations } from './Animations/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+export const blockRouterMetaData = [
+  BlockRouterMetaDataText,
+  blockRouterMetaData121220231,
+  BlockRouterMetaDataAnimations,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
