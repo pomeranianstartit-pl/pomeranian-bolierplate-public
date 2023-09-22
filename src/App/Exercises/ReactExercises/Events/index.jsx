f931ca7921a71c9e84e87fc52c175af5224c0da0
annamatyasek <annamatyasek@gmail.com>
2023-09-22 19:45:42 +0200

    adding styles

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -2,23 +2,43 @@ import React, { useState } from 'react';
 
 import './style.css';
 
-// 1. Dodać element <button>Jakiś tekst</button>
-// 2. Import useState z React
-// 3. Zadeklarowanie zmiennej która będzie przetrzymywać stan
-// 4. Zadeklarowanie zmiennej która będzie funkcją zmieniającą stan
-// 5. Ostylować
-
 export function Events() {
   const [text, setText] = useState('Nie kliknięto we mnie');
+  const [counter, setCounter] = useState(0);
+  const [inputText, setInputText] = useState('');
 
   function handleOnClick() {
     setText('Kliknięto we mnie');
+
+    if (counter >= 3) {
+      return setCounter((counter) => counter + 2);
+    }
+
+    setCounter((counter) => counter + 1);
+  }
+
+  function handleOnChange(event) {
+    setInputText(event.target.value);
+  }
+
+  function handleGreeting() {
+    alert(`Cześć 👋 ${inputText} 🥳`);
   }
 
   return (
     <div>
       <h2>Cześć! 👋</h2>
-      <button onClick={handleOnClick}>{text}</button>
+      <input
+        className="events-input"
+        type="text"
+        onChange={(event) => handleOnChange(event)}
+      />
+      <button className="events-btn" onClick={handleGreeting}>
+        Kliknij
+      </button>
+      <button className="events-btn" onClick={handleOnClick}>
+        {counter > 0 ? `${text} ${counter} razy` : text}
+      </button>
     </div>
   );
 }
