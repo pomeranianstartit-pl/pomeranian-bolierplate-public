93bed93fed92d6480b8953bb86e9d4425809fecd
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-09-22 21:14:41 +0200

    animation, intro

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,13 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataSelectors } from './Selectors/router-data';
+import { blockRouterMetaData as blockRouterMetaDataText } from './Text/router-data';
+import { blockRouterMetaData as blockRouterMetaDataAnimations } from './Animations/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaDataSelectors,
+  blockRouterMetaDataText,
+  blockRouterMetaDataAnimations,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
