8a045dfc76cb377193d6a7accfece15f1b09eb28
przemo <przemyslaw.kocjan94@gmail.com>
2023-09-23 14:37:23 +0200

    pomeranian-logo

--- src/App/Exercises/Js/Exercise-js-variables-2/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-2/index.jsx
@@ -2,7 +2,7 @@ import './styles.css';
 
 export function Exercise() {
   const mathResult1 = 2 * 5;
-
+  let mathResult2 = 3 / 2;
   return (
     <div className="playing-with-variables">
       <section>
@@ -12,7 +12,7 @@ export function Exercise() {
         </pre>
         {/* Uzupełnij wykorzystując zmienne*/}
         <pre>
-          <code>3 : 2 = ___</code>
+          <code>3 : 2 = {mathResult2}</code>
         </pre>
         <pre>
           {/* kwadrat liczby */}
