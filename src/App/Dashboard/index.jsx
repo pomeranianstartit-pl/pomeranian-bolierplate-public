11ce53fa80ea5899dda728b01218a901bbfca569
bartoszte <bart.tarnopolski@gmail.com>
2023-09-23 14:37:16 +0200

    logo in header etc

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,6 +1,18 @@
 import './styles.css';
 
-export const Dashboard = () => {
+// export const Dashboard = () => {
+//   return (
+//     <div className="dashboard">
+//       <h1>Dashboard</h1>
+//       <p>
+//         Strona główna kursanta (o sobie, zdjęcie, opis narzędzi wykorzystywanych
+//         na kursie oraz umejętności zdobytych na kursie)
+//       </p>
+//     </div>
+//   );
+// };
+
+export function Dashboard() {
   return (
     <div className="dashboard">
       <h1>Dashboard</h1>
@@ -10,4 +22,4 @@ export const Dashboard = () => {
       </p>
     </div>
   );
-};
+}
--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -6,7 +6,8 @@ export function Exercise() {
   const string3 = 'Ulubiony bohater z serialu "Gra o tron" to ' + string1;
 
   const string2 = `Ulubiony bohater z serialu "Gra o tron" to: ${string1}`;
-
+  let userName = 'Bartosz';
+  let userNumb = 32;
   return (
     <div className="playing-with-variables">
       <section>
@@ -19,7 +20,7 @@ export function Exercise() {
         <p>{string2}</p>
         <p>{string3}</p>
         {/* Uzupełnij zanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>Witaj ____! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
+        <p>Witaj {userName}! Jesteś {userNumb} uzytkownikiem odwiedzającym tę stronę!</p>
       </section>
     </div>
   );
