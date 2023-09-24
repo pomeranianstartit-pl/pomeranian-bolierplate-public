56491b12cfc9c6587e5573b2588997858bb6c6f9
Artur Szwemiński <szweminskiartur@gmail.com>
2023-09-24 13:43:06 +0200

    added exercise with input and onChange event handler

---
+++ src/App/Exercises/ReactExercises/ReactOnChange/router-data.jsx
@@ -0,0 +1,10 @@
+import { ReactOnChange } from './ReactOnChange';
+
+export const ReactOnChangeMetaData = {
+  path: 'react-on-change',
+  date: '24-09-2023',
+  linkLabel: 'React nauka eventu on change',
+  blockNo: 8,
+  element: <ReactOnChange />,
+  tags: ['components', 'import', 'export', 'onchange', 'event'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,8 +3,10 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
 import { ReactOnClickRouterData } from './ReactOnClick/router-data';
+import { ReactOnChangeMetaData } from './ReactOnChange/router-data';
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactOnClickRouterData,
+  ReactOnChangeMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
