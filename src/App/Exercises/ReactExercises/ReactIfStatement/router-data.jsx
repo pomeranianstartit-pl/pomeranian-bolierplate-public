803a651aaee50f0e8f11a78e96f78d360560a090
Patrycja <patrycja.ojewska@gmail.com>
2023-09-24 15:34:15 +0200

    added excercise with conditional rendering and if statements

---
+++ src/App/Exercises/ReactExercises/ReactIfStatement/router-data.jsx
@@ -0,0 +1,10 @@
+import { ReactIfStatement } from './ReactIfStatement';
+
+export const ReactIfStatementMetaData = {
+  path: 'reacts-if-statements',
+  date: '24-09-2023',
+  linkLabel: 'Reacts ćwiczenia - if statements',
+  blockNo: 1,
+  element: <ReactIfStatement />,
+  tags: ['components', 'import', 'export', 'if', 'else'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactOnClickRouterData } from './ReactOnCLick/router-data';
 import { ReactOnChangeRouterData } from './ReactOnChange/router-data';
+import { ReactIfStatementMetaData } from './ReactIfStatement/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactOnClickRouterData,
   ReactOnChangeRouterData,
+  ReactIfStatementMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
