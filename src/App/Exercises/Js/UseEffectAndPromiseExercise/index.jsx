2ff2d288512461c2d8e5c31071a5c2c84cd01d1b
Mike <michal.sikorski@pomeranianstartit.pl>
2023-09-24 12:49:41 +0200

    Ćwiczenie Promies

---
+++ src/App/Exercises/Js/UseEffectAndPromiseExercise/index.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+export const UseEffectAndPromiseExercise = () => {
+  const [data, setData] = useState();
+
+  function loadUser() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > 0.9) {
+          reject('Błąd pobrania');
+        }
+        resolve({ id: 1, name: 'John Doe' });
+      }, 2000);
+    });
+  }
+
+  function loadUserDetails(userId) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ id: userId, age: 30, country: 'Poland' });
+      });
+    });
+  }
+
+  useEffect(() => {
+    loadUser()
+      .then((result) => loadUserDetails(result.id))
+      .then((userDetails) => setData(userDetails))
+      .catch((err) => console.log(`Przechwycony błąd: ${err}`));
+  }, []);
+
+  return (
+    <div>
+      <h1>UseEffectAndPromiseExercise</h1>
+      <p>LoadUser: {JSON.stringify(data)}</p>
+    </div>
+  );
+};
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -22,6 +22,7 @@ import { blockRouterMetaData as OOPzadania } from './OOP-Zadania/router-data';
 import { blockRouterMetaData as SavedInput } from './SavedInput/router-data';
 import { blockRouterMetaData as JsonXMLStorage } from './JsonXMLStorage/router-data';
 import { blockRouterMetaData as PromisesAndMe } from './PromisesAndMe/router-data';
+import { blockRouterMetaData as UseEffectAndPromiseExercise } from './UseEffectAndPromiseExercise/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
@@ -46,5 +47,6 @@ export const blockRouterMetaData = [
   SavedInput,
   JsonXMLStorage,
   PromisesAndMe,
+  UseEffectAndPromiseExercise,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
