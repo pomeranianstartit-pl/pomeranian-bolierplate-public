e96a2d8e1ceed81c73143ad534bbf1cc044487d1
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-09-24 13:43:38 +0200

    added excercise with input and onChange event handler

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactOnClickMetaData } from './ReactOnClick/router-data';
+import { ReactOnChangeMetaData } from './ReactOnChange/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactOnClickMetaData,
+  ReactOnChangeMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
