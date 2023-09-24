77c54e3049ec0a1a27d284882f653c05c4771d01
KarolinaDuljas <duljas.karolina@gmail.com>
2023-09-24 17:15:48 +0200

    Skonczone zadanie z api request

--- src/App/Projects/ApiRequestData/ApiRequestDataApp.jsx
+++ src/App/Projects/ApiRequestData/ApiRequestDataApp.jsx
@@ -30,45 +30,6 @@ export const ApiRequestDataApp = ({ API_URL, endOfUrl }) => {
           </tr>
         ))}
       </tbody>
-
-      {/* {endOfUrl == '/posts' &&
-        data.map((dataItem) => {
-          return (
-            <tr>
-              <td>{JSON.stringify(dataItem.userId)}</td>
-              <td>{JSON.stringify(dataItem.id)}</td>
-              <td>{JSON.stringify(dataItem.title)}</td>
-              <td>{JSON.stringify(dataItem.body)}</td>
-            </tr>
-          );
-        })}
-      {endOfUrl == '/comments' &&
-        data.map((dataItem) => {
-          return (
-            <tr>
-              <td>{JSON.stringify(dataItem.postId)}</td>
-              <td>{JSON.stringify(dataItem.id)}</td>
-              <td>{JSON.stringify(dataItem.name)}</td>
-              <td>{JSON.stringify(dataItem.email)}</td>
-              <td>{JSON.stringify(dataItem.body)}</td>
-            </tr>
-          );
-        })}
-      {endOfUrl == '/users' &&
-        data.map((dataItem) => {
-          return (
-            <tr>
-              <td>{JSON.stringify(dataItem.id)}</td>
-              <td>{JSON.stringify(dataItem.name)}</td>
-              <td>{JSON.stringify(dataItem.username)}</td>
-              <td>{JSON.stringify(dataItem.email)}</td>
-              <td>{JSON.stringify(dataItem.address)}</td>
-              <td>{JSON.stringify(dataItem.phone)}</td>
-              <td>{JSON.stringify(dataItem.website)}</td>
-              <td>{JSON.stringify(dataItem.company)}</td>
-            </tr>
-          );
-        })} */}
     </table>
   );
 };
