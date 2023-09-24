839c25d20832544083d656d079eac5926decd910
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-09-24 12:12:10 +0200

    added excericite withuseState and onClick

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
+import { ReactOnClickMetaData } from './ReactOnClick/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  ReactOnClickMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
