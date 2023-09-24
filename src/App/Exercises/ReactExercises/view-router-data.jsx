530dc6e97f10fb552a9222dd2d1cd982fb11f4e5
ArekFrel <arkadiuszfrel@gmail.com>
2023-09-24 12:08:28 +0200

    New Exercise added - onClick

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,9 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+import { ReactOnClickMetaData } from './ReactOnClick/router-data';
+export const blockRouterMetaData = [
+    SubRouteExampleMetaData, 
+    ReactOnClickMetaData
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
