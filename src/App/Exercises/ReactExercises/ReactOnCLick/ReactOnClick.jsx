0eb4427e97003b90a48d85a3d6a825b74111a51f
Patrycja <patrycja.ojewska@gmail.com>
2023-09-24 12:09:17 +0200

    event

--- src/App/Exercises/ReactExercises/ReactOnCLick/ReactOnClick.jsx
+++ src/App/Exercises/ReactExercises/ReactOnCLick/ReactOnClick.jsx
@@ -5,18 +5,28 @@ import { useState } from 'react';
 // }
 
 export function ReactOnCLick() {
-  const [text, setText] = useState('Który przycisk został kliknięty?');
+  const [text, setText] = useState('test');
+  const [number, setNumber] = useState(0);
 
   function handleClick(event) {
-    setText(event.target.innerHTML, 'event');
+    setText(event.target.innerHTML);
+  }
+
+  function handleClickNumber(event) {
+    setNumber(event.target.innerHTML);
   }
 
   return (
     <div>
-      <h1>React on Click</h1>
+      <h1>React OnClick</h1>
       <button onClick={handleClick}>Drugi przycisk</button>
       <button onClick={handleClick}>Pierwszy przycisk</button>
+
+      <button onClick={handleClickNumber}>5</button>
+      <button onClick={handleClickNumber}>10</button>
+
       <p>{text}</p>
+      <p>{number}</p>
     </div>
   );
 }
