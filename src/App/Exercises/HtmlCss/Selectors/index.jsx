e63cbb50644f81d9bf67cd9d1a2a8d4bba447137
arleta dabek <itikotka@gmail.com>
2023-09-25 17:17:01 +0200

    selectors

---
+++ src/App/Exercises/HtmlCss/Selectors/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div>
+      <p>To jest jeden paragraf</p>
+      <div>Jestem bardzo unikalnym elementem HTML</div>
+      <p className='secondParagraph'>to jest drugi paragraf</p>
+      <button id="sellOpelButtonId">Srzedam opla</button>
+      <div>
+        <a href="http://wp.pl">Przenieś mnie do wp.pl</a>
+      </div>
+
+      <div>
+        <a href="http://onet.pl">Przenieś mnie do onet.pl</a>
+      </div>
+    </div>
+  );
+}
