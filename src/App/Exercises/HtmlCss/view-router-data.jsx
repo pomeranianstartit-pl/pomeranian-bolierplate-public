516748082916a444b71699fb4852d05a221e7432
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-09-25 20:58:30 +0200

    ex 2023-09-25

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -4,11 +4,19 @@ import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-
 import { blockRouterMetaData as blockRouterMetaDataSelectors } from './Selectors/router-data';
 import { blockRouterMetaData as blockRouterMetaDataText } from './Text/router-data';
 import { blockRouterMetaData as blockRouterMetaDataAnimations } from './Animations/router-data';
+import { blockRouterMetaData as blockRouterMetaDataColors } from './Colors/router-data';
+import { blockRouterMetaData as blockRouterMetaDataOpacity } from './Opacity/router-data';
+import { blockRouterMetaData as blockRouterMetaDataBackground } from './Background/router-data';
+import { blockRouterMetaData as blockRouterMetaDataVideoAudioIframe } from './VideoAudioIframe/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaDataSelectors,
   blockRouterMetaDataText,
   blockRouterMetaDataAnimations,
+  blockRouterMetaDataColors,
+  blockRouterMetaDataOpacity,
+  blockRouterMetaDataBackground,
+  blockRouterMetaDataVideoAudioIframe,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
