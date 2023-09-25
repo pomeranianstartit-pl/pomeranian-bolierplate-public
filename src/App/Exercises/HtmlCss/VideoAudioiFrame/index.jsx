86ba7f1d5e46261b1647f7dc8afd2b15bfe49803
Patryk Idzikowski <patryk.idzikowski@interia.pl>
2023-09-25 20:58:50 +0200

    dodalismy pare cwiczen

---
+++ src/App/Exercises/HtmlCss/VideoAudioiFrame/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div>
+      <iframe
+        title="Jak zarobić milion w 5 minut?"
+        width="765"
+        height="430"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
