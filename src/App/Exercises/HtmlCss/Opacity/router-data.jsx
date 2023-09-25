2c32461919a6d15ea0b9e94344e5725a1a2e3f12
Irmina <144358461+irminak@users.noreply.github.com>
2023-09-25 21:02:27 +0200

    Colors, background, opacity, video (#5)

---
+++ src/App/Exercises/HtmlCss/Opacity/router-data.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'opacity-exercise',
+  date: '22-05-2023',
+  linkLabel: 'Opacity',
+  blockNo: 5,
+  element: <Exercise />,
+  tags: [
+    'css',
+    'color',
+    'text-transform',
+    'text-decoration',
+    'font-family',
+    'font-size',
+    'font-weight',
+    'font-style',
+    'line-height',
+    'letter-spacing',
+    'word-spacing',
+    'text-align',
+  ],
+};
---
+++ src/App/Exercises/HtmlCss/VideoAudioIframe/router-data.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'video-audio-iframe',
+  date: '22-05-2023',
+  linkLabel: 'Video Audio Iframe',
+  blockNo: 6,
+  element: <Exercise />,
+  tags: [
+    'css',
+    'color',
+    'text-transform',
+    'text-decoration',
+    'font-family',
+    'font-size',
+    'font-weight',
+    'font-style',
+    'line-height',
+    'letter-spacing',
+    'word-spacing',
+    'text-align',
+  ],
+};
