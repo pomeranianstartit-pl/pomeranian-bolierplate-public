08ecf1ca52e7df991aa0acf348c148df42cbfc12
Irmina Kalinowska <irminakalinowska0@gmail.com>
2023-09-25 21:00:35 +0200

    Colors, background, opacity, video

---
+++ src/App/Exercises/HtmlCss/Background/router-data.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'backgroung-exercise',
+  date: '22-05-2023',
+  linkLabel: 'Background',
+  blockNo: 4,
+  element: <Exercise />,
+  tags: [
+    'css',
+    'color',
+    'text-transform',
+    'text-decoration',
+    'font-family',
+    'font-size',
+    'font-weight',
+    'font-style',
+    'line-height',
+    'letter-spacing',
+    'word-spacing',
+    'text-align',
+  ],
+};
---
+++ src/App/Exercises/HtmlCss/Colors/router-data.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'colors-exercise',
+  date: '22-05-2023',
+  linkLabel: 'Colors',
+  blockNo: 13,
+  element: <Exercise />,
+  tags: [
+    'css',
+    'color',
+    'text-transform',
+    'text-decoration',
+    'font-family',
+    'font-size',
+    'font-weight',
+    'font-style',
+    'line-height',
+    'letter-spacing',
+    'word-spacing',
+    'text-align',
+  ],
+};
