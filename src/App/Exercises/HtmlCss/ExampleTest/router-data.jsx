9291e1029e19bb93f44ee15a04a9b7dfa15c4f01
DawidMazur <50441054+DawidMazur@users.noreply.github.com>
2023-09-26 21:29:00 +0200

    configuration

---
+++ src/App/Exercises/HtmlCss/ExampleTest/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ExampleComponent } from './ExampleComponent';
+
+export const blockRouterMetaData = {
+  path: '12-12-2023-1uniq',
+  date: '12-12-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie 2 z dnia 12-12-2023',
+  blockNo: 1,
+  element: <ExampleComponent />,
+  tags: ['flex', 'grid', 'video'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,5 +2,10 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as blockRouterMetaDataEx2 } from './ExampleTest/router-data';
+
+export const blockRouterMetaData = [
+    blockRouterMetaData121220231,
+    blockRouterMetaDataEx2,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
