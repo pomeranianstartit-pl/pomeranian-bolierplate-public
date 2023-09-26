2110a18b082b2f6cbfc00201eafc6fb7019b447f
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-09-26 12:29:42 +0200

    added exercise with text stylings etc

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,5 +2,16 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as blockRouterMetaDataColors } from './Colors/router-data';
+import { blockRouterMetaData as blockRouterMetaDataOpacity } from './Opacity/router-data';
+import { blockRouterMetaData as blockRouterMetaDataBackground } from './Background/router-data';
+import { blockRouterMetaData as blockRouterMetaDataAudioVideoIfrem } from './AudioVideoIframe/router-data';
+
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  blockRouterMetaDataColors,
+  blockRouterMetaDataOpacity,
+  blockRouterMetaDataBackground,
+  blockRouterMetaDataAudioVideoIfrem,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
