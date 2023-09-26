f27cc6e2a44a6bf32f8793e8ebb90ec670cfbc77
annamatyasek <annamatyasek@gmail.com>
2023-09-26 17:47:36 +0200

    audio-frame changes

--- src/App/Exercises/HtmlCss/Colors/index.jsx
+++ src/App/Exercises/HtmlCss/Colors/index.jsx
@@ -1,9 +1,21 @@
-import './style.css';
+import './styles.css';
 
 export function Exercise() {
   return (
-    <div>
-      <p>Jestem przykładowym routem</p>
+    <div className="colors-wrapper">
+      <div className="colorName">Kolor przy użyciu: colorName</div>
+
+      <div className="shortenHex">Kolor przy użyciu: shortenHex</div>
+
+      <div className="normalHex">Kolor przy użyciu: normalHex</div>
+
+      <div className="rgb">Kolor przy użyciu: rgb</div>
+
+      <div className="rgba">Kolor przy użyciu: rgba</div>
+
+      <div className="hsl">Kolor przy użyciu: hsl</div>
+
+      <div className="hsla">Kolor przy użyciu: hsla</div>
     </div>
   );
 }
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,15 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataColors } from './Colors/router-data';
+import { blockRouterMetaData as blockRouterMetaDataOpacity } from './Opacity/router-data';
+import { blockRouterMetaData as blockRouterMetaDataBackground } from './Background/router-data';
+import { blockRouterMetaData as blockRouterMetaDataVideoAudioIframe } from './VideoAudioframe/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaDataColors,
+  blockRouterMetaDataOpacity,
+  blockRouterMetaDataBackground,
+  blockRouterMetaDataVideoAudioIframe,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
