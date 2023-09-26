c218ff20fbe84a81e8db35b98c539791e8144cd8
arleta dabek <itikotka@gmail.com>
2023-09-26 23:57:59 +0200

    exercise - text

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataSelectors } from './Selectors/router-data';
+import { blockRouterMetaData as blockRouterMetaDateText } from './Text/router-data';
 
 export const blockRouterMetaData = [
     blockRouterMetaData121220231, 
-    blockRouterMetaDataSelectors
+    blockRouterMetaDataSelectors, 
+    blockRouterMetaDateText
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
