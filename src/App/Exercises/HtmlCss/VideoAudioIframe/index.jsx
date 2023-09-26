17b91a9882c4b0c06eb1edefbacfbe847f17438c
caffeine108 <kamila.wyroslak@gmail.com>
2023-09-26 19:51:43 +0200

    added exercise with box-model

--- src/App/Exercises/HtmlCss/VideoAudioIframe/index.jsx
+++ src/App/Exercises/HtmlCss/VideoAudioIframe/index.jsx
@@ -23,7 +23,7 @@ export function Exercise() {
         src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
       ></iframe>
 
-      <figure>
+      <figure className="audio-class-name">
         <figcaption>Przykładowe audio</figcaption>
 
         <audio controls src={sampleAudio}>
