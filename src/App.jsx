d1e9670e7c826e3a8c6421cb2891cc0e96a4ecf9
caffeine108 <kamila.wyroslak@gmail.com>
2023-09-26 21:02:20 +0200

    added faq page and props excercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-
 import { NotFound } from './App/Components/NotFound/NotFound';
-
 import { Blocks } from './App/Blocks';
 import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
 import { Exercises } from './App/Exercises';
 import { Calendar } from './App/Calendar';
-import { CV } from './App/CV';
 import { Blog } from './App/Blog';
+import { CV } from './App/CV';
+import { FAQ } from './App/FAQ';
 
 export function App() {
   return (
@@ -21,6 +20,7 @@ export function App() {
           <Route path="cv/*" element={<CV />} />
           <Route path="exercises/*" element={<Exercises />} />
           <Route path="calendar/*" element={<Calendar />} />
+          <Route path="FAQ/*" element={<FAQ />} />
           <Route path="blocks/*" element={<Blocks />} />
           <Route path="blog/" element={<Blog />} />
           <Route path="*" element={<NotFound />} />
