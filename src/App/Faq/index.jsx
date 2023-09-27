8181a2b671739202ed55d642f1de92575ee019f4
Katarzyna Winklas <winklas.k@gmail.com>
2023-09-27 20:59:11 +0200

    faq edited

--- src/App/Faq/index.jsx
+++ src/App/Faq/index.jsx
@@ -1,43 +1,17 @@
-import { useState } from 'react';
+import { FaqNote } from './components/FaqNote';
+
+import { mockData } from './data';
 
 import './styles.css';
 
-function FaqNote(props) {
-  console.log(props, 'props');
-
-  const [display, setDisplay] = useState(false);
-
-  function handleButton() {
-    setDisplay(!display);
-  }
-
-  return (
-    <div className="faq-note-wrapper">
-      <h2>{props.title}</h2>
-
-      <p>{props.desciption}</p>
-
-      {display && <div>Wyświetl mnie!</div>}
-
-      <button onClick={handleButton}>Kliknij</button>
-    </div>
-  );
-}
-
 export function Faq() {
+  function renderNote(note) {
+    return <FaqNote title={note.title} desciption={note.description} />;
+  }
   return (
     <div className="faq">
       <h1>Faq</h1>
-
-      <FaqNote title="Ile facet powinien wziąć na klate" desciption="test1" />
-
-      <FaqNote title="Ile lat żyją psy" desciption="test1" />
-
-      <FaqNote title="Ala ma kota" desciption="test1" />
-
-      <FaqNote title="Kot ma ale" desciption="test1" />
-
-      <FaqNote title="Ile kobieta powinna wziąć na klate" desciption="test1" />
+      {mockData.map(renderNote)}
     </div>
   );
 }
