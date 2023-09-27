a77b1d4564873a73d58d39b063215d1de9060ce0
arleta dabek <itikotka@gmail.com>
2023-09-28 00:20:25 +0200

    text correction and start animations

--- src/App/Exercises/HtmlCss/Text/router-data.jsx
+++ src/App/Exercises/HtmlCss/Text/router-data.jsx
@@ -3,18 +3,24 @@ import React from 'react';
 import { Exercise } from './index';
 
 export const blockRouterMetaData = {
-  path: 'animation-exercise',
+  path: 'text-exercise',
   date: '22-09-2023',
-  linkLabel: 'HTML & CSS - ćwiczenie dotyczące animacji',
+  linkLabel: 'HTML & CSS - ćwiczenie dotyczące tekstu',
   blockNo: 11,
   element: <Exercise />,
-  tags: [
+  tags: 
+  [
     'css',
-    'animation',
-    'transition',
-    'transform',
-    'animation-name',
-    'animation-duration',
-    'animation-delay',
+    'color',
+    'text-transform',
+    'text-decoration ',
+    'font-family',
+    'font-size',
+    'font-weight',
+    'font-style',
+    'line-heihgt',
+    'letter-spacing', 
+    'word-spacing',
+    'text-align',
   ],
 };
