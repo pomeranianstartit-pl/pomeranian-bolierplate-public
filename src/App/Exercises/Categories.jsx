b1b0ed0199b888876f3e0993c3c8a2c96234b7bc
Adam Karcz <adam.karcz.1984@gmail.com>
2023-09-27 09:08:32 +0200

    categories styling & upgrade

--- src/App/Exercises/Categories.jsx
+++ src/App/Exercises/Categories.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import html5Icon from '../Images/TechStack/html5.svg';
+import jsIcon from '../Images/TechStack/js.svg';
+import reactIcon from '../Images/TechStack/react.svg';
 
 import './categories.css';
 
@@ -9,9 +12,26 @@ export const Categories = () => {
       <div className="exercises-categories">
         <h2>Kategorie</h2>
 
-        <NavLink to="html-css">HTML & CSS - lista ćwiczeń</NavLink>
-        <NavLink to="js">JS - lista ćwiczeń</NavLink>
-        <NavLink to="react">React - lista ćwiczeń</NavLink>
+        <NavLink className="exercises-category" to="html-css">
+          <img
+            src={html5Icon}
+            alt="Ćwiczenia HTML & CSS"
+            className="category-icon"
+          />
+          HTML & CSS - lista ćwiczeń
+        </NavLink>
+        <NavLink className="exercises-category" to="js">
+          <img src={jsIcon} alt="Ćwiczenia JS" className="category-icon" />
+          JS - lista ćwiczeń
+        </NavLink>
+        <NavLink className="exercises-category" to="react">
+          <img
+            src={reactIcon}
+            alt="Ćwiczenia React"
+            className="category-icon"
+          />
+          React - lista ćwiczeń
+        </NavLink>
         {/* <NavLink to="web-api">Web API - lista ćwiczeń</NavLink>
         <NavLink to="async">Asynchroniczność - lista ćwiczeń</NavLink>    ////these lines are hidden due to course unclean exercises categorization
         <NavLink to="firebase">Firebase - lista ćwiczeń</NavLink> */}
