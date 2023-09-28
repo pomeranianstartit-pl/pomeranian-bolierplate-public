bd3486ca47d9ffce88b70b1a2a389cc33869d738
krzysztofjesiolowski <jesiolowskikrzysztof@gmail.com>
2023-09-28 20:46:30 +0200

    added excersites with useState and onClick

---
+++ src/App/Exercises/ReactExercises/ReactOnClick/router-data.jsx
@@ -0,0 +1,10 @@
+import { ReactOnClick } from './ReactOnClick';
+
+export const ReactOnClickMetaData = {
+  path: 'react-on-click',
+  date: '28-09-2023',
+  linkLabel: 'React nauka eventu onClick',
+  blockNo: 8,
+  element: <ReactOnClick />,
+  tags: ['components', 'import', 'export', 'onClick', 'event'],
+};
