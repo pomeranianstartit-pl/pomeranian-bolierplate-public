ac80793c535bb7ab34dd4d92d140a111967da064
Mateusz Tywczynski <mateusz.tywczynski@gmail.com>
2023-09-28 18:42:11 +0200

    first tables

---
+++ src/App/Exercises/HtmlCss/TablesTennis/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'tables-tennis',
+  date: '12-12-2023',
+  linkLabel: 'Tabelki - style',
+  blockNo: 14,
+  element: <Exercise />,
+  tags: ['tables', 'html', 'css'],
+};
