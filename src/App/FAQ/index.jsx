36ec9ed4e9af4ec7367c3d40a75a73ea90fbd1f4
caffeine108 <kamila.wyroslak@gmail.com>
2023-09-28 16:09:30 +0100

    FAQ added with function and arrow

--- src/App/FAQ/index.jsx
+++ src/App/FAQ/index.jsx
@@ -1,41 +1,17 @@
-import { useState } from 'react';
+import { FAQNote } from '.';
+
+import { mockData } from './data';
+
 import './styles.css';
 
-function FAQNote(props) {
-  console.log(props, 'props');
-  const [display, setDisplay] = useState(false);
-
-  function handleButton() {
-    setDisplay(!display);
-  }
-
-  return (
-    <div className="faq-note-wrapper">
-      <h2>{props.title}</h2>
-      <p>{props.desciption}</p>
-
-      {display && <div>Wyświetl mnie!</div>}
-
-      <button onClick={handleButton}>Kliknij</button>
-    </div>
-  );
-}
-
 export function FAQ() {
+  function renderNote(note) {
+    return <FAQNote title={note.title} desciption={note.description} />;
+  }
   return (
     <div className="faq">
       <h1>Faq</h1>
-      <FAQNote
-        title="Ile facet powinien wziąć na klate"
-        desciption="testasdasdasd1"
-      />
-      <FAQNote title="Ile lat żyją psy" desciption="teasdasdasdasdst2" />
-      <FAQNote title="Ala ma kota" desciption="zzzzzzzzzzzztest3" />
-      <FAQNote title="Kot ma ale" desciption="aaaaaaaaaaaaaaaaaaatest4" />
-      <FAQNote
-        title="Ile kobieta powinna wziąć na klate"
-        desciption="xxxxxxxxxxxxtest5"
-      />
+      {mockData.map(renderNote)}
     </div>
   );
 }
