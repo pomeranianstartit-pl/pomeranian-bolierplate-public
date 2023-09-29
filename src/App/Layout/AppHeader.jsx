a077a8348bdac454e181749cac8a026bf72a6604
annamatyasek <annamatyasek@gmail.com>
2023-09-29 19:29:11 +0200

    styling header

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,11 +1,25 @@
 import './styles/header.css';
 
 import { ReactComponent as PomeranianLogo } from '../Images/start-it-logo.svg';
+import { ReactComponent as Setting } from '../Images/setting.svg';
+import { ReactComponent as Ellipse } from '../Images/Ellipse 2.svg';
+import { ReactComponent as RightArrow } from '../Images/right-arrow 1.svg';
 
 export function AppHeader() {
   return (
     <header>
       <PomeranianLogo />
+      <div className="header--menu">
+        <Setting />
+        <Ellipse />
+        <div className="header-menu-user">
+          {' '}
+          <p>Anna</p>
+          <p>kursant</p>
+        </div>
+        <RightArrow />
+      </div>
+
       {/* <HeaderMenu /> */}
     </header>
   );
