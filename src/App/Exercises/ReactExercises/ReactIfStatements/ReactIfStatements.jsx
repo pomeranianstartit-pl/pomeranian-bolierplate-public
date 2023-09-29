98730c89dfba1f576c6ad995379ac39849790096
krzysztofjesiolowski <jesiolowskikrzysztof@gmail.com>
2023-09-29 09:34:55 +0200

    added excersite with random number

--- src/App/Exercises/ReactExercises/ReactIfStatements/ReactIfStatements.jsx
+++ src/App/Exercises/ReactExercises/ReactIfStatements/ReactIfStatements.jsx
@@ -4,22 +4,15 @@ import './style.css';
 
 export function ReactIfStatements() {
   const [text, setText] = useState('Nie kliknięto przycisku!');
-
   const [clickedCount, setClickedCount] = useState(0);
 
   function handleClick() {
     // również możliwe zamiast linijek 17 i 23
-
     // setClickedCount(clickedCount + 1);
-
     // if (warunek) {
-
     //  kod który zostanie wykonany JEŚLI warunek jest prawdziwy
-
     // } else {
-
     //  kod który zostanie wykonany JEŚLI warunek jest fałszywy
-
     // }
 
     const newClickedCount = clickedCount + 1;
@@ -29,16 +22,13 @@ export function ReactIfStatements() {
     } else {
       setText('Kliknięto przycisk mniej niż 5 razy!');
     }
-
     setClickedCount(newClickedCount);
   }
 
   return (
     <div>
       <h1>React If Statements</h1>
-
       <button onClick={handleClick}>Kliknij mnie!</button>
-
       <p className="text">{text}</p>
       <p className="number">{clickedCount}</p>
       {clickedCount > 15 && (
