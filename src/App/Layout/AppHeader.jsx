9a8ab7396fd8a8a9153a7ff57665bff03b4df27b
annamatyasek <annamatyasek@gmail.com>
2023-09-29 21:09:59 +0200

    create separate component for HeaderMenu

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,26 +1,13 @@
 import './styles/header.css';
 
 import { ReactComponent as PomeranianLogo } from '../Images/start-it-logo.svg';
-import { ReactComponent as Setting } from '../Images/setting.svg';
-import { ReactComponent as Ellipse } from '../Images/Ellipse 2.svg';
-import { ReactComponent as RightArrow } from '../Images/right-arrow 1.svg';
+import { HeaderMenu } from '../Components/HeaderMenu';
 
 export function AppHeader() {
   return (
     <header>
       <PomeranianLogo />
-      <div className="header--menu">
-        <Setting />
-        <Ellipse />
-        <div className="header-menu-user">
-          {' '}
-          <p>Anna</p>
-          <p>kursant</p>
-        </div>
-        <RightArrow />
-      </div>
-
-      {/* <HeaderMenu /> */}
+      <HeaderMenu />
     </header>
   );
 }
