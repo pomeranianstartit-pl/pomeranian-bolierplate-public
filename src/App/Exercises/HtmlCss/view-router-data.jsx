9ed4a104630f026723e6749e189c6dcea7155df9
przemo <przemyslaw.kocjan94@gmail.com>
2023-09-29 20:28:44 +0200

    Styles.css selectors  exercise

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,9 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
-
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { SelectorsAndCascadeRouterMetaData } from './SelectorsAndCascade/router-data';
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  SelectorsAndCascadeRouterMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
