fd384d7e553b66d317fad3932843c3eed7ab5fc9
arleta dabek <itikotka@gmail.com>
2023-09-29 11:39:52 +0200

    exercise: box model, overflow

---
+++ src/App/Exercises/HtmlCss/BoxModel/router-data.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'box-model',
+  date: '26-09-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie dotyczące box modelu',
+  blockNo: 13,
+  element: <Exercise />,
+  tags: 
+  [
+    'css',
+    'display',
+    'box-model', 
+    'margin', 
+    'padding', 
+    'inline', 
+
+    
+  ],
+};
