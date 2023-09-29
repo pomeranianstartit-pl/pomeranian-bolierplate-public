e48a2d6c8f0fed4fe3d916967db199276d951898
DawidMazur <50441054+DawidMazur@users.noreply.github.com>
2023-09-29 16:19:23 +0200

    .

--- src/App/Blog/Author.jsx
+++ src/App/Blog/Author.jsx
@@ -1,8 +1,8 @@
 export const Author = ({ author, authorDescription }) => {
-    return (
-        <div>
-            <h3 className="blog-post-author">{author}</h3>
-            <p className="blog-post-author-description">{authorDescription}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="blog-post-author">{author}</h3>
+      <p className="blog-post-author-description">{authorDescription}</p>
+    </div>
+  );
 };
