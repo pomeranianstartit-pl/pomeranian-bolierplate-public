92be35d02353cfdb3d6940573245771b61c1a200
Patrycja <patrycja.ojewska@gmail.com>
2023-09-29 20:28:40 +0200

    css selectors

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SelectorsAndCascade } from './index';
+
+export const SelectorsAndCascadeRouterMetaData = {
+  path: 'selectors-and-cascade',
+  date: '29-09-2023',
+  linkLabel: 'Selektory i kaskadowość',
+  blockNo: 1,
+  element: <SelectorsAndCascade />,
+  tags: ['selectors', 'class', 'atribute'],
+};
