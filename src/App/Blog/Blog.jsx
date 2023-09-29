90bf7bd4cc8175ecf9c708c04334525415361d5b
ArekFrel <arkadiuszfrel@gmail.com>
2023-09-29 20:56:02 +0200

    blog10_zad3 initialization

---
+++ src/App/Blog/Blog.jsx
@@ -0,0 +1,5 @@
+import './Blog.css';
+
+export function Blog() {
+    return <div>Blog</div>;
+}
--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -19,6 +19,9 @@ export function AppAside() {
           <li>
             <NavLink to="cv">CV</NavLink>
           </li>
+          <li>
+            <NavLink to="blog">Blog</NavLink>
+          </li>
         </ul>
       </nav>
       <p style={{ padding: '1rem 0' }}>Sidebar items, widgets, etc</p>
