5319b1dde737102eabe04d03cd56f185d4d7491b
bartoszte <bart.tarnopolski@gmail.com>
2023-09-30 15:28:13 +0200

    blok11 animations exercise figma

--- src/App/Exercises/HtmlCss/CssAnimations/index.jsx
+++ src/App/Exercises/HtmlCss/CssAnimations/index.jsx
@@ -3,7 +3,23 @@ import './styles.css';
 export function CssAnimations() {
   return (
     <div>
-      <div className="animation">css animations</div>
+      <div className="">css animations</div>
+
+      {/* To jest makieta 1 */}
+      <div>
+        <h2 className="color-primary style-h2 heading-animation">
+          Masz pytania?
+        </h2>
+        <h2 className="style-h2">Nasz specjalista chętnie Ci pomoże.</h2>
+        <address className="style-address">
+          <p>Eleonora Frąckiewiczówna</p>
+          <p className="font-weight-600 color-primary margin-bottom-17">
+            Specjalista ds. finansowania
+          </p>
+          <p>efrackiewoczowna@rtc.pl</p>
+          <p className="number-animation">500 677 543</p>
+        </address>
+      </div>
     </div>
   );
 }
