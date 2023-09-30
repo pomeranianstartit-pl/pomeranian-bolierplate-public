ecb61b996326cc1ca1e1dcf05cf2728ac4baae32
Artur Szwemiński <szweminskiartur@gmail.com>
2023-09-30 16:45:43 +0200

    pictures and backgrodund pictures

---
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+export function ImageFiles() {
+  return (
+    <div className="images-files">
+      <img
+        src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png"
+        alt=""
+      ></img>
+      <img
+        src="https://img.freepik.com/darmowe-zdjecie/strzal-zblizenie-piekny-kotek-krajowych-imbir-siedzi-na-bialej-powierzchni_181624-35913.jpg"
+        alt=""
+      />
+      <img src="https://www.svgrepo.com/show/85124/cat.svg" alt="" />
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -4,6 +4,7 @@ import { CssAnimationsMetaData } from './CssAnimations/router-data';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
+import { ImageFilesMetaData } from './ImageFiles/router-data';
 
 import { TextFundamentsMetaData } from './TextFundaments/router-data';
 // import { SelectorAndCascadeRouterMetaData } from './SelectorsAndCascade/router-data';
@@ -15,5 +16,6 @@ export const blockRouterMetaData = [
   GoogleFontsMetaData,
   CssAnimationsMetaData,
   ColorsMetaData,
+  ImageFilesMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
