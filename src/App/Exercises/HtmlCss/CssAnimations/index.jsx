ff958972d8ccc830e189bc626d7f61bfb9b4d422
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-09-30 15:27:06 +0200

    css-anim

--- src/App/Exercises/HtmlCss/CssAnimations/index.jsx
+++ src/App/Exercises/HtmlCss/CssAnimations/index.jsx
@@ -1,5 +1,21 @@
 import './style.css';
 
 export function CssAnimations() {
-  return <div className="animation"></div>;
+  return (
+    <div>
+      <div className="question-wrapper">
+        <h2 className="color-primary">Masz pytania</h2>
+        <h2>Nasz specjalista chętnie Ci pomoże.</h2>
+
+        <address className="address">
+          <p>Eleonora Frąckiewiczówna</p>
+          <p className="font-weight color-primary">
+            Specjalista ds. finansowania
+          </p>
+          <p>efrackiewoczowna@rtc.pl</p>
+          <p>500 677 543</p>
+        </address>
+      </div>
+    </div>
+  );
 }
