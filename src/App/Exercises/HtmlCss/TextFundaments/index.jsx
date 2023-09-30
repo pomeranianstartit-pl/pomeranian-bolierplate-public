c0f40e7df35b832f3e14dab0ef11cd4c248e8e3f
przemo <przemyslaw.kocjan94@gmail.com>
2023-09-30 13:00:48 +0200

    text format exercise

---
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -0,0 +1,43 @@
+import './style.css';
+
+export function TextFundaments() {
+  return (
+    <div className="blok11_z1">
+      <div className="font-size">Wielkość czcionki</div>
+      <div className="font-weight">Grubość czcionki</div>
+      <div className="font-style">Italic</div>
+      <div className="text-decoration">Podkreslenie</div>
+      <div className="text-transform">Transformacja</div>
+      <div className="text-shadow">Tekst z cieniem</div>
+      <div className="box-shadow">Div z cieniem</div>
+      <div className="special-text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum magni,
+        explicabo deserunt sequi ea dolore voluptatum quibusdam perferendis
+        fugit natus repellat laboriosam alias tenetur in. Qui, cum hic.
+        Possimus, fuga!
+      </div>
+      <div className="">
+        Tekst do porwnania
+        <span className="sup">sup</span>
+        <span className="sub">sub</span>
+        <sup>sup</sup>
+        <sub>sub</sub>
+      </div>
+      <ol className="list-ol" start="4" reversed>
+        <li>tekst 1</li>
+        <li>tekst 2</li>
+        <li>tekst 3</li>
+      </ol>
+      <ul className="list-ul">
+        <li>tekst 1</li>
+        <li>tekst 2</li>
+        <li>
+          tekst 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          sed rerum eveniet delectus quam pariatur, vero repudiandae aspernatur
+          ipsam consequuntur at voluptatem quia sapiente quae. Iste autem iure
+          dolorem quod.
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,9 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
-
+import { TextFundamentsRouterMetaData } from './TextFundaments/router-data';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 //import { SelectorsAndCascadeRouterMetaData } from './SelectorsAndCascade/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   // SelectorsAndCascadeRouterMetaData,
+  TextFundamentsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
