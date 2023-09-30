04e496e9da29e0338e5778a8a78591c3e1266bb4
Karol <wojnos94@o2.pl>
2023-09-30 14:47:15 +0200

    animacja

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,11 +3,13 @@ import { TextFundamentsRouterMetaData } from './TextFundaments/router-data';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 // import { SelectorsAndCascadeMetaData } from './SelectorsAndCascade/router-data';
 import { GoogleFontsRouterMetaData } from './GoogleFonts/router-data';
+import { CssAnimationsRouterMetaData } from './CssAnimations/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   // SelectorsAndCascadeMetaData,
   TextFundamentsRouterMetaData,
   GoogleFontsRouterMetaData,
+  CssAnimationsRouterMetaData,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
