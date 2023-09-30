3ed4b86b6e80c91d6c548e1881b25a532ca76757
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-09-30 13:00:55 +0200

    Text Fundaments

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as BlockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 // import { SelectorsAndCascadeMetaData } from './SelectorsAndCascade/router-data';
+import { TextFundamentsMetaData } from './TextFundaments/router-data';
 
 export const blockRouterMetaData = [
   BlockRouterMetaData121220231,
   // SelectorsAndCascadeMetaData,
+  TextFundamentsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
