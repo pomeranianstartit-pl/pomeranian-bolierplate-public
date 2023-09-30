d84a7f37c95a788160455d3b82b45d5807efddd4
ArekFrel <arkadiuszfrel@gmail.com>
2023-09-30 17:01:39 +0200

    filter to be continue

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,22 +1,23 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
-import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+// import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { TextFundamentsMetaData } from './TextFundaments/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
-import { CssAnimationMetaData} from './CssAnimation/router-data';
+import { CssAnimationMetaData } from './CssAnimation/router-data';
 import { ColorsMetaData } from './Colors/router-data';
 import { ImageFileMetaData } from './ImageFile/router-data';
-
+import { CssFilterMetaData } from './CssFilter/router-data';
 
 // import { SelectorsAndCascadesMetaData } from './SelectorsAndCascades/router-data';
 
 export const blockRouterMetaData = [
-  blockRouterMetaData121220231,
+  // blockRouterMetaData121220231,
   TextFundamentsMetaData,
-  GoogleFontsMetaData,  
+  GoogleFontsMetaData,
   CssAnimationMetaData,
   ColorsMetaData,
-  ImageFileMetaData
-  
+  ImageFileMetaData,
+  CssFilterMetaData,
+
   // SelectorsAndCascadesMetaData,
 ];
 
