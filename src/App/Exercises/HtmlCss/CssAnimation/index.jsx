b733cd806c82e9594b800889565ffe6ffabd29fe
ArekFrel <arkadiuszfrel@gmail.com>
2023-09-30 15:26:51 +0200

    zanimacje i ćwiczenie

---
+++ src/App/Exercises/HtmlCss/CssAnimation/index.jsx
@@ -0,0 +1,22 @@
+import './styles.css';
+
+export function CssAnimation() {
+  return (
+    <div>
+      <div className="animation"></div>
+      <div>       
+        <div>
+          <h2 className="first_header"><p><span>Masz pytania?</span></p>
+          <p> Nasz specjalista chętnie Ci pomoże.</p></h2>
+          
+          <address className='contact'>
+            <p>Eleonora Frąckiewiczówna</p>
+            <p><span>Specjalista ds. finansowania</span></p>
+            <p>efrackiewoczowna@rtc.pl</p>
+            <p>500 677 543</p>
+          </address>
+        </div>        
+      </div>
+    </div>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,13 +2,15 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { TextFundamentsMetaData } from './TextFundaments/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
+import { CssAnimationMetaData} from './CssAnimation/router-data'
 
 // import { SelectorsAndCascadesMetaData } from './SelectorsAndCascades/router-data';
 
 export const blockRouterMetaData = [
-  TextFundamentsMetaData,
-  GoogleFontsMetaData,
   blockRouterMetaData121220231,
+  TextFundamentsMetaData,
+  GoogleFontsMetaData,  
+  CssAnimationMetaData
   
   // SelectorsAndCascadesMetaData,
 ];
