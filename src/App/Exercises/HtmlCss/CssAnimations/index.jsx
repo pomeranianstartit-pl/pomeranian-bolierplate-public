ca1aced849cae6a82eac18f2b353523b7cf8fcb4
przemo <przemyslaw.kocjan94@gmail.com>
2023-09-30 15:27:14 +0200

    animation exercise 2

--- src/App/Exercises/HtmlCss/CssAnimations/index.jsx
+++ src/App/Exercises/HtmlCss/CssAnimations/index.jsx
@@ -1,4 +1,22 @@
 import './styles.css';
 export function CssAnimations() {
-  return <div className="animation"></div>;
+  return (
+    <div className="question-wrapper">
+      <h2 className="color-primary header2">Masz pytania</h2>
+
+      <h2 className="header2">Nasz specjalista chętnie Ci pomoże.</h2>
+
+      <address>
+        <p className="paragraph">Eleonora Frąckiewiczówna</p>
+
+        <p className="font-weight-600 color-primary margin-bottom-17 paragraph animation2">
+          Specjalista ds. finansowania
+        </p>
+
+        <p className="paragraph">efrackiewoczowna@rtc.pl</p>
+
+        <p className="paragraph">500 677 543</p>
+      </address>
+    </div>
+  );
 }
