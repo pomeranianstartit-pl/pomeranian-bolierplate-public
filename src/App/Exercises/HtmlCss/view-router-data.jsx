7cda11098fa731dbe01074605b260b4bd5046f8a
ArekFrel <arkadiuszfrel@gmail.com>
2023-09-30 16:03:11 +0200

    colors zadanie

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,7 +2,8 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { TextFundamentsMetaData } from './TextFundaments/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
-import { CssAnimationMetaData} from './CssAnimation/router-data'
+import { CssAnimationMetaData} from './CssAnimation/router-data';
+import { ColorsMetaData } from './Colors/router-data';
 
 // import { SelectorsAndCascadesMetaData } from './SelectorsAndCascades/router-data';
 
@@ -10,7 +11,8 @@ export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   TextFundamentsMetaData,
   GoogleFontsMetaData,  
-  CssAnimationMetaData
+  CssAnimationMetaData,
+  ColorsMetaData,
   
   // SelectorsAndCascadesMetaData,
 ];
