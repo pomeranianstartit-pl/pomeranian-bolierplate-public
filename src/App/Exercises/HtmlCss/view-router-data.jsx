5085baaa0f343f6ca538985f19dd0efd3470690e
krzysztofjesiolowski <jesiolowskikrzysztof@gmail.com>
2023-09-30 13:00:39 +0200

    cwiczenia z tekstem blok11

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,10 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as BlockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 // import { SelectorsAndCascadeMetaData } from './SelectorsAndCascade/router-data';
-
+import { TextFundamentsRouterMetaData } from './TextFundaments/router-data';
 export const blockRouterMetaData = [
   BlockRouterMetaData121220231,
   // SelectorsAndCascadeMetaData,
+  TextFundamentsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
