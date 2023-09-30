b6e47b6f7bfe58a64d07e1394b81980b63dfe9ad
Artur Szwemiński <szweminskiartur@gmail.com>
2023-09-30 11:31:44 +0200

    blog done

---
+++ src/App/Blog/Signature.jsx
@@ -0,0 +1,8 @@
+export function Signature(props) {
+  return (
+    <div>
+      <h5 className="blog-author">{props.author}</h5>
+      <h6 className="blog-author-position">{props.signatureauthor}</h6>
+    </div>
+  );
+}
---
+++ src/App/Blog/Title.jsx
@@ -0,0 +1,3 @@
+export function Title(props) {
+  return <h2 className="blog-title">{props.title}</h2>;
+}
