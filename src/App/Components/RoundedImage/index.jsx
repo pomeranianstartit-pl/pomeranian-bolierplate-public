915b581a55a7e77a85cdbb351832425262ea271e
krzysztofjesiolowski <jesiolowskikrzysztof@gmail.com>
2023-10-01 10:14:17 +0200

    obrobka zdjecia

---
+++ src/App/Components/RoundedImage/index.jsx
@@ -0,0 +1,28 @@
+import './style.css';
+
+// props = {
+//     src: "adres do obrazka"
+// }
+
+// props.src
+// props = {
+// RoundedImage ({ src }) {
+// src
+// }
+
+export function RoundedImage({ src, size }) {
+  // size. width
+  // size height
+  return (
+    <div
+      className="roundedImage"
+      style={{
+        // backgroundImage: 'url(${src})',
+        backgroundImage: 'url(' + src + ')',
+        width: size.width,
+        height: size.height + 'px',
+      }}
+    ></div>
+  );
+}
+// to powyzej formatuje zdjecie do stylowania w kolku
