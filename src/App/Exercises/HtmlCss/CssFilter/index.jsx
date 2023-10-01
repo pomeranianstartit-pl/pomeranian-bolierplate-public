00ebcde16571ffe2f9abf50ee323d7555290240f
Artur Szwemiński <szweminskiartur@gmail.com>
2023-10-01 09:20:04 +0200

    css filter

--- src/App/Exercises/HtmlCss/CssFilter/index.jsx
+++ src/App/Exercises/HtmlCss/CssFilter/index.jsx
@@ -2,11 +2,12 @@ import './style.css';
 
 export function CssFilter() {
   return (
-    <div cssFilter="">
+    <div className="cssFilters">
       <img
         src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png"
         alt=""
       />
+      <div className="black"></div>
     </div>
   );
 }
