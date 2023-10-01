9cf5a8efe2ee5960161d30771dbb78ccf9830774
bartoszte <bart.tarnopolski@gmail.com>
2023-10-01 10:21:16 +0200

    blok12 roundedImage added gray filter

--- src/App/Components/RoundedImage/RoundedImage.jsx
+++ src/App/Components/RoundedImage/RoundedImage.jsx
@@ -4,6 +4,9 @@ export function RoundedImage({ src, size }) {
   return (
     <div
       className="roundedImage"
+      // dwa nawiasy klamrowe bo
+      // 1 => przekazujemy cos innego niz string, wtedy dla np zmiennej jest potrzebny wasik
+      // 2 => chcemy przekazac obiekt, a obiekt definiujemy poprzez nawiasy klamrowe (wasy)
       style={{
         backgroundImage: `url(${src})`,
         width: size.width,
