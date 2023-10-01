e4891d897e52ce783a96e0b8b08dc50c5c2e90d1
Artur Szwemiński <szweminskiartur@gmail.com>
2023-10-01 10:14:16 +0200

    logo in circle

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -1,3 +1,4 @@
+import { RoundedImage } from '../../../Components/RoundedImage/RoundedImage';
 import './style.css';
 
 export function ImageFiles() {
@@ -7,11 +8,15 @@ export function ImageFiles() {
         src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png"
         alt=""
       ></img>
-      <img
-        src="https://img.freepik.com/darmowe-zdjecie/strzal-zblizenie-piekny-kotek-krajowych-imbir-siedzi-na-bialej-powierzchni_181624-35913.jpg"
-        alt=""
-      />
       <img src="https://www.svgrepo.com/show/85124/cat.svg" alt="" />
+
+      <RoundedImage
+        src="https://www.svgrepo.com/show/85124/cat.svg"
+        size={{
+          width: '100px',
+          height: 100,
+        }}
+      />
     </div>
   );
 }
