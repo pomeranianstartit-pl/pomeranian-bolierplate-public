fc5c348b2c9dedd1462321e25f9d56214001d729
Patrycja <patrycja.ojewska@gmail.com>
2023-10-01 12:37:24 +0200

    boxModels

---
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -0,0 +1,51 @@
+import './styles.css';
+
+export function BoxModel() {
+  return (
+    <div className="boxModel">
+      <div className="div1">kontent</div>
+      <div className="div2">kontent</div>
+      <span>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+        minima ullam minus veritatis totam deserunt nobis quia laudantium atque
+        at excepturi, id non sequi, iusto officiis voluptatem temporibus quaerat
+        fuga?
+      </span>
+      <p className="p1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque alias
+        error minima quae perferendis similique dolorum at a dolorem ipsum,
+        facilis est voluptatem vel fugiat dolore maiores ad enim sapiente?
+        Adipisci quo incidunt earum omnis veniam rerum minus aliquam enim amet
+        doloribus iste optio ducimus, numquam dolorem facere aut quos sit! Enim
+        dolore consectetur obcaecati facere excepturi quaerat et fuga?
+        Necessitatibus, neque eum totam saepe veritatis deleniti ipsam tempore
+        minima architecto repellat natus vero! Iusto, mollitia blanditiis,
+        voluptatum nemo est magni quos dolor praesentium quaerat similique
+        suscipit nulla, illum dolorum? Odit voluptatem quis est sequi illo
+        dignissimos nulla eveniet commodi velit cumque, neque accusamus quae in
+        suscipit porro. Atque veniam quibusdam animi id tenetur expedita libero
+        fugiat ex numquam. Similique? Dolorum sapiente laborum nulla eligendi,
+        itaque amet quam reprehenderit facere. Voluptatibus, consequuntur,
+        pariatur reprehenderit recusandae asperiores corrupti repellendus omnis
+        illum, ratione adipisci at fugit impedit! Explicabo quis sed error
+        itaque. Ullam ad sapiente numquam vero beatae tenetur sunt est delectus
+        obcaecati magni hic repellat, voluptate nam porro? Quod, ad quam!
+        Reiciendis beatae perferendis pariatur ea est ipsum hic aut? Omnis! Enim
+        animi, atque earum voluptatibus sequi asperiores. Id architecto
+        officiis, laborum consequuntur reprehenderit reiciendis officia unde
+        tenetur delectus voluptatum. Nulla nisi dolorem perferendis possimus?
+        Saepe exercitationem vel quas nostrum ipsa! Amet saepe quia
+        perspiciatis, autem, sapiente optio nemo accusamus laboriosam neque
+        voluptate ad, reiciendis quasi facilis. Consectetur saepe debitis
+        explicabo doloribus odio qui? Possimus quaerat optio, odio vel
+        distinctio impedit. Sequi, id. Earum mollitia obcaecati sint inventore
+        minima soluta enim iste et incidunt fugiat, odit aliquid repellat
+        assumenda perspiciatis. Nobis laborum delectus qui natus earum voluptate
+        dolorem vel dignissimos molestiae. Quasi minima ducimus eaque fugit,
+        totam distinctio repellendus voluptates, in commodi dolorum nemo, quia
+        voluptate officiis praesentium consequatur. Fugit consequuntur quia
+        placeat alias incidunt numquam odio error dolores? Consequuntur, vero.
+      </p>
+    </div>
+  );
+}
