acc35ab96cc6af2141a95fb9249d8b3349616b4d
DawidMazur <50441054+DawidMazur@users.noreply.github.com>
2023-10-01 09:20:14 +0200

    css filters

--- src/App/Exercises/HtmlCss/CssFilter/index.jsx
+++ src/App/Exercises/HtmlCss/CssFilter/index.jsx
@@ -7,6 +7,8 @@ export function CssFilter() {
         src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_960_720.png"
         alt=""
       />
+
+      <div className="black"></div>
     </div>
   );
 }
