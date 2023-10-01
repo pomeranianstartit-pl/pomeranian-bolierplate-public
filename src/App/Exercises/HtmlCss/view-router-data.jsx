6c4db5e4b4e11b1cfa461237994309bb98c78052
ArekFrel <arkadiuszfrel@gmail.com>
2023-10-01 11:17:05 +0200

    view_router_data ordered

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,4 +1,5 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
+// import { SelectorsAndCascadesMetaData } from './SelectorsAndCascades/router-data';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { TextFundamentsMetaData } from './TextFundaments/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
@@ -7,18 +8,15 @@ import { ColorsMetaData } from './Colors/router-data';
 import { ImageFileMetaData } from './ImageFile/router-data';
 import { MediaFilesMetaData } from './MediaFiles/router-data';
 
-// import { SelectorsAndCascadesMetaData } from './SelectorsAndCascades/router-data';
-
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
+  // SelectorsAndCascadesMetaData,
   TextFundamentsMetaData,
   GoogleFontsMetaData,
   CssAnimationMetaData,
   ColorsMetaData,
   ImageFileMetaData,
-  MediaFilesMetaData
-
-  // SelectorsAndCascadesMetaData,
+  MediaFilesMetaData,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
