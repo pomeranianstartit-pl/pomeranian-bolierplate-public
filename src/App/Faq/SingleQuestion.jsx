de63b3115c8decdf6366bf0cbb108458d5f884bf
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-10-01 15:00:53 +0200

    question

---
+++ src/App/Faq/SingleQuestion.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export function SingleQuestion({ question, answer }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="faq-style">
+      <div>
+        <div onClick={() => setIsOpen(!isOpen)} className="div1">
+          <img className="button-text-up" src="../chevron.down.svg" alt="" />
+          <div className="button-text">
+            {question}
+            <span className="lorem">{answer}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="div1">
+          <img className="button-text-up" src="../chevron.down.svg" alt="" />
+          <div className="button-text">
+            {question}
+            <span className="lorem">{answer}</span>
+          </div>
+        </div>
+      </div>
+      {isOpen && <p className="faq-style">{answer}</p>}
+    </div>
+  );
+}
---
+++ src/App/Faq/index.jsx
@@ -0,0 +1,51 @@
+import { SingleQuestion } from './SingleQuestion';
+import './faq.css';
+
+export function Faq() {
+  return (
+    <div className="faq-style">
+      <h1>Faq</h1>
+      <div className="wstep">
+        Tutaj znajdź odpowiedzi na najczęściej zadawane pytania
+      </div>
+      <div>
+        <div className="div1">
+          <img className="button-text-up" src="../chevron.down.svg" alt="" />
+          <div className="button-text">
+            Jak mogę zapisać się na szkolenie?
+            <span className="lorem">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+              rutrum ipsum, leo et in. Mattis porttitor volutpat placerat
+              suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas
+              aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium
+              tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="button">
+        <img src="../chevron.right.svg" alt="" />
+        <div className="button-text ">Jak mogę zapisać się na szkolenie?</div>
+      </div>
+      <div className="button">
+        <img src="../chevron.right.svg" alt="" />
+        <div className="button-text ">Jak mogę zapisać się na szkolenie?</div>
+      </div>
+      <div className="button">
+        <img src="../chevron.right.svg" alt="" />
+        <div className="button-text ">Jak mogę zapisać się na szkolenie?</div>
+      </div>
+
+      <SingleQuestion
+        question="Jak mogę zapisać się na szkolenie?"
+        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+    rutrum ipsum, leo et in. Mattis porttitor volutpat placerat
+    suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas
+    aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium
+    tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor
+    sit amet, consectetur adipiscing elit"
+      />
+    </div>
+  );
+}
