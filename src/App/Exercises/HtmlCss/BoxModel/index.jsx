af9f337268eafdc8ffdeeab667774628af769576
przemo <przemyslaw.kocjan94@gmail.com>
2023-10-01 12:39:17 +0200

    boxModel, box sizing, overflow, css units

---
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -0,0 +1,52 @@
+import './styles.css';
+
+export function BoxModel() {
+  return (
+    <div className="boxModel">
+      <div className="div1">Kontent</div>
+      <div className="div2">Kontent</div>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, et quam?
+        In, modi sapiente consectetur eum corrupti, nesciunt nostrum neque
+        delectus sequi aliquam itaque nam, error obcaecati eius pariatur
+        accusamus!
+      </span>
+      <p className="p1">
+        <span></span>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio sequi
+        fugit aspernatur officia quaerat, minus nihil dicta ea sapiente
+        perferendis reiciendis officiis, ab voluptate qui saepe neque hic
+        exercitationem cupiditate. Velit possimus doloribus, praesentium quos
+        nihil, in suscipit repellat esse vero ipsam aperiam, nobis voluptatum
+        odit eveniet molestiae labore perferendis repellendus quo porro? Maxime
+        alias fugit odit rem quis voluptates. Voluptate maxime enim consequatur
+        eligendi asperiores id itaque magni dolor. Eos quia iure ex amet
+        accusamus sequi eligendi repellat tempore fugiat. Alias fugiat possimus
+        iusto nihil dolore. Deserunt, tempora recusandae. Ipsum animi, quam ad
+        cumque maxime ratione deserunt consectetur eos explicabo error!
+        Molestiae tempora omnis sequi dolorum facere! Esse quia dolorem modi
+        rerum nemo quas corrupti velit optio recusandae ducimus? Dolor
+        consequatur omnis inventore at dolorum laborum harum nulla impedit ea
+        quia. Quia possimus aliquid impedit repellat cum quasi inventore dolorum
+        quaerat iure, dolorem culpa qui nisi nihil repudiandae est. Cum commodi
+        rem magnam incidunt repellendus accusamus quo in, aliquid corrupti! Ab
+        dolorum facere molestias earum dolores ut eligendi sequi repudiandae
+        blanditiis natus, aliquam at distinctio nesciunt architecto! Rerum,
+        similique! Facilis fugit quae repudiandae, earum eaque repellendus
+        aperiam. A, accusantium ullam aspernatur accusamus placeat velit
+        incidunt ipsum aliquam unde! Tempore accusamus autem voluptatem
+        voluptate modi amet aspernatur explicabo eius cupiditate. Neque illo
+        iste, accusamus iure aperiam recusandae obcaecati qui labore sint
+        provident explicabo ducimus esse, nihil nostrum laborum vitae nisi
+        placeat aut at ipsum adipisci? Ipsum quisquam recusandae excepturi
+        magnam. Pariatur, facere. Culpa rerum, veniam suscipit explicabo nulla
+        enim mollitia in laboriosam minus ipsam nobis itaque ut soluta iusto
+        facere, sapiente provident quos asperiores impedit nostrum id ex, sit
+        aspernatur! Magni ea odio sint natus porro necessitatibus, iusto
+        repudiandae accusamus, quae totam reprehenderit aspernatur possimus
+        maiores dolorem quaerat autem et corporis ex aut iure quia aliquam
+        aliquid sit! Laboriosam, totam.
+      </p>
+    </div>
+  );
+}
