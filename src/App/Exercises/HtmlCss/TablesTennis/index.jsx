344bb842ca58441f05c586518eec14669adc828d
arleta dabek <itikotka@gmail.com>
2023-10-02 17:07:58 +0200

    tabela-start

---
+++ src/App/Exercises/HtmlCss/TablesTennis/index.jsx
@@ -0,0 +1,58 @@
+import './styles.css';
+
+export function Exercise() {
+  return (
+    <div className="table-ten-wrapper">
+      {/*tabela - ćwiczenia- table-tennis */}
+
+      <table className="first-table">
+     
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>PLAYER_ID</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>...</td>
+          </tr>
+
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>...</td>
+          </tr>
+
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>...</td>
+          </tr>
+
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>...</td>
+          </tr>
+
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>...</td>
+          </tr>
+
+         
+        </tbody>
+
+      </table>
+
+    </div>
+
+  );
+
+}
