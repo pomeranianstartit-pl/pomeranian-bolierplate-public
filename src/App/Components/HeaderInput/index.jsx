2cd8ed840de923654fb814f5c9382a31be607074
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-10-04 21:50:02 +0200

    Positioning exercise with Figma

---
+++ src/App/Components/HeaderInput/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+import { SearchIcon } from '../Icons/SearchIcon';
+
+const HeaderInput = () => {
+  return (
+    <div class="input-container">
+      <input type="text" class="image-input" placeholder="Search" />
+    </div>
+  );
+};
+
+export default HeaderInput;
