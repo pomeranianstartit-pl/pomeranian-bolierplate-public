1454ffdfca200da13eae8cbb82747ee34ebfbfa8
Mateusz Tywczyński <mateusz.tywczynski@gmail.com>
2023-10-04 19:02:39 +0200

    added exercise with positioning

---
+++ src/App/Exercises/HtmlCss/PositioningExercise/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'positioning-exercise',
+  date: '04-10-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie dotyczące positioningu',
+  blockNo: 15,
+  element: <Exercise />,
+  tags: ['css', 'color', 'position', 'absolute', 'relative', 'fixed', 'sticky'],
+};
