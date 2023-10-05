c324678dd6020bb4bc1bf2e936f4757e5dc9dc04
Piotr <wojcik.piotr003@gmail.com>
2023-10-05 21:15:27 +0200

    PomerLogo

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './styles/header.css';
 
-import PomeranianLogo from '../Images/start-it-logo.svg';
+import PomerLogo from '../Images/start-it-logo.svg';
 //  ../ wyjscie folder wyzej , ./ szukanie w tym samym folderze
 export function AppHeader() {
   return (
     <header>
       <div>
-        <img src={PomeranianLogo} alt="Logo" />
+        <img src={PomerLogo} alt="Logo" />
       </div>
     </header>
   );
