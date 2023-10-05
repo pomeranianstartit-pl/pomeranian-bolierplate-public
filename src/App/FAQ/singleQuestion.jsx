44535167015cb99e021a6c71939d5454b5713349
ArekFrel <arkadiuszfrel@gmail.com>
2023-10-05 14:15:03 +0200

    first steps to faq_augmnetation

--- src/App/FAQ/singleQuestion.jsx
+++ src/App/FAQ/singleQuestion.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react';
 
 export function SingleQuestion({ answer, question }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  }
   return (
     <div>
-      <div className="questionBlock">
+      <div className="questionBlock" onClick={handleClick}>
         <h4 className="questionHead">
           <img src="/icons/chevron.down.svg" alt="logo" />
           {question}
