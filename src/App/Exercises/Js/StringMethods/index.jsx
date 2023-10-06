8f2fea6c46344c1134a3605343f9645bd2784454
Kala <10560007+caffeine108@users.noreply.github.com>
2023-10-06 19:57:10 +0200

    Feat/empty values and strings (#17)
    * exercise undefined and null
    
    * feat: empty values string theory and examples
    
    * feat: string methods notes and examples

---
+++ src/App/Exercises/Js/StringMethods/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// 1. length - sprawdza dlugosc
+// 2. charAt() - zwraca nam litere pod podanym indeksem
+// 3. trim() - ucina nam puste miejsca po obu koncach stringa (zostawia te w srodku)
+// 4. replace() - zamienia podany string na inny podany (tylko pierwszy)
+// 5. replaceAll() - zamienia podany string na inny podany (wszystkie)
+// 6. split() - zamienia string w tablice z elementami stringa
+// 7. indexOf() - zwraca index pod ktorym jest szukana fraza
+// 8. toUpperCase()/toLowerCase() - podnosi do wielkiej lub malej litery
+// 9. includes() - zwraca boolean czy string zawiera dany ciag znakow
+// 10. startsWith() - zwraca boolean czy string zaczyna sie od danego ciagu znakow
+
+// cwiczenia w linkach ze strony mdn web docs
+
+const StringMethods = () => {
+  return <div>StringMethods</div>;
+};
+
+export { StringMethods };
---
+++ src/App/Exercises/Js/StringMethods/router-data.jsx
@@ -0,0 +1,10 @@
+import { StringMethods } from './index';
+
+export const stringMethodsMetaData = {
+  path: 'string-methods',
+  date: '06-10-2023',
+  linkLabel: 'Metody na Stringi',
+  blockNo: 19,
+  element: <StringMethods />,
+  tags: ['javascript', 'upperCase', 'string', 'censure', 'method'],
+};
