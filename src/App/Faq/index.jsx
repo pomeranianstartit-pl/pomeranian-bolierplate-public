e2c8e7ecf0a9f343e9bd1657045c585f7ade14cf
Karol <wojnos94@o2.pl>
2023-10-06 20:25:45 +0200

    blok 15, strona ala CV

--- src/App/Faq/index.jsx
+++ src/App/Faq/index.jsx
@@ -8,24 +8,35 @@ export function Faq() {
 
       <div className="faq-item">
         <div className="faq-item-header">
-          <img src="/Ikony/chevron.svg" alt="" />
-          <h2 className="faq-item-title">Jak mogę zapisać się na szkolenie?</h2>
+          <nav className="dropdown">
+            <a href="#">
+              <button>
+                <img src="/Ikony/chevron.svg" alt="" />
+              </button>
+            </a>
+            <h2 className="faq-item-title">
+              Jak mogę zapisać się na szkolenie?
+            </h2>
+            <div className="text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Eleifend rutrum ipsum, leo et in. Mattis porttitor volutpat
+                placerat suspendisse ante in cursus etiam ullamcorper.
+                Sollicitudin egestas aliquam, adipiscing adipiscing iaculis
+                habitant. Viverra pretium tincidunt nisl pellentesque ut
+                adipiscing non.Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit
+              </p>
+            </div>
+          </nav>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
-          rutrum ipsum, leo et in. Mattis porttitor volutpat placerat
-          suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas
-          aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium
-          tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit
-        </p>
       </div>
       <SingleQuestion
-        question="Jak moge zapisać się an szkolenie?"
+        question="Jak moge zapisać się na szkolenie?"
         answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend rutrum ipsum, leo et in. Mattis porttitor volutpat placerat suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       />
       <SingleQuestion
-        question="Jak moge zapisać się an szkolenie?"
+        question="Jak moge zapisać się na szkolenie?"
         answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend rutrum ipsum, leo et in. Mattis porttitor volutpat placerat suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       />
 
--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,12 +3,25 @@ import React from 'react';
 import './styles/header.css';
 
 import PomeranianLogo from '../Images/start-it-logo.svg';
+import settingLogo from '../Images/setting.svg';
+import { RoundedImage } from '../Components/RoundedImage/RoundedImage';
+import snowWolf from '../Images/snow-wolf.png';
+import { Arrow } from '../Components/Icons/Arrow';
 
 export function AppHeader() {
   return (
     <header>
-      <div>
+      <div className="header-conteiner">
         <img src={PomeranianLogo} alt="Logo" />
+        <div className="header-menu">
+          <img src={settingLogo} alt="settingLogo" />
+          <RoundedImage src={snowWolf} size={{ width: 50, height: 50 }} />
+          <p className="student-item">
+            <span>Karol</span>
+            <span>kursant</span>
+          </p>
+          <Arrow />
+        </div>
       </div>
     </header>
   );
