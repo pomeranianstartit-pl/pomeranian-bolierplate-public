5ef0902ef99a667d565c54c035fb7e5fcda7b41e
Piotr <wojcik.piotr003@gmail.com>
2023-10-07 12:35:37 +0200

    tech stack

--- src/App/Exercises/ReactExercises/ReactOnChange/ReactOnChange.jsx
+++ src/App/Exercises/ReactExercises/ReactOnChange/ReactOnChange.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
+
 export function ReactOnChange() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState(' ');
 
   function handleChange(event) {
     setName(event.target.value);
   }
+
   return (
     <div>
       <h1>React OnChange</h1>
       <div>
-        <input onChange={} type="text" placeholder="Wpisz swoje imię" />
+        <input
+          type="text"
+          placeholder="Wpisz swoje imię"
+          value={name}
+          OnChange={handleChange}
+        />
       </div>
+      <div>Moje imię to: {name}</div>
     </div>
   );
 }
