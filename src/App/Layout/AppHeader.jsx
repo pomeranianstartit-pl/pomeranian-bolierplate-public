335467e53b7ba95f4de03afb22261e0aed1483aa
ArekFrel <arkadiuszfrel@gmail.com>
2023-10-08 12:21:58 +0200

    pop up menu added_2

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import './styles/header.css';
-
 import PomeranianLogo from '../Images/start-it-logo.svg';
 import SettingsIcon from '../Images/setting.svg';
 import snowWolf from '../Images/snow-wolf.png';
-// import toggleArrow from '../Images/toggle-arrow.svg';
 import { ArrowDown } from '../Components/Icons/Arrow';
 import { RoundedImage } from '../Components/RoundedImage/roundedImage';
+import { PopUpMenu } from './components/PopUpMenu';
 
 export function AppHeader() {
   const [isArrowOpen, setIsArrowOpen] = useState(false);
@@ -31,20 +30,20 @@ export function AppHeader() {
             <div className="menu-relative">
               <ArrowDown onClick={toggleArrow} className="arrow" />
               {isArrowOpen && (
-                <div className="login-menu">
-                  <div className="arrow-line">
-                    <ArrowDown onClick={toggleArrow} className="arrow" />
-                  </div>
-                  <button className="login-button">zaloguj się</button>
-                  <div>
-                    Nie masz konta? <span> Zarejestruj się</span>
-                  </div>
-                </div>
+                <PopUpMenu func={toggleArrow} />
+                // <div onClick={toggleArrow} className="login-menu">
+                //   <div className="arrow-line">
+                //     <ArrowDown onClick={toggleArrow} className="arrow" />
+                //   </div>
+                //   <button className="login-button">zaloguj się</button>
+                //   <div>
+                //     Nie masz konta? <span> Zarejestruj się</span>
+                //   </div>
+                // </div>
               )}
             </div>
           </div>
         </div>
-        {/* <img src={toggleArrow} alt="logo" /> */}
       </div>
     </header>
   );
