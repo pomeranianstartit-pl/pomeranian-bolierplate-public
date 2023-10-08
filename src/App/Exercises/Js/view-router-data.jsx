8d0fd6c902fbe8fa483a0fe3a10f0c36c3ebb9c6
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-10-08 16:48:34 +0200

    boolean-numbers

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -3,10 +3,14 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exercise-js-variables-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
+import { BooleanMetaData } from './Boolean/router-data';
+import { blockRouterMetaDataNumbers } from './Numbers/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
+  BooleanMetaData,
+  blockRouterMetaDataNumbers,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
