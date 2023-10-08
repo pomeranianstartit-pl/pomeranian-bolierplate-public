f0cacf93c2420512cc46477cc9d24deb2315daad
przemo <przemyslaw.kocjan94@gmail.com>
2023-10-08 16:47:20 +0200

    booleans and numbers lesson

---
+++ src/App/Exercises/Js/Boolean/router-data.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Boolean } from './boolean';
+
+export const BooleanRouterMetaData = {
+  path: 'boolean-exercise',
+
+  date: '08.10.2023',
+
+  linkLabel: 'Ćwiczenia związane z boolean',
+
+  blockNo: 18,
+
+  element: <Boolean />,
+
+  tags: [
+    'true',
+
+    'false',
+
+    'boolean',
+
+    'operators',
+
+    'operands',
+
+    '&&',
+
+    '||',
+
+    '!',
+
+    '===',
+
+    '!==',
+
+    '==',
+
+    '!=',
+
+    '>',
+
+    '<',
+
+    '>=',
+
+    '<=',
+  ],
+};
---
+++ src/App/Exercises/Js/Numbers/router-data.jsx
@@ -0,0 +1,15 @@
+import { Numbers } from './numbers';
+
+export const NumbersRouterMetaData = {
+  path: 'numbers-exercise',
+
+  date: '08-10-2023',
+
+  linkLabel: 'Ćwiczenia związane z liczbami',
+
+  blockNo: 18,
+
+  element: <Numbers />,
+
+  tags: ['numbers', 'math', '+', '-', '*', '/', 'Math'],
+};
