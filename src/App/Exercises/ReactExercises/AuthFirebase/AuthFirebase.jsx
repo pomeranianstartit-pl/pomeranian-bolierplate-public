3d28b2cec21c167363e56f18a074410bd0718279
EMuras <edyta.muras@pomeranianstartit.pl>
2023-10-08 11:59:42 +0200

    added login and register functions

--- src/App/Exercises/ReactExercises/AuthFirebase/AuthFirebase.jsx
+++ src/App/Exercises/ReactExercises/AuthFirebase/AuthFirebase.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import {firebaseConfig} from '../../../Firebase/firebaseConfig'
-
+import React, { useState } from 'react';
 import './style.css';
 
-export function AuthFirebase () {
-  console.log(firebaseConfig);
+export const AuthFirebase = () => {
   return (
-    <div>
+    <>
       <h1>Autoryzacja z Firebase</h1>
-    </div>
+    </>
   );
-}
+};
