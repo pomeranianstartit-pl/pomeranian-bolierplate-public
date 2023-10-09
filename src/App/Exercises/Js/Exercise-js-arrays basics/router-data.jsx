ff119ea6e553ae88a36d3754d654a4268c9012f2
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-10-09 21:15:06 +0200

    exercise array & loops

---
+++ src/App/Exercises/Js/Exercise-js-arrays basics/router-data.jsx
@@ -0,0 +1,15 @@
+import { ExerciseJsArrayBasics } from './index';
+
+export const ExerciseJsArrayBasicsMetaData = {
+  path: 'js-arrays-basics-wcz5',
+
+  date: null,
+
+  linkLabel: 'Tablice w js (blok 20)',
+
+  blockNo: 20,
+
+  element: <ExerciseJsArrayBasics />,
+
+  tags: ['array', 'javascript'],
+};
