25f67cf9e4b7ab362f3c7c6623904c446f4b2e92
ArekFrel <arkadiuszfrel@gmail.com>
2023-10-09 23:33:20 +0200

    some icons added

--- src/App/Techstack/index.jsx
+++ src/App/Techstack/index.jsx
@@ -4,6 +4,13 @@ import Jira from '../Images/tech-stack/jira.png';
 import Js from '../Images/tech-stack/js.svg';
 import Css from '../Images/tech-stack/css.svg';
 import Html from '../Images/tech-stack/html.svg';
+import ReactIcon from '../Images/tech-stack/react.svg';
+import Git from '../Images/tech-stack/git.svg';
+import VSC from '../Images/tech-stack/vscode.svg';
+import Discord from '../Images/tech-stack/discord.svg';
+import Python from '../Images/tech-stack/python.svg';
+import Django from '../Images/tech-stack/django.svg';
+import SQL from '../Images/tech-stack/sql.svg';
 
 
 import { MyIcon } from './SingleIcon';
@@ -25,13 +32,13 @@ export const TechStack = () => {
             <MyIcon ImgSrc={Css} name={'CSS'} />
             <MyIcon ImgSrc={Js} name={'JS'} />
             <MyIcon ImgSrc={Html} name={'html'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
-            <MyIcon ImgSrc={Js} name={'JS'} />
+            <MyIcon ImgSrc={ReactIcon} name={'React'} />
+            <MyIcon ImgSrc={Git} name={'Git'} />
+            <MyIcon ImgSrc={VSC} name={'VS Code'} />
+            <MyIcon ImgSrc={Discord} name={'Discord'} />
+            <MyIcon ImgSrc={Python} name={'Python'} />
+            <MyIcon ImgSrc={Django} name={'Django'} />
+            <MyIcon ImgSrc={SQL} name={'SQL'} />
             <MyIcon ImgSrc={Js} name={'JS'} />
           </div>
           <div></div>
