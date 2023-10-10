6b36fcb9099d97556e42bfd9317662145e54cb39
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-10-10 20:35:00 +0200

    feat array theory and exercises

--- src/App/Exercises/Js/ArrayBasics/index.jsx
+++ src/App/Exercises/Js/ArrayBasics/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ArrayMethods } from './ArrayMethods';
+// import + po return dodaje <ArrayMethods />
 
 /*
 1. continue - pomija iteracje, ale pętla wykonuje się dalej
@@ -7,10 +9,10 @@ import React from 'react';
 4. sort, toSorted - sortuje tablice (sort mutuje oryginalna tablice, a toSorted nie mutuje)
 */
 
-const NUMBERS = [1, 3, 66, 9, 155, 2];
+//const NUMBERS = [1, 3, 66, 9, 155, 2];
 
 export const ArrayBasics = () => {
-  console.log('-----');
+  //console.log('-----');
   // Continue
   //   for (let i = 5; i > 0; i--) {
   //     if (i > 3) {
@@ -38,9 +40,14 @@ export const ArrayBasics = () => {
   //   const firstEven = NUMBERS.find((num) => num % 2 === 0);
   //   console.log(firstEven);
 
-  const sortedArray = [...NUMBERS].sort((a, b) => a - b);
-  console.log(sortedArray);
-  console.log(NUMBERS);
+  //const sortedArray = [...NUMBERS].sort((a, b) => a - b);
+  //console.log(sortedArray);
+  //console.log(NUMBERS);
 
-  return <div>ArrayBasics</div>;
+  return (
+    <div>
+      ArrayBasics
+      <ArrayMethods />
+    </div>
+  );
 };
