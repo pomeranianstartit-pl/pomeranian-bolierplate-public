9c249ac6e58c49651de04ffb644edec57149aeb6
annamatyasek <annamatyasek@gmail.com>
2023-10-10 21:21:24 +0200

    fixed3

--- src/App/Components/Icons/FaqIcon.jsx
+++ src/App/Components/Icons/FaqIcon.jsx
@@ -2,8 +2,8 @@ export function FAQIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="23"
-      height="23"
+      width="43"
+      height="43"
       viewBox="0 0 43 43"
       fill="none"
     >
