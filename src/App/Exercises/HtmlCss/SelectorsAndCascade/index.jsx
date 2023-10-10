dbb02966e06c773b6c011eec1a0433b2a6404bdc
Piotr <wojcik.piotr003@gmail.com>
2023-10-10 21:02:30 +0200

    css selectors

--- src/App/Exercises/HtmlCss/SelectorsAndCascade/index.jsx
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/index.jsx
@@ -3,11 +3,17 @@ import './SelectorsAndCascade.css';
 export function SelectorsAndCascade() {
   return (
     <div className="selectorsAndCascade">
-      <div className="unikalna-klasa1">Tekst 1</div>
+      <div className="unikalna-klasa1">
+        <span>Tekst1.1</span>
+        <div></div>
+        <span>Tekst1.2</span>
+        <span>Tekst1</span>
+        <div className="wewnetrzny-div">wewnętrzny tekst</div>
+      </div>
       <div id="drugi-div" className="unikalna-klasa2">
         Tekst 2
       </div>
-      <div className="unikalna-klasa3">Tekst 4</div>
+      <div className="unikalna-klasa3">Tekst 3</div>
       <input type="text" />
     </div>
   );
