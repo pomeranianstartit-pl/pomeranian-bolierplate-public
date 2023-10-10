120e5e4805d759fd6eba5e2e4f4c4ab134a582ef
Piotr <wojcik.piotr003@gmail.com>
2023-10-11 01:21:12 +0200

    TextFundaments start

---
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+export function TextFundaments() {
+  return (
+    <div className="blok11_z1">
+      <div className="font-size">Wielkość czcionki</div>
+      <div className="font-weight">Grubość czcionki</div>
+    </div>
+  );
+}
