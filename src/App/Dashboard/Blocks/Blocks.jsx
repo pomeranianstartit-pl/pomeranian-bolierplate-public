98757ef4e59c7b3b1e8cfee0b7048ba199faae10
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-10-10 19:28:50 +0200

    Dashboard blocks and Aside

--- src/App/Dashboard/Blocks/Blocks.jsx
+++ src/App/Dashboard/Blocks/Blocks.jsx
@@ -1,7 +1,11 @@
 import './styles.css';
 import { Block } from './Block';
-import { PersonalCardIcon } from '../../Components/Icons/PersonalCardIcon.jsx';
+import { PersonalCardIconRed } from '../../Components/Icons/PersonalCardIconRed.jsx';
 import { ExerciseIcon } from '../../Components/Icons/ExerciseIcon';
+import { BlockCV } from './BlockCV';
+import { BlogIcon } from '../../Components/Icons/BlogIcon';
+import { TechStackIcon } from '../../Components/Icons/TechStackIcon';
+import { FaqIcon } from '../../Components/Icons/FaqIcon';
 
 export const Blocks = () => {
   return (
@@ -12,9 +16,9 @@ export const Blocks = () => {
       </p>
       <div className="blocks">
         <div>
-          <Block
+          <BlockCV
             title="Moje CV"
-            icon={<PersonalCardIcon />}
+            icon={<PersonalCardIconRed />}
             description="podgląd cv wraz z doświadczeniem"
           />
         </div>
@@ -27,9 +31,23 @@ export const Blocks = () => {
         </div>
         <div>
           <Block
-            title="Ćwiczenia"
-            icon={<ExerciseIcon />}
-            description="wszystkie wykonane ćwiczenia"
+            title="Blog"
+            icon={<BlogIcon />}
+            description="wpisy blogowe o technologii front-end"
+          />
+        </div>
+        <div>
+          <Block
+            title="Tech stack"
+            icon={<TechStackIcon />}
+            description="stack technologiczny realizowany na kursie"
+          />
+        </div>
+        <div>
+          <Block
+            title="FAQ"
+            icon={<FaqIcon />}
+            description="odpowiedzi na najczęstsze pytania"
           />
         </div>
       </div>
