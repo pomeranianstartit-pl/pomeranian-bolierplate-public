e582f398a922eadc74953718ed46fc878230189d
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-10-12 21:00:18 +0200

    SetInterval SetTime & exercise VanishString and Timer

---
+++ src/App/Exercises/Js/VanishString/router-data.jsx
@@ -0,0 +1,15 @@
+import { VanishString } from './index';
+
+export const VanishStringMetaData = {
+  path: 'Vanish-String',
+
+  date: '12.10.2023',
+
+  linkLabel: 'Zniknięcie tekstu',
+
+  blockNo: 21,
+
+  element: <VanishString />,
+
+  tags: ['setTimeout', 'timeout', 'time', 'string'],
+};
