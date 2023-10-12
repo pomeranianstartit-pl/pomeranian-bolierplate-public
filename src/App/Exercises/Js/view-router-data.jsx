9e032b17fea041650c4fff37b6d773a01721e88f
Karol <wojnos94@o2.pl>
2023-10-12 20:42:05 +0200

    numbers, kalkulator wstep

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,10 +4,12 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataBooleans } from './Boolean/router-data';
+import { blockRouterMetaData as blockRouterMetaDataNumbers } from './Numbers/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataBooleans,
+  blockRouterMetaDataNumbers,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
