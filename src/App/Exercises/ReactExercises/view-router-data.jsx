b0668a5b1bcbcce4d912feeac827897c0ba11a4a
Mateusz Tywczyński <mateusz.tywczynski@gmail.com>
2023-10-13 21:00:23 +0200

    game functionality v1

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,6 +3,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
 import { EventsMetaData } from './Events/router-data';
+import { hitTheMoleMetaData } from './HitTheMole/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData, EventsMetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  EventsMetaData,
+  hitTheMoleMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
