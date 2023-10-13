a6646bb9b7cc01214fec5a8c67c371f72457ae26
Piotr <wojcik.piotr003@gmail.com>
2023-10-13 22:03:34 +0200

    loops-objects-strings

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -5,12 +5,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataBooleans } from './Boolean/router-data';
 import { blockRouterMetaData as blockRouterMetaDataNumbers } from './Numbers/router-data';
-
+import { blockRouterMetaDataExerciseObjectsArrayWeek4 } from './Exercise-js-arrays-objects-week4/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataBooleans,
   blockRouterMetaDataNumbers,
+  blockRouterMetaDataExerciseObjectsArrayWeek4,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
