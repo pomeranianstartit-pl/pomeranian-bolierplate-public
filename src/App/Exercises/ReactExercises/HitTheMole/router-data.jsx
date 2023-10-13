a2ca27c87dbcc9358150f7c90dc89defdcc33d86
KasiaWin <144037578+KasiaWin@users.noreply.github.com>
2023-10-13 21:10:44 +0200

    mole game added (#16)

---
+++ src/App/Exercises/ReactExercises/HitTheMole/router-data.jsx
@@ -0,0 +1,15 @@
+import { HitTheMole } from './index';
+
+export const hitTheMoleMetaData = {
+  path: 'hit-the-mole',
+
+  date: '12.10.2023',
+
+  linkLabel: 'Uderz w kreta',
+
+  blockNo: 23,
+
+  element: <HitTheMole />,
+
+  tags: ['javascript', 'setTimeout', 'time', 'string'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
+import { hitTheMoleMetaData } from './HitTheMole/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  hitTheMoleMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
