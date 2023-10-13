5ed26862357c87e13c91b3a3d7cadb9d1f910d29
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-10-13 16:54:59 +0200

    Vanish and Timer exercises

--- src/App/Exercises/Js/VanishString/index.jsx
+++ src/App/Exercises/Js/VanishString/index.jsx
@@ -14,3 +14,50 @@ export const VanishString = () => {
     </div>
   );
 };
+
+// rozwiązanie od SS
+/*import { useEffect, useState } from 'react';
+
+ 
+
+export const Timer = () => {
+
+  const [text, setText] = useState('');
+
+ 
+
+  const handleClick = () => {
+
+    setText('Hello World');
+
+  };
+
+ 
+
+  useEffect(() => {
+
+    setTimeout(() => {
+
+      setText('');
+
+    }, 3000);
+
+  }, [text]);
+
+ 
+
+  return (
+
+    <div>
+
+      <button onClick={handleClick}>Wyświetl tekst</button>
+
+ 
+
+      {text && <div>{text}</div>}
+
+    </div>
+
+  );
+
+}; */
