7ae2525c0c758d7020fa1904c46110c9234e65ec
arleta dabek <itikotka@gmail.com>
2023-10-14 12:37:23 +0200

    feat: header menu-buttons

--- src/App/Layout/components/LoginPanel/index.jsx
+++ src/App/Layout/components/LoginPanel/index.jsx
@@ -9,9 +9,9 @@ const LoginPanel = ({ onClick }) => {
     <div onClick={onClick} className="header-panel">
       <img onClick={onClick} className="more" src={toggleArrow} alt="more" />
       <div className="panel-content">
-        <button className="login-button">Zaloguj się</button>
+        <button className="login-button">ZALOGUJ SIĘ</button>
         <div>
-          Nie masz konta
+          Nie masz konta?
           <button className="register-button">Zarejestruj się</button>
         </div>
       </div>
