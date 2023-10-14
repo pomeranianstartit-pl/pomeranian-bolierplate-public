e871094264f443cf472faf0c6b7c7b1bd30201a1
Karol <wojnos94@o2.pl>
2023-10-14 15:49:27 +0200

    daty i czas

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -5,11 +5,18 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataBooleans } from './Boolean/router-data';
 import { blockRouterMetaData as blockRouterMetaDataNumbers } from './Numbers/router-data';
+import { blockRouterMetaDataObjectsArrays } from './Exercise-js-arrays-objects/router-data';
+import { blockRouterMetaData as blockRouterMetaDataObjects } from './JsObjectBasic/router-data';
+import { blockRouterMetaData as blockRouterMetaDataDataTime } from './DateTime/router-data';
+
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataBooleans,
   blockRouterMetaDataNumbers,
+  blockRouterMetaDataObjectsArrays,
+  blockRouterMetaDataObjects,
+  blockRouterMetaDataDataTime,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
