c178bab42e891b39347e164658bafc688357e4b0
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-10-16 13:24:23 +0200

    inputtext

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -3,34 +3,33 @@ import React, { useState } from 'react';
 import './style.css';
 
 export function Events() {
-  const [text, setText] = useState ("Nie kliknięto we mnie");
-  const [counter, setCounter] = useState (0);
-  const [inputText, setInputText] = useState ("");
+  const [text, setText] = useState('Nie kliknięto we mnie');
+  const [counter, setCounter] = useState(0);
+  const [inputText, setInputText] = useState('');
 
   function handleOnClick() {
-    setText("Kliknięto we mnie");
+    setText('Kliknięto we mnie');
     if (counter >= 3) {
       setCounter((counter) => counter + 2);
+    } else {
+      setCounter((counter) => counter + 1);
     }
-    else {
-    setCounter((counter) => counter + 1);
-  }}
-    function handleOnChange(event){
-      setInputText(event.target.value);
-    }
+  }
+  function handleOnChange(event) {
+    setInputText(event.target.value);
+  }
 
-    function handleGreeting() {
-      alert("Cześć ${inputText}");
-    }
-
+  function handleGreeting() {
+    alert(`Cześć ${inputText}`);
+  }
 
   return (
     <div>
       <h2>Cześć!</h2>
-      <input 
-      className='events-input' 
-      type='text' 
-      onChange={(event) => handleOnChange(event)}
+      <input
+        className="events-input"
+        type="text"
+        onChange={(event) => handleOnChange(event)}
       />
       <button className="events-btn" onClick={handleOnClick}>
         {counter > 0 ? `${text} ${counter} razy` : text}
@@ -41,7 +40,6 @@ export function Events() {
     </div>
   );
 }
-
 
 // 1. Dodać element <button>Jakiś tekst</button>
 
@@ -59,35 +57,35 @@ export function Events() {
 //function handleOnClick() {
 //  setText('Kliknięto we mnie');
 
- // if (counter >= 3) {
+// if (counter >= 3) {
 //    return setCounter((counter) => counter + 2);
- // }
+// }
 
-  //setCounter((counter) => counter + 1);
+//setCounter((counter) => counter + 1);
 
-  // If..else if..else
-  // if (counter >= 5) {
-  //   setCounter((counter) => counter + 2);
-  // } else if (counter >= 3) {
-  //   setCounter((counter) => counter + 3);
-  // } else {
-  //   setCounter((counter) => counter + 1);
-  // }
+// If..else if..else
+// if (counter >= 5) {
+//   setCounter((counter) => counter + 2);
+// } else if (counter >= 3) {
+//   setCounter((counter) => counter + 3);
+// } else {
+//   setCounter((counter) => counter + 1);
+// }
 
-  // Ternary operator
-  // counter >= 3
-  //   ? setCounter((counter) => counter + 2)
-  //   : setCounter((counter) => counter + 1);
+// Ternary operator
+// counter >= 3
+//   ? setCounter((counter) => counter + 2)
+//   : setCounter((counter) => counter + 1);
 
-  // if..else
-  // if (counter >= 3) {
-  //   setCounter((counter) => counter + 2);
-  // } else {
-  //   setCounter((counter) => counter + 1);
-  // }
+// if..else
+// if (counter >= 3) {
+//   setCounter((counter) => counter + 2);
+// } else {
+//   setCounter((counter) => counter + 1);
+// }
 
-  // if + if
-  // if (counter < 3) {
-  //   //         początkowa   początkowa + 1
-  //   setCounter((counter) => counter + 1);
-  // }
+// if + if
+// if (counter < 3) {
+//   //         początkowa   początkowa + 1
+//   setCounter((counter) => counter + 1);
+// }
