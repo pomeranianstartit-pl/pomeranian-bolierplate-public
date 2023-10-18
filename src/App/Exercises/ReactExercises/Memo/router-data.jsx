f32e4bcfb4c193dede302085c698a1453c102b5c
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-10-18 20:54:26 +0200

    Memo first steps

---
+++ src/App/Exercises/ReactExercises/Memo/router-data.jsx
@@ -0,0 +1,15 @@
+import { Memo } from './index';
+
+export const MemoMetaData = {
+  path: 'Memo',
+
+  date: '13-10-2023',
+
+  linkLabel: 'Memo',
+
+  blockNo: 23,
+
+  element: <Memo />,
+
+  tags: ['components', 'setInterval', 'filter', 'map', 'useState'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,10 +2,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { EventsMetaData } from './Events/router-data';
 import { HitTheMoleMetaData } from './HitTheMole/router-data';
+import { MemoMetaData } from './Memo/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   EventsMetaData,
   HitTheMoleMetaData,
+  MemoMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
