9af156c4872dc3dd7ea3655df808edb72bd2c14f
Mateusz Tywczyński <mateusz.tywczynski@gmail.com>
2023-10-18 20:25:24 +0200

    Memo game v1

---
+++ src/App/Exercises/ReactExercises/Memo/router-data.jsx
@@ -0,0 +1,15 @@
+import { MemoGame } from './index';
+
+export const MemoGameMetaData = {
+  path: 'memo-game',
+
+  date: '18-10-2023',
+
+  linkLabel: 'Memo',
+
+  blockNo: 23,
+
+  element: <MemoGame />,
+
+  tags: ['components', 'setInterval', 'filter', 'map', 'useState'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -4,10 +4,12 @@ import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
 import { EventsMetaData } from './Events/router-data';
 import { hitTheMoleMetaData } from './HitTheMole/router-data';
+import { MemoGameMetaData } from './Memo/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   EventsMetaData,
   hitTheMoleMetaData,
+  MemoGameMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
