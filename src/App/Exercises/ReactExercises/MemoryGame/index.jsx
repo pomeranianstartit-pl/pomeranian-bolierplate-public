417cf550e10b1e30658ee4ca0051bd19ad84587a
Katarzyna Winklas <winklas.k@gmail.com>
2023-10-20 16:35:22 +0200

    memo game added

--- src/App/Exercises/ReactExercises/MemoryGame/index.jsx
+++ src/App/Exercises/ReactExercises/MemoryGame/index.jsx
@@ -1,7 +1,18 @@
 import './styles.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const cards = [
+  { id: 1, key: 'A' },
+  { id: 2, key: 'U' },
+  { id: 3, key: 'K' },
+  { id: 4, key: 'R' },
+  { id: 3, key: 'K' },
+  { id: 2, key: 'U' },
+  { id: 1, key: 'A' },
+  { id: 4, key: 'R' },
+];
+
+const cards16 = [
   { id: 1, key: 'A' },
   { id: 2, key: 'U' },
   { id: 3, key: 'K' },
@@ -10,20 +21,63 @@ const cards = [
   { id: 6, key: 'U' },
   { id: 7, key: 'A' },
   { id: 8, key: 'R' },
+  { id: 1, key: '1' },
+  { id: 2, key: '2' },
+  { id: 3, key: '3' },
+  { id: 4, key: '4' },
+  { id: 5, key: '3' },
+  { id: 6, key: '2' },
+  { id: 7, key: '1' },
+  { id: 8, key: '4' },
 ];
 
+const cards24 = [
+  { id: 1, key: 'A' },
+  { id: 2, key: 'U' },
+  { id: 3, key: 'K' },
+  { id: 4, key: 'R' },
+  { id: 5, key: 'K' },
+  { id: 6, key: 'U' },
+  { id: 7, key: 'A' },
+  { id: 8, key: 'R' },
+  { id: 1, key: '1' },
+  { id: 2, key: '2' },
+  { id: 3, key: '3' },
+  { id: 4, key: '4' },
+  { id: 5, key: '3' },
+  { id: 6, key: '2' },
+  { id: 7, key: '1' },
+  { id: 8, key: '4' },
+  { id: 9, key: '8' },
+  { id: 10, key: '7' },
+  { id: 11, key: '6' },
+  { id: 11, key: '6' },
+  { id: 10, key: '7' },
+  { id: 9, key: '8' },
+  { id: 12, key: '9' },
+  { id: 12, key: '9' },
+];
 const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
 const game_time = 120;
 const keys = ['A', 'U', 'K', 'R', '5', 'S', 'P', 'W', 'Q', 'F'];
 
 export const MemoryGame = () => {
   const [isGameStarted, setIsGameStarted] = useState(false);
-  const [gameCards, setgameCards] = useState(cards);
+  const [gameCards, setCards] = useState(cards);
+  const [gameCards16, setgameCards16] = useState(cards16);
+  const [gameCards24, setCards24] = useState(cards24);
   const [initialTime, setInitialTime] = useState(game_time);
   const [isGameEnded, setGameEnded] = useState(false);
+  const [memoCardsId, setMemoCardsId] = useState(getRandomInt(8));
+  const [memoCards16Id, setMemoCards16Id] = useState(getRandomInt(16));
+  const [memoCards24Id, setMemoCards24Id] = useState(getRandomInt(24));
+  const [intervalId, setIntervalId] = useState(null);
   const [time, setTime] = useState(game_time);
   const [score, setScore] = useState(0);
+
   const handleStartGame = () => {
+    setCards(shuffleArray(gameCards));
+
     setTime(game_time);
     setScore(0);
     setIsGameStarted(true);
@@ -32,6 +86,14 @@ export const MemoryGame = () => {
   const handleStopGame = () => {
     setIsGameStarted(false);
   };
+  // zmiana kart dla innej gry
+  useEffect(() => {
+    setInterval(() => {
+      setMemoCardsId(getRandomInt(8));
+      setMemoCards16Id(getRandomInt(16));
+      setMemoCards24Id(getRandomInt(24));
+    }, isGameEnded);
+  });
 
   function shuffleArray(s) {
     for (let i = s.length - 1; i > 0; i--) {
@@ -44,6 +106,7 @@ export const MemoryGame = () => {
 
   return (
     <div className="memory-game">
+      <h2>MEMO</h2>
       <h2>
         Gra polegająca na zapamiętywaniu odkrytych kafli i łączeniu ich w pary
       </h2>
@@ -54,6 +117,7 @@ export const MemoryGame = () => {
           <div className="option-wrapper">
             <div className="title">Czas do końca</div>
             <div className="content">
+              {/* disable true oznacza, że przycisk jest wyłączony */}
               <button disabled={true}>{time}</button>
             </div>
           </div>
@@ -71,7 +135,7 @@ export const MemoryGame = () => {
             <div className="title">Przyciski sterujące</div>
             <div className="content">
               <button className="stop" onClick={handleStopGame}>
-                Stop
+                PASS
               </button>
             </div>
           </div>
@@ -87,30 +151,6 @@ export const MemoryGame = () => {
           </div>
 
           <div className="game-over">Gratulacje, zdobyłeś {score} punktów</div>
-
-          {/* CZAS gry */}
-          <div className="option-wrapper">
-            <div className="title">Czas gry</div>
-            <div className="content">
-              <button className="current">1:35</button>
-            </div>
-          </div>
-
-          {/* liczba ruchów */}
-          <div className="option-wrapper">
-            <div className="title"></div>
-            <div className="content">
-              <button className="current">2</button>
-            </div>
-          </div>
-
-          {/* PRZYCISKI STERUJĄCE */}
-          <div className="option-wrapper">
-            <div className="title">Przyciski sterujące</div>
-            <div className="content">
-              <button onClick={handleStartGame}>PASS</button>
-            </div>
-          </div>
         </div>
       ) : (
         <div>
