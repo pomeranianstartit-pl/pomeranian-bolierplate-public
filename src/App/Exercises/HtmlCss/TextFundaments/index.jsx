16a080026511662d0d62cf7860b238c3b7e20320
Piotr <wojcik.piotr003@gmail.com>
2023-10-21 00:06:58 +0200

    textFundaments content and css

--- src/App/Exercises/HtmlCss/TextFundaments/index.jsx
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -4,6 +4,17 @@ export function TextFundaments() {
     <div className="blok11_z1">
       <div className="font-size">Wielkość czcionki</div>
       <div className="font-weight">Grubość czcionki</div>
+      <div className="font-style">Italic</div>
+      <div className="text-decoration">Podkreślenie</div>
+      <div className="text-transform">Transformacja slowo2</div>
+      <div className="text-shadow">Tekst z cieniem</div>
+      <div className="">
+        Tekst do porwnania
+        <span className="sup">sup</span>
+        <span className="sub">sub</span>
+        <sup>sup</sup>
+        <sub>sub</sub>
+      </div>
     </div>
   );
 }
