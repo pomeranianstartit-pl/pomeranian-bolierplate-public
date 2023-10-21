6560f0f5917ba700d9f4eea7bc7e8e39f23a9ba6
Piotr <wojcik.piotr003@gmail.com>
2023-10-21 17:01:30 +0200

    CssAnimation business card

--- src/App/Exercises/HtmlCss/CssAnimations/index.jsx
+++ src/App/Exercises/HtmlCss/CssAnimations/index.jsx
@@ -1,5 +1,25 @@
 import './styles.css';
 
 export function CssAnimations() {
-  return <p className="animation">Animations</p>;
+  return (
+    <div>
+      {/* To jest content dotyczący makiety_1 */}
+
+      <div className="question-wrapper">
+        <h2 className="color-primary header2">Masz pytania</h2>
+
+        <h2 className="header2">Nasz specjalista chętnie Ci pomoże.</h2>
+
+        <address className="address">
+          <p>Eleonora Frąckiewiczówna</p>
+
+          <p className="font-weight-600 color-primary margin-bottom-17">
+            Specjalista ds. finansowania
+          </p>
+          <p>efrackiewoczowna@rtc.pl</p>
+          <p>500 677 543</p>
+        </address>
+      </div>
+    </div>
+  );
 }
