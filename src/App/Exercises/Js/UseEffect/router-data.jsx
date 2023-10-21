b675270bf891c631c4e64849b5cd9ab13a5cc2e4
Piotr <wojcik.piotr003@gmail.com>
2023-10-21 16:09:18 +0200

    Timeout Interval UseEffect and VanishString start

---
+++ src/App/Exercises/Js/UseEffect/router-data.jsx
@@ -0,0 +1,10 @@
+import { UseEffect } from './index';
+
+export const blockRouterMetaDataUseEffect = {
+  path: 'UseEffect',
+  date: null,
+  linkLabel: 'UseEffect',
+  blockNo: 23,
+  element: <UseEffect />,
+  tags: ['useEffect', 'react'],
+};
---
+++ src/App/Exercises/Js/VanishString/router-data.jsx
@@ -0,0 +1,10 @@
+import { VanishString } from './index';
+
+export const blockRouterMetaDataVanishString = {
+  path: 'VanishString',
+  date: null,
+  linkLabel: 'VanishString',
+  blockNo: 23,
+  element: <VanishString />,
+  tags: ['VanishString', 'react', 'useEffect'],
+};
