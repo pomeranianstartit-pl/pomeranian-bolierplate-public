73d9596f7b367588db7276a5d92223a12b1b1340
KasiaWin <144037578+KasiaWin@users.noreply.github.com>
2023-10-21 23:34:13 +0200

    Header1 (#21)
    * some changes
    
    * header updated

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -4,6 +4,7 @@ import LoginPanel from '../Components/LoginPanel';
 
 import './styles/header.css';
 import { SettingIcon } from '../Components/Icons/SettingIcon';
+import { SearchIcon } from '../Components/Icons/SearchIcon';
 import { ArrowIcon } from '../Components/Icons/ArrowIcon';
 import { ElipseIcon } from '../Components/Icons/ElipseIcon';
 import { ReactComponent as PomeranianLogo } from '../Images/start-it-logo.svg';
@@ -11,7 +12,7 @@ import { ReactComponent as PomeranianLogo } from '../Images/start-it-logo.svg';
 export const AppHeader = ({ name, position }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleArrowClick = () => {
+  const handleArrowIconClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
@@ -22,21 +23,27 @@ export const AppHeader = ({ name, position }) => {
         {/* // component react */}
         <PomeranianLogo />
       </div>
-
-      <div className="header-menu">
+      <div className="sidebar">
+        <i class="uil uil-search" SearchIcon></i>
+        <input type="text" placeholder="Serach here..." />
+      </div>
+      <div className="setting-icon">
         <SettingIcon />
       </div>
-      <ElipseIcon />
+      <div className="elipse-Icon">
+        <ElipseIcon />
+      </div>
 
       {/* <HeaderMenu /> */}
       <div className="header-menu-user">
         <p>Katarzyna</p>
         <p>kursantka</p>
       </div>
+      <div className="drop-down-menu">
+        <ArrowIcon onClick={handleArrowIconClick} />
 
-      <ArrowIcon onClick={handleArrowClick} />
-
-      {isMenuOpen && <LoginPanel onClick={handleArrowClick} />}
+        {isMenuOpen && <LoginPanel onClick={handleArrowIconClick} />}
+      </div>
     </header>
   );
 };
