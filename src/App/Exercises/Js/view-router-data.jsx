280340aa371fabcca5bcd12fe816475571585d4e
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-10-24 20:57:27 +0200

    feat/oop 4pillars, Class

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -14,6 +14,7 @@ import { jsSetTimeoutMetaData } from './JsSetTimeout/router-data';
 import { jsSetIntervalMetaData } from './JsSetInterval/router-data';
 import { vanishStringMetaData } from './VanishString/router-data';
 import { timerMetaData } from './Timer/router-data';
+import { oopMetaData } from './OOP/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
@@ -30,6 +31,7 @@ export const blockRouterMetaData = [
   jsSetIntervalMetaData,
   vanishStringMetaData,
   timerMetaData,
+  oopMetaData,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
