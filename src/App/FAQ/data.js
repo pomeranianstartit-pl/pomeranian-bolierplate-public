4f433754661f8221f350f2795258781b432c01e6
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-10-26 13:01:56 +0200

    answer faq change

--- src/App/FAQ/data.js
+++ src/App/FAQ/data.js
@@ -2,7 +2,7 @@ export const mockData = [
   {
     title: 'Jak mogę zapisać się na szkolenie?',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend rutrum ipsum, leo et in. Mattis porttitor volutpat placerat suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      'Jeśli chcesz wziąć udział w szkoleniu po prostu zadzwoń pod numer: 00-000-000-000.',
   },
   {
     title: 'Ile facet powinien wziąć na klate',
