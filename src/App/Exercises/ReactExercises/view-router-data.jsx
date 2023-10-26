5ad9cf8327ee441a9f17cba4af7f6c68a04a5e10
Kala <10560007+caffeine108@users.noreply.github.com>
2023-10-26 21:26:53 +0200

    Feat/memo (#26)
    * momo game without components
    
    * green and read colors for pairs added
    
    * corrections for memo game - handleClick and pairing
    
    * memo logic correction

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { EventsMetaData } from './Events/router-data';
 import { hitTheMoleMetaData } from './HitTheMole/router-data';
+import { memoMetaData } from './Memo/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   EventsMetaData,
   hitTheMoleMetaData,
+  memoMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
