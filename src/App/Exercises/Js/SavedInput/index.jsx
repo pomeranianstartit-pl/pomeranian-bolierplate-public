d3ccb5c99e8913d192b2d2cb16c8595a40e11ec5
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-10-27 04:17:56 +0200

    feat/json-storage + exercises without CSS

--- src/App/Exercises/Js/SavedInput/index.jsx
+++ src/App/Exercises/Js/SavedInput/index.jsx
@@ -1,41 +1,90 @@
 import { useState, useEffect } from 'react';
 
-export const SavedInput = () => {
-  const [count, setCount] = useState(0);
+// const users = [{ id: 1, name: 'Janek' }];
 
-  const handleUpdateCount = () => {
-    setCount(count + 1);
+export const SavedInput = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [users, setUsers] = useState([]);
+  const [idCounter, setIdCounter] = useState(1);
+  const [error, setError] = useState('');
+
+  const handleAddNick = () => {
+    if (!inputValue) {
+      setError('Pole nie może być puste');
+      return;
+    }
+
+    if (users.find((user) => user.name === inputValue)) {
+      setError('Taki nick już istnieje');
+      return;
+    }
+
+    const newNick = {
+      name: inputValue,
+      id: idCounter,
+    };
+
+    setUsers([...users, newNick]);
+    setIdCounter(idCounter + 1);
+    setInputValue('');
   };
 
-  const handleResetCount = () => {
-    setCount(0);
-    // usuń element z localStorage - bo wykonaliśmy operacje zerowania count
-    localStorage.removeItem('count');
+  const handleRemoveNick = (id) => {
+    const newUsers = users.filter((user) => user.id !== id);
+    setUsers(newUsers);
+  };
+
+  const handleInputUpdate = (e) => {
+    setError('');
+    setInputValue(e.target.value);
   };
 
   useEffect(() => {
-    // pobierz wartość count z localStorage
-    const storedCount = localStorage.getItem('count');
+    const localUsers = JSON.parse(localStorage.getItem('list-nicki')) || [];
+    const localIdCounter = parseInt(localStorage.getItem('list-nicki-index'));
 
-    // sprawdź czy istnieje taki klucz w lokaStorage
-    if (storedCount) {
-      // jeżeli istnieje to ustaw wartość count jako state komponentu
-      // Number() - konwertuje string na number - bo localStorage zwraca string
-      setCount(Number(storedCount));
-    }
+    setUsers([...localUsers]);
+    setIdCounter(localIdCounter);
   }, []);
 
   useEffect(() => {
-    if (count === 0) return;
-    // zapisz wartość count do localStorage na każdej zmianie stanu count
-    localStorage.setItem('count', count);
-  }, [count]);
+    if (!users.length) return;
+
+    localStorage.setItem('list-nicki', JSON.stringify(users));
+    localStorage.setItem('list-nicki-index', idCounter);
+  }, [idCounter, users]);
 
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={handleUpdateCount}>+1 count</button>
-      <button onClick={handleResetCount}>Reset</button>
+      {/* Lewa sekcja */}
+      <div>
+        <div>
+          <label htmlFor="input">Nick</label>
+          <input value={inputValue} onChange={handleInputUpdate} />
+        </div>
+
+        {error && <div>{error}</div>}
+
+        <div>
+          <button disabled={!inputValue} onClick={handleAddNick}>
+            Dodaj
+          </button>
+        </div>
+      </div>
+
+      {/* Prawa sekcja */}
+      <div>
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>
+                {user.name} {user.id}
+              </div>
+              <button onClick={() => handleRemoveNick(user.id)}>X</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
