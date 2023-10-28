9f0cd87dedfcc5c491c4f388814a5bc8b5b30a29
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-10-28 16:42:16 +0200

    Memo midium

--- src/App/Exercises/Js/MemoGames/MemoGame.jsx
+++ src/App/Exercises/Js/MemoGames/MemoGame.jsx
@@ -2,23 +2,23 @@ import './style.css';
 import { Button } from './Components/Button/Button';
 import { Label } from './Components/Label/Label';
 import { GoBackButton } from '../../../Components/GoBack/GoBack';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Tile } from './Components/Tile/Tile';
 import { getAlphabet, shuffle } from './Components/Utilities';
 
-const ELEMENTS = [4, 16, 20];
+const ELEMENTS = [8, 16, 20];
 const characters = getAlphabet(10);
 
 export const MemoGame = () => {
   const [noOfElements, setNoOfElements] = useState(null);
   const [tiles, setTiles] = useState([]);
-
-  console.log('noOfElements', noOfElements);
+  const [firstClick, setFirstClick] = useState();
+  const [secondClick, setSecondClick] = useState();
 
   function getInitialTiles(size) {
     const charactersSubset = characters.slice(0, size / 2);
     const allCharacters = [...charactersSubset, ...charactersSubset];
-    // const shuffledCharacters = allCharacters.sort(() => Math.random() - 0.5);
+
     const shuffledCharacters = shuffle(allCharacters);
 
     //transform flat alphabet arrat to array of objects with specyfic letters with pairs of object with this same data
@@ -26,7 +26,6 @@ export const MemoGame = () => {
       return { index, value: character, isVisible: false, variant: 'neutral' };
     });
     console.log('characterObject', characterObject);
-
     return characterObject;
   }
 
@@ -36,20 +35,109 @@ export const MemoGame = () => {
     } else {
     }
   }
+
+  function handleTileClick(index) {
+    if (tiles.some((tile) => tile.index === index && tile.isVisible === true))
+      return;
+
+    setTiles(
+      tiles.map((tile) => ({
+        ...tile,
+        isVisible: tile.index === index ? true : tile.isVisible,
+      }))
+    );
+
+    if (firstClick === undefined) {
+      setFirstClick(index);
+    } else {
+      setSecondClick(index);
+    }
+  }
+
+  function handleResetIncorrect(index) {
+    setTiles((oldTiles) => {
+      const newTiles = oldTiles.map((tile) => {
+        if (tile.index === index) {
+          return {
+            ...tile,
+            isVisible: false,
+            variant: 'neutral',
+          };
+        } else {
+          return tile;
+        }
+      });
+
+      return newTiles;
+    });
+  }
+
+  useEffect(() => {
+    let timeoutIdArray = [];
+
+    tiles
+      .filter((tile) => tile.variant === 'incorrect')
+      .forEach((tile) => {
+        const timeoutId = setTimeout(handleResetIncorrect, 500, tile.index);
+
+        timeoutIdArray.push(timeoutId);
+      });
+
+    return () => {
+      timeoutIdArray.forEach((id) => {
+        clearTimeout(id);
+      });
+    };
+  }, [tiles]);
+
+  useEffect(() => {
+    if (firstClick !== undefined && secondClick !== undefined) {
+      setTiles((oldTiles) => {
+        const newTiles = [...oldTiles];
+        const first = newTiles[firstClick];
+        const second = newTiles[secondClick];
+
+        let variant = 'neutral';
+
+        if (first.value === second.value) {
+          variant = 'correct';
+        } else {
+          variant = 'incorrect';
+        }
+
+        newTiles[firstClick] = { ...first, variant };
+        newTiles[secondClick] = { ...second, variant };
+
+        return newTiles;
+      });
+      setFirstClick(undefined);
+      setSecondClick(undefined);
+    }
+  }, [firstClick, secondClick]);
+
+  useEffect(() => {
+    console.log('tiles', tiles);
+    console.log('firstClick', firstClick);
+    console.log('secondClick', secondClick);
+  });
+
   return (
     <div>
       <GoBackButton buttonTitle="< MEMO" />
+
       <p>
         Gra polegająca na zapamiętywaniu odkrytych kafli i łączeniu ich w pary
       </p>
+
       <div className="memo-controls-panel">
         <Label>LICZBA ELEMENTÓW</Label>
-
         {ELEMENTS.map((element) => (
           <Button
             key={element}
-            value={element + ' Element'}
-            onClick={() => setNoOfElements(element)}
+            value={element + ' Elementów'}
+            onClick={() => {
+              setNoOfElements(element);
+            }}
             variant={element !== noOfElements ? 'primary' : 'secondary'}
           />
         ))}
@@ -58,18 +146,17 @@ export const MemoGame = () => {
         <Label>PRZYCISKI STERUJĄCE</Label>
         <Button value="START" onClick={handleStart} />
       </div>
+
       <div className="memo-board">
-        {tiles.map(
-          ({ index, isVisible, variant = 'neutral', onClick, value }) => (
-            <Tile
-              key={index}
-              value={value}
-              onClick={() => {}}
-              isVisible={isVisible}
-              variant={variant}
-            />
-          )
-        )}
+        {tiles.map(({ index, value, isVisible, variant }) => (
+          <Tile
+            key={index}
+            value={value}
+            onClick={() => handleTileClick(index)}
+            isVisible={isVisible}
+            variant={variant}
+          />
+        ))}
       </div>
     </div>
   );
