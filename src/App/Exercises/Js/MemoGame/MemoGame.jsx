14d8eeb149736ef782f561c4f4b3a2ddb4ad5c4b
Karol <wojnos94@o2.pl>
2023-10-28 16:44:25 +0200

    gra memo kontynuacja

--- src/App/Exercises/Js/MemoGame/MemoGame.jsx
+++ src/App/Exercises/Js/MemoGame/MemoGame.jsx
@@ -13,12 +13,13 @@ const characters = getAlphabet(10);
 export const MemoGame = () => {
   const [noOfElements, setNoOfElements] = useState(null);
   const [tiles, setTiles] = useState([]);
-  console.log('noOfElements', noOfElements);
+  const [firstClick, setFirstClick] = useState();
+  const [secondClick, setSecondClick] = useState();
 
   function getInitialTiles(size) {
     const charactersSubset = characters.slice(0, size / 2);
     const allCharacters = [...charactersSubset, ...charactersSubset];
-    // const shuffledCharacters = allCharacters.sort(() => Math.random() - 0.5);
+
     const shuffledCharacters = shuffle(allCharacters);
 
     //transform flat alphabet arrat to array of objects with specyfic letters with pairs of object with this same data
@@ -35,26 +36,116 @@ export const MemoGame = () => {
     } else {
     }
   }
+
+  function handleTileClick(index) {
+    if (tiles.some((tile) => tile.index === index && tile.isVisible === true))
+      return;
+
+    setTiles(
+      tiles.map((tile) => ({
+        ...tile,
+        isVisible: tile.index === index ? true : tile.isVisible,
+      }))
+    );
+
+    if (firstClick === undefined) {
+      setFirstClick(index);
+    } else {
+      setSecondClick(index);
+    }
+  }
+
+  function handleResetIncorrect(index) {
+    setTiles((oldTiles) => {
+      const newTiles = oldTiles.map((tile) => {
+        if (tile.index === index) {
+          return {
+            ...tile,
+            isVisible: false,
+            variant: 'neutral',
+          };
+        } else {
+          return tile;
+        }
+      });
+
+      return newTiles;
+    });
+  }
+
+  useEffect(() => {
+    let timeoutIdArray = [];
+
+    tiles
+      .filter((tile) => tile.variant === 'incorrect')
+      .forEach((tile) => {
+        const timeoutId = setTimeout(handleResetIncorrect, 500, tile.index);
+
+        timeoutIdArray.push(timeoutId);
+      });
+
+    return () => {
+      timeoutIdArray.forEach((id) => {
+        clearTimeout(id);
+      });
+    };
+  }, [tiles]);
+
+  useEffect(() => {
+    if (firstClick !== undefined && secondClick !== undefined) {
+      setTiles((oldTiles) => {
+        const newTiles = [...oldTiles];
+        const first = newTiles[firstClick];
+        const second = newTiles[secondClick];
+
+        let variant = 'neutral';
+
+        if (first.value === second.value) {
+          variant = 'correct';
+        } else {
+          variant = 'incorrect';
+        }
+
+        newTiles[firstClick] = { ...first, variant };
+        newTiles[secondClick] = { ...second, variant };
+
+        return newTiles;
+      });
+      setFirstClick(undefined);
+      setSecondClick(undefined);
+    }
+  }, [firstClick, secondClick]);
+
+  useEffect(() => {
+    console.log('tiles', tiles);
+    console.log('firstClick', firstClick);
+    console.log('secondClick', secondClick);
+  });
+
   return (
     <div>
       <GoBackButton buttonTitle="< MEMO" />
+
       <p>
         Gra polegająca na zapamiętywaniu odkrytych kafli i łączeniu ich w pary
       </p>
+
       <div className="memo-controls-panel">
         <Label>LICZBA ELEMENTÓW</Label>
         {ELEMENTS.map((element) => (
           <Button
             key={element}
             value={element + ' Elementów'}
-            onClick={() => setNoOfElements(element)}
+            onClick={() => {
+              setNoOfElements(element);
+            }}
             variant={element !== noOfElements ? 'primary' : 'secondary'}
           />
         ))}
       </div>
       <div className="memo-controls-panel">
         <Label>PRZYCISKI STERUJĄCE</Label>
-        <Button value="START" onClick={handleStart}></Button>
+        <Button value="START" onClick={handleStart} />
       </div>
 
       <div className="memo-board">
@@ -62,7 +153,7 @@ export const MemoGame = () => {
           <Tile
             key={index}
             value={value}
-            onClick={() => {}}
+            onClick={() => handleTileClick(index)}
             isVisible={isVisible}
             variant={variant}
           />
