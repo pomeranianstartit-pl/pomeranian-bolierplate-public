23cacae4ca6b120af28185262296be4495ea8c79
EMuras <edyta.muras@pomeranianstartit.pl>
2023-10-31 12:53:02 +0100

    forgotten commit

--- src/App/Exercises/HtmlCss/index.jsx
+++ src/App/Exercises/HtmlCss/index.jsx
@@ -21,15 +21,11 @@ export function HtmlCssExercises() {
   return (
     <div>
       <p>HTML & CSS - lista ćwiczeń</p>
-
       <GoBackLink label="Zamknij" />
-
       <hr />
-
       <Routes>
         <Route path="" element={<ExerciseLinks />} />
       </Routes>
-
       {element}
     </div>
   );
@@ -37,25 +33,20 @@ export function HtmlCssExercises() {
 
 export function HtmlCssExerciseItem() {
   const element = useRoutes(htmlBlockRouterData);
-
   const location = useLocation();
-
   const { pathname } = location;
 
   // const pathname = location.pathname
 
   const exerciseRouteData = getRouterMetaDataByCurrentPath(
     pathname,
-
     htmlBlockRouterMetaData
   );
 
   return (
     <div className="exercise-item-layout">
       <ExerciseItemHeader data={exerciseRouteData} />
-
       <hr />
-
       <div className="exercise-item-content">{element}</div>
     </div>
   );
