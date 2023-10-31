6e251c63801bc1ddf1d7f787c1545e002dd45159
EMuras <edyta.muras@pomeranianstartit.pl>
2023-10-31 11:42:30 +0100

    form exercise

--- src/App/Exercises/HtmlCss/SelfStudy/SelfStudy.jsx
+++ src/App/Exercises/HtmlCss/SelfStudy/SelfStudy.jsx
@@ -1,21 +1,88 @@
 import './styles.css';
 
-export const SelfStudy = () => {
-  const text1 = 'powiększ mnie!';
-  console.log(text1.toUpperCase());
-  const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI';
-  console.log(text2.toLowerCase());
-  const text3 = '$#%#^ wytnij te dziwne znaki na początku!';
-  console.log(text3.slice(6));
-  const text4 = 'sprawdź, czy zawieram słowo "czy"';
-  console.log(text4.includes('czy'));
-  const text5 =
-    'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"';
-  console.log(text5.charAt(2));
-  const text6 = 'pies zamień każde słowo pies, na słowo kot pies';
-  console.log(text6.replaceAll('pies', 'kot'));
-  const text7 = 'podziel, ten, string, od, przecinków';
-  console.log(text7.split(','));
+// const x = [5, 8, 10, 23, 48, 60];
 
+// for (const numberr of x){
+//   console.log(numberr/5);
+// }
+
+// const x = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// for (const y of x) {
+//   if (y % 2 === 0) {
+//     console.log(
+//       `%cLiczba ${y} jest parzysta`,
+//       'background-color: yellow; color: black'
+//     );
+//   } else {
+//     console.log(
+//       `%cLiczba ${y} jest nieparzysta`,
+//       'background-color: green; color: black'
+//     );
+//   }
+// }
+
+// const colors = [];
+// colors.unshift(1, 2, 3, 4);
+// colors.shift();
+// colors.push(222, 33, 44, 55);
+// colors.pop();
+// console.log(colors);
+
+// const numbers = [0, 0, 1, 1, 2, 2, 2];
+// const colors = ['red', 'green', 'blue', true, 123];
+
+// const cars = [123, true, 'audi', 'bmw', 'mercedes', 'ferrari', '🤷‍♂️', '👀'];
+
+// const numbers2 = console.log(numbers.slice(2));
+// const numbers3 = console.log(numbers.slice(-3));
+
+// const randomStaff = console.log(colors.splice(-2));
+// console.log(colors);
+
+// const newCars = console.log(cars.splice(2, 4));
+// console.log(cars);
+
+// const newNumbers = numbers.forEach(number)
+
+// const letters = ['c', 'd'];
+
+// letters.unshift('a', 'b');
+// letters.push('e', 'f');
+// console.log(letters);
+// console.log(letters.includes('c'));
+
+// const food = ['hamburger', 'eggs', 'apple'];
+// const numbers = [1, 2, 3];
+
+// const mix = console.log(...numbers, ...food);
+
+// const arry = [1, 5, 13, 26, 48];
+
+// const newArry = arry.map((number) => number * 5);
+
+// console.log(newArry);
+
+// for (const number of newArry) {
+//   if (number % 2 === 0) {
+//     console.log(`Liczba parzysta: ${number}`);
+//   } else {
+//     console.log(`Liczba nieparzysta: ${number}`);
+//   }
+// }
+
+const colour = ['niebieski'];
+colour.unshift('pink');
+colour.push('white');
+
+for (let i = 0; i < colour.length; i++) {
+  console.log(
+    `Mój ulubiony kolor to: ${
+      colour[i].charAt(0).toUpperCase() + colour[i].slice(1)
+    }`
+  );
+}
+
+export const SelfStudy = () => {
   return <div></div>;
 };
