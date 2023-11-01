ce5b540d3cb565539000a2a2f940d8634dd7d272
arleta dabek <itikotka@gmail.com>
2023-11-01 15:28:10 +0100

    saved input exercise

---
+++ src/App/Exercises/Js/SavedInput/router-data.jsx
@@ -0,0 +1,11 @@
+import { Exercise } from './index'; 
+
+export const savedInputMetaData = {
+   
+  path: 'saved-input',
+  date: '26.10.2023',
+  linkLabel:'zapisany nick',
+  blockNo: 33,
+  element: <Exercise />, 
+  tags: ['storage', 'parse', 'json', 'javascript', 'js']
+};
