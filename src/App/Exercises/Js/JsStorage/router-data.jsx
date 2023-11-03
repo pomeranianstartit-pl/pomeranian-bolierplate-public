98a41dd308d412c0410bf223b162d71dd09d19ea
Anna <wiszann@gmail.comgit config --global user.email wiszann@gmail.com>
2023-11-03 21:00:39 +0100

    json excercise

---
+++ src/App/Exercises/Js/JsStorage/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsStorage } from './JsStrorage';
+
+export const JsStorageMetaData = {
+  path: 'js-storage',
+  date: 3 - 11 - 2023,
+  linkLabel: 'JsStorage',
+  blockNo: 20,
+  element: <JsStorage />,
+  tags: ['cookies', 'storage', 'parse', 'json', 'javascript', 'js'],
+};
---
+++ src/App/Exercises/Js/SavedInput/router-data.jsx
@@ -0,0 +1,10 @@
+import { SavedInput } from './SavedInput';
+
+export const SavedInputMetaData = {
+  path: 'saved-input',
+  date: 3 - 11 - 2023,
+  linkLabel: 'SavedInput',
+  blockNo: 20,
+  element: <SavedInput />,
+  tags: ['storage', 'parse', 'json', 'javascript', 'js'],
+};
