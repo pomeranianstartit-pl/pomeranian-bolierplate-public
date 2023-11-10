d6a8d6edd13b151a8abfed58bd40d618aa0bbb55
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-11-10 20:36:49 +0100

    FormNew with css

--- src/App/Exercises/ReactExercises/FormNew/index.jsx
+++ src/App/Exercises/ReactExercises/FormNew/index.jsx
@@ -16,7 +16,7 @@ const schema = yup
       .string()
       .email('Pole nie jest emailem')
       .required('Pole mail jest wymagane!'),
-    agreements: yup.boolean().oneOf([true], 'Pole agreements jest wymagane!'),
+    agreements: yup.boolean().oneOf([true], 'Pole obowiązkowe!'),
     address: yup.string().required('Pole address jest wymagane!'),
     num: yup
       .number()
@@ -43,7 +43,7 @@ export const FormNew = () => {
       num: '+48 888 888 888',
       gender: 'other',
       mail: 'jan.kowalski@gmail.com',
-      paymentMethod: '',
+      paymentMethod: false,
       agreements: false,
       product: '',
       additionaloptions: '',
@@ -62,148 +62,200 @@ export const FormNew = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>ZAMÓWIENIE PRODUKTU</h2>
-      <label>
-        <div>Wybierz produkt*</div>
-        <select
-          aria-invalid={errors.product ? 'true' : 'false'}
-          {...register('product')}
-        >
-          <option value="kurs front-end">kurs front-end</option>
-          <option value="kurs back-end">kurs back-end</option>
-          <option value="kurs UI">kurs UI</option>
-        </select>
-      </label>
-      {errors.product && <p role="alert">{errors.product.message}</p>}
+      <div className="allform">
+        <div className="onepart">
+          <h2 className="h2form">ZAMÓWIENIE PRODUKTU</h2>
+          <label>
+            <div className="opisform">Wybierz produkt*</div>
+            <select
+              aria-invalid={errors.product ? 'true' : 'false'}
+              {...register('product')}
+            >
+              <option value="kurs front-end">kurs front-end</option>
+              <option value="kurs back-end">kurs back-end</option>
+              <option value="kurs UI">kurs UI</option>
+            </select>
+          </label>
+          {errors.product && <p role="alert">{errors.product.message}</p>}
 
-      <label>
-        <div>Wybierz formę płatności*</div>
+          <label>
+            <div className="opisform">Wybierz formę płatności*</div>
+            <div>
+              <div>
+                <div className="checkbox">
+                  <input
+                    type="radio"
+                    value="blik"
+                    aria-invalid={errors.paymentMethod ? 'true' : 'false'}
+                    {...register('paymentMethod')}
+                  />
+                </div>
+                Blik
+              </div>
 
-        <div>
-          <input
-            type="radio"
-            value="blik"
-            aria-invalid={errors.paymentMethod ? 'true' : 'false'}
-            {...register('paymentMethod')}
-          />
-          Blik
+              <div>
+                <div className="checkbox">
+                  <input
+                    type="radio"
+                    value="przelew"
+                    aria-invalid={errors.paymentMethod ? 'true' : 'false'}
+                    {...register('paymentMethod')}
+                  />
+                </div>
+                Przelew
+              </div>
+              <div>
+                <div className="checkbox">
+                  <input
+                    type="radio"
+                    value="paypal"
+                    aria-invalid={errors.paymentMethod ? 'true' : 'false'}
+                    {...register('paymentMethod')}
+                  />
+                </div>
+                Paypal
+              </div>
+            </div>
+          </label>
+          {errors.paymentMethod && (
+            <p role="alert">{errors.paymentMethod.message}</p>
+          )}
+
+          <label>
+            <div className="opisform">Opcje dodatkowe do zamówienia</div>
+            <div className="checkbox">
+              <input type="checkbox" {...register('additionaloptions')} />
+              Ustawienie środowiska
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" {...register('additionaloptions')} />
+              intro do GitHub
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" {...register('additionaloptions')} />
+              materiały dodatkowe
+            </div>
+          </label>
         </div>
+        <div className="onepart">
+          <h2 className="h2form">DANE DO REALIZACJI ZAMÓWIENIA</h2>
+          <label>
+            <div className="opisform">Imię i nazwisko*</div>
+            <div className="inputtext">
+              <input
+                aria-invalid={errors.example ? 'true' : 'false'}
+                {...register('example')}
+              />
+            </div>
+          </label>
+          {errors.example && (
+            <p role="alert">{errors.exampleRequired.message}</p>
+          )}
+          <label>
+            <div className="opisform">Twój pseudonim*</div>
+            <div className="inputtext">
+              <input
+                aria-invalid={errors.exampleRequired ? 'true' : 'false'}
+                {...register('exampleRequired')}
+              />
+            </div>
+          </label>
+          {errors.exampleRequired && (
+            <p role="alert">{errors.exampleRequired.message}</p>
+          )}
+          <label>
+            <div className="opisform">Adres do wysyłki*</div>
+            <div className="inputtext">
+              <input
+                aria-invalid={errors.address ? 'true' : 'false'}
+                {...register('address')}
+              />
+            </div>
+          </label>
+          {errors.address && <p role="alert">{errors.address.message}</p>}
+          <label>
+            <div className="opisform">Adres e-mail*</div>
+            <div className="inputtext">
+              <input
+                aria-invalid={errors.mail ? 'true' : 'false'}
+                {...register('mail')}
+              />
+            </div>
+          </label>
+          {errors.mail && <p role="alert">{errors.mail.message}</p>}
+          <label>
+            <div className="opisform">Numer kontaktowy*</div>
+            <div className="inputtext">
+              <input
+                aria-invalid={errors.num ? 'true' : 'false'}
+                {...register('num')}
+              />
+            </div>
+          </label>
+          {errors.num && <p role="alert">{errors.num.message}</p>}
 
-        <div>
-          <input
-            type="radio"
-            value="przelew"
-            aria-invalid={errors.paymentMethod ? 'true' : 'false'}
-            {...register('paymentMethod')}
-          />
-          Przelew
+          <label>
+            <div className="opisform">Dodatkowe uwagi do zamówienia</div>
+            <div className="textareatext">
+              <textarea
+                aria-invalid={errors.comments ? 'true' : 'false'}
+                {...register('comments ')}
+              />
+            </div>
+          </label>
         </div>
-        <div>
-          <input
-            type="radio"
-            value="paypal"
-            aria-invalid={errors.paymentMethod ? 'true' : 'false'}
-            {...register('paymentMethod')}
-          />
-          Paypal
+        <div className="onepart">
+          <h2 className="h2form">ZAKŁADANIE KONTA</h2>
+          <div className="opisform">Chcę założyć konto razem z zamówieniem</div>
+          <label>
+            <div className="checkbox">
+              <input type="checkbox" {...register('account')} />
+              Zakładam konto
+            </div>
+          </label>
+          <label>
+            <div className="opisform">Moje hasło</div>
+            <div className="inputtext">
+              <input type="password" />
+            </div>
+          </label>
+          <label>
+            <div className="opisform">Powtórz hasło</div>
+            <div className="inputtext">
+              <input type="password" />
+            </div>
+          </label>
         </div>
-      </label>
-      {errors.paymentMethod && (
-        <p role="alert">{errors.paymentMethod.message}</p>
-      )}
+        <div className="onepart">
+          <h2 className="h2form">ZGODY I NEWSLETTER</h2>
+          <div className="opisform">
+            Realizując zamówienie, akceptujesz regulamin naszego sklepu
+          </div>
+          <label>
+            <div className="checkbox">
+              <input type="checkbox" {...register('agreements')} />
+              akceptuję regulamin*
+            </div>
+          </label>
+          {errors.agreements && <p role="alert">{errors.agreements.message}</p>}
 
-      <label>
-        <div>Opcje dodatkowe do zamówienia</div>
-        <input type="checkbox" {...register('additionaloptions')} />
-        Ustawienie środowiska
-        <input type="checkbox" {...register('additionaloptions')} />
-        intro do GitHub
-        <input type="checkbox" {...register('additionaloptions')} />
-        materiały dodatkowe
-      </label>
-
-      <h2>DANE DO REALIZACJI ZAMÓWIENIA</h2>
-      <label>
-        <div>Imię i nazwisko*</div>
+          <div className="opisform">
+            Dołącz do naszego newslettera z promocjami dla naszych klientów
+          </div>
+          <label>
+            <div className="checkbox">
+              <input type="checkbox" {...register('newsletter')} />
+              Zapisuję się na listę mailingową
+            </div>
+          </label>
+        </div>
+      </div>
+      <div className="buttonrealizuj">
         <input
-          aria-invalid={errors.example ? 'true' : 'false'}
-          {...register('example')}
+          type="submit"
+          class="SKŁADAM ZAMÓWIENIE"
+          value="SKŁADAM ZAMÓWIENIE"
         />
-      </label>
-      {errors.example && <p role="alert">{errors.exampleRequired.message}</p>}
-      <label>
-        <div>Twój pseudonim*</div>
-        <input
-          aria-invalid={errors.exampleRequired ? 'true' : 'false'}
-          {...register('exampleRequired')}
-        />
-      </label>
-      {errors.exampleRequired && (
-        <p role="alert">{errors.exampleRequired.message}</p>
-      )}
-      <label>
-        <div>Adres do wysyłki*</div>
-        <input
-          aria-invalid={errors.address ? 'true' : 'false'}
-          {...register('address')}
-        />
-      </label>
-      {errors.address && <p role="alert">{errors.address.message}</p>}
-      <label>
-        <div>Adres e-mail*</div>
-        <input
-          aria-invalid={errors.mail ? 'true' : 'false'}
-          {...register('mail')}
-        />
-      </label>
-      {errors.mail && <p role="alert">{errors.mail.message}</p>}
-      <label>
-        <div>Numer kontaktowy*</div>
-        <input
-          aria-invalid={errors.num ? 'true' : 'false'}
-          {...register('num')}
-        />
-      </label>
-      {errors.num && <p role="alert">{errors.num.message}</p>}
-
-      <label>
-        <div>Dodatkowe uwagi do zamówienia</div>
-        <textarea
-          aria-invalid={errors.comments ? 'true' : 'false'}
-          {...register('comments ')}
-        />
-      </label>
-
-      <h2>ZAKŁADANIE KONTA</h2>
-      <div>Chcę założyć konto razem z zamówieniem</div>
-      <label>
-        <input type="checkbox" {...register('account')} />
-        Zakładam konto
-      </label>
-      <label>
-        <div>Moje hasło</div>
-        <input type="password" />
-      </label>
-      <label>
-        <div>Powtórz hasło</div>
-        <input type="password" />
-      </label>
-
-      <h2>ZGODY I NEWSLETTER</h2>
-      <div>Realizując zamówienie, akceptujesz regulamin naszego sklepu</div>
-      <label>
-        <input type="checkbox" {...register('agreements')} />
-        Zgoda na przetwarzanie danych
-      </label>
-      {errors.agreements && <p role="alert">{errors.agreements.message}</p>}
-
-      <div>Dołącz do naszego newslettera z promocjami dla naszych klientów</div>
-      <label>
-        <input type="checkbox" {...register('newsletter')} />
-        Zapisuję się na listę mailingową
-      </label>
-      <div>
-        <input type="submit" />
       </div>
     </form>
   );
