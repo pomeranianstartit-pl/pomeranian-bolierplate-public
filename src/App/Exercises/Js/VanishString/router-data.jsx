9a6e8db2cfeb54b14639cef372bae822cb764bdb
annamatyasek <annamatyasek@gmail.com>
2023-11-17 11:54:25 +0100

    tasks with setTimeout , setInterval

---
+++ src/App/Exercises/Js/VanishString/router-data.jsx
@@ -0,0 +1,10 @@
+import { VanishString } from './index';
+
+export const vanishStringMetaData = {
+  path: 'vanish-string',
+  date: '12.10.2023',
+  linkLabel: 'Zniknięcie tekstu',
+  blockNo: 21,
+  element: <VanishString />,
+  tags: ['setTimeout', 'timeout', 'time', 'string'],
+};
