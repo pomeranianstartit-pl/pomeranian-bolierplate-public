b68852e16b040f4f05f04e744e26a4d0e136bf81
ArekFrel <arkadiuszfrel@gmail.com>
2023-11-19 09:26:57 +0100

    lesson of testing

---
+++ src/App/Exercises/Js/Testing/router-data.jsx
@@ -0,0 +1,11 @@
+import { TestedComponent } from "./TestedComponent";
+
+
+export const TestedComponentMetaData = {
+  path: 'testing',
+  date: '12-12-2023',
+  linkLabel: 'Blok 45 - Testing',
+  blockNo: 45,
+  element: <TestedComponent />,
+  tags: ['testing', 'jest', 'rtl'],
+};
