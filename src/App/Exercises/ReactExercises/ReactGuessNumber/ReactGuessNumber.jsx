a5753affdfaf715edf1a9de3778b6919f826c3b4
ArekFrel <arkadiuszfrel@gmail.com>
2024-01-17 18:13:02 +0100

    slight changes

--- src/App/Exercises/ReactExercises/ReactGuessNumber/ReactGuessNumber.jsx
+++ src/App/Exercises/ReactExercises/ReactGuessNumber/ReactGuessNumber.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 import './style.css';
 
@@ -7,50 +7,59 @@ function generateRandomNumber() {
 }
 
 export function ReactGuessNumber() {
+  const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
+  const [inputNumber, setInputmNumber] = useState(0);
+  const [text, setText] = useState('');
 
-  const [randomNumber, setRandomNumber] = useState(generateRandomNumber())
-  const [inputNumber, setInputmNumber] = useState(0)
-  const [text, setText] = useState('')  
-
-  function handleChange(event) {    
+  function handleChange(event) {
     setInputmNumber(event.target.value);
-    console.log(typeof(inputNumber))
+    console.log(typeof inputNumber);
   }
 
-  function handleClickStart () {    
-    if (inputNumber.length==0) {
-      setText("nic nie wpisałeś/aś")}
-      else {
+  function handleClickStart() {
+    if (inputNumber.length === 0) {
+      setText('nic nie wpisałeś/aś');
+    } else {
       if (inputNumber < randomNumber) {
-        setText('Jestem Większa')
+        setText('Jestem Większa');
       } else if (inputNumber > randomNumber) {
-        setText('Jestem Mniejsza')
+        setText('Jestem Mniejsza');
       } else {
-        setText('Gratulacje, trafiaś/eś')
+        setText('Gratulacje, trafiaś/eś');
       }
-      console.log(randomNumber, "randomnumber")
-      console.log(inputNumber, "input number")
+      console.log(randomNumber, 'randomnumber');
+      console.log(inputNumber, 'input number');
     }
   }
-      
-  function handleClickReset () {
-    setRandomNumber(generateRandomNumber())
-    console.log(randomNumber)
+
+  function handleClickReset() {
+    setRandomNumber(generateRandomNumber());
+    console.log(randomNumber);
   }
 
   return (
-    <div id="frame">
-      <h2>Jaką jestem liczbą?</h2>
-      <button id="startButton"onClick={handleClickStart}>START</button>
-      <div>
-        <input 
-        onChange={handleChange}
-        type="number"
-        value={inputNumber}
-        placeholder='Wpisz liczbę pomiędzy 1 a 10'/>
+    <div id="contener">
+      <div id="frame">
+        <h2>Jaką jestem liczbą?</h2>
+        <button id="startButton" onClick={handleClickStart}>
+          START
+        </button>
+        <div>
+          <input
+            onChange={handleChange}
+            type="number"
+            value={inputNumber}
+            placeholder="Wpisz liczbę pomiędzy 1 a 10"
+            className="guess-input"
+          />
         </div>
-      <p>{text}</p>
-      <button onClick={handleClickReset}>RESET</button>
+        <div className="text-holder">
+          <p>{text}</p>
+        </div>
+        <button id="reset-button" onClick={handleClickReset}>
+          RESET
+        </button>
+      </div>
     </div>
   );
 }
--- src/App/FAQ/faq.jsx
+++ src/App/FAQ/faq.jsx
@@ -25,6 +25,15 @@ export function FAQ() {
     tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit
     amet, consectetur adipiscing elit"
       />
+      <SingleQuestion
+        question="Warum sind Die Katzen Einzelgänger?"
+        answer="Die Katzen sind Einzelganger Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+    rutrum ipsum, leo et in. Mattis porttitor volutpat placerat
+    suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas
+    aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium
+    tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit"
+      />
       {/* <button className="button button-red">button</button> */}
     </div>
   );
