781f50661862e2e29c76739957a294bc0bc06073
Piowija <piotrwijas123@gmail.com>
2024-03-27 15:20:56 +0100

    properties

--- src/App/Exercises/HtmlCss/MediaFiles/router-data.jsx
+++ src/App/Exercises/HtmlCss/MediaFiles/router-data.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { MediaFiles } from './MediaFiles';
 
 export const mediaFilesRouterMetaData = {
   path: 'media-files',
   date: '31-08-2023',
   linkLabel: 'Pliki multimedialne',
   blockNo: 1,
-  element: ,
+  element: <MediaFiles />,
   tags: ['html', 'video', 'audio', 'iframe'],
 };
