330f0f4d9aea9627c69b6f21901d3902b4ca8a96
Piowija <piotrwijas123@gmail.com>
2024-03-27 22:11:25 +0100

    properties

--- src/App/Exercises/HtmlCss/BoxModel/BoxModel.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/BoxModel.jsx
@@ -6,17 +6,19 @@ export const BoxModel = () => {
   return (
     <div>
       BoxModel:
-      <div className="div_nr1">Przykładowy div</div>
+      <div className="div_nr1">Przykładowy div1</div>
       <div className="div_nr2">Przykładowy div2</div>
       {/* IF statement */}
-      <p className="p_nr1">Przykładowy p</p>
-      {isVisible && <p className="p_nr2">Przykładowy p</p>}
+      <p className="p_nr1">Przykładowy p1</p>
+      {isVisible && <p className="p_nr2">Przykładowy p2</p>}
       <p className={isNotVisible ? 'p_nr1' : 'p_nr2'}>Przykładowy p</p>
       <h1 className="h1_nr1">Przykładowy h1</h1>
+      {/* Display */}
       <span className="span_nr1">Przykładowy span1</span>
       <span className="span_nr2">Przykładowy span2</span>
       <span className="span_nr3">Przykładowy span3</span>
-      {/* <span className="span_nr1">Przykładowy span</span> */}
+      {/* Overflow */}
+      <div className="div_nr3">Przykładowy div3</div>
     </div>
   );
 };
